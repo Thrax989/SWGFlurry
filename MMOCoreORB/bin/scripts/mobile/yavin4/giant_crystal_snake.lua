09d6ee8ba0bd976ecca8d8cca35b038e488696a7
Thrax989 <thrax989@yahoo.com>
2016-12-30 02:04:15 -0800

    [Added] Yavin 4 npc revision

--- MMOCoreORB/bin/scripts/mobile/yavin4/giant_crystal_snake.lua
+++ MMOCoreORB/bin/scripts/mobile/yavin4/giant_crystal_snake.lua
@@ -3,18 +3,18 @@ giant_crystal_snake = Creature:new {
 	socialGroup = "snake",
 	faction = "",
 	level = 31,
-	chanceHit = 1.19,
-	damageMin = 410,
-	damageMax = 450,
-	baseXp = 4097,
-	baseHAM = 8500,
-	baseHAMmax = 10500,
-	armor = 1,
-	resists = {150,150,150,150,150,150,150,150,125},
+	chanceHit = 0.39,
+	damageMin = 310,
+	damageMax = 330,
+	baseXp = 3097,
+	baseHAM = 8300,
+	baseHAMmax = 10100,
+	armor = 0,
+	resists = {20,20,20,20,20,20,20,20,-1},
 	meatType = "meat_carnivore",
-	meatAmount = 150,
+	meatAmount = 7,
 	hideType = "hide_scaley",
-	hideAmount = 150,
+	hideAmount = 4,
 	boneType = "",
 	boneAmount = 0,
 	milk = 0,
@@ -32,8 +32,8 @@ giant_crystal_snake = Creature:new {
 	weapons = {"creature_spit_small_toxicgreen"},
 	conversationTemplate = "",
 	attacks = {
-		{"strongpoison",""},
-		{"posturedownattack",""}
+		{"posturedownattack",""},
+		{"strongpoison",""}
 	}
 }
 
