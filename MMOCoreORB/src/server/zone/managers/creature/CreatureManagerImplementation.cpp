2b4692f518361465db42f149040eda6f0369e27b
Thrax989 <thrax989@yahoo.com>
2016-12-31 01:10:51 -0800

    [Added[ Creature manager Scale back
    creature_harvesting Scaled back from *10 harvest to *4

--- MMOCoreORB/src/server/zone/managers/creature/CreatureManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/creature/CreatureManagerImplementation.cpp
@@ -671,7 +671,7 @@ void CreatureManagerImplementation::droidHarvest(Creature* creature, CreatureObj
 		return;
 	}
 	int ownerSkill = owner->getSkillMod("creature_harvesting");
-	int quantityExtracted = int(quantity * 10 * float(ownerSkill / 100.0f));
+	int quantityExtracted = int(quantity * 4 * float(ownerSkill / 100.0f));
 	// add in droid bonus
 	quantityExtracted = MAX(quantityExtracted, 3);
 	ManagedReference<ResourceSpawn*> resourceSpawn = resourceManager->getCurrentSpawn(restype, droid->getZone()->getZoneName());
@@ -836,7 +836,7 @@ void CreatureManagerImplementation::harvest(Creature* creature, CreatureObject* 
 		player->sendSystemMessage("Tried to harvest something this creature didn't have, please report this error");
 		return;
 	}
-	int quantityExtracted = int(quantity * 10 * float(player->getSkillMod("creature_harvesting") / 100.0f));
+	int quantityExtracted = int(quantity * 4 * float(player->getSkillMod("creature_harvesting") / 100.0f));
 	quantityExtracted = MAX(quantityExtracted, 3);
 
 	ManagedReference<ResourceSpawn*> resourceSpawn = resourceManager->getCurrentSpawn(restype, player->getZone()->getZoneName());
