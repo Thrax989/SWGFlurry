8a4c98f7889251539b647ebba7aa68c76ac0877a
Bobvious <bob@shew.com>
2017-01-02 15:47:11 -0700

    All Building Medical Rating Bonuses to be 100
    Missed a few

--- MMOCoreORB/bin/scripts/object/building/player/city/theater_naboo.lua
+++ MMOCoreORB/bin/scripts/object/building/player/city/theater_naboo.lua
@@ -51,7 +51,7 @@ object_building_player_city_theater_naboo = object_building_player_city_shared_t
 	planetMapCategory = "theater",
 	abilityRequired = "place_theater",
 	skillMods = {
-		{"private_medical_rating", 110},
+		{"private_medical_rating", 100},
 		{"private_buff_mind", 125},
 		{"private_med_battle_fatigue", 15}
 		{"private_med_wound_mind", 15},
