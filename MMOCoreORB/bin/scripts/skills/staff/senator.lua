586a721437873329a55fe131e7a3e914aa549871
qrave <napokam@icloud.com>
2017-01-09 12:07:18 +0000

    Create senator.lua

---
+++ MMOCoreORB/bin/scripts/skills/staff/senator.lua
@@ -0,0 +1,31 @@
+senator = {
+	skillName = "senator",
+	parentName = "",
+	graphType = 4,
+	godOnly = 0,
+	title = 1,
+	profession = 0,
+	hidden = 0,
+	moneyRequired = 0,
+	pointsRequired = 0,
+	skillsRequiredCount = 0,
+	skillsRequired = {},
+	preclusionSkills = {},
+	xpType = "",
+	xpCost = 0,
+	xpCap = 0,
+	missionsRequired = {},
+	apprenticeshipsRequired = 0,
+	statsRequired = {},
+	speciesRequired = {},
+	jediStateRequired = 0,
+	skillAbility = {},
+	commands = {
+	},
+	skillModifiers = {},
+	schematicsGranted = {},
+	schematicsRevoked = {},
+	searchable = 0
+}
+
+addSkill(senator)
