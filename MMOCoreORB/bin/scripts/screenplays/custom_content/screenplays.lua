32e07a40fd5980ccb830b3aa9248aeceffb29225
TOXIC <thrax989@yahoo.com>
2017-01-09 23:33:33 -0800

    [DISABLED]

--- MMOCoreORB/bin/scripts/screenplays/custom_content/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/custom_content/screenplays.lua
@@ -6,7 +6,7 @@
 --includeFile("custom_content/cities/ghomrassen_mos_rassen.lua")
 
 --Dungeons
-includeFile("custom_content/dungeon/generals_bunker.lua")
+--includeFile("custom_content/dungeon/generals_bunker.lua")
 
 --Race Tracks
 --includeFile("custom_content/racetracks/mosrassen.lua")
