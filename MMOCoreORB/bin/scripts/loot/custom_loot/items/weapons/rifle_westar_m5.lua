8cfcd286ba38bd15051a520e4ffe9f82df52b863
Thrax989 <thrax989@yahoo.com>
2017-01-21 15:39:43 -0800

    [Added] Scaled  rifle nge items down to vanilla T21 rifle loot base templates
    T21 rifle stats will be the base for the rifle looted nge weapons .
    Stats will increase in some items more then others. crafters will still
    be able to make weapons better then looted . Schematics will come at a
    later publish to craft nge weapons

--- MMOCoreORB/bin/scripts/loot/custom_loot/items/weapons/rifle_westar_m5.lua
+++ MMOCoreORB/bin/scripts/loot/custom_loot/items/weapons/rifle_westar_m5.lua
@@ -8,7 +8,7 @@ rifle_westar_m5 = {
 		{"maxdamage",170,360,0},
 		{"attackspeed",12.7,7.4,0},
 		{"woundchance",11,25,0},
-		{"hitpoints",1750,2500,0},
+		{"hitpoints",750,750,0},
 		{"attackhealthcost",62,31,0},
 		{"attackactioncost",53,26,0},
 		{"attackmindcost",115,57,0},
