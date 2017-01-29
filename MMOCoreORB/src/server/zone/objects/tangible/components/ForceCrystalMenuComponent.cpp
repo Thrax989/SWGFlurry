f2f58da4a5c5fd1ec25a2b79469da87013b5c1f8
TOXIC <thrax989@yahoo.com>
2017-01-29 04:55:40 -0800

    [Reset]Custom Force Crystals to base core3 code

--- MMOCoreORB/src/server/zone/objects/tangible/components/ForceCrystalMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/ForceCrystalMenuComponent.cpp
@@ -1,69 +1,23 @@
 /*
  * ForceCrystalMenuComponent.cpp
  */
-/**
- * 
- * Authors TOXIC
- * 
- * PLEASE DO NOT STEAL OUR WORK
- * ASK BEFOR USING
- * Contact Me Here http://projectphoenix.com.shivtr.com/
- * Created on: 9/19/2016
- */
 
 #include "ForceCrystalMenuComponent.h"
 #include "server/zone/objects/creature/CreatureObject.h"
 #include "server/zone/objects/scene/SceneObject.h"
-#include "server/zone/objects/player/PlayerObject.h"
 #include "server/zone/packets/object/ObjectMenuResponse.h"
-#include "server/zone/ZoneServer.h"
-#include "ForceShrineMenuComponent1.h"
-#include "server/zone/objects/player/sui/messagebox/SuiMessageBox.h"
-#include "server/zone/managers/skill/SkillManager.h"
-#include "server/zone/managers/player/PlayerManager.h"
-#include "server/zone/packets/player/PlayMusicMessage.h"
-#include "server/zone/managers/creature/CreatureManager.h"
-#include "server/zone/objects/region/CityRegion.h"
-#include "server/zone/packets/player/PlayMusicMessage.h"
-#include "server/chat/ChatManager.h"
-#include "server/zone/managers/visibility/VisibilityManager.h"
-#include "server/zone/objects/player/sui/callbacks/BountyHuntSuiCallback.h"
-#include "server/zone/objects/player/sui/inputbox/SuiInputBox.h"
 
 void ForceCrystalMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, ObjectMenuResponse* menuResponse, CreatureObject* player) const {
 
 	TangibleObjectMenuComponent::fillObjectMenuResponse(sceneObject, menuResponse, player);
 
-	menuResponse->addRadialMenuItem(20, 3, "Jedi Resurrection");
+	menuResponse->addRadialMenuItem(20, 3, "@quest/force_sensitive/intro:crystal_journal"); // Display Journal
 }
 
 int ForceCrystalMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* creature, byte selectedID) const {
 	if (selectedID != 20)
 		return 0;
-	creature->sendExecuteConsoleCommand("/ui action questJournal");
-	ManagedReference<PlayerObject*> ghost = creature->getPlayerObject();
-	if (ghost == NULL)
-	return 0;
-	if (creature->getPosture() != CreaturePosture::DEAD){
-		creature->sendSystemMessage("@jedi_trials:show_respect");
-		return 0;
-		} else {
-		int rand = System::random(14) + 1;
-		StringBuffer sysmsg;
-		sysmsg << "@jedi_trials:force_shrine_wisdom_" << rand;
-		creature->sendSystemMessage(sysmsg.toString());
-		if (ghost->getJediState() >= 2) {
-			int jediVis1 = ghost->getVisibility();
-			PlayMusicMessage* pmm = new PlayMusicMessage("sound/music_become_light_jedi.snd");
-  			ghost->sendMessage(pmm);
-			StringBuffer messageVis;
-			messageVis << "\\#00CC00 Your Visibility is at: " << jediVis1;
-			creature->sendSystemMessage(messageVis.toString());
-		}
-	}
 
-	creature->playEffect("clienteffect/trap_electric_01.cef", "");
-	creature->setPosture(CreaturePosture::UPRIGHT);
-	sceneObject->destroyObjectFromWorld(true);
+	creature->sendExecuteConsoleCommand("/ui action questJournal");
 	return 0;
 }
