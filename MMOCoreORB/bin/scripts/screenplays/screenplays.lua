2560bf9749289bf2da8a2cd291a4a866d7d8953f
TOXIC <thrax989@yahoo.com>
2017-02-02 05:25:33 -0800

    [Updated] Screenplay include

--- MMOCoreORB/bin/scripts/screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/screenplays.lua
@@ -60,6 +60,7 @@ includeFile("tools/firework_event.lua")
 
 -- GCW
 includeFile("gcw/newsnet.lua")
+includeFile("gcw/pvp.lua")
 includeFile("gcw/recruiters/recruiterConvoHandler.lua")
 includeFile("gcw/recruiters/recruiterScreenplay.lua")
 
