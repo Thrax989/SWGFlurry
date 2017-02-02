53a661d619567854d27e2ef799525387444f7ccf
TOXIC <thrax989@yahoo.com>
2017-02-02 15:39:48 +0000

    [Added] Created pvp teleport safety  script for no clone camping on kaas pvp planet

---
+++ MMOCoreORB/bin/scripts/screenplays/gcw/pvp1.lua
@@ -0,0 +1,66 @@
+local ObjectManager = require("managers.object.object_manager")
+
+pvp1 = ScreenPlay:new {
+	numberOfActs = 1,
+  	questString = "pvp1",
+  	questdata = Object:new {
+    	activePlayerName = "initial",
+    	}
+}
+  
+registerScreenPlay("pvp1", true)
+  
+function pvp1:start()
+    	self:spawnActiveAreas()
+end
+  
+function pvp1:spawnActiveAreas()
+	local pSpawnArea = spawnSceneObject("kaas", "object/active_area.iff", -5067, 80, -2279, 0, 0, 0, 0, 0)
+    
+	if (pSpawnArea ~= nil) then
+		local activeArea = LuaActiveArea(pSpawnArea)
+	        activeArea:setCellObjectID(0)
+	        activeArea:setRadius(5)
+	        createObserver(ENTEREDAREA, "pvp1", "notifySpawnArea", pSpawnArea)
+	        createObserver(EXITEDAREA, "pvp1", "notifySpawnAreaLeave", pSpawnArea)
+	    end
+end
+ 
+function pvp1:notifySpawnArea(pActiveArea, pMovingObject)
+	
+	if (not SceneObject(pMovingObject):isCreatureObject()) then
+		return 0
+	end
+	
+	return ObjectManager.withCreatureObject(pMovingObject, function(player)
+		if (player:isAiAgent()) then
+			return 0
+		end
+		
+		if (player:isImperial() or player:isRebel()) then
+			player:sendSystemMessage("Teleporting you to saftey!")
+			player:teleport(-5108, 81, -2106, 0)
+		else
+			player:sendSystemMessage("Teleporting you to saftey!")
+		end
+		return 0
+	end)
+end
+
+function pvp1:notifySpawnAreaLeave(pActiveArea, pMovingObject)
+	
+	if (not SceneObject(pMovingObject):isCreatureObject()) then
+		return 0
+	end
+	
+	return ObjectManager.withCreatureObject(pMovingObject, function(player)
+		if (player:isAiAgent()) then
+			return 0
+		end
+		
+		if (player:isImperial() or player:isRebel()) then
+			player:sendSystemMessage("You are safe for now!")
+		end
+		return 0
+	end)
+end
