ae91ed97032ccbb386de8d71685f8cf5561c119a
Thrax989 <thrax989@yahoo.com>
2017-02-15 17:59:59 -0800

    [Added] More to phase 2 3 and 4 for droid compound

--- MMOCoreORB/bin/scripts/screenplays/poi/rori_imperial_encampment.lua
+++ MMOCoreORB/bin/scripts/screenplays/poi/rori_imperial_encampment.lua
@@ -17,34 +17,34 @@ function RoriImperialEncampmentScreenPlay:start()
 end
 
 function RoriImperialEncampmentScreenPlay:spawnMobiles()
---Boss 1 Trigger
+--Droid 1 Trigger
 	local T1Droid = spawnMobile("rori", "death_watch_s_battle_droid", 1, -5628.4, 75.9027, -5666.37, 170, 0)print("Trigger 1 Loaded")
         	createObserver(OBJECTDESTRUCTION, "RoriImperialEncampmentScreenPlay", "notify1DroidDead", T1Droid)
---Boss 2 Trigger
+--Droid 2 Trigger
 	local T2Droid = spawnMobile("rori", "death_watch_s_battle_droid", 1, -5622.79, 75.9766, -5666.37, 168, 0)print("Trigger 2 Loaded")
         	createObserver(OBJECTDESTRUCTION, "RoriImperialEncampmentScreenPlay", "notify2DroidDead", T2Droid)
---Boss 3 Trigger
+--Droid 3 Trigger
 	local T3Droid = spawnMobile("rori", "death_watch_s_battle_droid", 1, -5622.62, 75.9714, -5657.63, 0, 0)print("Trigger 3 Loaded")
         	createObserver(OBJECTDESTRUCTION, "RoriImperialEncampmentScreenPlay", "notify3DroidDead", T3Droid)
---Boss 4 Trigger
+--Droid 4 Trigger
 	local T4Droid = spawnMobile("rori", "death_watch_s_battle_droid", 1, -5628.39, 75.9222, -5657.63, 346, 0)print("Trigger 4 Loaded")
         	createObserver(OBJECTDESTRUCTION, "RoriImperialEncampmentScreenPlay", "notify4DroidDead", T4Droid)
---Boss 5 Trigger
+--Droid 5 Trigger
 	local T5Droid = spawnMobile("rori", "death_watch_s_battle_droid", 1, -5626.45, 75.9471, -5661.81, 12, 0)print("Trigger 5 Loaded")
         	createObserver(OBJECTDESTRUCTION, "RoriImperialEncampmentScreenPlay", "notify5DroidDead", T5Droid)
---Boss 6 Trigger
+--Droid 6 Trigger
 	local T6Droid = spawnMobile("rori", "death_watch_s_battle_droid", 1, -5501.52, 75.9154, -5659.77, 0, 0)print("Trigger 6 Loaded")
         	createObserver(OBJECTDESTRUCTION, "RoriImperialEncampmentScreenPlay", "notify6DroidDead", T6Droid)
---Boss 7 Trigger
+--Droid 7 Trigger
 	local T7Droid = spawnMobile("rori", "death_watch_s_battle_droid", 1, -5507.28, 75.9701, -5659.77, 359, 0)print("Trigger 7 Loaded")
         	createObserver(OBJECTDESTRUCTION, "RoriImperialEncampmentScreenPlay", "notify7DroidDead", T7Droid)
---Boss 8 Trigger
+--Droid 8 Trigger
 	local T8Droid = spawnMobile("rori", "death_watch_s_battle_droid", 1, -5507.48, 75.9386, -5668.51, 54, 0)print("Trigger 8 Loaded")
         	createObserver(OBJECTDESTRUCTION, "RoriImperialEncampmentScreenPlay", "notify8DroidDead", T8Droid)
---Boss 9 Trigger
+--Droid 9 Trigger
 	local T9Droid = spawnMobile("rori", "death_watch_s_battle_droid", 1, -5501.78, 75.7897, -5668.51, 345, 0)print("Trigger 9 Loaded")
         	createObserver(OBJECTDESTRUCTION, "RoriImperialEncampmentScreenPlay", "notify9DroidDead", T9Droid)
---Boss 10 Trigger
+--Droid 10 Trigger
 	local T10Droid = spawnMobile("rori", "death_watch_s_battle_droid", 1, -5504.32, 75.9226, -5664.54, 116, 0)print("Trigger 10 Loaded")
         	createObserver(OBJECTDESTRUCTION, "RoriImperialEncampmentScreenPlay", "notify10DroidDead", T10Droid)
 end
@@ -64,25 +64,51 @@ end
 --Phase 2 Of 10 Instance
 function RoriImperialEncampmentScreenPlay:notify2DroidDead(T2Droid, pKiller)
 	local player = LuaCreatureObject(pKiller)
-        local pBoss = spawnMobile("rori", "alert_droideka", 0, -5506.46, 75.8935, -5675.95, 299, 0) print("Phase 2 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5611.96, 75.99, -5645.66, 283, 0) print("Phase 2 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5611.82, 75.995, -5647.94, 255, 0) print("Phase 2 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5611.86, 75.9982, -5650.57, 267, 0) print("Phase 2 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5611.84, 75.9997, -5653.28, 261, 0) print("Phase 2 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5611.86, 76, -5656.02, 253, 0) print("Phase 2 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5611.87, 76, -5658.72, 261, 0) print("Phase 2 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5611.91, 76, -5661.29, 264, 0) print("Phase 2 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5611.82, 76, -5664.05, 268, 0) print("Phase 2 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5611.88, 76, -5666.64, 268, 0) print("Phase 2 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5611.85, 76, -5669.31, 270, 0) print("Phase 2 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5611.82, 76, -5671.94, 264, 0) print("Phase 2 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5611.89, 76, -5674.65, 257, 0) print("Phase 2 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5611.91, 76, -5677.16, 262, 0) print("Phase 2 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5611.99, 76, -5679.13, 268, 0) print("Phase 2 Started")
         return 0
 end
 --Phase 3 Of 10 Instance
 function RoriImperialEncampmentScreenPlay:notify3DroidDead(T3Droid, pKiller)
 	local player = LuaCreatureObject(pKiller)
-        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5626.45, 75.9471, -5661.81, 12, 0) print("Phase 3 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5608.7, 76, -5679.15, 78, 0) print("Phase 3 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5608.73, 76, -5677.17, 348, 0) print("Phase 3 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5608.79, 76, -5674.58, 111, 0) print("Phase 3 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5608.68, 76, -5671.91, 0, 0) print("Phase 3 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5608.59, 76, -5669.17, 26, 0) print("Phase 3 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5608.74, 76, -5666.56, 240, 0) print("Phase 3 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5608.77, 76, -5664.05, 326, 0) print("Phase 3 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5608.8, 76, -5661.21, 341, 0) print("Phase 3 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5608.77, 76, -5658.59, 0, 0) print("Phase 3 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5608.78, 76, -5655.99, 270, 0) print("Phase 3 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5608.79, 76, -5653.22, 83, 0) print("Phase 3 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5608.79, 76, -5650.61, 344, 0) print("Phase 3 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5608.72, 75.9994, -5647.88, 104, 0) print("Phase 3 Started")
+        local pBoss = spawnMobile("rori", "death_watch_s_battle_droid", 0, -5608.7, 75.9974, -5645.74, 82, 0) print("Phase 3 Started")
         return 0
 end
 --Phase 4 Of 10 Instance
 function RoriImperialEncampmentScreenPlay:notify4DroidDead(T4Droid, pKiller)
 	local player = LuaCreatureObject(pKiller)
-        local pBoss = spawnMobile("rori", "alert_droideka", 1, -5623.49, 75.9406, -5649.98, 323, 0) print("Phase 4 Started")
-        local pBoss = spawnMobile("rori", "alert_droideka", 1, -5622.9, 75.9354, -5648.03, 311, 0) print("Phase 4 Started")
-        local pBoss = spawnMobile("rori", "alert_droideka", 1, -5621.94, 75.9294, -5645.49, 33, 0) print("Phase 4 Started")
-        local pBoss = spawnMobile("rori", "alert_droideka", 1, -5620.89, 75.9249, -5643.14, 26, 0) print("Phase 4 Started")
-        local pBoss = spawnMobile("rori", "alert_droideka", 1, -5619.84, 75.9168, -5640.48, 150, 0) print("Phase 4 Started")
-        local pBoss = spawnMobile("rori", "alert_droideka", 1, -5619, 75.909, -5638.13, 350, 0) print("Phase 4 Started")
-        local pBoss = spawnMobile("rori", "alert_droideka", 1, -5617.83, 75.8999, -5634.71, 221, 0) print("Phase 4 Started")
+        local pBoss = spawnMobile("rori", "alert_droideka", 0, -5623.49, 75.9406, -5649.98, 323, 0) print("Phase 4 Started")
+        local pBoss = spawnMobile("rori", "alert_droideka", 0, -5622.9, 75.9354, -5648.03, 311, 0) print("Phase 4 Started")
+        local pBoss = spawnMobile("rori", "alert_droideka", 0, -5621.94, 75.9294, -5645.49, 33, 0) print("Phase 4 Started")
+        local pBoss = spawnMobile("rori", "alert_droideka", 0, -5620.89, 75.9249, -5643.14, 26, 0) print("Phase 4 Started")
+        local pBoss = spawnMobile("rori", "alert_droideka", 0, -5619.84, 75.9168, -5640.48, 150, 0) print("Phase 4 Started")
+        local pBoss = spawnMobile("rori", "alert_droideka", 0, -5619, 75.909, -5638.13, 350, 0) print("Phase 4 Started")
+        local pBoss = spawnMobile("rori", "alert_droideka", 0, -5617.83, 75.8999, -5634.71, 221, 0) print("Phase 4 Started")
         return 0
 end
 --Phase 5 Of 10 Instance
