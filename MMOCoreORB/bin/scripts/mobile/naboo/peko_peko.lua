861e08df4b10160c88c02725b947845908bf831b
Thrax989 <thrax989@yahoo.com>
2017-02-19 11:13:55 -0800

    Revert "Removed loot from Naboo Avian Meat creatures"
    This reverts commit 8126b48d1ea60375459cfdd52111fde560fec9c4.

--- MMOCoreORB/bin/scripts/mobile/naboo/peko_peko.lua
+++ MMOCoreORB/bin/scripts/mobile/naboo/peko_peko.lua
@@ -27,7 +27,22 @@ peko_peko = Creature:new {
 
 	templates = {"object/mobile/peko_peko.iff"},
 	controlDeviceTemplate = "object/intangible/pet/peko_peko_hue.iff",
-
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 10000000},
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "wearables_all", chance = 5000000},
+				{group = "loot_kit_parts", chance = 2500000},
+				{group = "tailor_components", chance = 2500000}
+			},
+			lootChance = 10000000
+		},
+		},
 	weapons = {},
 	conversationTemplate = "",
 	attacks = {
