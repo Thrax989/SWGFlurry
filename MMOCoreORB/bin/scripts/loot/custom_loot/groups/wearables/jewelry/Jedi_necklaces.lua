b79ab9b8b9341c35cc201dff906e0babfaa5cecf
Bobvious <bob@shew.com>
2017-03-06 03:21:48 +0100

    Added Customized Jedi/LS Toughness Jewelry and Loot Groups

---
+++ MMOCoreORB/bin/scripts/loot/custom_loot/groups/wearables/jewelry/Jedi_necklaces.lua
@@ -0,0 +1,10 @@
+jedi_necklaces = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "necklace_jedi_LSTJT", weight = 10000000}, -- Necklace +15 LS / Jedi Toughness
+	}
+}
+
+addLootGroupTemplate("jedi_necklaces", jedi_necklaces)
---
+++ MMOCoreORB/bin/scripts/loot/custom_loot/items/wearables/jewelry/necklace_jedi_LSTJT.lua
@@ -0,0 +1,13 @@
+necklace_jedi_LSTJT = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Cameo of Grand Master Satelle Shan",
+	directObjectTemplate = "object/tangible/wearables/necklace/necklace_opulent_cameo.iff",
+	craftingValues = {},
+	customizationStringNames = {},
+	customizationValues = {},
+	skillMods = {{"jedi_toughness", 15},
+				{"lightsaber_toughness", 15}}
+	}
+
+addLootItemTemplate("necklace_jedi_LSTJT", necklace_jedi_LSTJT)
