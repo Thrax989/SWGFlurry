b4f878e15095b4e9afccef18f23a94857837f416
Gungy <gungy34@gmail.com>
2017-03-08 21:19:10 +0000

    Add files via upload

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/bone/armor_smc_s01_bicep_l.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/bone/armor_smc_s01_bicep_l.lua
@@ -42,14 +42,10 @@
 
 
 object_tangible_wearables_armor_bone_armor_smc_s01_bicep_l = object_tangible_wearables_armor_bone_shared_armor_smc_s01_bicep_l:new {
-	templateType = ARMOROBJECT,
-
 	playerRaces = { "object/creature/player/bothan_male.iff",
 				"object/creature/player/bothan_female.iff",
 				"object/creature/player/human_male.iff",
 				"object/creature/player/human_female.iff",
-				"object/creature/player/ithorian_male.iff",
-				"object/creature/player/ithorian_female.iff",
 				"object/creature/player/moncal_male.iff",
 				"object/creature/player/moncal_female.iff",
 				"object/creature/player/rodian_male.iff",
@@ -60,8 +56,6 @@ object_tangible_wearables_armor_bone_armor_smc_s01_bicep_l = object_tangible_wea
 				"object/creature/player/trandoshan_female.iff",
 				"object/creature/player/twilek_male.iff",
 				"object/creature/player/twilek_female.iff",
-				"object/creature/player/wookiee_male.iff",
-				"object/creature/player/wookiee_female.iff",
 				"object/creature/player/zabrak_male.iff",
 				"object/creature/player/zabrak_female.iff",
 				"object/mobile/vendor/aqualish_female.iff",
@@ -75,8 +69,6 @@ object_tangible_wearables_armor_bone_armor_smc_s01_bicep_l = object_tangible_wea
 				"object/mobile/vendor/human_female.iff",
 				"object/mobile/vendor/human_male.iff",
 				"object/mobile/vendor/ishi_tib_male.iff",
-				"object/mobile/vendor/ithorian_female.iff",
-				"object/mobile/vendor/ithorian_male.iff",
 				"object/mobile/vendor/moncal_female.iff",
 				"object/mobile/vendor/moncal_male.iff",
 				"object/mobile/vendor/nikto_male.iff",
@@ -90,32 +82,35 @@ object_tangible_wearables_armor_bone_armor_smc_s01_bicep_l = object_tangible_wea
 				"object/mobile/vendor/twilek_female.iff",
 				"object/mobile/vendor/twilek_male.iff",
 				"object/mobile/vendor/weequay_male.iff",
-				"object/mobile/vendor/wookiee_female.iff",
-				"object/mobile/vendor/wookiee_male.iff",
 				"object/mobile/vendor/zabrak_female.iff",
 				"object/mobile/vendor/zabrak_male.iff" },
-
 	
-	-- kineticeffectiveness, energyeffectiveness, electricaleffectiveness, stuneffectiveness, blasteffectiveness, heateffectiveness, coldeffectiveness, acideffectiveness, restraineffectiveness
-	vulnerability = KINETIC + BLAST + STUN + HEAT + COLD + ACID + LIGHTSABER,
+	templateType = ARMOROBJECT,
+	objectMenuComponent = "ArmorObjectMenuComponent",
+	
+	vulnerability = BLAST + STUN + HEAT + ELECTRICITY + ACID + LIGHTSABER,
+
+	specialResists = COLD,
 
 	-- These are default Blue Frog stats
-	healthEncumbrance = 6,
+	healthEncumbrance = 9,
 	actionEncumbrance = 15,
-	mindEncumbrance = 4,
+	mindEncumbrance = 7,
+
+	maxCondition = 30000,
 
 	-- LIGHT, MEDIUM, HEAVY
 	rating = LIGHT,
 
-	kinetic = 0,
+	kinetic = 20,
 	energy = 20,
-	electricity = 10,
+	electricity = 0,
 	stun = 0,
 	blast = 0,
 	heat = 0,
-	cold = 0,
+	cold = 25,
 	acid = 0,
-	lightSaber = 0
+	lightSaber = 0,
 }
 
 ObjectTemplates:addTemplate(object_tangible_wearables_armor_bone_armor_smc_s01_bicep_l, "object/tangible/wearables/armor/bone/armor_smc_s01_bicep_l.iff")
