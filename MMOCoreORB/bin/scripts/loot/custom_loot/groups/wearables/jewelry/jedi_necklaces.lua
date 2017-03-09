a6748dee62a0a06823463478066be39b681e16de
bobvious <bob@shew.com>
2017-03-08 19:54:35 -0700

    fixed capitalization in Jedi necklace group

--- MMOCoreORB/bin/scripts/loot/custom_loot/groups/wearables/jewelry/jedi_necklaces.lua
+++ MMOCoreORB/bin/scripts/loot/custom_loot/groups/wearables/jewelry/jedi_necklaces.lua
@@ -3,7 +3,7 @@ jedi_necklaces = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "necklace_jedi_LSTJT", weight = 10000000}, -- Necklace +15 LS / Jedi Toughness
+		{itemTemplate = "necklace_jedi_LSTJT", weight = 10000000}, -- Necklace +15 LS  /  Jedi Toughness
 	}
 }
 
