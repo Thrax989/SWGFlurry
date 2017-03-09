693bdcaea1c878e02c49f13d0b261aeaac32ce6b
Thrax989 <thrax989@yahoo.com>
2017-03-09 01:33:30 -0800

    [Updated] Rartest JT/LST double stated necklaces piece to drop at 1 %
    jedi necklaces
    Single
    1 Jedi toughness necklace +10 stats
    1 Light Saber toughness necklace +10 stats
    
    Double Stats Rare
    1 Jedi toughness + Light Saber toughness necklace 10 both Stats

--- MMOCoreORB/bin/scripts/loot/custom_loot/groups/wearables/jewelry/Jedi_necklaces.lua
+++ MMOCoreORB/bin/scripts/loot/custom_loot/groups/wearables/jewelry/Jedi_necklaces.lua
@@ -3,7 +3,9 @@ jedi_necklaces = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "necklace_jedi_LSTJT", weight = 10000000}, -- Necklace +15 LS / Jedi Toughness
+		{itemTemplate = "necklace_jedi_LST", weight = 5000000},
+		{itemTemplate = "necklace_jedi_JT", weight = 4900000},
+		{itemTemplate = "necklace_jedi_LSTJT", weight = 100000}
 	}
 }
 
