45ad8e0e6a8f57af25234a7593051f1a0da229fa
TOXIC <thrax989@yahoo.com>
2017-03-10 15:33:16 -0800

    [Added] Lightsaber Resist Formula to LS armor to increase decay when struck with a saber
    Prep work for Light saber Resistance armor
    Not too much decay but enough to notice the saber hitting you

--- MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
+++ MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -1180,10 +1180,20 @@ int CombatManager::getArmorReduction(TangibleObject* attacker, WeaponObject* wea
 			sendMitigationCombatSpam(defender, armor, (int)dmgAbsorbed, ARMOR);
 		}
 
-		// inflict condition damage
-		Locker alocker(armor);
-
-		armor->inflictDamage(armor, 0, damage * 0.1, true, true);
+ 		// inflict condition damage
+ 		StringBuffer damageInfo;
+ 		damageInfo
+ 		<< "Damage Type is: "
+ 		<< damageType
+ 		<< " | Your Armor Resistance to LS is: "
+ 		<< getArmorObjectReduction(armor, 16);
+ 		info(damageInfo);
+ 		Locker alocker(armor);
+ 		if (getArmorObjectReduction(armor, 16) > 0 && damageType == 16) {
+ 			armor->inflictDamage(armor, 0, damage * 1.0, true, true);
+ 		} else {
+  			armor->inflictDamage(armor, 0, damage * 0.1, true, true);
+ 		}
 	}
 
 	return damage;
