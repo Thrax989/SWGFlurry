d9fed9ec7bcae2adbd2cf89e0409ea81fd7d411d
TOXIC <thrax989@yahoo.com>
2017-03-10 19:41:15 -0800

    [Updated] Recreated Roll shot command increased power

--- MMOCoreORB/src/server/zone/objects/creature/commands/RollShotCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RollShotCommand.h
@@ -23,7 +23,21 @@ public:
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
+		ManagedReference<SceneObject*> targetObject = creature->getZoneServer()->getObject(target);
+
+		if (targetObject == NULL || !targetObject->isCreatureObject() || targetObject->isPlayerCreature())
+			return INVALIDTARGET;
+
+		CreatureObject* targetCreature = cast<CreatureObject*>(targetObject.get());
+
+		if (targetCreature == NULL)
+			return INVALIDTARGET;
+
+		if (!targetCreature->isAttackableBy(creature))
+			return INVALIDTARGET;
+
 		CreatureObject* player = cast<CreatureObject*>(creature);
+
  		if (!player->checkCooldownRecovery("roll_shot")){
  			Time* cdTime = player->getCooldownTime("roll_shot");
  			int timeleft = floor((float)cdTime->miliDifference() /1000) * -1;
@@ -31,20 +45,29 @@ public:
  			player->sendSystemMessage("Roll shot to is on Cooldown");
  			return GENERALERROR;
  		}
+
  		player->addCooldown("roll_shot", 5 * 1000); //5 second cooldown
 		player->playEffect("clienteffect/lair_med_damage_smoke.cef");
 
-		int ret = doCombatAction(creature, target);
+		int res = doCombatAction(creature, target);
 
-		if (ret != SUCCESS)
-			return ret;
+		CombatManager* combatManager = CombatManager::instance();
 
-		if (creature->isDizzied() && System::random(100) < 50) {
-			creature->queueDizzyFallEvent();
-			creature->playEffect("clienteffect/ui_missile_aquiring.cef", "");
+		if (res == SUCCESS) {
+			Locker clocker(targetCreature, creature);
+
+			targetCreature->playEffect("clienteffect/ui_missile_aquiring.cef", "");
+
+			if (creature->isPlayerCreature())
+				creature->sendSystemMessage("Attack has successfully landed");
+
+		} else {
+
+			if (creature->isPlayerCreature())
+				creature->sendSystemMessage("Attack has failed to land");
 		}
 
-		return SUCCESS;
+		return res;
 	}
 
 };
