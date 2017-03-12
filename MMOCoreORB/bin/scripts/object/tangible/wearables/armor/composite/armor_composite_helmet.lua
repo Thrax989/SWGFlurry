3491ee6eb965ae11b2f637979b3a2e5c3ef94eb7
Thrax989 <thrax989@yahoo.com>
2017-03-12 06:09:32 -0700

    [Added] LS resist to test armor on test

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_helmet.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_helmet.lua
@@ -100,9 +100,9 @@ object_tangible_wearables_armor_composite_armor_composite_helmet = object_tangib
 
 
 	-- Damage types in WeaponObject
-	vulnerability = STUN + LIGHTSABER,
+	vulnerability = STUN,
 
-	specialResists = ELECTRICITY,
+	specialResists = ELECTRICITY + LIGHTSABER,
 
 	-- These are default Blue Frog stats
 	healthEncumbrance = 80,
@@ -122,7 +122,7 @@ object_tangible_wearables_armor_composite_armor_composite_helmet = object_tangib
 	heat = 65,
 	cold = 65,
 	acid = 65,
-	lightSaber = 0,
+	lightSaber = 75,
 
 	numberExperimentalProperties = {1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 2, 1},
 	experimentalProperties = {"XX", "XX", "XX", "XX", "OQ", "SR", "OQ", "UT", "MA", "OQ", "MA", "OQ", "MA", "OQ", "XX", "XX", "OQ", "SR", "XX"},
