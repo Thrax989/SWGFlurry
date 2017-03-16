d4a42b4424151c1bdf054c4b56ccd596b2d599d2
TOXIC <thrax989@yahoo.com>
2017-03-16 02:15:11 -0700

    [Added] Created Rare Loot Cube Item Generation for collections pack

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/item/loot_cube.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/item/loot_cube.lua
@@ -1,1 +1,17 @@
-object_tangible_item_loot_cube = object_tangible_item_shared_loot_cube:new {}ObjectTemplates:addTemplate(object_tangible_item_loot_cube, "object/tangible/item/loot_cube.iff")
+object_tangible_item_loot_cube = object_tangible_item_shared_loot_cube:new {
+  
+	templateType = LOOTKIT,
+	
+	gameObjectType = 8233,
+	
+	--These are used to determine which components are necessary in the loot kit to finish the item
+	collectibleComponents = {"object/tangible/loot/misc/datadisk_corrupt.iff", "object/tangible/loot/simple_kit/magnetic_reader.iff", "object/tangible/loot/simple_kit/magnetic_burner.iff", "object/tangible/loot/simple_kit/recovery_software.iff", "object/tangible/loot/simple_kit/wiring.iff"},
+	
+	collectibleReward = {"object/tangible/loot/simple_kit/datadisk.iff"},
+
+	deleteComponents = 0,
+
+	attributes = {}
+}
+
+ObjectTemplates:addTemplate(object_tangible_item_loot_cube, "object/tangible/item/loot_cube.iff")
