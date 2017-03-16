36b28b727142ff03656ced5fca0b9f633a2c7d5a
TOXIC <thrax989@yahoo.com>
2017-03-16 05:57:24 -0700

    [Update] Loot cube

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/item/loot_cube.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/item/loot_cube.lua
@@ -5,9 +5,9 @@ object_tangible_item_loot_cube = object_tangible_item_shared_loot_cube:new {
 	gameObjectType = 8233,
 	
 	--These are used to determine which components are necessary in the loot kit to finish the item
-	collectibleComponents = {"object/tangible/loot/misc/datadisk_corrupt.iff", "object/tangible/loot/misc/datadisk_corrupt.iff", "object/tangible/loot/misc/datadisk_corrupt.iff"},
+	collectibleComponents = {"object/tangible/jedi/jedi_holocron_dark.iff", "object/tangible/jedi/jedi_holocron_light.iff", "object/tangible/mission/quest_item/luthik_uwyr_q3_needed.iff"},
 	
-	collectibleReward = {"object/tangible/loot/simple_kit/datadisk.iff"},
+	collectibleReward = {"object/tangible/loot/mustafar/jedi_relic_01.iff"},
 
 	deleteComponents = 0,
 
