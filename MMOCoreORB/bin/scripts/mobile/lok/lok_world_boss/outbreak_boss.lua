60cca33f009757334f328a61524ba66ac248d41c
TOXIC <thrax989@yahoo.com>
2017-03-17 08:09:52 -0700

    [Fixed] World boss template

--- MMOCoreORB/bin/scripts/mobile/lok/lok_world_boss/outbreak_boss.lua
+++ MMOCoreORB/bin/scripts/mobile/lok/lok_world_boss/outbreak_boss.lua
@@ -31,180 +31,180 @@ outbreak_boss = Creature:new {
   lootGroups = {
 		{
 			groups = {
-        {group = "pearls_flawless", chance = 10000000},
+        			{group = "pearls_flawless", chance = 10000000},
 				{group = "armor_attachments", chance = 10000000},
 				{group = "clothing_attachments", chance = 10000000}
-			},
+		},
 			lootChance = 10000000
 		},
-    {
-      groups = {
-        {group = "pearls_flawless", chance = 10000000},
-        {group = "armor_attachments", chance = 10000000},
-        {group = "clothing_attachments", chance = 10000000}
-      },
-      lootChance = 10000000
-    },
-    {
-      groups = {
-        {group = "pearls_flawless", chance = 10000000},
+   		{
+			groups = {
+        			{group = "pearls_flawless", chance = 10000000},
+        			{group = "armor_attachments", chance = 10000000},
+        			{group = "clothing_attachments", chance = 10000000}
+      		},
+      			lootChance = 10000000
+    		},
+    		{
+      			groups = {
+        			{group = "pearls_flawless", chance = 10000000},
 				{group = "armor_attachments", chance = 10000000},
 				{group = "clothing_attachments", chance = 10000000}
-      },
-      lootChance = 10000000
-    },
-    {
-      groups = {
-        {group = "pearls_flawless", chance = 5000000},
-        {group = "armor_attachments", chance = 2500000},
-        {group = "clothing_attachments", chance = 2500000}
-      },
-      lootChance = 10000000
-    },
-    {
-      groups = {
-        {group = "armor_attachments", chance = 10000000},
-        {group = "clothing_attachments", chance = 10000000}
-      },
-      lootChance = 10000000
-    },
-    {
-      groups = {
-        {group = "armor_attachments", chance = 10000000},
-        {group = "clothing_attachments", chance = 10000000}
-      },
-      lootChance = 10000000
-    },
-    {
-      groups = {
-        {group = "armor_attachments", chance = 10000000},
-        {group = "clothing_attachments", chance = 10000000}
-      },
-      lootChance = 10000000
-    },
-    {
-      groups = {
-        {group = "armor_attachments", chance = 10000000},
-        {group = "clothing_attachments", chance = 10000000}
-      },
-      lootChance = 10000000
-    },
+      		},
+      			lootChance = 10000000
+    		},
+   		{
+      			groups = {
+        			{group = "pearls_flawless", chance = 5000000},
+       	 			{group = "armor_attachments", chance = 2500000},
+        			{group = "clothing_attachments", chance = 2500000}
+      		},
+      			lootChance = 10000000
+    		},
+    		{
+      			groups = {
+        			{group = "armor_attachments", chance = 10000000},
+        			{group = "clothing_attachments", chance = 10000000}
+      		},
+      			lootChance = 10000000
+    		},
+    		{
+     	 		groups = {
+        			{group = "armor_attachments", chance = 10000000},
+        			{group = "clothing_attachments", chance = 10000000}
+      		},
+      			lootChance = 10000000
+    		},
+    		{
+      			groups = {
+        			{group = "armor_attachments", chance = 10000000},
+       	 			{group = "clothing_attachments", chance = 10000000}
+      		},
+     			 lootChance = 10000000
+    		},
+    		{
+     			groups = {
+        			{group = "armor_attachments", chance = 10000000},
+        			{group = "clothing_attachments", chance = 10000000}
+      		},
+      			lootChance = 10000000
+    		},
 		{
 			groups = {
 				{group = "g_named_crystals", chance = 10000000}
-			},
-			lootChance = 10000000
-	},
-  {
-    groups = {
-      {group = "g_ep3_chiss_poacher_backpack", chance = 10000000},
-      {group = "barc_speeder_deed_group", chance = 10000000}
-    },
-    lootChance = 10000000
-  },
-  {
-    groups = {
-      {group = "av_21_pp", chance = 10000000}
-    },
-    lootChance = 10000000
-  },
-  {
-    groups = {
-      {group = "av_21_scheme", chance = 10000000}
-    },
-    lootChance = 10000000
-  },
-	{
-		groups = {
-			{group = "nightsister_rare", chance = 5000000}
 		},
-		lootChance = 10000000
+			lootChance = 10000000
+		},
+  		{
+   			groups = {
+      				{group = "g_ep3_chiss_poacher_backpack", chance = 10000000},
+      				{group = "barc_speeder_deed_group", chance = 10000000}
+    		},
+    			lootChance = 10000000
+  		},
+  		{
+    			groups = {
+      				{group = "av_21_pp", chance = 10000000}
+    		},
+    			lootChance = 10000000
+  		},
+  		{
+    			groups = {
+      				{group = "av_21_scheme", chance = 10000000}
+    		},
+    			lootChance = 10000000
+  		},
+		{
+			groups = {
+				{group = "nightsister_rare", chance = 5000000}
+		},
+			lootChance = 10000000
 		},
 		{
 			groups = {
-			{group = "krayt_tissue_rare", chance = 10000000}
+				{group = "krayt_tissue_rare", chance = 10000000}
 		},
-		lootChance = 10000000
+			lootChance = 10000000
 		},
 		{
-		groups = {
-			{group = "looted_container", chance = 10000000}
-				},
-				lootChance = 10000000
-			},
-      {
-      groups = {
-        {group = "nyms_rare", chance = 10000000}
-          },
-          lootChance = 10000000
-        },
-        {
-        groups = {
-          {group = "gorax_common", chance = 10000000}
-            },
-            lootChance = 10000000
-          },
-			    {
+			groups = {
+				{group = "looted_container", chance = 10000000}
+		},
+			lootChance = 10000000
+		},
+      		{
+     	 	 	groups = {
+        			{group = "nyms_rare", chance = 10000000}
+          	},
+          		lootChance = 10000000
+        	},
+        	{
+        		groups = {
+          			{group = "gorax_common", chance = 10000000}
+            	},
+            		lootChance = 10000000
+          	},
+		{
 			groups = {
 				{group = "weapons_all", chance = 10000000}
-					},
-					lootChance = 10000000
-				},
-        {
-          groups = {
-            {group = "blacksunhelm1", chance = 10000000}
-              },
-              lootChance = 10000000
-            },
-          {
-          groups = {
-            {group = "skill_buffs", chance = 10000000}
-              },
-              lootChance = 10000000
-            },
-        {
-        groups = {
-          {group = "wearables_rare", chance = 10000000}
-            },
-            lootChance = 10000000
-          },
-          {
-          groups = {
-            {group = "death_watch_bunker_art", chance = 10000000}
-            },
-            lootChance = 10000000
-        },
-        {
-        groups = {
-          {group = "bounty_hunter_armor", chance = 10000000}
-          },
-          lootChance = 10000000
-        },
-        {
-          groups = {
-            {group = "acklay", chance = 10000000}
-          },
-          lootChance = 10000000
-        },
-        {
-          groups = {
-            {group = "acklay", chance = 10000000}
-          },
-          lootChance = 10000000
-        },
-        {
-        groups = {
-          {group = "fire_breathing_spider", chance = 10000000}
-            },
-            lootChance = 10000000
-          },
-          {
-          groups = {
-            {group = "fire_breathing_spider", chance = 10000000}
-              },
-              lootChance = 10000000
-            },
-	},
+		},
+			lootChance = 10000000
+		},
+        	{
+          		groups = {
+           			 {group = "blacksunhelm1", chance = 10000000}
+              	},
+              		lootChance = 10000000
+            	},
+          	{
+          		groups = {
+            			{group = "skill_buffs", chance = 10000000}
+              	},
+              		lootChance = 10000000
+            	},
+        	{
+        		groups = {
+          			{group = "wearables_rare", chance = 10000000}
+            	},
+           		 lootChance = 10000000
+          	},
+          	{
+          		groups = {
+            			{group = "death_watch_bunker_art", chance = 10000000}
+            	},
+            		lootChance = 10000000
+        	},
+        	{
+        		groups = {
+          			{group = "bounty_hunter_armor", chance = 10000000}
+          	},
+          		lootChance = 10000000
+        	},
+        	{
+          		groups = {
+            			{group = "acklay", chance = 10000000}
+          	},
+          		lootChance = 10000000
+        	},
+        	{
+          		groups = {
+            			{group = "acklay", chance = 10000000}
+          	},
+          		lootChance = 10000000
+        	},
+        	{
+        		groups = {
+          			{group = "fire_breathing_spider", chance = 10000000}
+            	},
+            		lootChance = 10000000
+          	},
+          	{
+          		groups = {
+            			{group = "fire_breathing_spider", chance = 10000000}
+              	},
+              		lootChance = 10000000
+            	},
+		},
 	weapons = {},
 	reactionStf = "@npc_reaction/slang",
 	attacks = {
