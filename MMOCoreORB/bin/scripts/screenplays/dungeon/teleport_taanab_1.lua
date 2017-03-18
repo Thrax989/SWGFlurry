ff0b1adf0219aa6a01a35e306878ca71c8615d25
TOXIC <thrax989@yahoo.com>
2017-03-18 05:28:03 -0700

    [Updated] Taanab port 1 placement

--- MMOCoreORB/bin/scripts/screenplays/dungeon/teleport_taanab_1.lua
+++ MMOCoreORB/bin/scripts/screenplays/dungeon/teleport_taanab_1.lua
@@ -20,7 +20,7 @@ end
 
 function teleport_taanab_1ScreenPlay:spawnMobiles()
 
-	local pCollector1 = spawnMobile("corellia", "taanabport1", 1, -158.97, 28, -4705.32, 175, 0 )
+	local pCollector1 = spawnMobile("corellia", "taanabport1", 1, -159.018, 28, -4686.68, 350, 0 )
 
 	local collector1 = LuaCreatureObject(pCollector1)
 	collector1:setOptionsBitmask(264)
