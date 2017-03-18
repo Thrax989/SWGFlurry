fe4982a098c670a013cbd1fd12aa2091c51ac434
TOXIC <thrax989@yahoo.com>
2017-03-18 05:40:53 -0700

    [Updated] Mandalore port1 placement

--- MMOCoreORB/bin/scripts/screenplays/dungeon/teleport_mandalore_1.lua
+++ MMOCoreORB/bin/scripts/screenplays/dungeon/teleport_mandalore_1.lua
@@ -20,7 +20,7 @@ end
 
 function teleport_mandalore_1ScreenPlay:spawnMobiles()
 
-	local pCollector1 = spawnMobile("corellia", "mandaloreport1", 1, -158.97, 28, -4705.32, 175, 0 )
+	local pCollector1 = spawnMobile("corellia", "mandaloreport1", 1, -190.995, 28, -4686.68, 3, 0 )
 
 	local collector1 = LuaCreatureObject(pCollector1)
 	collector1:setOptionsBitmask(264)
