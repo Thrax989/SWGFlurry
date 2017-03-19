eb05d131fa807a2740fc8c68812f3968721ec02b
Hellucard <eclipsesql@gmail.com>
2017-03-20 00:43:38 +0100

    Increased Damage, Changed Resis

--- MMOCoreORB/bin/scripts/mobile/dantooine/janta_chieftain.lua
+++ MMOCoreORB/bin/scripts/mobile/dantooine/janta_chieftain.lua
@@ -4,13 +4,13 @@ janta_chieftain = Creature:new {
 	faction = "janta_tribe",
 	level = 300,
 	chanceHit = 60.00,
-	damageMin = 2000,
-	damageMax = 3400,
+	damageMin = 2800,
+	damageMax = 3500,
 	baseXp = 27849,
-	baseHAM = 521000,
-	baseHAMmax = 892000,
+	baseHAM = 500000,
+	baseHAMmax = 900000,
 	armor = 0,
-	resists = {50,50,50,50,50,50,50,50,50},
+	resists = {80,80,80,80,80,80,80,80,80},
 	meatType = "",
 	meatAmount = 0,
 	hideType = "",
@@ -34,7 +34,7 @@ janta_chieftain = Creature:new {
 				{group = "clothing_attachments", chance = 1000000},
 				{group = "crystals_premium", chance = 4000000},
 				{group = "pearls_flawless", chance = 4000000}
-			},
+		},
 			lootChance = 10000000
 		},
 		{
@@ -43,43 +43,43 @@ janta_chieftain = Creature:new {
 				{group = "aakuan_common", chance = 4000000},
 				{group = "g_named_crystals", chance = 2000000}
 
-			},
+		},
 			lootChance = 8000000
-	},
-	{
-		groups = {
-			{group = "janta_common", chance = 3000000},
-			{group = "armor_attachments", chance = 3000000},
-			{group = "clothing_attachments", chance = 4000000}
+		},
+		{
+			groups = {
+				{group = "janta_common", chance = 3000000},
+				{group = "armor_attachments", chance = 3000000},
+				{group = "clothing_attachments", chance = 4000000}
 
 		},
-		lootChance = 6000000
+			lootChance = 6000000
 		},
 		{
 			groups = {
-			{group = "krayt_tissue_rare", chance = 10000000}
+				{group = "krayt_tissue_rare", chance = 10000000}
 		},
-		lootChance = 6000000
+			lootChance = 6000000
 		},
 		{
-		groups = {
-			{group = "looted_container", chance = 10000000},
+			groups = {
+				{group = "looted_container", chance = 10000000},
 				},
-				lootChance = 6000000
-			},
-			{
+			lootChance = 6000000
+		},
+		{
 			groups = {
 				{group = "pearls_flawless", chance = 10000000},
-					},
-					lootChance = 6000000
-				},
-				{
-				groups = {
-					{group = "janta_rare", chance = 10000000},
-						},
-						lootChance = 2000000
-					},
-					},
+		},
+			lootChance = 6000000
+		},
+		{
+			groups = {
+				{group = "janta_rare", chance = 10000000},
+		},
+			lootChance = 2000000
+		},
+		},
 
 	weapons = {"unarmed_weapons"},
 	reactionStf = "@npc_reaction/slang",
