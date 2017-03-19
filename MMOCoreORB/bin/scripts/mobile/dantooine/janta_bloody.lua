275a2c890a7fad14a31f73cca336908be1bbe7d6
Hellucard <eclipsesql@gmail.com>
2017-03-20 00:50:35 +0100

    Increased HAM

--- MMOCoreORB/bin/scripts/mobile/dantooine/janta_bloody.lua
+++ MMOCoreORB/bin/scripts/mobile/dantooine/janta_bloody.lua
@@ -7,8 +7,8 @@ janta_bloody = Creature:new {
   damageMin = 2800,
   damageMax = 3500,
   baseXp = 0,
-  baseHAM = 300000,
-  baseHAMmax = 500000,
+  baseHAM = 800000,
+  baseHAMmax = 800000,
   armor = 2,
   resists = {80,80,80,80,80,80,80,80,80},
   meatType = "",
@@ -34,40 +34,40 @@ janta_bloody = Creature:new {
           {group = "clothing_attachments", chance = 1000000},
           {group = "crystals_premium", chance = 4000000},
           {group = "pearls_flawless", chance = 4000000}
-        },
+},
         lootChance = 10000000
-      },
-      {
+},
+{
         groups = {
           {group = "wearables_rare", chance = 5000000},
           {group = "aakuan_common", chance = 3000000},
           {group = "nightsister_common", chance = 2000000}
 
-        },
+},
         lootChance = 10000000
-    },
-    {
+},
+{
       groups = {
         {group = "axkva_min", chance = 4000000},
         {group = "armor_attachments", chance = 3000000},
         {group = "clothing_attachments", chance = 3000000}
 
-      },
+},
       lootChance = 6000000
-      },
-      {
-        groups = {
-        {group = "axkva_min", chance = 10000000}
-      },
-      lootChance = 4000000
-      },
-      {
-      groups = {
-        {group = "looted_container", chance = 10000000},
-          },
+},
+{
+          groups = {
+                   {group = "axkva_min", chance = 10000000}
+},
+          lootChance = 4000000
+},
+{
+          groups = {
+                   {group = "looted_container", chance = 10000000},
+},
           lootChance = 2000000
-       },
-  },
+},
+},
 
   weapons = {"mixed_force_weapons"},
   conversationTemplate = "",
