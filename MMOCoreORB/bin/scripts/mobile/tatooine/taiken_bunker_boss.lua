07d48f558830d7d92806f64e4494f26867d0de0c
Hellucard <eclipsesql@gmail.com>
2017-03-20 00:56:14 +0100

    Increased Damage, HAM and Removed Loot

--- MMOCoreORB/bin/scripts/mobile/tatooine/taiken_bunker_boss.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/taiken_bunker_boss.lua
@@ -1,15 +1,15 @@
 taiken_bunker_boss = Creature:new {
-  customName = "Lord Niax",
+  customName = "Supreme Leader Gnox",
   socialGroup = "geonosian",
   level = 300,
-  chanceHit = 80.00,
-  damageMin = 2800,
-  damageMax = 3600,
+  chanceHit = 100.00,
+  damageMin = 3200,
+  damageMax = 4000,
   baseXp = 0,
-  baseHAM = 500000,
-  baseHAMmax = 800000,
+  baseHAM = 1000000,
+  baseHAMmax = 1000000,
   armor = 3,
-  resists = {100,90,20,90,20,90,90,90,65},
+  resists = {100,90,90,90,90,90,90,90,90},
   meatType = "",
   meatAmount = 0,
   hideType = "",
@@ -33,46 +33,40 @@ taiken_bunker_boss = Creature:new {
   				{group = "clothing_attachments", chance = 1000000},
   				{group = "crystals_premium", chance = 4000000},
   				{group = "pearls_flawless", chance = 4000000}
-  			},
+},
         lootChance = 10000000
-  		},
-      {
+},
+{
         groups = {
   				{group = "wearables_rare", chance = 4000000},
   				{group = "aakuan_common", chance = 3000000},
           {group = "g_named_crystals", chance = 3000000}
 
-  			},
+},
   			lootChance = 8000000
-  	},
-    {
+},
+{
       groups = {
         {group = "axkva_min", chance = 4000000},
         {group = "armor_attachments", chance = 3000000},
         {group = "clothing_attachments", chance = 3000000}
 
-      },
-      lootChance = 6000000
-      },
-      {
+},
+        lootChance = 6000000
+},
+{
         groups = {
         {group = "krayt_tissue_rare", chance = 10000000}
-      },
-      lootChance = 4000000
-      },
-      {
-      groups = {
-        {group = "looted_container", chance = 10000000},
-          },
-          lootChance = 4000000
-        },
-        {
+},
+        lootChance = 10000000
+},
+{
         groups = {
-          {group = "pearls_flawless", chance = 10000000},
-            },
-            lootChance = 5000000
-          },
-    },
+        {group = "looted_container", chance = 10000000},
+},
+          lootChance = 10000000
+},
+},
   weapons = {"dark_jedi_weapons_gen4"},
   conversationTemplate = "",
   reactionStf = "@npc_reaction/fancy",
