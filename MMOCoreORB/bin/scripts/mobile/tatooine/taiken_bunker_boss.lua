59a29f1835f43a12a599707f1989149276d069db
Hellucard <eclipsesql@gmail.com>
2017-03-21 14:32:44 +0100

    [Fixed] Loot

--- MMOCoreORB/bin/scripts/mobile/tatooine/taiken_bunker_boss.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/taiken_bunker_boss.lua
@@ -3,8 +3,8 @@ taiken_bunker_boss = Creature:new {
   socialGroup = "geonosian",
   level = 300,
   chanceHit = 100.00,
-  damageMin = 3200,
-  damageMax = 4000,
+  damageMin = 500,
+  damageMax = 6000,
   specialDamageMult = 5.0,
   baseXp = 0,
   baseHAM = 1500000,
@@ -29,55 +29,77 @@ taiken_bunker_boss = Creature:new {
     "object/mobile/dressed_lord_nyax.iff"},
     lootGroups = {
   		{
-  			groups = {
-  				{group = "armor_attachments", chance = 1000000},
-  				{group = "clothing_attachments", chance = 1000000},
-  				{group = "crystals_premium", chance = 4000000},
-  				{group = "pearls_flawless", chance = 4000000}
+  			      groups = {
+  				            {group = "armor_attachments", chance = 1000000},
+  				            {group = "clothing_attachments", chance = 1000000},
+  				            {group = "crystals_premium", chance = 4000000},
+  				            {group = "pearls_flawless", chance = 4000000}
 },
-        lootChance = 10000000
+                      lootChance = 10000000
 },
 {
-        groups = {
-  				{group = "wearables_rare", chance = 10000000},
-  				{group = "aakuan_common", chance = 10000000},
-          {group = "g_named_crystals", chance = 10000000}
+            groups = {
+  				          {group = "wearables_rare", chance = 10000000},
+  				          {group = "aakuan_common", chance = 10000000}
 
 },
-  			lootChance = 8000000
+  			            lootChance = 10000000
 },
 {
-      groups = {
-        {group = "axkva_min", chance = 10000000},
-        {group = "armor_attachments", chance = 10000000},
-        {group = "clothing_attachments", chance = 10000000}
+            groups = {
+                    {group = "g_named_crystals", chance = 10000000}
 
 },
-        lootChance = 8000000
+  			            lootChance = 8000000
 },
 {
-        groups = {
-        {group = "krayt_tissue_rare", chance = 10000000}
+            groups = {
+                    {group = "armor_attachments", chance = 10000000},
+                    {group = "clothing_attachments", chance = 10000000}
 },
-        lootChance = 10000000
-},
-{
-        groups = {
-        {group = "looted_container", chance = 10000000}
-},
-          lootChance = 10000000
+                    lootChance = 10000000
 },
 {
-        groups = {
-        {group = "apocalypse", chance = 10000000}
+            groups = {
+                    {group = "axkva_min", chance = 10000000}
 },
-          lootChance = 10000000
+                    lootChance = 8000000
 },
 {
-        groups = {
-        {group = "theme_park_reward_imperial_kaja", chance = 10000000}
+    			  groups = {
+      				      {group = "av_21_pp", chance = 10000000}
 },
-          lootChance = 10000000
+    			          lootChance = 5000000
+},
+{
+            groups = {
+                    {group = "krayt_tissue_rare", chance = 10000000}
+},
+                    lootChance = 10000000
+},
+{
+            groups = {
+                    {group = "looted_container", chance = 10000000}
+},
+                    lootChance = 10000000
+},
+{
+             groups = {
+                    {group = "apocalypse", chance = 10000000}
+},
+                    lootChance = 10000000
+},
+{
+          		groups = {
+            			{group = "fire_breathing_spider", chance = 10000000}
+},
+              		        lootChance = 10000000
+},
+{
+             groups = {
+                     {group = "theme_park_reward_imperial_kaja", chance = 10000000}
+},
+                     lootChance = 10000000
 },
 },
   weapons = {"dark_jedi_weapons_gen4"},
