ac43be57497a55d8824b53cb94975eb26ee1d409
TOXIC <thrax989@yahoo.com>
2017-03-23 01:51:53 -0700

    [Updated ]Guard  Door Entrance droids for mini first encounter

--- MMOCoreORB/bin/scripts/screenplays/poi/yavin4_geocave.lua
+++ MMOCoreORB/bin/scripts/screenplays/poi/yavin4_geocave.lua
@@ -20,12 +20,12 @@ function yavin4geocaveScreenPlay:spawnMobiles()
 
 --Door 1 Trigger
 
-	local T1Droid = spawnMobile("yavin4", "death_watch_s_battle_droid", 1, -5628.4, 75.9027, -5666.37, 170, 0)print("Trigger 1 Loaded")
+	local T1Droid = spawnMobile("yavin4", "alert_droideka", 1800, 2.99904, 8.84073, 19.9977, 8, 1627781)print("Trigger 1 Loaded")
         	createObserver(OBJECTDESTRUCTION, "yavin4geocaveScreenPlay", "notify1DroidDead", T1Droid)
 
 --Door 2 Trigger
 
-	local T2Droid = spawnMobile("yavin4", "death_watch_s_battle_droid", 1, -5622.79, 75.9766, -5666.37, 168, 0)print("Trigger 2 Loaded")
+	local T2Droid = spawnMobile("yavin4", "alert_droideka", 1800, 9.9116, 8.803, 19.4693, 0, 1627781)print("Trigger 2 Loaded")
         	createObserver(OBJECTDESTRUCTION, "yavin4geocaveScreenPlay", "notify2DroidDead", T2Droid)
 end
 
@@ -41,6 +41,6 @@ end
 
 function yavin4geocaveScreenPlay:notify2DroidDead(T2Droid, pKiller)
 	local player = LuaCreatureObject(pKiller)
-        local pBoss = spawnMobile("yavin4", "death_watch_s_battle_droid", 0, -5611.96, 75.99, -5645.66, 283, 0) print("Phase 2 Started")
+        local pBoss = spawnMobile("yavin4", "alert_droideka", 0, -5611.96, 75.99, -5645.66, 283, 0) print("Phase 2 Started")
         return 0
 end
