2b647db21ec442336d991e1bcf9143bf0527f7fa
TOXIC <thrax989@yahoo.com>
2017-03-23 05:34:39 -0700

    [Updated] Unit errors

--- MMOCoreORB/bin/scripts/mobile/mandalore/PykeSyndicateCriminal.lua
+++ MMOCoreORB/bin/scripts/mobile/mandalore/PykeSyndicateCriminal.lua
@@ -35,11 +35,11 @@ pyke_syndicate_criminal = Creature:new {
 				{group = "pistols", chance = 750000},
 				{group = "rifles", chance = 750000},
 				{group = "carbines", chance = 750000},
-				{group = "armor_attachments", chance = 500000},				
+				{group = "clothing_attachments", chance = 500000},				
 				{group = "junk", chance = 700000},
-				{group = "clothing_attachments", chance = 50000},
+				{group = "armor_attachments", chance = 50000},
 			},
-			lootChance = 3500000
+			lootChance = 10000000
 		}	
 	},
 	weapons = {"pirate_weapons_heavy"},
