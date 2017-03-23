50249894e4bbffb97378e592ef60a62ab6d5da4c
Thrax989 <thrax989@yahoo.com>
2017-03-23 01:40:53 -0700

    [Added] Geo cave dungeon revamp . Added first set of paired Triggers

--- MMOCoreORB/bin/scripts/screenplays/poi/yavin4_geocave.lua
+++ MMOCoreORB/bin/scripts/screenplays/poi/yavin4_geocave.lua
@@ -18,4 +18,29 @@ end
 
 function yavin4geocaveScreenPlay:spawnMobiles()
 
+--Door 1 Trigger
+
+	local T1Droid = spawnMobile("yavin4", "death_watch_s_battle_droid", 1, -5628.4, 75.9027, -5666.37, 170, 0)print("Trigger 1 Loaded")
+        	createObserver(OBJECTDESTRUCTION, "yavin4geocaveScreenPlay", "notify1DroidDead", T1Droid)
+
+--Door 2 Trigger
+
+	local T2Droid = spawnMobile("yavin4", "death_watch_s_battle_droid", 1, -5622.79, 75.9766, -5666.37, 168, 0)print("Trigger 2 Loaded")
+        	createObserver(OBJECTDESTRUCTION, "yavin4geocaveScreenPlay", "notify2DroidDead", T2Droid)
+end
+
+--Phase 1 Of 2 Door Trigger
+
+function yavin4geocaveScreenPlay:notify1DroidDead(T1Droid, pKiller)
+	local player = LuaCreatureObject(pKiller)
+        local pBoss = spawnMobile("yavin4", "alert_droideka", 0, -5622.48, 75.9636, -5673.65, 97, 0) print("Phase 1 Started")
+        return 0
+end
+
+--Phase 2 Of 2 Door Trigger
+
+function yavin4geocaveScreenPlay:notify2DroidDead(T2Droid, pKiller)
+	local player = LuaCreatureObject(pKiller)
+        local pBoss = spawnMobile("yavin4", "death_watch_s_battle_droid", 0, -5611.96, 75.99, -5645.66, 283, 0) print("Phase 2 Started")
+        return 0
 end
