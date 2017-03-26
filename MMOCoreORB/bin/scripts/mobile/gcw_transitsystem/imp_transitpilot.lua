aadf1bff3c8c0d4a66f2d8ef0d77d72b580b0083
TOXIC <thrax989@yahoo.com>
2017-03-26 08:32:16 -0700

    [Updated] Rebel Ports

--- MMOCoreORB/bin/scripts/mobile/gcw_transitsystem/imp_transitpilot.lua
+++ MMOCoreORB/bin/scripts/mobile/gcw_transitsystem/imp_transitpilot.lua
@@ -24,7 +24,7 @@ imp_transitpilot = Creature:new {
    ferocity = 0,
    pvpBitmask = NONE,
    creatureBitmask = NONE,
-   optionsBitmask = 264, --for conversation
+   optionsBitmask = INVULNERABLE + INTERESTING,
    diet = HERBIVORE,
 
    templates = {"object/mobile/dressed_imperial_lieutenant_m.iff"},
