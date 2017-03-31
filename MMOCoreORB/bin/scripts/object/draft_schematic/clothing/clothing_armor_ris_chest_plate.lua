3d82716f25eb77b33b346f652e3aaf32b24de118
TOXIC <thrax989@yahoo.com>
2017-03-30 18:02:03 -0700

    [Rebase]

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_ris_chest_plate.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_ris_chest_plate.lua
@@ -73,9 +73,6 @@ object_draft_schematic_clothing_clothing_armor_ris_chest_plate = object_draft_sc
    targetTemplate = "object/tangible/wearables/armor/ris/armor_ris_chest_plate.iff",
 
    additionalTemplates = {
-   "object/tangible/wearables/armor/mandalorian/armor_mandalorian_chest_plate.iff",
-   "object/tangible/wearables/armor/mandalorian_rebel/armor_mandalorian_rebel_chest_plate.iff",
-   "object/tangible/wearables/armor/mandalorian_imperial/armor_mandalorian_imperial_chest_plate.iff",
              }
 
 }
