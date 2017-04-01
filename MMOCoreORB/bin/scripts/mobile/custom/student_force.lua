ab83a5cfbeefeb9008372fecd43574bb272a238b
TOXIC <thrax989@yahoo.com>
2017-04-01 02:42:23 -0700

    [Updated] Force Students

---
+++ MMOCoreORB/bin/scripts/mobile/custom/student_force.lua
@@ -0,0 +1,55 @@
+student_force = Creature:new {
+	--objectName = "@mob/creature_names:",
+	customName = "Student of the force",
+	socialGroup = "self",
+	pvpFaction = "",
+	faction = "",
+	level = 300,
+	chanceHit = 100.00,
+	damageMin = 1800,
+	damageMax = 4310,
+	baseXp = 27849,
+	baseHAM = 5021000,
+	baseHAMmax = 5920000,
+	armor = 0,
+	resists = {100,100,100,100,100,100,100,100,100},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = KILLER + STALKER,
+	optionsBitmask = AIENABLED,
+	diet = HERBIVORE,
+	scale = 0.5,
+	templates = {"object/mobile/ep3/palpatine_hologram.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 5000000},
+				{group = "armor_all", chance = 2500000},
+				{group = "weapons_all", chance = 2500000}
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "wearables_all", chance = 5000000},
+				{group = "loot_kit_parts", chance = 2500000},
+				{group = "tailor_components", chance = 2500000}
+			},
+			lootChance = 10000000
+		},
+		},
+	weapons = {"dark_jedi_weapons_gen4"},
+	outfit = "student_of_the_force_outfit",
+	reactionStf = "@npc_reaction/slang",
+	attacks = merge(lightsabermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(student_force, "student_force")
---
+++ MMOCoreORB/bin/scripts/mobile/custom/student_force2.lua
@@ -0,0 +1,56 @@
+student_force2 = Creature:new {
+	--objectName = "@mob/creature_names:",
+	customName = "Student of the force",
+	socialGroup = "self",
+	pvpFaction = "",
+	faction = "",
+	level = 300,
+	chanceHit = 30,
+	damageMin = 350,
+	damageMax = 1250,
+	specialDamageMult = 7.5,
+	baseXp = 28549,
+	baseHAM = 385000,
+	baseHAMmax = 471000,
+	armor = 3,
+	resists = {90,90,90,90,90,90,90,90,90},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 1,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = PACK + KILLER + HEALER,
+	optionsBitmask = AIENABLED,
+	diet = HERBIVORE,
+	scale = 0.5,
+	templates = {"object/mobile/som/obi_wan_ghost.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 5000000},
+				{group = "armor_all", chance = 2500000},
+				{group = "weapons_all", chance = 2500000}
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "wearables_all", chance = 5000000},
+				{group = "loot_kit_parts", chance = 2500000},
+				{group = "tailor_components", chance = 2500000}
+			},
+			lootChance = 10000000
+		},
+		},
+	weapons = {"dark_jedi_weapons_gen4"},
+	outfit = "student_of_the_force_outfit",
+	reactionStf = "@npc_reaction/slang",
+	attacks = merge(lightsabermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(student_force2, "student_force2")
