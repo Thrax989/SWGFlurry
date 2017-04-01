3d3aeeb3535c849729e97d29fdbd943c245ea823
TOXIC <thrax989@yahoo.com>
2017-04-01 04:43:30 -0700

    [Added] 1-2 triggers 1-2 phases for begining encounter

--- MMOCoreORB/bin/scripts/screenplays/poi/kaas_imperial_garrison_triggers.lua
+++ MMOCoreORB/bin/scripts/screenplays/poi/kaas_imperial_garrison_triggers.lua
@@ -16,5 +16,25 @@ end
 
 function kaas_imperial_garison_triggers:spawnMobiles()
 
+--Kaas 1 Trigger
+	local T1Kaas = spawnMobile("kaas", "NPC", 1, -5628.4, 75.9027, -5666.37, 170, 0)print("Trigger 1 Loaded")
+        	createObserver(OBJECTDESTRUCTION, "kaas_imperial_garrison_triggersScreenPlay", "notify1KaasDead", T1Kaas)
 
+--Kaas 2 Trigger
+	local T2Kaas = spawnMobile("kaas", "NPC", 1, -5622.79, 75.9766, -5666.37, 168, 0)print("Trigger 2 Loaded")
+        	createObserver(OBJECTDESTRUCTION, "kaas_imperial_garrison_triggersScreenPlay", "notify2KaasDead", T2Kaas)
+end
+
+--Phase 1 Of 2 Instance
+function kaas_imperial_garrison_triggersScreenPlay:notify1KaasDead(T1Kaas, pKiller)
+	local player = LuaCreatureObject(pKiller)
+        local pBoss = spawnMobile("kaas", "NPC", 0, -5622.48, 75.9636, -5673.65, 97, 0) print("Phase 1 Started")
+        return 0
+end
+
+--Phase 2 Of 2 Instance
+function kaas_imperial_garrison_triggersScreenPlay:notify2KaasDead(T2Kaas, pKiller)
+	local player = LuaCreatureObject(pKiller)
+        local pBoss = spawnMobile("kaas", "NPC", 0, -5611.96, 75.99, -5645.66, 283, 0) print("Phase 2 Started")
+        return 0
 end
