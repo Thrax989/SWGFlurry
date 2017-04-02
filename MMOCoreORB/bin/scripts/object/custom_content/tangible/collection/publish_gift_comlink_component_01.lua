9d05d4203f2555e4831fe680a5898d5dc7642d1d
TOXIC <thrax989@yahoo.com>
2017-04-02 07:02:43 -0700

    [Reset] Collection com link test until item container is added to tangable item

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/collection/publish_gift_comlink_component_01.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/collection/publish_gift_comlink_component_01.lua
@@ -1,17 +1,5 @@
 
 object_tangible_collection_publish_gift_comlink_component_01 = object_tangible_collection_shared_publish_gift_comlink_component_01:new {
 
-	templateType = LOOTKIT,
-	
-	gameObjectType = 8233,
-	
-	--items required for output reward
-	collectibleComponents = {"object/tangible/item/loot_credit_chip.iff", "object/tangible/item/loot_credit_chip.iff", "object/tangible/item/loot_credit_chip.iff"},
-	
-	collectibleReward = {"object/tangible/collection/publish_gift_comlink_component_10.iff"},
-
-	deleteComponents = 0,
-
-	attributes = {}
 }
 ObjectTemplates:addTemplate(object_tangible_collection_publish_gift_comlink_component_01, "object/tangible/collection/publish_gift_comlink_component_01.iff")
