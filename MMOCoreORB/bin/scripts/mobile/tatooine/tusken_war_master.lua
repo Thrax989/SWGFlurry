c158a5bef42c4630f4adc7381c4aa608cc972fac
Hellucard <eclipsesql@gmail.com>
2017-04-11 13:11:32 +0200

    [Update] Tusken

--- MMOCoreORB/bin/scripts/mobile/tatooine/tusken_war_master.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/tusken_war_master.lua
@@ -2,10 +2,10 @@ tusken_war_master = Creature:new {
 	objectName = "@mob/creature_names:tusken_war_master",
 	socialGroup = "tusken_raider",
 	faction = "tusken_raider",
-	level = 53,
+	level = 150,
 	chanceHit = 0.53,
-	damageMin = 410,
-	damageMax = 530,
+	damageMin = 210,
+	damageMax = 430,
 	baseXp = 5190,
 	baseHAM = 10000,
 	baseHAMmax = 13000,
@@ -28,20 +28,25 @@ tusken_war_master = Creature:new {
 	templates = {"object/mobile/tusken_raider.iff"},
 	lootGroups = {
 		{
-			groups = {
-				{group = "junk", chance = 1500000},
-				{group = "tusken_common", chance = 3500000},
-				{group = "wearables_common", chance = 1000000},
-				{group = "wearables_uncommon", chance = 500000},
-				{group = "bone_armor", chance = 750000},
-				{group = "chitin_armor", chance = 750000},
-				{group = "armor_attachments", chance = 500000},
-				{group = "clothing_attachments", chance = 500000},
-				{group = "color_crystals", chance = 500000},
-				{group = "crystals_okay", chance = 500000}
-			},
+		groups = {
+			{group = "junk", chance = 1000000},
+			{group = "tusken_common", chance = 4000000},
+			{group = "wearables_common", chance = 1000000},
+			{group = "wearables_uncommon", chance = 500000},
+			{group = "armor_attachments", chance = 500000},
+			{group = "clothing_attachments", chance = 500000},
+			{group = "color_crystals", chance = 500000}
+	},
 			lootChance = 10000000
-		}
+	},
+	{
+                groups = {
+                	{group = "junk", chance = 5000000},
+                	{group = "armor_attachments", chance = 2500000},
+                	{group = "clothing_attachments", chance = 2500000}
+	},
+                	lootChance = 10000000
+	},
 	},
 	weapons = {"tusken_weapons"},
 	conversationTemplate = "",
