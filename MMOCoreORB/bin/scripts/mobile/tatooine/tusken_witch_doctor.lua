1ce77f360dce3e671c2d064219f27f2de65cbff1
Hellucard <eclipsesql@gmail.com>
2017-04-11 12:41:13 +0200

    [Update] Tusken

--- MMOCoreORB/bin/scripts/mobile/tatooine/tusken_witch_doctor.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/tusken_witch_doctor.lua
@@ -2,15 +2,15 @@ tusken_witch_doctor = Creature:new {
 	objectName = "@mob/creature_names:tusken_witch_doctor",
 	socialGroup = "tusken_raider",
 	faction = "tusken_raider",
-	level = 202,
-	chanceHit = 16,
-	damageMin = 1145,
-	damageMax = 2000,
+	level = 250,
+	chanceHit = 10.00,
+	damageMin = 1000,
+	damageMax = 1500,
 	baseXp = 19201,
-	baseHAM = 160000,
-	baseHAMmax = 195000,
-	armor = 2,
-	resists = {165,195,165,200,105,130,145,180,135},
+	baseHAM = 100000,
+	baseHAMmax = 150000,
+	armor = 1,
+	resists = {40,40,40,40,40,40,40,40,20},
 	meatType = "",
 	meatAmount = 0,
 	hideType = "",
@@ -27,28 +27,43 @@ tusken_witch_doctor = Creature:new {
 
 	templates = {"object/mobile/tusken_raider.iff"},
 	lootGroups = {
-		{
-	        	groups = {
-				{group = "tusken_common", chance = 5000000},
-				{group = "wearables_scarce", chance = 4000000},
-				{group = "color_crystals", chance = 500000},
-				{group = "crystals_premium", chance = 500000}
-			},
-			lootChance = 10000000
+	        {	
+		groups = {
+			{group = "tusken_common", chance = 5000000},
+			{group = "wearables_scarce", chance = 4000000},
+			{group = "color_crystals", chance = 500000},
+			{group = "crystals_premium", chance = 500000}
 		},
-		{
-	        	groups = {
-				{group = "armor_attachments", chance = 5000000},
-				{group = "clothing_attachments", chance = 5000000}
-			},
-			lootChance = 4000000
-		},
-		{
-	        	groups = {
-				{group = "junk", chance = 10000000}
-			},
-			lootChance = 10000000
-		}
+		lootChance = 10000000
+	},
+	{
+	        groups = {
+			{group = "armor_attachments", chance = 5000000},
+			{group = "clothing_attachments", chance = 5000000}
+	},
+		lootChance = 10000000
+	},
+	{
+	        groups = {
+			{group = "armor_attachments", chance = 5000000},
+			{group = "clothing_attachments", chance = 5000000}
+	},
+		lootChance = 10000000
+	},
+	{
+	        groups = {
+			{group = "junk", chance = 10000000}
+	},
+		lootChance = 10000000
+	},
+	{
+                groups = {
+                	{group = "junk", chance = 5000000},
+                	{group = "armor_attachments", chance = 2500000},
+                	{group = "clothing_attachments", chance = 2500000}
+	},
+                	lootChance = 10000000
+	},
 	},
 	weapons = {"tusken_weapons"},
 	conversationTemplate = "",
