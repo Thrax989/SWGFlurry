4017d1139829c111b84c7bae53f4de30972713de
TOXIC <thrax989@yahoo.com>
2017-04-15 04:37:29 -0400

    [Added] Doaba Guerfel Shuttleport teleport

--- MMOCoreORB/src/server/zone/managers/sui/SuiManager.cpp
+++ MMOCoreORB/src/server/zone/managers/sui/SuiManager.cpp
@@ -845,6 +845,21 @@ void SuiManager::handleCharacterBuilderSelectItem(CreatureObject* player, SuiBox
  				        player->switchZone("corellia", -335, 28, -4639);
  					player->subtractCashCredits(5000);
 			        }
+			} else if (templatePath == "doaba_guerfel_shuttleport_travel") {
+				if (player->getCashCredits() < 4999) {
+		                ManagedReference<SuiMessageBox*> box = new SuiMessageBox(player, SuiWindowType::CITY_ADMIN_CONFIRM_UPDATE_TYPE);
+		                box->setPromptTitle("Doaba Guerfel Shuttleport");
+		                box->setPromptText("Travel Coast 5,000 credits. (Cash)");
+		                box->setOkButton(true, "@cancel");
+		                box->setUsingObject(player);
+		                player->getPlayerObject()->addSuiBox(box);
+		                player->sendMessage(box->generateMessage());
+			        }
+				if (player->getCashCredits() > 4999) {
+					player->sendSystemMessage("Thank you for your travels.");
+ 				        player->switchZone("corellia", 3007, 280, 4996);
+ 					player->subtractCashCredits(5000);
+			        }
 			} else if (templatePath == "world_boss_1") {
 				if (player->getCashCredits() < 99999) {
 		                ManagedReference<SuiMessageBox*> box = new SuiMessageBox(player, SuiWindowType::CITY_ADMIN_CONFIRM_UPDATE_TYPE);
