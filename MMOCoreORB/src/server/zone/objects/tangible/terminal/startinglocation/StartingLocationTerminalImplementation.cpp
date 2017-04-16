a0750050cfb8429d871178fc4a5163055b87467f
TOXIC <thrax989@yahoo.com>
2017-04-15 23:44:46 -0700

    [Added] Xp Mode When Creating Character Added Xp sui callback to choose 5 xp options

--- MMOCoreORB/src/server/zone/objects/tangible/terminal/startinglocation/StartingLocationTerminalImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/terminal/startinglocation/StartingLocationTerminalImplementation.cpp
@@ -6,13 +6,17 @@
  */
 
 #include "server/zone/objects/tangible/terminal/startinglocation/StartingLocationTerminal.h"
+#include "server/zone/objects/player/sui/listbox/SuiListBox.h"
+#include "server/zone/objects/player/sui/callbacks/ExpSelectSuiCallback.h"
 #include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/objects/player/PlayerObject.h"
 #include "server/zone/managers/player/PlayerManager.h"
 
 void StartingLocationTerminalImplementation::initializeTransientMembers() {
 	TerminalImplementation::initializeTransientMembers();
 
 	setLoggingName("StartingLocationTerminal");
+	setInfoLogLevel();
 }
 
 int StartingLocationTerminalImplementation::handleObjectMenuSelect(CreatureObject* player, byte selectedID) {
@@ -22,6 +26,29 @@ int StartingLocationTerminalImplementation::handleObjectMenuSelect(CreatureObjec
 			info("handleObjectMenuSelect: not authorized");
 
 		return 1;
+	}
+	
+	info("player->getSelectedExpMode() is "+ player->getSelectedExpMode());
+	
+	if(player->getSelectedExpMode() == 0 ){
+		if(!player->getPlayerObject()->hasSuiBoxWindowType(SuiWindowType::XP_SELECT)){
+			ManagedReference<PlayerObject*> ghost = player->getPlayerObject();
+			ManagedReference<SuiListBox*> xpBox = new SuiListBox(player, SuiWindowType::XP_SELECT, 0);
+			xpBox->setCallback(new ExpSelectSuiCallback(player->getZoneServer()));
+			xpBox->setPromptTitle("Select Experience");
+			xpBox->setPromptText("Choose Xp Level Gain");
+			xpBox->addMenuItem("Insane: 20x Exp",0);
+			xpBox->addMenuItem("Extreme: 15x Exp",1);
+			xpBox->addMenuItem("Easy: 10x Exp",2);
+			xpBox->addMenuItem("Medium: 5x Exp",3);
+			xpBox->addMenuItem("Normal: 1x Exp",4);
+
+			ghost->addSuiBox(xpBox);
+			player->sendMessage(xpBox->generateMessage());
+			return 1;
+		}else{
+			return 1;
+		}
 	}
 
 	server->getPlayerManager()->sendStartingLocationsTo(player);
