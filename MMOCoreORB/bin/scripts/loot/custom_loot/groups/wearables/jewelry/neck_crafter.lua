d8aaf26a5b6f95cab2dba767c0cae0e2513fbb83
Hellucard <eclipsesql@gmail.com>
2017-04-25 17:15:26 +0200

    Add files via upload

---
+++ MMOCoreORB/bin/scripts/loot/custom_loot/groups/wearables/jewelry/neck_crafter.lua
@@ -0,0 +1,10 @@
+neck_crafter = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+    {itemTemplate = "necklace_crafter", weight = 10000000},
+	}
+}
+
+addLootGroupTemplate("neck_crafter", neck_crafter)
