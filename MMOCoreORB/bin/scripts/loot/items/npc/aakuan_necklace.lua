e2bc1fc7ebee8742e243dffc1ae5c173c170137a
Hellucard <eclipsesql@gmail.com>
2017-04-27 12:41:10 +0200

    Add files via upload

---
+++ MMOCoreORB/bin/scripts/loot/items/npc/aakuan_necklace.lua
@@ -0,0 +1,21 @@
+aakuan_necklace = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Hellucards Necklace of Defense",
+	directObjectTemplate = "object/tangible/wearables/necklace/necklace_ice_pendant.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {},
+	skillMods = {
+		{"ranged_defense", 25},
+		{"melee_defense", 25},
+		{"blind_defense", 25},
+		{"dizzy_defense", 25},
+		{"knockdown_defense", 25},
+		{"stun_defense", 25},
+		{"intimidate_defense", 25},
+	}
+}
+
+addLootItemTemplate("aakuan_necklace", aakuan_necklace)
