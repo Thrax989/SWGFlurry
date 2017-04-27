15c38b31c025d8ef2b87468f779d513595f752e9
Hellucard <eclipsesql@gmail.com>
2017-04-27 09:24:13 +0200

    Update skill_buff_carbine_speed.lua

--- MMOCoreORB/bin/scripts/object/tangible/skill_buff/skill_buff_carbine_speed.lua
+++ MMOCoreORB/bin/scripts/object/tangible/skill_buff/skill_buff_carbine_speed.lua
@@ -47,7 +47,7 @@ object_tangible_skill_buff_skill_buff_carbine_speed = object_tangible_skill_buff
 	attributeListComponent = "SkillBuffObjectAttributeListComponent",
 	
 	duration = 900,
-	useCount = 5,
+	useCount = 15,
 	modifiers = { "carbine_speed", 25 },
 	buffName = "carbine_speed",
 	buffCRC = 0xC6148607
