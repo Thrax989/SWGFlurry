9662f015df9409d6e557d3b60cdb1797ce7ffeba
Hellucard <eclipsesql@gmail.com>
2017-04-27 23:29:59 +0200

    Add files via upload

---
+++ MMOCoreORB/bin/scripts/mobile/outfits/custom_armor_c09_outfit.lua
@@ -0,0 +1,17 @@
+custom_armor_c09_outfit = {
+	{
+		{objectTemplate = "object/tangible/wearables/armor/rebel/spec/force/armor_rebel_spec_force_bicep_l.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/armor/rebel/spec/force/armor_rebel_spec_force_bicep_r.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/armor/rebel/spec/force/armor_rebel_spec_force_boots.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/armor/rebel/spec/force/armor_rebel_spec_force_bracer_l.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/armor/rebel/spec/force/armor_rebel_spec_force_bracer_r.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/armor/rebel_spec_force/armor_rebel_spec_force_chest_plate.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/armor/rebel/spec/force/armor_rebel_spec_force_gloves.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/armor/rebel/spec/force/armor_rebel_spec_force_helmet.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/armor/rebel/spec/force/armor_rebel_spec_force_leggings.iff", customizationVariables = {} },
+    {objectTemplate = "object/tangible/wearables/backpack/backpack_s07.iff", customizationVariables = {} }
+
+	}
+}
+
+addOutfitGroup("custom_armor_c09_outfit", custom_armor_c09_outfit)
