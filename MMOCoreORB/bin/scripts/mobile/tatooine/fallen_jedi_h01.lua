ca8dc5cdf1a809775ddcdfac20e68bac63ee4130
Hellucard <eclipsesql@gmail.com>
2017-04-27 23:31:20 +0200

    Add files via upload

---
+++ MMOCoreORB/bin/scripts/mobile/tatooine/fallen_jedi_h01.lua
@@ -0,0 +1,155 @@
+fallen_jedi_h01 = Creature:new {
+	customName = "Fallen Jedi of Corusant",
+	socialGroup = "kun",
+	pvpFaction = "",
+	faction = "",
+	level = 300,
+	chanceHit = 50.00,
+	damageMin = 1000,
+	damageMax = 1500,
+	specialDamageMult = 2.0,
+	baseXp = 0,
+	baseHAM = 300000,
+	baseHAMmax = 300000,
+	armor = 2,
+	resists = {65,65,65,65,65,65,65,65,20},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = KILLER + STALKER,
+	optionsBitmask = AIENABLED,
+	diet = HERBIVORE,
+	scale = 1.2,
+
+	templates = {"object/mobile/mos_taike_guard_old.iff"},
+  outfit = "custom_armor_c09_outfit",
+  lootGroups = {
+	{
+			groups = {
+				      {group = "armor_attachments", chance = 5000000},
+				      {group = "clothing_attachments", chance = 5000000}
+   },
+			        lootChance = 10000000
+   },
+   {
+			groups = {
+        			{group = "armor_attachments", chance = 5000000},
+        			{group = "clothing_attachments", chance = 5000000}
+   },
+      			        lootChance = 10000000
+   },
+   {
+      	 groups = {
+				      {group = "armor_attachments", chance = 5000000},
+				      {group = "clothing_attachments", chance = 5000000}
+   },
+      			        lootChance = 10000000
+   },
+   {
+      			groups = {
+       	 			{group = "armor_attachments", chance = 5000000},
+        			{group = "clothing_attachments", chance = 5000000}
+   },
+      			        lootChance = 10000000
+   },
+   {
+      			groups = {
+        			{group = "armor_attachments", chance = 5000000},
+        			{group = "clothing_attachments", chance = 5000000}
+   },
+      			        lootChance = 10000000
+   },
+   {
+     	 		 groups = {
+        			 {group = "armor_attachments", chance = 5000000},
+        			 {group = "clothing_attachments", chance = 5000000}
+   },
+      			          lootChance = 10000000
+   },
+   {
+      			groups = {
+        			{group = "armor_attachments", chance = 5000000},
+       	 			{group = "clothing_attachments", chance = 5000000}
+   },
+     			        lootChance = 10000000
+   },
+   {
+     			groups = {
+        			{group = "armor_attachments", chance = 5000000},
+        			{group = "clothing_attachments", chance = 5000000}
+   },
+      			       	 lootChance = 10000000
+   },
+   {
+           groups = {
+             {group = "armor_attachments", chance = 5000000},
+             {group = "clothing_attachments", chance = 5000000}
+   },
+                    lootChance = 10000000
+   },
+   {
+			groups = {
+				{group = "g_named_crystals", chance = 10000000}
+   },
+			        lootChance = 10000000
+   },
+
+     {
+		         groups = {
+				{group = "saberhand26", chance = 10000000}
+     },
+			         lootChance = 2000000
+     },
+     {
+			 groups = {
+				 {group = "skill_buff_jedi", chance = 10000000}
+     },
+			          lootChance = 2000000
+     },
+     {
+        		 groups = {
+          			 {group = "saberhand27", chance = 10000000}
+     },
+           		         lootChance = 10000000
+     },
+     {
+          		 groups = {
+            			 {group = "nge1", chance = 5000000},
+                   {group = "nge2", chance = 5000000}
+     },
+          		          lootChance = 2000000
+     },
+     {
+             groups = {
+                 {group = "nge3", chance = 10000000}
+     },
+                     lootChance = 2000000
+     },
+     {
+        		 groups = {
+          			 {group = "nectar_buff_f01", chance = 10000000}
+    },
+            		         lootChance = 10000000
+    },
+    {
+          		groups = {
+            			{group = "hellucard_neck", chance = 10000000}
+    },
+              		        lootChance = 1000000
+    },
+    },
+
+	weapons = {"dark_jedi_weapons_gen4"},
+	reactionStf = "@npc_reaction/military",
+	attacks = merge(lightsabermaster,forcepowermaster,fencermaster,forcewielder,brawlermaster)
+
+}
+
+CreatureTemplates:addCreatureTemplate(fallen_jedi_h01, "fallen_jedi_h01")
---
+++ MMOCoreORB/bin/scripts/mobile/tatooine/hellucard_boss.lua
@@ -0,0 +1,176 @@
+hellucard_boss = Creature:new {
+	customName = "<<< Hellucard >>>",
+	socialGroup = "kun",
+	pvpFaction = "",
+	faction = "",
+	level = 300,
+	chanceHit = 100.00,
+	damageMin = 6000,
+	damageMax = 8000,
+	specialDamageMult = 10.0,
+	baseXp = 0,
+	baseHAM = 2000000,
+	baseHAMmax = 2000000,
+	armor = 3,
+	resists = {95,95,95,95,95,95,95,95,95},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = KILLER + STALKER,
+	optionsBitmask = AIENABLED,
+	diet = HERBIVORE,
+	scale = 1.2,
+
+	templates = {"object/mobile/mos_taike_guard_old.iff"},
+  outfit = "custom_armor_c08_outfit",
+  lootGroups = {
+	{
+			groups = {
+        			{group = "pearls_flawless", chance = 5000000},
+				      {group = "armor_attachments", chance = 2500000},
+				      {group = "clothing_attachments", chance = 2500000}
+   },
+			        lootChance = 10000000
+   },
+   {
+			groups = {
+        			{group = "pearls_flawless", chance = 5000000},
+        			{group = "armor_attachments", chance = 2500000},
+        			{group = "clothing_attachments", chance = 2500000}
+   },
+      			        lootChance = 10000000
+   },
+   {
+      	 groups = {
+        			{group = "pearls_flawless", chance = 5000000},
+				      {group = "armor_attachments", chance = 2500000},
+				      {group = "clothing_attachments", chance = 2500000}
+   },
+      			        lootChance = 10000000
+   },
+   {
+      			groups = {
+        			{group = "pearls_flawless", chance = 5000000},
+       	 			{group = "armor_attachments", chance = 2500000},
+        			{group = "clothing_attachments", chance = 2500000}
+   },
+      			        lootChance = 10000000
+   },
+   {
+      			groups = {
+        			{group = "armor_attachments", chance = 5000000},
+        			{group = "clothing_attachments", chance = 5000000}
+   },
+      			        lootChance = 10000000
+   },
+   {
+     	 		 groups = {
+        			 {group = "armor_attachments", chance = 5000000},
+        			 {group = "clothing_attachments", chance = 5000000}
+   },
+      			          lootChance = 10000000
+   },
+   {
+      			groups = {
+        			{group = "armor_attachments", chance = 5000000},
+       	 			{group = "clothing_attachments", chance = 5000000}
+   },
+     			        lootChance = 10000000
+   },
+   {
+     			groups = {
+        			{group = "armor_attachments", chance = 5000000},
+        			{group = "clothing_attachments", chance = 5000000}
+   },
+      			       	 lootChance = 10000000
+   },
+   {
+           groups = {
+             {group = "armor_attachments", chance = 5000000},
+             {group = "clothing_attachments", chance = 5000000}
+   },
+                    lootChance = 10000000
+   },
+   {
+			groups = {
+				{group = "g_named_crystals", chance = 10000000}
+   },
+			        lootChance = 10000000
+   },
+    {
+			groups = {
+				{group = "nightsister_rare", chance = 10000000}
+    },
+	                        lootChance = 10000000
+    },
+    {
+			groups = {
+				{group = "krayt_tissue_rare", chance = 10000000}
+     },
+			        lootChance = 10000000
+     },
+     {
+		         groups = {
+				{group = "saberhand26", chance = 10000000}
+     },
+			         lootChance = 10000000
+     },
+     {
+			 groups = {
+				 {group = "skill_buff_jedi", chance = 10000000}
+     },
+			          lootChance = 10000000
+     },
+     {
+        		 groups = {
+          			 {group = "saberhand27", chance = 10000000}
+     },
+           		         lootChance = 10000000
+     },
+     {
+          		 groups = {
+            			 {group = "nge1", chance = 5000000},
+                   {group = "nge2", chance = 5000000}
+     },
+          		          lootChance = 10000000
+     },
+     {
+             groups = {
+                 {group = "nge3", chance = 10000000}
+     },
+                     lootChance = 10000000
+     },
+     {
+          		groups = {
+            			{group = "acklays", chance = 10000000}
+     },
+          		        lootChance = 10000000
+     },
+     {
+        		 groups = {
+          			 {group = "nectar_buff_f01", chance = 10000000}
+    },
+            		         lootChance = 10000000
+    },
+    {
+          		groups = {
+            			{group = "hellucard_neck", chance = 10000000}
+    },
+              		        lootChance = 5000000
+    },
+    },
+
+	weapons = {"dark_jedi_weapons_gen4"},
+	reactionStf = "@npc_reaction/slang",
+	attacks = merge(lightsabermaster,forcepowermaster,fencermaster,forcewielder,brawlermaster)
+
+}
+
+CreatureTemplates:addCreatureTemplate(hellucard_boss, "hellucard_boss")
