d3fc20c163824c88c3d048bd90b0cbc41aa46233
TOXIC <thrax989@yahoo.com>
2017-05-03 07:55:07 -0700

    [Updated] A few unit test errors

--- MMOCoreORB/bin/scripts/mobile/tatooine/tatooine_world_boss/outbreak_boss_two.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/tatooine_world_boss/outbreak_boss_two.lua
@@ -164,13 +164,13 @@ outbreak_boss_two = Creature:new {
     },
     {
           		groups = {
-            			{group = "sanerhand26", chance = 10000000}
+            			{group = "saberhand26", chance = 10000000}
     },
               		        lootChance = 10000000
     },
     {
           		groups = {
-            			{group = "sanerhand27", chance = 10000000}
+            			{group = "saberhand27", chance = 10000000}
     },
               		        lootChance = 10000000
     },
