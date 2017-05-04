09b41ae8c0b759e7c467b6e85fa230f42b383041
Weezy <Weezyyy@users.noreply.github.com>
2017-05-04 22:52:52 +1000

    Update DarthCaedus.lua
    added loot group and adjusted stats

--- MMOCoreORB/bin/scripts/mobile/mandalore/DarthCaedus.lua
+++ MMOCoreORB/bin/scripts/mobile/mandalore/DarthCaedus.lua
@@ -4,15 +4,15 @@ darth_caedus = Creature:new {
 	socialGroup = "sith_shadow",
 	pvpFaction = "sith_shadow",
 	faction = "sith_shadow",
-	level = 225,
-	chanceHit = 4.750000,
-	damageMin = 2445,
-	damageMax = 4200,
+	level = 300,
+	chanceHit = 75.00,
+	damageMin = 1750,
+	damageMax = 3750,
 	baseXp = 13273,
-	baseHAM = 950000,
-	baseHAMmax = 1750000,
-	armor = 2,
-	resists = {80,80,80,80,80,80,80,80,60},
+	baseHAM = 1000000,
+	baseHAMmax = 1500000,
+	armor = 3,
+	resists = {60,60,60,60,60,60,60,60,45},
 	meatType = "",
 	meatAmount = 0,
 	hideType = "",
@@ -29,65 +29,79 @@ darth_caedus = Creature:new {
 
 	templates = {"object/mobile/dressed_fs_village_enemy_mellichae.iff"},
 	lootGroups = {
-		
-	
-		{
-	        	groups = 
-			{
-				{group = "wearables_scarce", chance = 10000000},
-			},
-			lootChance = 6500000,
-		},
-		
-		{
-	        	groups = 
-			{
-				{group = "clothing_attachments", chance = 10000000},
-			},
-			lootChance = 7500000,
-		},
-		{
-	        	groups = 
-			{
-				{group = "armor_attachments", chance = 10000000},
-			},
-			lootChance = 7500000,
-		},
-		{
-	        	groups = 
-			{
-				{group = "armor_attachments", chance = 10000000},
-			},
-			lootChance = 7500000,
-		},
-		{
-	        	groups = 
-			{
-				{group = "clothing_attachments", chance = 10000000},
-			},
-			lootChance = 7500000,
-		},
-		{
-	        	groups = 
-			{
-				{group = "clothing_attachments", chance = 10000000},
-			},
-			lootChance = 7500000,
-		},
-	   	{
-			groups = {
-				{group = "wearables_scarce", chance = 10000000},
-			},
-			lootChance = 8500000,
-		},
-		{
-			groups = {
-				{group = "wearables_scarce", chance = 10000000},
-			},
-			lootChance = 8500000
-		},
-				
-	},
+  		{
+  			      groups = {
+  				            {group = "armor_attachments", chance = 1000000},
+  				            {group = "clothing_attachments", chance = 1000000},
+  				            {group = "crystals_premium", chance = 4000000},
+  				            {group = "pearls_flawless", chance = 4000000}
+},
+                      lootChance = 10000000
+},
+{
+            groups = {
+                    {group = "g_named_crystals", chance = 10000000}
+
+},
+  			            lootChance = 8000000
+},
+{
+            groups = {
+                    {group = "armor_attachments", chance = 5000000},
+                    {group = "clothing_attachments", chance = 5000000}
+},
+                    lootChance = 10000000
+},
+{
+            groups = {
+                    {group = "armor_attachments", chance = 5000000},
+                    {group = "clothing_attachments", chance = 5000000}
+},
+                    lootChance = 10000000
+},
+{
+            groups = {
+                    {group = "axkva_min", chance = 10000000}
+},
+                    lootChance = 8000000
+},
+{
+            groups = {
+                    {group = "krayt_tissue_rare", chance = 10000000}
+},
+                    lootChance = 10000000
+},
+{
+            groups = {
+                    {group = "saberhand27", chance = 10000000}
+},
+                    lootChance = 10000000
+},
+{
+             groups = {
+                    {group = "nge2", chance = 10000000}
+},
+                    lootChance = 10000000
+},
+{
+          		groups = {
+                  {group = "acklays", chance = 10000000}
+},
+              		  lootChance = 10000000
+},
+{
+             groups = {
+                     {group = "theme_park_reward_imperial_kaja", chance = 10000000}
+},
+                     lootChance = 10000000
+},
+{
+             groups = {
+                     {group = "saberhand27", chance = 10000000}
+},
+                     lootChance = 10000000
+},
+},
 	weapons = {"dark_jedi_weapons_gen3"},
 	reactionStf = "@npc_reaction/slang",
 	attacks = merge(lightsabermaster,forcepowermaster,forcewielder)
