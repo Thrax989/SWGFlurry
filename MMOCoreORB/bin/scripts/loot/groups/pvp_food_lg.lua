b5e2621d994b4e619db419d97f91e6938931c70b
Weezy <Weezyyy@users.noreply.github.com>
2017-05-04 11:11:54 +1000

    Update pvp_food_lg.lua
    removed a loot from loot group, adjusted loot percentages to suit removal

--- MMOCoreORB/bin/scripts/loot/groups/pvp_food_lg.lua
+++ MMOCoreORB/bin/scripts/loot/groups/pvp_food_lg.lua
@@ -4,9 +4,8 @@ pvp_food_lg = {
 	maximumLevel = 0,
 	lootItems = {
 		{itemTemplate = "pvp_food", weight = 500000},
-		{itemTemplate = "pvp_food_one", weight = 3000000},
-		{itemTemplate = "pvp_food_two", weight = 3000000},
-		{itemTemplate = "pvp_food_three", weight = 3000000},
+		{itemTemplate = "pvp_food_one", weight = 4000000},
+		{itemTemplate = "pvp_food_three", weight = 4000000},
 		{itemTemplate = "pvp_food_four", weight = 500000},
   }
 }
