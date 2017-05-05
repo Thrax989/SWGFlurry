682c0ec529010d8199ba3c9a5bcc2aae3cb64272
Weezy <Weezyyy@users.noreply.github.com>
2017-05-05 12:20:53 +1000

    Update pvp_food_lg.lua
    fixed unit erros

--- MMOCoreORB/bin/scripts/loot/groups/pvp_food_lg.lua
+++ MMOCoreORB/bin/scripts/loot/groups/pvp_food_lg.lua
@@ -3,10 +3,10 @@ pvp_food_lg = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "pvp_food", weight = 500000},
+		{itemTemplate = "pvp_food", weight = 1000000},
 		{itemTemplate = "pvp_food_one", weight = 4000000},
 		{itemTemplate = "pvp_food_three", weight = 4000000},
-		{itemTemplate = "pvp_food_four", weight = 500000}
+		{itemTemplate = "pvp_food_four", weight = 1000000}
 		
 	}
 }
