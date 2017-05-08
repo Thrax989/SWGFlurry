b0c8ae45f754caa175378100c52501731fc71df0
Hellucard <eclipsesql@gmail.com>
2017-05-08 14:16:38 +0200

    Update clothing_armor_composite_bicep_l.lua

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_composite_bicep_l.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_composite_bicep_l.lua
@@ -76,7 +76,6 @@ object_draft_schematic_clothing_clothing_armor_composite_bicep_l = object_draft_
 "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_bicep_l.iff",
 "object/tangible/wearables/armor/marine/shared_armor_marine_bicep_l.iff",
 "object/tangible/wearables/armor/stormtrooper/shared_armor_stormtrooper_bicep_l.iff",
-"object/tangible/wearables/armor/bounty_hunter/shared_armor_bounty_hunter_bicep_l.iff",
              }
 
 }
