bb46f47490f5389f0786455cde8c5708d4ea37dd
Hellucard <eclipsesql@gmail.com>
2017-05-08 13:49:22 +0200

    Add files via upload

---
+++ MMOCoreORB/bin/scripts/loot/custom_loot/items/backpacks/neutral_ace.lua
@@ -0,0 +1,11 @@
+neutral_ace = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Neutral Ace Backpack",
+	directObjectTemplate = "object/tangible/wearables/backpack/backpack_s09.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+addLootItemTemplate("neutral_ace", neutral_ace)
---
+++ MMOCoreORB/bin/scripts/loot/custom_loot/items/backpacks/rebel_snow.lua
@@ -0,0 +1,11 @@
+rebel_snow = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Rebel Snow Backpack",
+	directObjectTemplate = "object/tangible/wearables/backpack/backpack_rebel_snow_soldier.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+addLootItemTemplate("rebel_snow", rebel_snow)
