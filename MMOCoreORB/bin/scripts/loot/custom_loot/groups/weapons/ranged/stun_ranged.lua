d39d836d42beedad17491a83765a6fc37e751b80
Hellucard <eclipsesql@gmail.com>
2017-05-08 19:02:07 +0200

    Add files via upload

---
+++ MMOCoreORB/bin/scripts/loot/custom_loot/groups/weapons/ranged/stun_ranged.lua
@@ -0,0 +1,13 @@
+stun_ranged = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "carbine_dc15", weight = 2500000},
+		{itemTemplate = "pistol_intimidator", weight = 2500000},
+		{itemTemplate = "rifle_naktra_crystal", weight = 2500000},
+		{itemTemplate = "rifle_t21_legendary", weight = 2500000},
+	}
+}
+
+addLootGroupTemplate("stun_ranged", stun_ranged)
