bada6b3c7d9a4d1f05fab9ec067cc7f86ad5c129
Hellucard <eclipsesql@gmail.com>
2017-05-09 01:39:54 +0200

    Add files via upload

---
+++ MMOCoreORB/bin/scripts/loot/custom_loot/items/goggles/goggles_g05.lua
@@ -0,0 +1,18 @@
+goggles_g05 = {
+  minimumLevel = 0,
+  maximumLevel = -1,
+  customObjectName = "Goggles Version V",
+  directObjectTemplate = "object/tangible/wearables/goggles/goggles_s05.iff",
+  craftingValues = {},
+  customizationStringNames = {},
+  customizationValues = {},
+  skillMods = {
+    {"onehandlightsaber_accuracy", 25},
+    {"polearmlightsaber_accuracy", 25},
+    {"twohandlightsaber_accuracy", 25},
+    {"jedi_force_power_regen", 25},
+    {"group_slope_move", 25},
+  }
+}
+
+addLootItemTemplate("goggles_g05", goggles_g05)
