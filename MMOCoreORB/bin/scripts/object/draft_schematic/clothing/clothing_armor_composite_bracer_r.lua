2d9f4fa52ef1a2b2799d3a144ed75762334993de
Hellucard <eclipsesql@gmail.com>
2017-05-08 14:17:30 +0200

    Update clothing_armor_composite_bracer_r.lua

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_composite_bracer_r.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_composite_bracer_r.lua
@@ -76,7 +76,6 @@ object_draft_schematic_clothing_clothing_armor_composite_bracer_r = object_draft
 "object/tangible/wearables/armor/stormtrooper/shared_armor_stormtrooper_bracer_r.iff",
 "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_bracer_r.iff",
 "object/tangible/wearables/armor/marine/shared_armor_marine_bracer_r.iff",
-"object/tangible/wearables/armor/bounty_hunter/shared_armor_bounty_hunter_bracer_r.iff",
              }
 
 }
