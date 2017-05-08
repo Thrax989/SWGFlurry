4a5558080725b705465934fb8ccd0af1da7091d9
Hellucard <eclipsesql@gmail.com>
2017-05-08 13:51:18 +0200

    Update meatlump_boss.lua

--- MMOCoreORB/bin/scripts/mobile/corellia/meatlump_boss.lua
+++ MMOCoreORB/bin/scripts/mobile/corellia/meatlump_boss.lua
@@ -91,6 +91,13 @@ meatlump_king = Creature:new {
   },
      lootChance = 10000000
   },
+  {
+      groups = {
+        {group = "backpacks_b01", chance = 5000000},
+        {group = "backpacks_b02", chance = 5000000}
+  },
+      lootChance = 10000000
+  },
   },
   weapons = {"dark_trooper_weapons"},
 reactionStf = "@npc_reaction/slang",
