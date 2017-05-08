40854b3ff5f22cadcb0d2789f44bf87ae25e59b6
Hellucard <eclipsesql@gmail.com>
2017-05-08 14:18:11 +0200

    Update clothing_armor_composite_leggings.lua

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_composite_leggings.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_composite_leggings.lua
@@ -76,7 +76,6 @@ object_draft_schematic_clothing_clothing_armor_composite_leggings = object_draft
 "object/tangible/wearables/armor/infiltrator/shared_armor_infiltrator_s01_leggings.iff",
 "object/tangible/wearables/armor/marine/shared_armor_marine_leggings.iff",
 "object/tangible/wearables/armor/stormtrooper/shared_armor_stormtrooper_leggings.iff",
-"object/tangible/wearables/armor/bounty_hunter/shared_armor_bounty_hunter_leggings.iff",
              }
 
 }
