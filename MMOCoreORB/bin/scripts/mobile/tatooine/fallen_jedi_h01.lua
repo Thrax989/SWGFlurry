ba6af18dc3d40019e2af2c4c0c82bd3f454b7e5c
Hellucard <eclipsesql@gmail.com>
2017-05-08 20:27:43 +0200

    Add files via upload

--- MMOCoreORB/bin/scripts/mobile/tatooine/fallen_jedi_h01.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/fallen_jedi_h01.lua
@@ -109,7 +109,7 @@ fallen_jedi_h01 = Creature:new {
         		 groups = {
           			 {group = "nectar_buff_f01", chance = 10000000}
     },
-            		         lootChance = 5000000
+            		         lootChance = 10000000
     },
     {
           		groups = {
@@ -117,17 +117,11 @@ fallen_jedi_h01 = Creature:new {
     },
               		        lootChance = 100000
     },
-    {
-        		 groups = {
-          			 {group = "saberhand26", chance = 10000000}
-    },
-           		         lootChance = 500000
-    },
     },
 
-	weapons = {"dark_jedi_weapons_gen4"},
+	weapons = {"fallen_weapons"},
 	reactionStf = "@npc_reaction/military",
-	attacks = merge(lightsabermaster,forcepowermaster,forcewielder)
+	attacks = merge(brawlermaster,pikemanmaster)
 
 }
 
