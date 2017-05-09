8539d04aefc302244be4f84967d738ba6cdfc6bc
Hellucard <eclipsesql@gmail.com>
2017-05-09 22:02:22 +0200

    Update hydra_boss.lua

--- MMOCoreORB/bin/scripts/mobile/tatooine/hydra_boss.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/hydra_boss.lua
@@ -122,6 +122,13 @@ hydra_boss = Creature:new {
 },
                     lootChance = 10000000
 },
+{
+          		groups = {
+                   		{group = "stun_weapons", chance = 5000000},
+                   		{group = "stun_ranged", chance = 5000000}
+},
+          			lootChance = 5000000
+},
 },
   weapons = {"imperial_weapons_heavy"},
   conversationTemplate = "",
