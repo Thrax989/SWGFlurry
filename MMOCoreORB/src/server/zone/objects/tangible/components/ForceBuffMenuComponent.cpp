d358d42f4406371d6269318d2af187cd7bc0c3ec
TOXIC <thrax989@yahoo.com>
2017-05-09 16:32:01 -0400

    [Added] Updated Buff

--- MMOCoreORB/src/server/zone/objects/tangible/components/ForceBuffMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/ForceBuffMenuComponent.cpp
@@ -29,40 +29,12 @@ void ForceBuffMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, Ob
 
 	TangibleObjectMenuComponent::fillObjectMenuResponse(sceneObject, menuResponse, player);
 
-
-if (player->hasSkill("force_title_jedi_novice"))
-	menuResponse->addRadialMenuItem(20, 3, "Force Buff");
+	menuResponse->addRadialMenuItem(20, 3, "Bacta Bomb");
 }
 
 int ForceBuffMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* creature, byte selectedID) const {
 	if (selectedID != 20)
 		return 0;
-	ManagedReference<PlayerObject*> ghost = creature->getPlayerObject();
-	if (ghost == NULL)
-	return 0;
-	if (creature->getPosture() != CreaturePosture::CROUCHED){
-		creature->sendSystemMessage("@jedi_trials:show_respect");
-		return 0;
-		} else {
-		int rand = System::random(14) + 1;
-		StringBuffer sysmsg;
-		sysmsg << "@jedi_trials:force_shrine_wisdom_" << rand;
-		creature->sendSystemMessage(sysmsg.toString());
-		if (ghost->getJediState() >= 2) {
-			int jediVis1 = ghost->getVisibility();
-			PlayMusicMessage* pmm = new PlayMusicMessage("sound/music_become_light_jedi.snd");
-  			ghost->sendMessage(pmm);
-			Zone* zone = creature->getZone();
-			//Broadcast to Server
- 			String playerName = creature->getFirstName();
- 			StringBuffer zBroadcast;
- 			zBroadcast << "\\#00E604" << playerName << " \\#63C8F9 Has used a force buff";
- 			creature->getZoneServer()->getChatManager()->broadcastGalaxy(NULL, zBroadcast.toString());
-			StringBuffer messageVis;
-			messageVis << "\\#00CC00 Your Visibility is at: " << jediVis1;
-			creature->sendSystemMessage(messageVis.toString());
-		}
-	}
 
 	creature->playEffect("clienteffect/bacta_bomb.cef", "");
 	creature->healDamage(creature, CreatureAttribute::HEALTH, 5000);
