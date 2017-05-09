d6d4dad7f5393e8866bf05b24b67c8daaa55f012
Hellucard <eclipsesql@gmail.com>
2017-05-09 12:05:59 +0200

    Add files via upload

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_ris_helmet.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_ris_helmet.lua
@@ -3,42 +3,42 @@
 
 --This File is part of Core3.
 
---This program is free software; you can redistribute 
---it and/or modify it under the terms of the GNU Lesser 
+--This program is free software; you can redistribute
+--it and/or modify it under the terms of the GNU Lesser
 --General Public License as published by the Free Software
---Foundation; either version 2 of the License, 
+--Foundation; either version 2 of the License,
 --or (at your option) any later version.
 
---This program is distributed in the hope that it will be useful, 
---but WITHOUT ANY WARRANTY; without even the implied warranty of 
---MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 --See the GNU Lesser General Public License for
 --more details.
 
---You should have received a copy of the GNU Lesser General 
+--You should have received a copy of the GNU Lesser General
 --Public License along with this program; if not, write to
 --the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
---Linking Engine3 statically or dynamically with other modules 
---is making a combined work based on Engine3. 
---Thus, the terms and conditions of the GNU Lesser General Public License 
+--Linking Engine3 statically or dynamically with other modules
+--is making a combined work based on Engine3.
+--Thus, the terms and conditions of the GNU Lesser General Public License
 --cover the whole combination.
 
---In addition, as a special exception, the copyright holders of Engine3 
---give you permission to combine Engine3 program with free software 
---programs or libraries that are released under the GNU LGPL and with 
---code included in the standard release of Core3 under the GNU LGPL 
---license (or modified versions of such code, with unchanged license). 
---You may copy and distribute such a system following the terms of the 
---GNU LGPL for Engine3 and the licenses of the other code concerned, 
---provided that you include the source code of that other code when 
+--In addition, as a special exception, the copyright holders of Engine3
+--give you permission to combine Engine3 program with free software
+--programs or libraries that are released under the GNU LGPL and with
+--code included in the standard release of Core3 under the GNU LGPL
+--license (or modified versions of such code, with unchanged license).
+--You may copy and distribute such a system following the terms of the
+--GNU LGPL for Engine3 and the licenses of the other code concerned,
+--provided that you include the source code of that other code when
 --and as the GNU LGPL requires distribution of source code.
 
---Note that people who make modified versions of Engine3 are not obligated 
---to grant this special exception for their modified versions; 
---it is their choice whether to do so. The GNU Lesser General Public License 
---gives permission to release a modified version without this exception; 
---this exception also makes it possible to release a modified version 
+--Note that people who make modified versions of Engine3 are not obligated
+--to grant this special exception for their modified versions;
+--it is their choice whether to do so. The GNU Lesser General Public License
+--gives permission to release a modified version without this exception;
+--this exception also makes it possible to release a modified version
 
 
 object_draft_schematic_clothing_clothing_armor_ris_helmet = object_draft_schematic_clothing_shared_clothing_armor_ris_helmet:new {
@@ -48,15 +48,15 @@ object_draft_schematic_clothing_clothing_armor_ris_helmet = object_draft_schemat
    customObjectName = "R.I.S Armor Helmet",
 
    craftingToolTab = 2, -- (See DraftSchematicObjectTemplate.h)
-   complexity = 45, 
-   size = 1, 
+   complexity = 45,
+   size = 1,
 
-   xpType = "crafting_clothing_armor", 
-   xp = 1166, 
+   xpType = "crafting_clothing_armor",
+   xp = 1166,
 
-   assemblySkill = "armor_assembly", 
-   experimentingSkill = "armor_experimentation", 
-   customizationSkill = "armor_customization", 
+   assemblySkill = "armor_assembly",
+   experimentingSkill = "armor_experimentation",
+   customizationSkill = "armor_customization",
 
    customizationOptions = {},
    customizationStringNames = {},
@@ -90,6 +90,9 @@ object_draft_schematic_clothing_clothing_armor_ris_helmet = object_draft_schemat
 "object/tangible/wearables/hat/shared_singing_mountain_clan_hat_s01.iff",
 "object/tangible/wearables/hat/shared_singing_mountain_clan_hat_s02.iff",
 "object/tangible/wearables/hat/shared_singing_mountain_clan_mask_s01.iff",
+"object/tangible/wearables/armor/bounty_hunter/shared_armor_bounty_hunter_helmet.iff",
+"object/tangible/wearables/armor/rebel/spec/force/shared_armor_rebel_spec_force_helmet.iff",
+"object/tangible/wearables/armor/rebel_assault/shared_armor_rebel_assault_helmet.iff",
              }
 
 }
