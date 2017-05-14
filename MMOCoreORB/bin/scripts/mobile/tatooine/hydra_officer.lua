c8e9c8e50b46d922e45ac9ee5caa925d8c83f61e
Hellucard <eclipsesql@gmail.com>
2017-05-14 18:43:01 +0200

    Update hydra_officer.lua

--- MMOCoreORB/bin/scripts/mobile/tatooine/hydra_officer.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/hydra_officer.lua
@@ -33,7 +33,7 @@ hydra_officer = Creature:new {
         {group = "clothing_attachments", chance = 2500000}
 
   },
-      lootChance = 10000000
+      lootChance = 8000000
   },
   {
       groups = {
@@ -42,7 +42,7 @@ hydra_officer = Creature:new {
         {group = "clothing_attachments", chance = 2500000}
 
   },
-      lootChance = 10000000
+      lootChance = 8000000
   },
   {
       groups = {
@@ -51,14 +51,14 @@ hydra_officer = Creature:new {
         {group = "clothing_attachments", chance = 2500000}
 
   },
-      lootChance = 10000000
+      lootChance = 8000000
   },
   {
       groups = {
         {group = "wearables_rare", chance = 5000000},
   		  {group = "aakuan_common", chance = 5000000}
   },
-      lootChance = 10000000
+      lootChance = 8000000
   },
   },
   weapons = {"imperial_weapons_heavy"},
