2857d647a3d9ca0e849ae06af047d6f47c98687e
Weezy <Weezyyy@users.noreply.github.com>
2017-05-23 02:54:28 +1000

    Add files via upload
    added 1 screen play for new boss

---
+++ MMOCoreORB/bin/scripts/screenplays/custom_content/random/oola_screenplay.lua
@@ -0,0 +1,19 @@
+oola_screenplay = ScreenPlay:new {
+	numberOfActs = 1,
+
+	screenplayName = "oola_screenplay",
+}
+
+registerScreenPlay("oola_screenplay", true)
+
+function oola_screenplay:start()
+	if (isZoneEnabled("corellia")) then
+		self:spawnMobiles()
+	end
+end
+
+function oola_screenplay:spawnMobiles()
+       spawnMobile("corellia", "oola_boss", 1800, -319,28,-4487,15, 0)
+       spawnMobile("tatooine", "oola_boss", 1800, 3515,5,-4858,15, 0)
+       spawnMobile("naboo", "oola_boss", 1800, -5173,6,4139,15, 0)
+end
