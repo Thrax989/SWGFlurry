90a805c12a5c93e7e1fba769b9b38b0ce6112881
Weezy <Weezyyy@users.noreply.github.com>
2017-05-23 02:33:27 +1000

    Add files via upload
    added 1 boss - oola

---
+++ MMOCoreORB/bin/scripts/mobile/custom/oola_boss.lua
@@ -0,0 +1,50 @@
+oola_boss = Creature:new {
+  customName = "Slave Girl Oola",
+  socialGroup = "self",
+  pvpFaction = "",
+  faction = "",
+  level = 300,
+  chanceHit = 35.00,
+  damageMin = 750,
+  damageMax = 1500,
+  baseXp = 10000,
+  baseHAM = 225000,
+  baseHAMmax = 275000,
+  armor = 1,
+  resists = {50,50,50,50,50,50,50,50,25},
+  meatType = "",
+  meatAmount = 0,
+  hideType = "",
+  hideAmount = 0,
+  boneType = "",
+  boneAmount = 0,
+  milk = 0,
+  tamingChance = 0,
+  ferocity = 0,
+  pvpBitmask = ATTACKABLE,
+  creatureBitmask = PACK + KILLER,
+  optionsBitmask = AIENABLED,
+  diet = HERBIVORE,
+
+  templates = {"object/mobile/oola.iff"},
+  lootGroups = {
+  {
+      groups = {
+        {group = "freedom_lg", chance = 10000000}
+  },
+      lootChance = 5000000
+  },
+  {
+      groups = {
+        {group = "hologram_lg", chance = 5000000},
+        {group = "hologram_lg2", chance = 5000000}
+  },
+      lootChance = 10000000
+  },
+  },
+weapons = {"nyaxs_weapons"},
+reactionStf = "@npc_reaction/fancy",
+attacks = merge(marksmanmaster,riflemanmaster,carbineermaster,brawlermaster,swordsmanmaster)
+}
+
+CreatureTemplates:addCreatureTemplate(oola_boss, "oola_boss")
