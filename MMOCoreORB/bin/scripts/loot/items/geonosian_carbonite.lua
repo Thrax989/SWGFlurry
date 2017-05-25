91845deded357a18874f6660cc45f7c4232395fc
Weezy <Weezyyy@users.noreply.github.com>
2017-05-25 22:52:12 +1000

    Add files via upload
    added 3 decoration loot items

---
+++ MMOCoreORB/bin/scripts/loot/items/geonosian_carbonite.lua
@@ -0,0 +1,11 @@
+geonosian_carbonite = {
+	minimumLevel = 0,
+	maximumLevel = 0,
+	customObjectName = "A Geonosian trapped in Carbonite",
+	directObjectTemplate = "object/tangible/veteran_reward/frn_vet_carbonite_geonosian.iff",
+	craftingValues = {},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("geonosian_carbonite", geonosian_carbonite)
