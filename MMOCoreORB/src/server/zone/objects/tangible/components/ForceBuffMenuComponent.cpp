0a21baecac141a0bf61212b4292db55784fe9fef
TOXIC <thrax989@yahoo.com>
2017-05-26 19:36:11 -0700

    [Updated] Event packs component casting

--- MMOCoreORB/src/server/zone/objects/tangible/components/ForceBuffMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/ForceBuffMenuComponent.cpp
@@ -8,6 +8,11 @@
 #include "server/zone/packets/object/ObjectMenuResponse.h"
 #include "server/zone/managers/player/PlayerManager.h"
 #include "templates/params/creature/CreatureAttribute.h"
+#include "server/zone/packets/player/PlayMusicMessage.h"
+#include "server/chat/ChatManager.h"
+#include "server/zone/managers/visibility/VisibilityManager.h"
+#include "server/zone/objects/player/sui/callbacks/BountyHuntSuiCallback.h"
+#include "server/zone/objects/player/sui/inputbox/SuiInputBox.h"
 
 
 void ForceBuffMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, ObjectMenuResponse* menuResponse, CreatureObject* player) const {
@@ -27,5 +32,11 @@ int ForceBuffMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, Cre
 	creature->healDamage(creature, CreatureAttribute::ACTION, 5000);
 	creature->healDamage(creature, CreatureAttribute::MIND, 5000);
 	sceneObject->destroyObjectFromWorld(true);
+	Zone* zone = creature->getZone();
+	//Broadcast to Server
+ 	String playerName = creature->getFirstName();
+ 	StringBuffer zBroadcast;
+ 	zBroadcast << "\\#00E604" << playerName << " \\#63C8F9 Has Used A Event Pack To Revive Their Body;
+ 	creature->getZoneServer()->getChatManager()->broadcastGalaxy(NULL, zBroadcast.toString());
 	return 0;
 }
