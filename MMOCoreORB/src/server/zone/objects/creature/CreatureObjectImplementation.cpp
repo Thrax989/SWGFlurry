219812d6794435bd1e24c12e88763724b286d330
kurdtkobain <dbz81487@yahoo.com>
2017-05-28 02:22:43 -0400

    cheap but effective

--- MMOCoreORB/src/server/zone/objects/creature/CreatureObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/CreatureObjectImplementation.cpp
@@ -2996,7 +2996,7 @@ bool CreatureObjectImplementation::isAttackableBy(CreatureObject* object, bool b
 	if (guildObject != NULL && guildObject->isInWaringGuild(object))
 		return true;
 
-	if ((object->getPvpStatusBitmask() & CreatureFlag::TEF))
+	if ((object->getPvpStatusBitmask() & CreatureFlag::ATTACKABLE) && (object->getPvpStatusBitmask() & CreatureFlag::AGGRESSIVE))
 		return true;
 	
 	return false;
