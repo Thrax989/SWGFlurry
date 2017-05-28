72834ca117e92fa95aeaf98055c4110056559a49
kurdtkobain <dbz81487@yahoo.com>
2017-05-28 02:42:12 -0400

    add teff to new checks

--- MMOCoreORB/src/server/zone/objects/creature/CreatureObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/CreatureObjectImplementation.cpp
@@ -3076,10 +3076,10 @@ bool CreatureObjectImplementation::isHealableBy(CreatureObject* object) {
 
 			//CombatManager::instance()->startCombat(object, defender, false);
 
-			BaseMessage* pvpstat = new UpdatePVPStatusMessage(defender, object, defender->getPvpStatusBitmask() | CreatureFlag::ATTACKABLE | CreatureFlag::AGGRESSIVE);
+			BaseMessage* pvpstat = new UpdatePVPStatusMessage(defender, object, defender->getPvpStatusBitmask() | CreatureFlag::ATTACKABLE | CreatureFlag::AGGRESSIVE | CreatureFlag::TEF);
 			object->sendMessage(pvpstat);
 
-			BaseMessage* pvpstat2 = new UpdatePVPStatusMessage(object, defender, object->getPvpStatusBitmask() | CreatureFlag::ATTACKABLE | CreatureFlag::AGGRESSIVE);
+			BaseMessage* pvpstat2 = new UpdatePVPStatusMessage(object, defender, object->getPvpStatusBitmask() | CreatureFlag::ATTACKABLE | CreatureFlag::AGGRESSIVE | CreatureFlag::TEF);
 			defender->sendMessage(pvpstat2);
 
 			//object->setCombatState();
