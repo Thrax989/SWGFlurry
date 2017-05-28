186e90341b18b3d7bb8a7a35265af3fcbc95750e
kurdtkobain <dbz81487@yahoo.com>
2017-05-28 00:01:33 -0400

    try ????????

--- MMOCoreORB/src/server/zone/objects/creature/CreatureObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/CreatureObjectImplementation.cpp
@@ -3073,10 +3073,10 @@ bool CreatureObjectImplementation::isHealableBy(CreatureObject* object) {
 
 			CombatManager::instance()->startCombat(object, defender, false);
 
-			BaseMessage* pvpstat = new UpdatePVPStatusMessage(defender, object, defender->getPvpStatusBitmask() | CreatureFlag::AGGRESSIVE);
+			BaseMessage* pvpstat = new UpdatePVPStatusMessage(defender, object, defender->getPvpStatusBitmask() | CreatureFlag::ATTACKABLE);
 			object->sendMessage(pvpstat);
 
-			BaseMessage* pvpstat2 = new UpdatePVPStatusMessage(object, defender, object->getPvpStatusBitmask() | CreatureFlag::AGGRESSIVE);
+			BaseMessage* pvpstat2 = new UpdatePVPStatusMessage(object, defender, object->getPvpStatusBitmask() | CreatureFlag::ATTACKABLE);
 			defender->sendMessage(pvpstat2);
 
 			object->setCombatState();
