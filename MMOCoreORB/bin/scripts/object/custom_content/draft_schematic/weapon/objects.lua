2251fb60fd377260520a241853791210b11b0198
Que <lovereseyes@gmail.com>
2017-06-09 06:00:04 -0400

    object
    object fix draft scheme

--- MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/weapon/objects.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/weapon/objects.lua
@@ -1,17 +1,17 @@
 --object_draft_schematic_weapon_shared_2h_sword_maul_legendary = SharedDraftSchematicObjectTemplate:new {
-	clientTemplateFileName = "object/draft_schematic/weapon/shared_2h_sword_maul_legendary.iff"
-}
-ObjectTemplates:addClientTemplate(object_draft_schematic_weapon_shared_2h_sword_maul_legendary, "object/draft_schematic/weapon/shared_2h_sword_maul_legendary.iff")
-
+--	clientTemplateFileName = "object/draft_schematic/weapon/shared_2h_sword_maul_legendary.iff"
+--}
+--ObjectTemplates:addClientTemplate(object_draft_schematic_weapon_shared_2h_sword_maul_legendary, "object/draft_schematic/weapon/shared_2h_sword_maul_legendary.iff")
+--}
 --object_draft_schematic_weapon_shared_2h_sword_sith = SharedDraftSchematicObjectTemplate:new {
-	clientTemplateFileName = "object/draft_schematic/weapon/shared_2h_sword_sith.iff"
-}
-ObjectTemplates:addClientTemplate(object_draft_schematic_weapon_shared_2h_sword_sith, "object/draft_schematic/weapon/shared_2h_sword_sith.iff")
+--	clientTemplateFileName = "object/draft_schematic/weapon/shared_2h_sword_sith.iff"
+--}
+--ObjectTemplates:addClientTemplate(object_draft_schematic_weapon_shared_2h_sword_sith, "object/draft_schematic/weapon/shared_2h_sword_sith.iff")
 
 --object_draft_schematic_weapon_shared_2h_sword_vibrosword = SharedDraftSchematicObjectTemplate:new {
-	clientTemplateFileName = "object/draft_schematic/weapon/shared_2h_sword_vibrosword.iff"
-}
-ObjectTemplates:addClientTemplate(object_draft_schematic_weapon_shared_2h_sword_vibrosword, "object/draft_schematic/weapon/shared_2h_sword_vibrosword.iff")
+--	clientTemplateFileName = "object/draft_schematic/weapon/shared_2h_sword_vibrosword.iff"
+--}
+--ObjectTemplates:addClientTemplate(object_draft_schematic_weapon_shared_2h_sword_vibrosword, "object/draft_schematic/weapon/shared_2h_sword_vibrosword.iff")
 
 object_draft_schematic_weapon_shared_pistol_d66 = SharedDraftSchematicObjectTemplate:new {
 	clientTemplateFileName = "object/draft_schematic/weapon/shared_pistol_d66.iff"
