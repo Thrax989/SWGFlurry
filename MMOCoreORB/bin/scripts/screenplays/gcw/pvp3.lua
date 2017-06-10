a0d06ebdfbb4615f3a3ac0dac24ddc25a8a6afb9
TOXIC <thrax989@yahoo.com>
2017-06-10 16:46:21 -0700

    [Updated] Trigger Logic For Destroyed Restuss

---
+++ MMOCoreORB/bin/scripts/screenplays/gcw/pvp3.lua
@@ -0,0 +1,64 @@
+local ObjectManager = require("managers.object.object_manager")
+
+pvp = ScreenPlay:new {
+	numberOfActs = 1,
+  	questString = "pvp",
+  	questdata = Object:new {
+    	activePlayerName = "initial",
+    	}
+}
+  
+registerScreenPlay("pvp", true)
+  
+function pvp:start()
+    	self:spawnActiveAreas()
+end
+  
+function pvp:spawnActiveAreas()
+    
+	if (pSpawnArea ~= nil) then
+		local activeArea = LuaActiveArea(pSpawnArea)
+	        activeArea:setCellObjectID(0)
+	        activeArea:setRadius(220)
+	        createObserver(ENTEREDAREA, "pvp", "notifySpawnArea", pSpawnArea)
+	        createObserver(EXITEDAREA, "pvp", "notifySpawnAreaLeave", pSpawnArea)
+	    end
+end
+ 
+function pvp:notifySpawnArea(pActiveArea, pMovingObject)
+	
+	if (not SceneObject(pMovingObject):isCreatureObject()) then
+		return 0
+	end
+	
+	return ObjectManager.withCreatureObject(pMovingObject, function(player)
+		if (player:isAiAgent()) then
+			return 0
+		end
+		
+		if (player:isImperial() or player:isNeutral() or player:isRebel()) then
+			player:setFactionStatus(2)
+		else
+			player:sendSystemMessage("You must be Rebel or Imperial or Neutral to enter the PvP zone!")
+			player:teleport(-5108, 81, -2106, 0)
+		end
+		return 0
+	end)
+end
+
+function pvp:notifySpawnAreaLeave(pActiveArea, pMovingObject)
+	
+	if (not SceneObject(pMovingObject):isCreatureObject()) then
+		return 0
+	end
+	
+	return ObjectManager.withCreatureObject(pMovingObject, function(player)
+		if (player:isAiAgent()) then
+			return 0
+		end
+		
+		if (player:isImperial() or player:isNeutral() or player:isRebel()) then
+		end
+		return 0
+	end)
+end
