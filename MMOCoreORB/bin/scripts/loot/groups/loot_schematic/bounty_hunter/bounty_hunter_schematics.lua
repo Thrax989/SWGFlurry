6887809a6e8afb1d3275584a9675c47876543dce
Weezy <Weezyyy@users.noreply.github.com>
2017-06-16 01:54:38 +1000

    Add files via upload
    loot groups from armour schematics

---
+++ MMOCoreORB/bin/scripts/loot/groups/loot_schematic/bounty_hunter/bounty_hunter_schematics.lua
@@ -0,0 +1,18 @@
+bounty_hunter_schematics = {
+    description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "bounty_hunter_bicep_l_schematic", weight = 1100000},
+		{itemTemplate = "bounty_hunter_bicep_r_schematic", weight = 1100000},
+		{itemTemplate = "bounty_hunter_boots_schematic", weight = 900000},
+		{itemTemplate = "bounty_hunter_bracer_l_schematic", weight = 1100000},
+		{itemTemplate = "bounty_hunter_bracer_r_schematic", weight = 1100000},
+		{itemTemplate = "bounty_hunter_chest_schematic", weight = 800000},
+		{itemTemplate = "bounty_hunter_gloves_schematic", weight = 1100000},
+		{itemTemplate = "bounty_hunter_leggings_schematic", weight = 1000000},
+		{itemTemplate = "bounty_hunter_helmet_schematic", weight = 800000}
+	}
+}
+
+addLootGroupTemplate("bounty_hunter_schematics", bounty_hunter_schematics)
---
+++ MMOCoreORB/bin/scripts/loot/groups/loot_schematic/mandalorian/mandalorian_schematics.lua
@@ -0,0 +1,18 @@
+mandalorian_schematics = {
+    description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "mandalorian_bicep_l_schematic", weight = 1100000},
+		{itemTemplate = "mandalorian_bicep_r_schematic", weight = 1100000},
+		{itemTemplate = "mandalorian_boots_schematic", weight = 900000},
+		{itemTemplate = "mandalorian_bracer_l_schematic", weight = 1100000},
+		{itemTemplate = "mandalorian_bracer_r_schematic", weight = 1100000},
+		{itemTemplate = "mandalorian_chest_schematic", weight = 800000},
+		{itemTemplate = "mandalorian_gloves_schematic", weight = 1100000},
+		{itemTemplate = "mandalorian_leggings_schematic", weight = 1000000},
+		{itemTemplate = "mandalorian_helmet_schematic", weight = 800000}
+	}
+}
+
+addLootGroupTemplate("mandalorian_schematics", mandalorian_schematics)
