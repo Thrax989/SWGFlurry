17aa596a3c30617165356d81d353e77542b01a8e
TOXIC <thrax989@yahoo.com>
2017-06-15 15:00:00 -0700

    [Added] Creature Obsservers along with Mob Tables to determine if faction is rebl or imperial during city swaps.
    Added Pvp GCW mob Tables
    Mob tables are Hard caped table 1-9
    If you add more Tables you will need to increase the hard cap

--- MMOCoreORB/bin/scripts/screenplays/gcw/roripvp.lua
+++ MMOCoreORB/bin/scripts/screenplays/gcw/roripvp.lua
@@ -5,10 +5,10 @@ RoriPvpScreenPlay = ScreenPlay:new {
 
 	planet = "",
 
-	gcwMobs = {},
+	pvpgcwMobs = {},
 }
 
-function RoriPvpScreenPlay:spawnGcwMobiles()
+function RoriPvpScreenPlay:spawnPvpGcwMobiles()
 	if (isZoneEnabled(self.planet)) then
 		local controllingFaction = getControllingFaction(self.planet)
 
@@ -22,8 +22,38 @@ function RoriPvpScreenPlay:spawnGcwMobiles()
 			end
 		end
 
-		for i = 1, #self.gcwMobs do
+		for i = 1, #self.pvpgcwMobs do
 			self:spawnMob(i, controllingFaction)
 		end
+	end
+end
+
+function RoriPvpScreenPlay:spawnMob(num, controllingFaction)
+	local mobsTable = self.pvpgcwMobs
+
+	if num <= 0 or num > #mobsTable then
+		return
+	end
+
+	local mobTable = mobsTable[num]
+	local pNpc = nil
+
+	if controllingFaction == FACTIONIMPERIAL then
+		pNpc = spawnMobile(self.planet, mobTable[1], 0, mobTable[3], mobTable[4], mobTable[5], mobTable[6], mobTable[7])
+
+		if pNpc ~= nil and mobTable[8] ~= "" then
+			self:setMoodString(pNpc, mobTable[8])
+		end
+	elseif controllingFaction == FACTIONREBEL then
+		pNpc = spawnMobile(self.planet, mobTable[2], 0, mobTable[3], mobTable[4], mobTable[5], mobTable[6], mobTable[7])
+
+		if pNpc ~= nil and mobTable[9] ~= "" then
+			self:setMoodString(pNpc, mobTable[9])
+		end
+	end
+
+	if pNpc ~= nil then
+		createObserver(CREATUREDESPAWNED, self.screenplayName, "onDespawn", pNpc)
+		writeData(SceneObject(pNpc):getObjectID(), num)
 	end
 end
