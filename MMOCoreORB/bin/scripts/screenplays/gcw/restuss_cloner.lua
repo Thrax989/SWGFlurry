6f5b6ac8f0ee69ef0884b8076d4fbbbca1139ca3
kurdtkobain <kurdtkobain@users.noreply.github.com>
2017-06-15 04:01:11 -0400

    forgot closing ")"

--- MMOCoreORB/bin/scripts/screenplays/gcw/restuss_cloner.lua
+++ MMOCoreORB/bin/scripts/screenplays/gcw/restuss_cloner.lua
@@ -14,6 +14,6 @@ end
 
 function restussClonerScreenPlay:spawnSceneObjects()
 spawnSceneObject("rori", "object/building/military/outpost_cloning_facility.iff", 5276.94, 79.4459, 6093.22, -0.999848, 0, 0, 0.0174522, 0)
-spawnSceneObject("rori", "object/tangible/terminal/terminal_bank.iff", 5185.61, 80, 5584.07, 1, math.rad(-90)
-spawnSceneObject("rori", "object/tangible/terminal/terminal_bazaar.iff", 5203, 80, 5584.36, 0, math.rad(-90)
+spawnSceneObject("rori", "object/tangible/terminal/terminal_bank.iff", 5185.61, 80, 5584.07, 1, math.rad(-90))
+spawnSceneObject("rori", "object/tangible/terminal/terminal_bazaar.iff", 5203, 80, 5584.36, 0, math.rad(-90))
 end
