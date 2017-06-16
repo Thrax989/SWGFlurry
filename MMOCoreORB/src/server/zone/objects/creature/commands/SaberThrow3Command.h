b8c96e4afba32feabf2ea3dc3121c13ba57b4095
TOXIC <thrax989@yahoo.com>
2017-06-15 23:18:03 -0700

    [Updated] Force throw test

--- MMOCoreORB/src/server/zone/objects/creature/commands/SaberThrow3Command.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/SaberThrow3Command.h
@@ -2,15 +2,16 @@
 				Copyright <SWGEmu>
 		See file COPYING for copying conditions.*/
 
-#ifndef SABERTHROW3COMMAND_H_
-#define SABERTHROW3COMMAND_H_
+#ifndef SABERTHROW2COMMAND_H_
+#define SABERTHROW2COMMAND_H_
 
+#include "server/zone/objects/scene/SceneObject.h"
 #include "JediCombatQueueCommand.h"
 
-class SaberThrow3Command : public JediCombatQueueCommand {
+class SaberThrow2Command : public JediCombatQueueCommand {
 public:
 
-	SaberThrow3Command(const String& name, ZoneProcessServer* server)
+	SaberThrow2Command(const String& name, ZoneProcessServer* server)
 		: JediCombatQueueCommand(name, server) {
 
 	}
@@ -27,9 +28,69 @@ public:
 			return NOJEDIARMOR;
 		}
 
+		ManagedReference<WeaponObject*> weapon = creature->getWeapon();
+
+		if (!weapon->isJediWeapon()) {
+			return INVALIDWEAPON;
+		}
+
+		Reference<SceneObject*> object = server->getZoneServer()->getObject(target);
+		ManagedReference<CreatureObject*> creatureTarget = cast<CreatureObject*>( object.get());
+
+		if (creatureTarget == NULL)
+			return GENERALERROR;
+
+
+		if (creature->getDistanceTo(object) > 32.f){
+			creature->sendSystemMessage("You are out of range.");
+			return GENERALERROR;
+		}		
+
+			PlayerManager* playerManager = server->getPlayerManager();
+
+		if (creature != creatureTarget && !CollisionManager::checkLineOfSight(creature, creatureTarget)) {
+			return GENERALERROR;
+		}
+
+		if (!creature->checkCooldownRecovery("used_Saber_Throw")) {
+					StringIdChatParameter stringId;
+
+					Time* cdTime = creature->getCooldownTime("used_Saber_Throw");
+
+
+					int timeLeft = floor((float)cdTime->miliDifference() / 1000) *-1;
+
+					stringId.setStringId("You must waiting....");
+					stringId.setDI(timeLeft);
+					creature->sendSystemMessage(stringId);
+					return 0;
+		}
+		
+		if (creature->isAttackableBy(creatureTarget) && creature->isInRange(creatureTarget, 32)) {
+
+			creature->addCooldown("used_Saber_Throw", 1);
+		}else{
+				return INVALIDTARGET;
+		}
+
+		const bool hasFr1 = creatureTarget->hasBuff(BuffCRC::JEDI_FORCE_RUN_1);
+		const bool hasFr2 = creatureTarget->hasBuff(BuffCRC::JEDI_FORCE_RUN_2);
+		const bool hasFr3 = creatureTarget->hasBuff(BuffCRC::JEDI_FORCE_RUN_3);
+
+		if(hasFr1 || hasFr2 || hasFr3) {
+			creature->sendSystemMessage(creatureTarget->getFirstName() + "'s Force Run has been disrupted by your attack!");
+			creatureTarget->sendSystemMessage("Your Force Run has been disrupted by" + creature->getFirstName() + ".");
+
+			Locker lock(creatureTarget);
+			if (hasFr1) { creatureTarget->removeBuff(BuffCRC::JEDI_FORCE_RUN_1); }
+			if (hasFr2) { creatureTarget->removeBuff(BuffCRC::JEDI_FORCE_RUN_2); }
+			if (hasFr3) { creatureTarget->removeBuff(BuffCRC::JEDI_FORCE_RUN_3); }
+
+		}
+
 		return doCombatAction(creature, target);
 	}
 
 };
 
-#endif //SABERTHROW3COMMAND_H_
+#endif //SABERTHROW2COMMAND_H_
