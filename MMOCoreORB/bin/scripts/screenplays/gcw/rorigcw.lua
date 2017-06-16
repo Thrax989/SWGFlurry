5baf226973bcc297bab35400b7290bcdf2355c37
TOXIC <thrax989@yahoo.com>
2017-06-15 17:24:33 -0700

    [Added] Test Stromtrooper / Rebel Trooper pvpgcwmob group for trigger city swapping

--- MMOCoreORB/bin/scripts/screenplays/gcw/rorigcw.lua
+++ MMOCoreORB/bin/scripts/screenplays/gcw/rorigcw.lua
@@ -6,7 +6,13 @@ RoriGcwScreenPlay = RoriPvpScreenPlay:new {
 	planet = "rori",
 
 	pvpgcwMobs = {
-		{"stormtrooper", "rebel_trooper", 0, 0, 0, 0, 0, "", ""},
+		{"stormtrooper", "rebel_trooper", 5399.5, 80.0, 5676.8, 161, 0, "", ""},
+		{"stormtrooper", "rebel_trooper", 5520.3, 80.0, 5535.2, 130, 0, "", ""},
+		{"stormtrooper", "rebel_trooper", 5521.7, 80.0, 5529.0, 80, 0, "", ""},
+		{"stormtrooper", "rebel_trooper", 5525.3, 80.0, 5533.2, -50, 0, "", ""},
+		{"stormtrooper", "rebel_trooper", 5313.2, 80.0, 5795.4, 95, 0, "", ""},
+		{"stormtrooper", "rebel_trooper", 5313.2, 80.0, 5791.2, 50, 0, "", ""},
+		{"stormtrooper", "rebel_trooper", 5269.9, 80.0, 5815.0, 77, 0, "", ""},	
 	},
 }
 
