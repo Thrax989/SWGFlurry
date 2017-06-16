32224fc3c1774c6b4d38fd4ee557abb26106bb2a
TOXIC <thrax989@yahoo.com>
2017-06-16 15:18:38 -0700

    [ADded] Cleaned up mobile deed templates

--- MMOCoreORB/bin/scripts/object/custom_content/mobile/vehicle/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/mobile/vehicle/serverobjects.lua
@@ -1,61 +1,62 @@
---Copyright (C) 2010 <SWGEmu>
-
-
---This File is part of Core3.
-
---This program is free software; you can redistribute
---it and/or modify it under the terms of the GNU Lesser
---General Public License as published by the Free Software
---Foundation; either version 2 of the License,
---or (at your option) any later version.
-
---This program is distributed in the hope that it will be useful,
---but WITHOUT ANY WARRANTY; without even the implied warranty of
---MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
---See the GNU Lesser General Public License for
---more details.
-
---You should have received a copy of the GNU Lesser General
---Public License along with this program; if not, write to
---the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-
---Linking Engine3 statically or dynamically with other modules
---is making a combined work based on Engine3.
---Thus, the terms and conditions of the GNU Lesser General Public License
---cover the whole combination.
-
---In addition, as a special exception, the copyright holders of Engine3
---give you permission to combine Engine3 program with free software
---programs or libraries that are released under the GNU LGPL and with
---code included in the standard release of Core3 under the GNU LGPL
---license (or modified versions of such code, with unchanged license).
---You may copy and distribute such a system following the terms of the
---GNU LGPL for Engine3 and the licenses of the other code concerned,
---provided that you include the source code of that other code when
---and as the GNU LGPL requires distribution of source code.
-
---Note that people who make modified versions of Engine3 are not obligated
---to grant this special exception for their modified versions;
---it is their choice whether to do so. The GNU Lesser General Public License
---gives permission to release a modified version without this exception;
---this exception also makes it possible to release a modified version
-
-
-
---Children folder includes
-
--- New Objects
+includeFile("custom_content/mobile/vehicle/vehicle_atat.lua")
+includeFile("custom_content/mobile/vehicle/vehicle_atpt.lua")
+includeFile("custom_content/mobile/vehicle/vehicle_atrt.lua")
+includeFile("custom_content/mobile/vehicle/vehicle_atst.lua")
+includeFile("custom_content/mobile/vehicle/vehicle_atxt.lua")
+includeFile("custom_content/mobile/vehicle/a1_deluxe_floater.lua")
+includeFile("custom_content/mobile/vehicle/air2_swoop_speeder.lua")
 includeFile("custom_content/mobile/vehicle/barc_speeder.lua")
---includeFile("custom_content/mobile/vehicle/grievous_wheel_bike.lua")
---includeFile("custom_content/mobile/vehicle/mustafar_panning_droid.lua")
---includeFile("custom_content/mobile/vehicle/walker_at_xt_player.lua")
+includeFile("custom_content/mobile/vehicle/barc_speeder_imperial.lua")
+includeFile("custom_content/mobile/vehicle/barc_speeder_rebel.lua")
+includeFile("custom_content/mobile/vehicle/basilisk_war_droid.lua")
+includeFile("custom_content/mobile/vehicle/fg_8t8_podracer.lua")
+includeFile("custom_content/mobile/vehicle/flare_s_swoop.lua")
+includeFile("custom_content/mobile/vehicle/flare_s_swoop_crafted.lua")
+includeFile("custom_content/mobile/vehicle/geonosian_speeder.lua")
+includeFile("custom_content/mobile/vehicle/gift_stap_speeder.lua")
+includeFile("custom_content/mobile/vehicle/grievous_wheel_bike.lua")
+includeFile("custom_content/mobile/vehicle/hoth_at_st.lua")
+includeFile("custom_content/mobile/vehicle/hover_chair.lua")
+includeFile("custom_content/mobile/vehicle/hoverlifter_speeder.lua")
+includeFile("custom_content/mobile/vehicle/hoverlifter_speeder_crafted.lua")
+includeFile("custom_content/mobile/vehicle/koro2_speeder.lua")
+includeFile("custom_content/mobile/vehicle/landspeeder_ab1.lua")
 includeFile("custom_content/mobile/vehicle/landspeeder_desert_skiff.lua")
 includeFile("custom_content/mobile/vehicle/landspeeder_lava_skiff.lua")
---includeFile("custom_content/mobile/vehicle/landspeeder_organa.lua")
+includeFile("custom_content/mobile/vehicle/landspeeder_organa.lua")
 includeFile("custom_content/mobile/vehicle/landspeeder_tantive4.lua")
+includeFile("custom_content/mobile/vehicle/landspeeder_tantive4_adv.lua")
 includeFile("custom_content/mobile/vehicle/landspeeder_usv5.lua")
+includeFile("custom_content/mobile/vehicle/landspeeder_usv5_s02.lua")
+includeFile("custom_content/mobile/vehicle/landspeeder_v35.lua")
+includeFile("custom_content/mobile/vehicle/light_bending_barc.lua")
+includeFile("custom_content/mobile/vehicle/mechno_chair.lua")
+includeFile("custom_content/mobile/vehicle/mustafar_panning_droid.lua")
+includeFile("custom_content/mobile/vehicle/pod_racer_balta_podracer.lua")
+includeFile("custom_content/mobile/vehicle/pod_racer_ipg_longtail.lua")
+includeFile("custom_content/mobile/vehicle/pod_racer_light_bending.lua")
 includeFile("custom_content/mobile/vehicle/pod_racer_one.lua")
 includeFile("custom_content/mobile/vehicle/pod_racer_two.lua")
---includeFile("custom_content/mobile/vehicle/sith_speeder.lua")
-includeFile("custom_content/mobile/vehicle/barc_speeder_rebel.lua")
-includeFile("custom_content/mobile/vehicle/barc_speeder_imperial.lua")
+includeFile("custom_content/mobile/vehicle/podracer_anakin.lua")
+includeFile("custom_content/mobile/vehicle/ric_920_speeder.lua")
+includeFile("custom_content/mobile/vehicle/senate_pod.lua")
+includeFile("custom_content/mobile/vehicle/single_pod_airspeeder.lua")
+includeFile("custom_content/mobile/vehicle/sith_speeder.lua")
+includeFile("custom_content/mobile/vehicle/snowspeeder.lua")
+includeFile("custom_content/mobile/vehicle/speeder_ric_920.lua")
+includeFile("custom_content/mobile/vehicle/stap_speeder.lua")
+includeFile("custom_content/mobile/vehicle/swamp_speeder.lua")
+includeFile("custom_content/mobile/vehicle/tcg_8_air_speeder.lua")
+includeFile("custom_content/mobile/vehicle/tcg_8_single_pod_airspeeder.lua")
+includeFile("custom_content/mobile/vehicle/tcg_at_pt_walker.lua")
+includeFile("custom_content/mobile/vehicle/tcg_hk47_jetpack.lua")
+includeFile("custom_content/mobile/vehicle/tcg_merr_sonn_jt12_jetpack.lua")
+includeFile("custom_content/mobile/vehicle/tcg_military_transport.lua")
+includeFile("custom_content/mobile/vehicle/tcg_republic_gunship.lua")
+includeFile("custom_content/mobile/vehicle/temp_walker1.lua")
+includeFile("custom_content/mobile/vehicle/walker_at_rt_camo.lua")
+includeFile("custom_content/mobile/vehicle/walker_at_rt_reg.lua")
+includeFile("custom_content/mobile/vehicle/walker_at_st.lua")
+includeFile("custom_content/mobile/vehicle/walker_at_xt.lua")
+includeFile("custom_content/mobile/vehicle/walker_at_xt_player.lua")
+includeFile("custom_content/mobile/vehicle/xj6_air_speeder.lua")
