c490ba144ddffe2584f8a4bd0ec4ea8191d9a23f
TOXIC <thrax989@yahoo.com>
2017-06-18 20:12:39 -0700

    [Updated] Unit test error

--- MMOCoreORB/bin/scripts/loot/groups/loot_schematic/bounty_hunter/bounty_hunter_schematics.lua
+++ MMOCoreORB/bin/scripts/loot/groups/loot_schematic/bounty_hunter/bounty_hunter_schematics.lua
@@ -5,13 +5,13 @@ bounty_hunter_schematics = {
 	lootItems = {
 		{itemTemplate = "bounty_hunter_bicep_l_schematic", weight = 1100000},
 		{itemTemplate = "bounty_hunter_bicep_r_schematic", weight = 1100000},
-		{itemTemplate = "bounty_hunter_boots_schematic", weight = 900000},
+		{itemTemplate = "bounty_hunter_boots_schematic", weight = 1200000},
 		{itemTemplate = "bounty_hunter_bracer_l_schematic", weight = 1100000},
 		{itemTemplate = "bounty_hunter_bracer_r_schematic", weight = 1100000},
-		{itemTemplate = "bounty_hunter_chest_schematic", weight = 800000},
+		{itemTemplate = "bounty_hunter_chest_schematic", weight = 1200000},
 		{itemTemplate = "bounty_hunter_gloves_schematic", weight = 1100000},
 		{itemTemplate = "bounty_hunter_leggings_schematic", weight = 1000000},
-		{itemTemplate = "bounty_hunter_helmet_schematic", weight = 800000}
+		{itemTemplate = "bounty_hunter_helmet_schematic", weight = 1100000}
 	}
 }
 
