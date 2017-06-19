16e1c97714917238b6198295222010e9ea38d096
TOXIC <thrax989@yahoo.com>
2017-06-18 20:20:08 -0700

    [Updated] Unit test erros

--- MMOCoreORB/bin/scripts/loot/groups/loot_schematic/mandalorian/mandalorian_schematics.lua
+++ MMOCoreORB/bin/scripts/loot/groups/loot_schematic/mandalorian/mandalorian_schematics.lua
@@ -3,15 +3,15 @@ mandalorian_schematics = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "mandalorian_bicep_l_schematic", weight = 1100000},
-		{itemTemplate = "mandalorian_bicep_r_schematic", weight = 1100000},
-		{itemTemplate = "mandalorian_boots_schematic", weight = 900000},
-		{itemTemplate = "mandalorian_bracer_l_schematic", weight = 1100000},
-		{itemTemplate = "mandalorian_bracer_r_schematic", weight = 1100000},
-		{itemTemplate = "mandalorian_chest_schematic", weight = 800000},
-		{itemTemplate = "mandalorian_gloves_schematic", weight = 1100000},
+		{itemTemplate = "mandalorian_bicep_l_schematic", weight = 1000000},
+		{itemTemplate = "mandalorian_bicep_r_schematic", weight = 1000000},
+		{itemTemplate = "mandalorian_boots_schematic", weight = 2000000},
+		{itemTemplate = "mandalorian_bracer_l_schematic", weight = 1000000},
+		{itemTemplate = "mandalorian_bracer_r_schematic", weight = 1000000},
+		{itemTemplate = "mandalorian_chest_schematic", weight = 50000},
+		{itemTemplate = "mandalorian_gloves_schematic", weight = 2000000},
 		{itemTemplate = "mandalorian_leggings_schematic", weight = 1000000},
-		{itemTemplate = "mandalorian_helmet_schematic", weight = 800000}
+		{itemTemplate = "mandalorian_helmet_schematic", weight = 500000}
 	}
 }
 
