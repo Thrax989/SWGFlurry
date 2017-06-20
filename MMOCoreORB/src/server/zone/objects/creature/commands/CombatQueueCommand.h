0aa727ff4a0bfeeb425652e84fd447929c5791af
TOXIC <thrax989@yahoo.com>
2017-06-19 23:36:50 -0700

    [Updated] Combat que

--- MMOCoreORB/src/server/zone/objects/creature/commands/CombatQueueCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/CombatQueueCommand.h
@@ -177,11 +177,11 @@ public:
 						if (targetCreature != NULL) {
 							if (targetCreature->isPlayerCreature()) {
 								if (!CombatManager::instance()->areInDuel(creature, targetCreature) && !targetCreature->hasBountyMissionFor(creature) && !creature->hasBountyMissionFor(targetCreature) && targetCreature->getFactionStatus() == FactionStatus::OVERT)
-									ghost->doFieldFactionChange(FactionStatus::OVERT);
-							} else if (targetCreature->isPet()) {
+                                                                       ghost->doFieldFactionChange(FactionStatus::OVERT);
+							} else if (targetCreature->isPet() && !targetCreature->isAttackableBy(creature)) {
 								ManagedReference<CreatureObject*> targetOwner = targetCreature->getLinkedCreature().get();
 
-								if (targetOwner != NULL && !creature->hasBountyMissionFor(targetOwner) && !targetOwner->hasBountyMissionFor(creature) && !CombatManager::instance()->areInDuel(creature, targetOwner) && targetOwner->getFactionStatus() == FactionStatus::OVERT) {
+								if (targetOwner != NULL && !CombatManager::instance()->areInDuel(creature, targetOwner) && targetOwner->getFactionStatus() == FactionStatus::OVERT) {
 										ghost->doFieldFactionChange(FactionStatus::OVERT);
 								}
 							} else {
