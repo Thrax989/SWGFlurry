832351568d4797c7a16895d7db9d31453ab6b455
TOXIC <thrax989@yahoo.com>
2017-06-19 17:14:48 -0700

    [Added] New isd mouse droid

---
+++ MMOCoreORB/bin/scripts/mobile/isd/isd_mouse_droid.lua
@@ -0,0 +1,47 @@
+isd_mouse_droid = Creature:new {
+	objectName = "@mob/creature_names:mouse_droid",
+	socialGroup = "self",
+	faction = "",
+	level = 200,
+	chanceHit = 35.00,
+	damageMin = 55,
+	damageMax = 65,
+	baseXp = 147,
+	baseHAM = 270,
+	baseHAMmax = 330,
+	armor = 0,
+	resists = {0,0,40,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0.000000,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = NONE,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/mouse_droid.iff"},
+	lootGroups = {},
+	weapons = {"creature_spit_heavy_flame"},
+	reactionStf = "@npc_reaction/slang",
+	attacks = {
+		{"creatureareacombo","StateAccuracyBonus=50"},
+		{"creatureareaknockdown","StateAccuracyBonus=50"},
+		{"knockdownattack","KnockdownChance=50"},
+		{"creatureareaknockdown","KnockdownChance=50"},
+		{"dizzyattack","DizzyChance=50"},
+		{"stunattack","StunChance=50"},
+		{"mildpoison","PoisonChance=50"},
+		{"intimidationattack","IntimidationChance=50"},
+		{"mediumpoison","PoisonChance=50"},
+		{"creatureareapoison","PoisonChance=50"},
+		{"strongpoison","PoisonChance=50"},
+		{"creatureareaattack","StateAccuracyBonus=50"}
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(isd_mouse_droid, "isd_mouse_droid")
