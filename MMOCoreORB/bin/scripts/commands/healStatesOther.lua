c6d6589d83ee1c8cd302a08e885e91f0b2294ee4
TOXIC <thrax989@yahoo.com>
2017-06-20 21:20:42 -0700

    [Updated] Heal States Other droped from 500 FC to 250

--- MMOCoreORB/bin/scripts/commands/healStatesOther.lua
+++ MMOCoreORB/bin/scripts/commands/healStatesOther.lua
@@ -44,7 +44,7 @@
 HealStatesOtherCommand = {
         name = "healstatesother",
 	healStates = STUNNED_STATE + DIZZY_STATE + BLINDED_STATE + INTIMIDATED_STATE + FEIGNDEATH_STATE,
-	forceCost = 500,
+	forceCost = 250,
 	range = 32
 }
 
