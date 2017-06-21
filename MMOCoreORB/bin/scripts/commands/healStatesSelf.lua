8c04aff48b7b84d4ed6d48eff40d329eddd5aca5
TOXIC <thrax989@yahoo.com>
2017-06-20 21:21:09 -0700

    [Updated] Heal States Other doped from 500 FC to 100

--- MMOCoreORB/bin/scripts/commands/healStatesSelf.lua
+++ MMOCoreORB/bin/scripts/commands/healStatesSelf.lua
@@ -44,7 +44,7 @@
 HealStatesSelfCommand = {
         name = "healstatesself",
 	healStates = STUNNED_STATE + DIZZY_STATE + BLINDED_STATE + INTIMIDATED_STATE,
-	forceCost = 500,
+	forceCost = 100,
 	range = 0
 }
 
