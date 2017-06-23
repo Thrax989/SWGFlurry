1c772ef724aba3bc9df3bec93b3c14ac95da511f
TOXIC <thrax989@yahoo.com>
2017-06-22 17:48:58 -0700

    [Updated] Typo

--- MMOCoreORB/bin/scripts/loot/groups/loot_schematic/mandalorian/mandalorian_schematics.lua
+++ MMOCoreORB/bin/scripts/loot/groups/loot_schematic/mandalorian/mandalorian_schematics.lua
@@ -8,7 +8,7 @@ mandalorian_schematics = {
 		{itemTemplate = "mandalorian_boots_schematic", weight = 2000000},
 		{itemTemplate = "mandalorian_bracer_l_schematic", weight = 1000000},
 		{itemTemplate = "mandalorian_bracer_r_schematic", weight = 1000000},
-		{itemTemplate = "mandalorian_chest_schematic", weight = 50000},
+		{itemTemplate = "mandalorian_chest_schematic", weight = 500000},
 		{itemTemplate = "mandalorian_gloves_schematic", weight = 2000000},
 		{itemTemplate = "mandalorian_leggings_schematic", weight = 1000000},
 		{itemTemplate = "mandalorian_helmet_schematic", weight = 500000}
