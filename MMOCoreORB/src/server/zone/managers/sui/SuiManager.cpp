dab73ac2987dc1149e9853f5bfc3753382c14994
TOXIC <thrax989@yahoo.com>
2017-06-29 20:49:37 -0700

    [Fixed] Taanab Travel Ports

--- MMOCoreORB/src/server/zone/managers/sui/SuiManager.cpp
+++ MMOCoreORB/src/server/zone/managers/sui/SuiManager.cpp
@@ -1953,7 +1953,7 @@ void SuiManager::handleCharacterBuilderSelectItem(CreatureObject* player, SuiBox
 				if (!player->isInCombat() && player->getCashCredits() > 4999) {
 		                ManagedReference<SuiMessageBox*> box = new SuiMessageBox(player, SuiWindowType::CITY_ADMIN_CONFIRM_UPDATE_TYPE);
 					player->sendSystemMessage("Thank you for your travels.");
- 				        player->switchZone("tanaab", 0, 0, 0);
+ 				        player->switchZone("taanab", 0, 0, 0);
  					player->subtractCashCredits(5000);
 					box->setForceCloseDistance(5.f);
 			        }
@@ -1970,7 +1970,7 @@ void SuiManager::handleCharacterBuilderSelectItem(CreatureObject* player, SuiBox
 				if (!player->isInCombat() && player->getCashCredits() > 4999) {
 		                ManagedReference<SuiMessageBox*> box = new SuiMessageBox(player, SuiWindowType::CITY_ADMIN_CONFIRM_UPDATE_TYPE);
 					player->sendSystemMessage("Thank you for your travels.");
- 				        player->switchZone("tanaab", 0, 0, 0);
+ 				        player->switchZone("taanab", 0, 0, 0);
  					player->subtractCashCredits(5000);
 					box->setForceCloseDistance(5.f);
 			        }
