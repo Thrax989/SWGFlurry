5e786f5cb32e41e78f958c6fc2e9ee061a73103f
TOXIC <thrax989@yahoo.com>
2017-06-30 23:32:02 -0400

    [Added] Rewrote Entire ISD Instance Added 10 Phases and 25 new boss triggers.

--- MMOCoreORB/bin/scripts/screenplays/dungeon/isd/isd.lua
+++ MMOCoreORB/bin/scripts/screenplays/dungeon/isd/isd.lua
@@ -16,6 +16,14 @@ print("Dungeon 1 Enabled Spawning ISD")
 end
 
 function ISDScreenPlay:spawnMobiles()
+
+
+--Teleport Travel 
+--spawnSceneObject("dungeon1", "object/tangible/terminal/terminal_gcw_publish_gift.iff", -38.7507, 173.835, 67.1737, 4336902, 1, 0, 0, 0)
+
+
+--Save 2nd room for loot creates
+--
 -- Base FloorStorm Troopers Guarding Door Entrances -- Guards at entrance 100% complete
 spawnMobile("dungeon1", "isd_stormtrooper", 1, 40.6567, 173.835, 34.0745, 261, 4336902)
 spawnMobile("dungeon1", "isd_stormtrooper", 1, 40.6779, 173.835, 29.086, 269, 4336902)
@@ -25,12 +33,12 @@ spawnMobile("dungeon1", "isd_stormtrooper", 1, -29.9486, 173.835, 0.12338, 35, 4
 spawnMobile("dungeon1", "isd_stormtrooper", 1, -33.501, 173.835, 3.61753, 53, 4336902)
 spawnMobile("dungeon1", "isd_stormtrooper", 1, -40.6873, 173.835, 34.0851, 95, 4336902)
 spawnMobile("dungeon1", "isd_stormtrooper", 1, -40.6764, 173.835, 29.0936, 71, 4336902)
-  
 --Elevator Control Shaft Guards  10% complete
 spawnMobile("dungeon1", "isd_stormtrooper", 1, -48.4635, 173.835, 21.3722, 78, 4336902)
 spawnMobile("dungeon1", "isd_stormtrooper", 1, -47.9885, 173.835, 17.3455, 89, 4336902)
 spawnMobile("dungeon1", "isd_stormtrooper", 1, 47.691, 173.835, 22.586, 260, 4336902)
 spawnMobile("dungeon1", "isd_stormtrooper", 1, 47.9413, 173.835, 17.3783, 269, 4336902)
+
 
 --Upper Floor Room Guards will patrol Elevator Points 50% complete
 spawnMobile("dungeon1", "isd_stormtrooper", 1, -47.941, 181.335, 17.5886, 80, 4336902)
@@ -41,6 +49,8 @@ spawnMobile("dungeon1", "isd_stormtrooper", 1, 40.7362, 181.335, 24.163, 262, 43
 spawnMobile("dungeon1", "isd_stormtrooper", 1, 40.7167, 181.335, 15.9608, 270, 4336902)
 spawnMobile("dungeon1", "isd_stormtrooper", 1, -40.7103, 181.335, 16.0802, 87, 4336902)
 spawnMobile("dungeon1", "isd_stormtrooper", 1, -40.75, 181.335, 24.1963, 87, 4336902)
+
+
 
 -- Left Hallway Troopers --way 3045 173 -2968 50% complete 
 spawnMobile("dungeon1", "isd_stormtrooper", 1, 44.309, 173.835, 29.086, 0, 4336902)
@@ -75,241 +85,109 @@ spawnMobile("dungeon1", "isd_stormtrooper", 1, -52.809, 173.835, 34.0861, 178, 4
 spawnMobile("dungeon1", "isd_stormtrooper", 1, -57.691, 173.835, 34.086, 179, 4336901)
 spawnMobile("dungeon1", "isd_stormtrooper", 1, -59.809, 173.835, 34.086, 181, 4336901)
 
---set up captin of the ISD -- NPC Tempalte will need to be changed Stromtroopers are BASE testing NPC
-spawnMobile("dungeon1", "isd_stormtrooper", 1, -0.641077, 181.335, -2.51546, 355, 4336902)
 
-    local pDroid = spawnMobile("dungeon1", "isd_mouse_droid", 180, -10.0259, 173.835, 18.2381, 349, 4336902)
+spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902)
 
-          createObserver(OBJECTDESTRUCTION, "ISDScreenPlay", "notifyDroidDead", pDroid)
 end
 
---Start Krix Swiftshadow Fight   
-function ISDScreenPlay:notifyDroidDead(pDroid, pKiller)
-  local player = LuaCreatureObject(pKiller)
-        local pBoss = spawnMobile("dungeon1", "krix_swift", 180, 0.0366271, 173.835, 18.1507, 351, 4336902)
- print("spawned Krix")
-      spatialChat(pBoss, "Welcome friends, You're just in time for the party.  Allow me to show you some hospitality!") 
-              createObserver(DAMAGERECEIVED, "ISDScreenPlay", "boss_damage", pBoss) print("observer set")
+
+spawnMobile("dungeon1", "stormtrooper", 1, -14.0146, 173.835, 22.8548, 330, 4336902)
+
+function ISDScreenPlay:spawnMobiles()
+
+
+
+--Droid 1 Trigger 1st DROIDEKA
+	local T1Droid = spawnMobile("dungeon1", "droideka", 1, -6.32669, 173.835, 12.5935, 304, 4336902)print("Trigger 1 Loaded")
+        	createObserver(OBJECTDESTRUCTION, "ISDScreenPlay", "notify1DroidDead", T1Droid)
+--Droid 2 Trigger 2nd DROIDEKA
+	local T2Droid = spawnMobile("dungeon1", "droideka", 1, 4.61058, 173.835, 12.7671, 341, 4336902)print("Trigger 2 Loaded")
+        	createObserver(OBJECTDESTRUCTION, "ISDScreenPlay", "notify2DroidDead", T2Droid)
+--Droid 3 Trigger
+	local T3Droid = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902)print("Trigger 3 Loaded")
+        	createObserver(OBJECTDESTRUCTION, "ISDScreenPlay", "notify3DroidDead", T3Droid)
+--Droid 4 Trigger
+	local T4Droid = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902)print("Trigger 4 Loaded")
+        	createObserver(OBJECTDESTRUCTION, "ISDScreenPlay", "notify4DroidDead", T4Droid)
+--Droid 5 Trigger
+	local T5Droid = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902)print("Trigger 5 Loaded")
+        	createObserver(OBJECTDESTRUCTION, "ISDScreenPlay", "notify5DroidDead", T5Droid)
+--Droid 6 Trigger
+	local T6Droid = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902)print("Trigger 6 Loaded")
+        	createObserver(OBJECTDESTRUCTION, "ISDScreenPlay", "notify6DroidDead", T6Droid)
+--Droid 7 Trigger
+	local T7Droid = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902)print("Trigger 7 Loaded")
+        	createObserver(OBJECTDESTRUCTION, "ISDScreenPlay", "notify7DroidDead", T7Droid)
+--Droid 8 Trigger
+	local T8Droid = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902)print("Trigger 8 Loaded")
+        	createObserver(OBJECTDESTRUCTION, "ISDScreenPlay", "notify8DroidDead", T8Droid)
+--Droid 9 Trigger
+	local T9Droid =spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902)print("Trigger 9 Loaded")
+        	createObserver(OBJECTDESTRUCTION, "ISDScreenPlay", "notify9DroidDead", T9Droid)
+--Droid 10 Trigger
+	local T10Droid = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902)print("Trigger 10 Loaded")
+        	createObserver(OBJECTDESTRUCTION, "ISDScreenPlay", "notify10DroidDead", T10Droid)
+end
+
+--Phase 1 Of 10 Instance KRIX BOSS
+function ISDScreenPlay:notify1DroidDead(T1Droid, pKiller)
+	local player = LuaCreatureObject(pKiller)
+        local pBoss = spawnMobile("dungeon1", "krix_swift", 1, -0.0544035, 173.835, 0.702554, 354, 4336902) print("Phase 1 Started")
         return 0
 end
-
-function ISDScreenPlay:boss_damage(playerObject, creatureObject, damage)
-  local player = LuaCreatureObject(playerObject)
-  local boss = LuaCreatureObject(creatureObject)
-  
-  health = boss:getHAM(0)
-  maxHealth = boss:getMaxHAM(0)
-
-  if (health <= (maxHealth * 0.90)) then print("checking HAM")
-    spatialChat(playerObject, "Well, it seems we need a more suitable welcome.  Get 'em Boys!") print("spatial")
-    writeData("ISDScreenPlay:spawnAdd1", 1) print("writing data")
-    if (readData("ISDScreenPlay:spawnAdd1") == 1) then print("checking spawnAdd1")
-      local pAdd1 = spawnMobile("dungeon1", "isd_stormtrooper", 180, 10.0035, 173.835, 18.2251, 345, 4336902)
-print("add spawned")
-      local firstTime = LuaCreatureObject(pAdd1)print("luaCreatureObject pointer")
-      spatialChat(pAdd1, "Time to Die!") print("spatial for add")
-      firstTime:engageCombat(pPlayer)print("engaging combat")
-    end
-    
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.85)) then print("checking HAM")
-   -- spatialChat(playerObject, "I don't know where Bill is!") print("spatial")
-    writeData("ISDScreenPlay:spawnAdd2", 1) print("writing data")
-    if (readData("ISDScreenPlay:spawnAdd2") == 1) then print("checking spawnAdd2")
-      local pAdd2 = spawnMobile("dungeon1", "isd_stormtrooper", 180, 19.9453, 173.835, 18.2015, 351, 4336902)
-print("add spawned")
-      local firstTime = LuaCreatureObject(pAdd2)print("luaCreatureObject pointer")
-      spatialChat(pAdd2, "Time to Die!") print("spatial for add")
-      firstTime:engageCombat(pPlayer)print("engaging combat")
-    end
-    
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.80)) then print("checking HAM")
-   -- spatialChat(playerObject, "I don't know where Bill is!") print("spatial")
-    writeData("ISDScreenPlay:spawnAdd3", 1) print("writing data")
-    if (readData("ISDScreenPlay:spawnAdd3") == 1) then print("checking spawnAdd3")
-      local pAdd3 = spawnMobile("dungeon1", "isd_stormtrooper", 180, 19.8971, 173.835, 27.1461, 262, 4336902)
-print("add spawned")
-      local firstTime = LuaCreatureObject(pAdd3)print("luaCreatureObject pointer")
-      spatialChat(pAdd3, "Time to Die!") print("spatial for add")
-      firstTime:engageCombat(pPlayer)print("engaging combat")
-    end
-    
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.75)) then print("checking HAM")
-   -- spatialChat(playerObject, "I don't know where Bill is!") print("spatial")
-    writeData("ISDScreenPlay:spawnAdd4", 1) print("writing data")
-    if (readData("ISDScreenPlay:spawnAdd4") == 1) then print("checking spawnAdd4")
-      local pAdd4 = spawnMobile("dungeon1", "isd_stormtrooper", 180, 9.96949, 173.835, 27.1469, 260, 4336902)
-print("add spawned")
-      local firstTime = LuaCreatureObject(pAdd4)print("luaCreatureObject pointer")
-      spatialChat(pAdd4, "Time to Die!") print("spatial for add")
-      firstTime:engageCombat(pPlayer)print("engaging combat")
-    end
-    
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.70)) then print("checking HAM")
-   -- spatialChat(playerObject, "I don't know where Bill is!") print("spatial")
-    writeData("ISDScreenPlay:spawnAdd5", 1) print("writing data")
-    if (readData("ISDScreenPlay:spawnAdd5") == 1) then print("checking spawnAdd5")
-      local pAdd5 = spawnMobile("dungeon1", "isd_stormtrooper", 180, 0.086636, 173.835, 27.1788, 269, 4336902)
-print("add spawned")
-      local firstTime = LuaCreatureObject(pAdd5)print("luaCreatureObject pointer")
-      spatialChat(pAdd5, "Time to Die!") print("spatial for add")
-      firstTime:engageCombat(pPlayer)print("engaging combat")
-    end
-    
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.65)) then print("checking HAM")
-   -- spatialChat(playerObject, "I don't know where Bill is!") print("spatial")
-    writeData("ISDScreenPlay:spawnAdd6", 1) print("writing data")
-    if (readData("ISDScreenPlay:spawnAdd6") == 1) then print("checking spawnAdd6")
-      local pAdd6 = spawnMobile("dungeon1", "isd_stormtrooper", 180, -9.91026, 173.835, 27.1188, 270, 4336902)
-print("add spawned")
-      local firstTime = LuaCreatureObject(pAdd6)print("luaCreatureObject pointer")
-      spatialChat(pAdd6, "Time to Die!") print("spatial for add")
-      firstTime:engageCombat(pPlayer)print("engaging combat")
-    end
-    
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.60)) then print("checking HAM")
-   -- spatialChat(playerObject, "I don't know where Bill is!") print("spatial")
-    writeData("ISDScreenPlay:spawnAdd7", 1) print("writing data")
-    if (readData("ISDScreenPlay:spawnAdd7") == 1) then print("checking spawnAdd7")
-      local pAdd7 = spawnMobile("dungeon1", "isd_stormtrooper", 180, -9.95836, 173.835, 35.9215, 81, 4336902)
-print("add spawned")
-      local firstTime = LuaCreatureObject(pAdd7)print("luaCreatureObject pointer")
-      spatialChat(pAdd7, "Time to Die!") print("spatial for add")
-      firstTime:engageCombat(pPlayer)print("engaging combat")
-    end
-    
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.55)) then print("checking HAM")
-   -- spatialChat(playerObject, "I don't know where Bill is!") print("spatial")
-    writeData("ISDScreenPlay:spawnAdd8", 1) print("writing data")
-    if (readData("ISDScreenPlay:spawnAdd8") == 1) then print("checking spawnAdd8")
-      local pAdd8 = spawnMobile("dungeon1", "isd_stormtrooper", 180, -0.359259, 173.835, 35.9656, 92, 4336902)
-print("add spawned")
-      local firstTime = LuaCreatureObject(pAdd8)print("luaCreatureObject pointer")
-      spatialChat(pAdd8, "Time to Die!") print("spatial for add")
-      firstTime:engageCombat(pPlayer)print("engaging combat")
-    end
-    
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.50)) then print("checking HAM")
-   -- spatialChat(playerObject, "I don't know where Bill is!") print("spatial")
-    writeData("ISDScreenPlay:spawnAdd9", 1) print("writing data")
-    if (readData("ISDScreenPlay:spawnAdd9") == 1) then print("checking spawnAdd9")
-      local pAdd9 = spawnMobile("dungeon1", "isd_stormtrooper", 180, 9.94771, 173.835, 36.0872, 98, 4336902)
-print("add spawned")
-      local firstTime = LuaCreatureObject(pAdd9)print("luaCreatureObject pointer")
-      spatialChat(pAdd9, "Time to Die!") print("spatial for add")
-      firstTime:engageCombat(pPlayer)print("engaging combat")
-    end
-    
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.45)) then print("checking HAM")
-   -- spatialChat(playerObject, "I don't know where Bill is!") print("spatial")
-    writeData("ISDScreenPlay:spawnAdd10", 1) print("writing data")
-    if (readData("ISDScreenPlay:spawnAdd10") == 1) then print("checking spawnAdd10")
-      local pAdd10 = spawnMobile("dungeon1", "isd_stormtrooper", 180, 19.737, 173.835, 36.0361, 96, 4336902)
-print("add spawned")
-      local firstTime = LuaCreatureObject(pAdd10)print("luaCreatureObject pointer")
-      spatialChat(pAdd10, "Time to Die!") print("spatial for add")
-      firstTime:engageCombat(pPlayer)print("engaging combat")
-    end
-    
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.40)) then print("checking HAM")
-   -- spatialChat(playerObject, "I don't know where Bill is!") print("spatial")
-    writeData("ISDScreenPlay:spawnAdd11", 1) print("writing data")
-    if (readData("ISDScreenPlay:spawnAdd11") == 1) then print("checking spawnAdd11")
-      local pAdd11 = spawnMobile("dungeon1", "isd_stormtrooper", 180, 20.1338, 173.835, 44.9507, 267, 4336902)
-print("add spawned")
-      local firstTime = LuaCreatureObject(pAdd11)print("luaCreatureObject pointer")
-      spatialChat(pAdd11, "Time to Die!") print("spatial for add")
-      firstTime:engageCombat(pPlayer)print("engaging combat")
-    end
-    
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.35)) then print("checking HAM")
-   -- spatialChat(playerObject, "I don't know where Bill is!") print("spatial")
-    writeData("ISDScreenPlay:spawnAdd12", 1) print("writing data")
-    if (readData("ISDScreenPlay:spawnAdd12") == 1) then print("checking spawnAdd12")
-      local pAdd12 = spawnMobile("dungeon1", "isd_stormtrooper", 180, 10.2458, 173.835, 44.9517, 267, 4336902)
-print("add spawned")
-      local firstTime = LuaCreatureObject(pAdd12)print("luaCreatureObject pointer")
-      spatialChat(pAdd12, "Time to Die!") print("spatial for add")
-      firstTime:engageCombat(pPlayer)print("engaging combat")
-    end
-    
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.30)) then print("checking HAM")
-   -- spatialChat(playerObject, "I don't know where Bill is!") print("spatial")
-    writeData("ISDScreenPlay:spawnAdd13", 1) print("writing data")
-    if (readData("ISDScreenPlay:spawnAdd13") == 1) then print("checking spawnAdd13")
-      local pAdd13 = spawnMobile("dungeon1", "isd_stormtrooper", 180, 0.16975, 173.835, 44.841, 264, 4336902)
-print("add spawned")
-      local firstTime = LuaCreatureObject(pAdd13)print("luaCreatureObject pointer")
-      spatialChat(pAdd13, "Time to Die!") print("spatial for add")
-      firstTime:engageCombat(pPlayer)print("engaging combat")
-    end
-    
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.25)) then print("checking HAM")
-   -- spatialChat(playerObject, "I don't know where Bill is!") print("spatial")
-    writeData("ISDScreenPlay:spawnAdd14", 1) print("writing data")
-    if (readData("ISDScreenPlay:spawnAdd14") == 1) then print("checking spawnAdd14")
-      local pAdd14 = spawnMobile("dungeon1", "isd_stormtrooper", 180, -9.88203, 173.835, 44.9118, 270, 4336902)
-print("add spawned")
-      local firstTime = LuaCreatureObject(pAdd14)print("luaCreatureObject pointer")
-      spatialChat(pAdd14, "Time to Die!") print("spatial for add")
-      firstTime:engageCombat(pPlayer)print("engaging combat")
-    end
-    
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.20)) then print("checking HAM")
-    spatialChat(playerObject, "Come on now, do yourselves a favor and just die.") print("spatial")       
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.15)) then print("checking HAM")
-    spatialChat(playerObject, "You're too late, this ship belongs to us!") print("spatial")       
-    return 1
-  
-  end
-  if (health <= (maxHealth * 0.10)) then print("checking HAM")
-    spatialChat(playerObject, "You'll never leave this hangar alive.") print("spatial")       
-    return 1
-  
-  end
-  return 0
+--Phase 2 Of 10 Instance
+function ISDScreenPlay:notify2DroidDead(T2Droid, pKiller)
+	local player = LuaCreatureObject(pKiller)
+        local pBoss = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902) print("Phase 2 Started")
+        return 0
+end
+--Phase 3 Of 10 Instance
+function ISDScreenPlay:notify3DroidDead(T3Droid, pKiller)
+	local player = LuaCreatureObject(pKiller)
+        local pBoss = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902) print("Phase 3 Started")
+        return 0
+end
+--Phase 4 Of 10 Instance
+function ISDScreenPlay:notify4DroidDead(T4Droid, pKiller)
+	local player = LuaCreatureObject(pKiller)
+        local pBoss = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902) print("Phase 4 Started")
+        return 0
+end
+--Phase 5 Of 10 Instance
+function ISDScreenPlay:notify5DroidDead(T5Droid, pKiller)
+	local player = LuaCreatureObject(pKiller)
+        local pBoss = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902) print("Phase 5 Started")
+	spatialChat(pBoss, "Support unit has arrived")
+        return 0
+end
+--Phase 6 Of 10 Instance
+function ISDScreenPlay:notify6DroidDead(T6Droid, pKiller)
+	local player = LuaCreatureObject(pKiller)
+        local pBoss = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902) print("Phase 6 Started")
+        return 0
+end
+--Phase 7 Of 10 Instance
+function ISDScreenPlay:notify7DroidDead(T7Droid, pKiller)
+	local player = LuaCreatureObject(pKiller)
+        local pBoss = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902) print("Phase 7 Started")
+        return 0
+end
+--Phase 8 Of 10 Instance
+function ISDScreenPlay:notify8DroidDead(T8Droid, pKiller)
+	local player = LuaCreatureObject(pKiller)
+        local pBoss = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902) print("Phase 8 Started")
+        return 0
+end
+--Phase 9 Of 10 Instance
+function ISDScreenPlay:notify9DroidDead(T9Droid, pKiller)
+	local player = LuaCreatureObject(pKiller)
+        local pBoss = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902) print("Phase 9 Started")
+        return 0
+end
+--Phase 10 Of 10 Instance
+function ISDScreenPlay:notify10DroidDead(T10Droid, pKiller)
+	local player = LuaCreatureObject(pKiller)
+        local pBoss = spawnMobile("dungeon1", "stormtrooper", 1, 0.188488, 173.835, 27.0266, 338, 4336902) print("Phase 10 Started")
+	spatialChat(pBoss, "Support unit has arrived")
+        return 0
 end
