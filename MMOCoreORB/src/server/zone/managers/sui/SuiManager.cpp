513879fffed008236efa710384d07255dfae6804
TOXIC <thrax989@yahoo.com>
2017-07-02 22:52:17 -0700

    [Added] Lok Spawn points for travel system

--- MMOCoreORB/src/server/zone/managers/sui/SuiManager.cpp
+++ MMOCoreORB/src/server/zone/managers/sui/SuiManager.cpp
@@ -1213,7 +1213,7 @@ void SuiManager::handleCharacterBuilderSelectItem(CreatureObject* player, SuiBox
 				if (!player->isInCombat() && player->getCashCredits() > 4999) {
 		                ManagedReference<SuiMessageBox*> box = new SuiMessageBox(player, SuiWindowType::CITY_ADMIN_CONFIRM_UPDATE_TYPE);
 					player->sendSystemMessage("Thank you for your travels.");
- 				        player->switchZone("lok", 0, 0, 0);
+ 				        player->switchZone("lok", 478.92676, 9, 5511.9565);
  					player->subtractCashCredits(5000);
 					box->setForceCloseDistance(5.f);
 			        }
