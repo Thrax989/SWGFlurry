9a162b84ba9907f57b92f849a5d74a8a6a964753
SlayerSG <lorellian@comcast.net>
2017-07-09 05:18:30 +0000

    Updated Resists to match Comp

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_chest_plate.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_chest_plate.lua
@@ -102,7 +102,7 @@ object_tangible_wearables_armor_stormtrooper_armor_stormtrooper_chest_plate = ob
 
 	specialResists = ELECTRICITY,
 
-	-- These are default Blue Frog stats
+	-- copied from comp
 	healthEncumbrance = 80,
 	actionEncumbrance = 80,
 	mindEncumbrance = 80,
@@ -112,9 +112,25 @@ object_tangible_wearables_armor_stormtrooper_armor_stormtrooper_chest_plate = ob
 	-- LIGHT, MEDIUM, HEAVY
 	rating = LIGHT,
 
+	kinetic = 65,
+	energy = 65,
 	electricity = 80,
-	stun = 60,
+	stun = 25,
+	blast = 65,
+	heat = 65,
+	cold = 65,
+	acid = 65,
 	lightSaber = 0,
+
+	numberExperimentalProperties = {1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 2, 1},
+	experimentalProperties = {"XX", "XX", "XX", "XX", "OQ", "SR", "OQ", "UT", "MA", "OQ", "MA", "OQ", "MA", "OQ", "XX", "XX", "OQ", "SR", "XX"},
+	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	experimentalGroupTitles = {"null", "null", "null", "exp_durability", "exp_quality", "exp_resistance", "exp_durability", "exp_durability", "exp_durability", "null", "null", "exp_resistance", "null"},
+	experimentalSubGroupTitles = {"null", "null", "sockets", "hit_points", "armor_effectiveness", "armor_integrity", "armor_health_encumbrance", "armor_action_encumbrance", "armor_mind_encumbrance", "armor_rating", "armor_special_type", "armor_special_effectiveness", "armor_special_integrity"},
+	experimentalMin = {0, 0, 0, 1000, 1, 30000, 25, 44, 25, 1, 0, 0, 0},
+	experimentalMax = {0, 0, 0, 1000, 40, 50000, 15, 26, 15, 1, 0, 0, 0},
+	experimentalPrecision = {0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0},
+	experimentalCombineType = {0, 0, 4, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1},
 }
 
 ObjectTemplates:addTemplate(object_tangible_wearables_armor_stormtrooper_armor_stormtrooper_chest_plate, "object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_chest_plate.iff")
