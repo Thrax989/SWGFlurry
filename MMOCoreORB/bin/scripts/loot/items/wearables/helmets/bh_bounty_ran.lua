99c4f019fe62c95c88c881aec996c3b0f90505b8
Weezy <Weezyyy@users.noreply.github.com>
2017-07-12 21:29:17 +1000

    Add files via upload
    helmets for testing

---
+++ MMOCoreORB/bin/scripts/loot/items/wearables/helmets/bh_bounty_ran.lua
@@ -0,0 +1,29 @@
+bh_bounty = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "an Elite Ranged Bounty Hunting Helmet",
+	directObjectTemplate = "object/tangible/wearables/armor/bounty_hunter/armor_bounty_hunter_helmet.iff",
+	craftingValues = {
+	         {"armor_rating",2,2,0},
+	         {"armor_effectiveness",0,0,0},
+		     {"restraineffectiveness",20,15,0}, 
+	         {"armor_integrity",20000,30000,0},
+	         {"armor_health_encumbrance",25,15,0},
+	         {"armor_action_encumbrance",22,13,0},
+	         {"armor_mind_encumbrance",30,18,0},
+	},
+	customizationStringNames = {},
+	customizationValues = {},
+	skillMods = 
+	{
+		{"carbine_accuracy",25},
+		{"heavy_rifle_lightning_accuracy",25},
+		{"pistol_accuracy",25},
+		{"ranged_defense",25},
+		{"melee_defense",25},
+		{"shieldduration",10},
+	},
+	certificationsRequired = {"combat_bountyhunter_master"}
+}
+
+addLootItemTemplate("bh_bounty", bh_bounty)
