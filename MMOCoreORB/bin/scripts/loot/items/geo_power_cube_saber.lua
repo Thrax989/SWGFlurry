d4f9a0dc5ad8ba34b25c2cfc8b4499d9b325e745
Weezy <Weezyyy@users.noreply.github.com>
2017-07-14 19:10:03 +1000

    Add files via upload
    possible loot item

---
+++ MMOCoreORB/bin/scripts/loot/items/geo_power_cube_saber.lua
@@ -0,0 +1,14 @@
+geo_power_cube_saber = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "a Lightsaber Resist Cube",
+	directObjectTemplate = "object/tangible/component/weapon/geonosian_power_cube_white.iff",
+	craftingValues = {
+		{"restraineffectiveness",1,2,0},
+		{"useCount",1,2,0},
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("geo_power_cube_saber", geo_power_cube_saber)
