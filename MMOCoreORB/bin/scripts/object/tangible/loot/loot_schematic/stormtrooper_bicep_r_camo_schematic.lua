0fc8aa3859c29de421a24ff78fb29570a37b5a30
SlayerSG <lorellian@comcast.net>
2017-07-15 21:29:07 +0000

    Stormtrooper Armor and Camo loot schematics

--- MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/stormtrooper_bicep_r_camo_schematic.lua
+++ MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/stormtrooper_bicep_r_camo_schematic.lua
@@ -41,16 +41,15 @@
 --this exception also makes it possible to release a modified version 
 
 
-object_tangible_loot_loot_schematic_stormtrooper_bracer_l_schematic = object_tangible_loot_loot_schematic_shared_stormtrooper_bracer_l_schematic:new {
+object_tangible_loot_loot_schematic_stormtrooper_bicep_r_camo_schematic = object_tangible_loot_loot_schematic_shared_stormtrooper_bicep_r_camo_schematic:new {
 	templateType = LOOTSCHEMATIC,
-	customName = "Stormtrooper Armor Left Bracer Schematic",
+	customName = "Camo Stormtrooper Armor Right Bicep Schematic",
 	objectMenuComponent = "LootSchematicMenuComponent",
 	attributeListComponent = "LootSchematicAttributeListComponent",
 	requiredSkill = "crafting_armorsmith_master",
-	targetDraftSchematic = "object/draft_schematic/clothing/shared_clothing_armor_stormtrooper_bracer_l.iff",
-	targetUseCount = 5,
-	noTrade = 1
+	targetDraftSchematic = "object/draft_schematic/clothing/shared_clothing_armor_stormtrooper_bicep_r_camo.iff",
+	targetUseCount = 1
 	
 }
 
-ObjectTemplates:addTemplate(object_tangible_loot_loot_schematic_stormtrooper_bracer_l_schematic, "object/tangible/loot/loot_schematic/stormtrooper_bracer_l_schematic.iff")
+ObjectTemplates:addTemplate(object_tangible_loot_loot_schematic_stormtrooper_bicep_r_camo_schematic, "object/tangible/loot/loot_schematic/stormtrooper_bicep_r_camo_schematic.iff")
