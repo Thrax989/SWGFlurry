487eb57a69c6e395650ad4320130dccf79bf0aa6
Weezy <Weezyyy@users.noreply.github.com>
2017-07-15 16:10:27 +1000

    Add files via upload
    loot added to lg

--- MMOCoreORB/bin/scripts/loot/groups/armor/helmet_lg.lua
+++ MMOCoreORB/bin/scripts/loot/groups/armor/helmet_lg.lua
@@ -3,12 +3,10 @@ helmet_lg = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "bh_bounty", weight = 1750000},
-		{itemTemplate = "bh_mando", weight = 1750000},
-		{itemTemplate = "bh_ris", weight = 1500000},
-		{itemTemplate = "bh_bounty_ran", weight = 1750000},
-		{itemTemplate = "bh_mando_ran", weight = 1750000},
-		{itemTemplate = "bh_ris_ran", weight = 1500000}
+		{itemTemplate = "bh_bounty_ran", weight = 3000000},
+		{itemTemplate = "bh_mando_ran", weight = 3000000},
+		{itemTemplate = "bh_ris_ran", weight = 3000000},
+		{itemTemplate = "geo_power_cube_saber", weight = 1000000}
 	
 	}
 }
