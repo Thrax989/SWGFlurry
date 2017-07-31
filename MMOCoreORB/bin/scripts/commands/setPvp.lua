1a4f76aa4b65b6db4a3fff8a983fdb664630bd1e
kurdtkobain <dbz81487@yahoo.com>
2017-07-31 19:45:12 -0400

    fix old header

--- MMOCoreORB/bin/scripts/commands/setPvp.lua
+++ MMOCoreORB/bin/scripts/commands/setPvp.lua
@@ -1,6 +1,6 @@
 -- PLEASE DO NOT STEAL OUR WORK
 -- ASK BEFOR USING
--- Contact Me Here http://projectphoenix.com.shivtr.com/
+-- Contact Me Here http://swgflurry.com
 -- Created on: 9/21/2016
 -- Authors: Kurdtkobain
 --
