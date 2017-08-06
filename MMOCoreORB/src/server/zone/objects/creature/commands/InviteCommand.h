8ed325afe80536d5ef475ed24e29b42d6ba5d474
TOXIC <thrax989@yahoo.com>
2017-08-05 20:55:25 -0700

    [Re-Added] Global Invites command

--- MMOCoreORB/src/server/zone/objects/creature/commands/InviteCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/InviteCommand.h
@@ -38,7 +38,25 @@ public:
 
 		GroupManager* groupManager = GroupManager::instance();
 
-		ManagedReference<SceneObject*> object = server->getZoneServer()->getObject(target);
+		ManagedReference<SceneObject*> object = NULL;
+		if (target != 0 && target != creature->getObjectID())
+			object = server->getZoneServer()->getObject(target);
+		else if (!arguments.isEmpty()) {
+			StringTokenizer tokenizer(arguments.toString());
+			if (tokenizer.hasMoreTokens()) {
+				String name;
+				tokenizer.getStringToken(name);
+				name = name.toLowerCase();
+				if (name != "self" && name != "this") {
+					try {
+						object = server->getPlayerManager()->getPlayer(name);
+					} catch (ArrayIndexOutOfBoundsException& ex) {
+						// this happens if the player wasn't found
+					}
+				}
+			}
+		}
+
 
 		if (object == NULL)
 			return GENERALERROR;
@@ -57,4 +75,3 @@ public:
 };
 
 #endif //INVITECOMMAND_H_
-
