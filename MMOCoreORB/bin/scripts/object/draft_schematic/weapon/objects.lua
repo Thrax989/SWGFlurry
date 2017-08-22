a586508bdf76ae84650c01c8bf791cafe0a501c3
SlayerSG <lorellian@comcast.net>
2017-08-21 20:14:13 -0400

    Update objects.lua
    disable Blasterfist

--- MMOCoreORB/bin/scripts/object/draft_schematic/weapon/objects.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/weapon/objects.lua
@@ -4108,11 +4108,9 @@ object_draft_schematic_weapon_shared_turbo_laser = SharedDraftSchematicObjectTem
 
 ObjectTemplates:addClientTemplate(object_draft_schematic_weapon_shared_turbo_laser, "object/draft_schematic/weapon/shared_turbo_laser.iff")
 
-object_draft_schematic_weapon_shared_blasterfist = SharedDraftSchematicObjectTemplate:new {
-clientTemplateFileName = "object/draft_schematic/weapon/shared_blasterfist.iff"
-}
-
-ObjectTemplates: addClientTemplate(object_draft_schematic_weapon_shared_blasterfist, "object/draft_schematic/weapon/shared_blasterfist.iff")
+--object_draft_schematic_weapon_shared_blasterfist = SharedDraftSchematicObjectTemplate:new {
+--clientTemplateFileName = "object/draft_schematic/weapon/shared_blasterfist.iff"}
+--ObjectTemplates: addClientTemplate(object_draft_schematic_weapon_shared_blasterfist, "object/draft_schematic/weapon/shared_blasterfist.iff")
 
 object_draft_schematic_weapon_shared_carbine_underslung_blast = SharedDraftSchematicObjectTemplate:new {
 clientTemplateFileName = "object/draft_schematic/weapon/shared_carbine_underslung_blast.iff"
