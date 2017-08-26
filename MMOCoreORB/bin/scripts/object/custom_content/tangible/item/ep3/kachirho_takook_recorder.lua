7a011efc536a98c9537923b45d6ef963ec27e9d2
TOXIC <thrax989@yahoo.com>
2017-08-25 20:34:26 -0700

    Revert "[Added] Template For Kachiro."
    This reverts commit ded1bd2cb89d2258c25bf7d087daf3ec9776b9d6.

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/item/ep3/kachirho_takook_recorder.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/item/ep3/kachirho_takook_recorder.lua
@@ -1,20 +1,5 @@
 
 object_tangible_item_ep3_kachirho_takook_recorder = object_tangible_item_ep3_shared_kachirho_takook_recorder:new {
-	--templateType = TRAP,
-	objectMenuComponent = "DnaExtractorMenuComponent",
-
-	useCount = 12,
-	effectiveness = 112,
-	attributes = {0, 3, 6},
-	
-	numberExperimentalProperties = {1, 1, 4, 1},
-	experimentalProperties = {"XX", "XX", "OQ", "MA", "DR", "PE", "XX"},
-	experimentalWeights = {1, 1, 1, 1, 1, 1, 1},
-	experimentalGroupTitles = {"null", "null", "exp_charges", "null"},
-	experimentalSubGroupTitles = {"null", "null", "charges", "hitpoints"},
-	experimentalMin = {0, 0, 1, 1000},
-	experimentalMax = {0, 0, 20, 1000},
-	experimentalPrecision = {0, 0, 0, 0},
-	experimentalCombineType = {0, 0, 1, 4},
+objectMenuComponent = "DnaExtractorMenuComponent",
 }
 ObjectTemplates:addTemplate(object_tangible_item_ep3_kachirho_takook_recorder, "object/tangible/item/ep3/kachirho_takook_recorder.iff")
