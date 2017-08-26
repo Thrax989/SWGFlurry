6d0b59da49e23727315d289f88c20173e2569aeb
TOXIC <thrax989@yahoo.com>
2017-08-25 19:48:34 -0700

    [Added] Kachirho Takook DNA Recorder

---
+++ MMOCoreORB/src/server/zone/objects/tangible/components/DnaExtractorMenuComponent.h
@@ -0,0 +1,14 @@
+#ifndef DNAEXTRACTORMENUCOMPONENT_H_
+#define DNAEXTRACTORMENUCOMPONENT_H_
+
+#include "TangibleObjectMenuComponent.h"
+#include "server/zone/packets/scene/AttributeListMessage.h"
+
+class DnaExtractorMenuComponent : public TangibleObjectMenuComponent {
+protected:
+public:
+	virtual void fillObjectMenuResponse(SceneObject* sceneObject, ObjectMenuResponse* menuResponse, CreatureObject* player) const;
+	virtual int handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* player, byte selectedID) const;
+};
+
+#endif /* DNAEXTRACTORMENUCOMPONENT_H_ */
