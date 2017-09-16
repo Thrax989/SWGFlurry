534d920d487c041064bbdbad4e5d3f65e1f1b884
TOXIC <thrax989@yahoo.com>
2017-09-15 18:04:01 -0700

    [Added] Created New Artifact Component For Custom Looted Tier 1-3 Artifacts.

---
+++ MMOCoreORB/src/server/zone/objects/tangible/components/ArtifactMenuComponent.cpp
@@ -0,0 +1,41 @@
+/*
+ * ArtifactMenuComponent.cpp
+ *
+ *  Created on: 09/15/2017
+ *      Author: TOXIC
+ */
+
+#include "HolocronMenuComponent.h"
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/objects/scene/SceneObject.h"
+#include "server/zone/objects/player/PlayerObject.h"
+#include "server/zone/ZoneServer.h"
+#include "server/zone/managers/jedi/JediManager.h"
+#include "server/chat/ChatManager.h"
+#include "server/zone/packets/player/PlayMusicMessage.h"
+#include "server/zone/managers/visibility/VisibilityManager.h"
+#include "server/zone/objects/player/sui/inputbox/SuiInputBox.h"
+#include "server/zone/managers/loot/LootManager.h"
+#include "server/zone/managers/player/PlayerManager.h"
+#include "server/chat/ChatManager.h"
+#include "server/zone/packets/player/PlayMusicMessage.h"
+
+void ForceBuffMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, ObjectMenuResponse* menuResponse, CreatureObject* player) const {
+
+	TangibleObjectMenuComponent::fillObjectMenuResponse(sceneObject, menuResponse, player);
+
+	menuResponse->addRadialMenuItem(20, 3, "Reveal Treasure");
+}
+
+int ArtifactMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* creature, byte selectedID) const {
+
+	if (selectedID != 20)
+		return 0;
+
+ 	ManagedReference<SceneObject*> inventory = creature->getSlottedObject("inventory");
+ 	ManagedReference<LootManager*> lootManager = creature->getZoneServer()->getLootManager();
+	lootManager->createLoot(inventory, "artifact", 300);
+	creature->setPosture(CreaturePosture::UPRIGHT);
+	sceneObject->destroyObjectFromWorld(true);
+	return 0;
+}
