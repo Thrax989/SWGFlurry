63035258a4e15e36c8fb5950216a3e66c7266c63
TOXIC <thrax989@yahoo.com>
2017-09-19 16:44:32 -0700

    [Added] Colection Items

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/furniture/terminal/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/furniture/terminal/serverobjects.lua
@@ -7,3 +7,5 @@ includeFile("custom_content/tangible/furniture/terminal/terminal_bank_wall_off_0
 includeFile("custom_content/tangible/furniture/terminal/terminal_bank_wall_off_02.lua")
 includeFile("custom_content/tangible/furniture/terminal/terminal_bank_wall_on_01.lua")
 includeFile("custom_content/tangible/furniture/terminal/terminal_bank_wall_on_02.lua")
+includeFile("custom_content/tangible/furniture/terminal/terminal_mini_vehicle.lua")
+includeFile("custom_content/tangible/furniture/terminal/terminal_serving_droid.lua")
