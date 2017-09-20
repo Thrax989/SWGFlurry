0ec119e7d1eebebda93508c4000c21164f875b80
TOXIC <thrax989@yahoo.com>
2017-09-20 00:27:54 -0400

    [Updated] Xp Component

--- MMOCoreORB/src/server/zone/objects/tangible/components/XpPlayerMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/XpPlayerMenuComponent.cpp
@@ -1,15 +1,33 @@
 /*
- * XpPlayerMenuComponent.cpp
+ * XpMenuComponent.cpp
  *
  *  Created on: 09/19/2017
  *      Author: TOXIC
  */
 
-#include "XpPlayerMenuComponent.h"
+#include "XpMenuComponent.h"
 #include "server/zone/objects/creature/CreatureObject.h"
 #include "server/zone/objects/scene/SceneObject.h"
+#include "server/zone/objects/player/PlayerObject.h"
+#include "server/zone/ZoneServer.h"
+#include "server/zone/managers/jedi/JediManager.h"
+#include "server/chat/ChatManager.h"
+#include "server/zone/packets/player/PlayMusicMessage.h"
+#include "server/zone/managers/visibility/VisibilityManager.h"
+#include "server/zone/objects/player/sui/inputbox/SuiInputBox.h"
+#include "server/zone/managers/loot/LootManager.h"
+#include "server/zone/managers/player/PlayerManager.h"
+#include "server/chat/ChatManager.h"
+#include "server/zone/packets/player/PlayMusicMessage.h"
+#include "server/zone/packets/object/ObjectMenuResponse.h"
 
-int XpPlayerMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* creature, byte selectedID) const {
+void XpMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, ObjectMenuResponse* menuResponse, CreatureObject* player) const {
+
+	TangibleObjectMenuComponent::fillObjectMenuResponse(sceneObject, menuResponse, player);
+}
+
+int XpMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* creature, byte selectedID) const {
+
 	if (selectedID != 20)
 		return 0;
 
