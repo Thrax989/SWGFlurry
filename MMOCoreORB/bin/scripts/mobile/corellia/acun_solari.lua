753268701b94c75e5411777b7ecb4e0e8abadf28
TOXIC <thrax989@yahoo.com>
2017-09-23 15:41:52 -0700

    [Added] Artifact loot groups along with Collection tier 1,2,3 loot crates (Corellia Planet)

--- MMOCoreORB/bin/scripts/mobile/corellia/acun_solari.lua
+++ MMOCoreORB/bin/scripts/mobile/corellia/acun_solari.lua
@@ -27,7 +27,34 @@ acun_solari = Creature:new {
 	diet = HERBIVORE,
 
 	templates = {"object/mobile/dressed_rebel_army_captain_bothan_male.iff"},
-	lootGroups = {},
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 10000000}				},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "artifact", chance = 10000000}			},
+			lootChance = 1500000
+		},
+		{
+			groups = {
+				{group = "tierone", chance = 10000000}			},
+			lootChance = 1500000
+		},
+		{
+			groups = {
+				{group = "tiertwo", chance = 10000000}
+		},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "tierthree", chance = 10000000}			},
+			lootChance = 500000
+		}
+	},
 	weapons = {},
 	conversationTemplate = "",
 	attacks = merge(brawlernovice,marksmannovice)
--- MMOCoreORB/bin/scripts/mobile/corellia/bageraset_bruiser.lua
+++ MMOCoreORB/bin/scripts/mobile/corellia/bageraset_bruiser.lua
@@ -27,7 +27,34 @@ bageraset_bruiser = Creature:new {
 
 	templates = {"object/mobile/bageraset_hue.iff"},
 	scale = 1.2,
-	lootGroups = {},
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 10000000}				},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "artifact", chance = 10000000}			},
+			lootChance = 1500000
+		},
+		{
+			groups = {
+				{group = "tierone", chance = 10000000}			},
+			lootChance = 1500000
+		},
+		{
+			groups = {
+				{group = "tiertwo", chance = 10000000}
+		},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "tierthree", chance = 10000000}			},
+			lootChance = 500000
+		}
+	},
 	weapons = {"creature_spit_small_yellow"},
 	conversationTemplate = "",
 	attacks = {
