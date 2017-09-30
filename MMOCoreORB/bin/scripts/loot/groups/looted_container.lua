88a08dd1bb5ad3eaee4e5bbbcaf4208d1a2e791f
SlayerSG <lorellian@comcast.net>
2017-09-29 23:52:33 -0400

    Update looted_container.lua
    typo

--- MMOCoreORB/bin/scripts/loot/groups/looted_container.lua
+++ MMOCoreORB/bin/scripts/loot/groups/looted_container.lua
@@ -40,7 +40,7 @@ looted_container = {
 		{itemTemplate = "attachment_armor", weight = 43860},
 
 		-- Rugs (25% chance)
-		{groupTemplate = "crafted_rugs", weight = 200000},
+		{groupTemplate = "crafted_rugs", weight = 500000},
 		{groupTemplate = "neg_house_loot_deed_01", weight = 1000000},
 		{groupTemplate = "neg_house_loot_deed_windowed", weight = 1000000},
 
