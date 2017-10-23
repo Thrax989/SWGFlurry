5e66f25e5e10196d1ebc69462b5b57b40515286e
TOXIC <thrax989@yahoo.com>
2017-10-23 05:44:42 -0700

    [Added] Updated server includes

--- MMOCoreORB/bin/scripts/screenplays/custom_screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/custom_screenplays/screenplays.lua
@@ -37,71 +37,28 @@ includeFile("custom_screenplays/dagobah/dagobah_yoda_house.lua")
 
 
 --Dungeon
---includeFile("custom_screenplays/dungeon/exarkunteleporter.lua")
---includeFile("custom_screenplays/dungeon/exarkunreturn.lua")
---includeFile("custom_screenplays/dungeon/nightsisterteleporter.lua")
---includeFile("custom_screenplays/dungeon/nightsisterreturn.lua")
---includeFile("custom_screenplays/dungeon/igteleporter.lua")
---includeFile("custom_screenplays/dungeon/igreturn.lua")
---includeFile("custom_screenplays/dungeon/avatarteleporter.lua")
---includeFile("custom_screenplays/dungeon/avatarreturn.lua")
---includeFile("custom_screenplays/dungeon/avatarplatform.lua")
---includeFile("custom_screenplays/dungeon/isdteleporter.lua")
---includeFile("custom_screenplays/dungeon/isdreturn.lua")
+includeFile("custom_screenplays/dungeon/exarkunteleporter.lua")
+includeFile("custom_screenplays/dungeon/exarkunreturn.lua")
+includeFile("custom_screenplays/dungeon/nightsisterteleporter.lua")
+includeFile("custom_screenplays/dungeon/nightsisterreturn.lua")
+includeFile("custom_screenplays/dungeon/igteleporter.lua")
+includeFile("custom_screenplays/dungeon/igreturn.lua")
+includeFile("custom_screenplays/dungeon/avatarteleporter.lua")
+includeFile("custom_screenplays/dungeon/avatarreturn.lua")
+includeFile("custom_screenplays/dungeon/avatarplatform.lua")
+includeFile("custom_screenplays/dungeon/isdteleporter.lua")
+includeFile("custom_screenplays/dungeon/isdreturn.lua")
 includeFile("custom_screenplays/dungeon/echobaseteleporter.lua")
 includeFile("custom_screenplays/dungeon/echobasereturn.lua")
---includeFile("custom_screenplays/dungeon/myyydrilteleporter.lua")
+includeFile("custom_screenplays/dungeon/myyydrilteleporter.lua")
 includeFile("custom_screenplays/dungeon/myyydrilreturn.lua")
 includeFile("custom_screenplays/dungeon/necrosisspawn.lua")
---includeFile("custom_screenplays/dungeon/imperialstardestroyer.lua")
---includeFile("custom_screenplays/dungeon/exarkuntomb.lua")
+includeFile("custom_screenplays/dungeon/imperialstardestroyer.lua")
+includeFile("custom_screenplays/dungeon/exarkuntomb.lua")
 includeFile("custom_screenplays/dungeon/exarkuntemple.lua")
---includeFile("custom_screenplays/dungeon/ig88.lua")
+includeFile("custom_screenplays/dungeon/ig88.lua")
 includeFile("custom_screenplays/dungeon/hothechobase.lua")
 includeFile("custom_screenplays/dungeon/hothechobaseelevators.lua")
-
-
-includeFile("custom_screenplays/instances/avatar/conversations/avatarfred_convo_handler.lua")
-includeFile("custom_screenplays/instances/avatar/avatar_instance.lua")
-includeFile("custom_screenplays/instances/avatar/avatarSpawnMaps.lua")
-
-includeFile("custom_screenplays/instances/exar_kun/conversations/exarkunfred_convo_handler.lua")
-includeFile("custom_screenplays/instances/exar_kun/exar_kun_instance.lua")
-includeFile("custom_screenplays/instances/exar_kun/exarkunSpawnMaps.lua")
-
-includeFile("custom_screenplays/instances/axkva_min/conversations/sascha_convo_handler.lua")
-includeFile("custom_screenplays/instances/axkva_min/axkva_min_instance.lua")
-includeFile("custom_screenplays/instances/axkva_min/axkvaminSpawnMaps.lua")
-
---includeFile("custom_screenplays/instances/decrepit_droid_factory/conversations/ddffred_convo_handler.lua")
---includeFile("custom_screenplays/instances/decrepit_droid_factory/decrepit_droid_factory_instance.lua")
---includeFile("custom_screenplays/instances/decrepit_droid_factory/ddfSpawnMaps.lua")
-
-includeFile("custom_screenplays/instances/ig_88/conversations/igfred_convo_handler.lua")
-includeFile("custom_screenplays/instances/ig_88/ig_88_instance.lua")
-includeFile("custom_screenplays/instances/ig_88/ig88SpawnMaps.lua")
-
-includeFile("custom_screenplays/instances/isd/conversations/isdfred_convo_handler.lua")
-includeFile("custom_screenplays/instances/isd/isd_instance.lua")
-includeFile("custom_screenplays/instances/isd/isdSpawnMaps.lua")
-
---includeFile("custom_screenplays/instances/lava_cave/conversations/lavacavefred_convo_handler.lua")
---includeFile("custom_screenplays/instances/lava_cave/lava_cave_instance.lua")
---includeFile("custom_screenplays/instances/lava_cave/lavacaveSpawnMaps.lua")
-
---includeFile("custom_screenplays/instances/old_republic_facility/conversations/orffred_convo_handler.lua")
---includeFile("custom_screenplays/instances/old_republic_facility/old_republic_facility_instance.lua")
---includeFile("custom_screenplays/instances/old_republic_facility/orfSpawnMaps.lua")
-
-includeFile("custom_screenplays/instances/sher_kar/conversations/leicia_convo_handler.lua")
-includeFile("custom_screenplays/instances/sher_kar/sher_kar_instance.lua")
-includeFile("custom_screenplays/instances/sher_kar/sherkarSpawnMaps.lua")
-
---includeFile("custom_screenplays/instances/working_droid_factory/conversations/saschail_convo_handler.lua")
---includeFile("custom_screenplays/instances/working_droid_factory/conversations/sevi_defo_convo_handler.lua")
---includeFile("custom_screenplays/instances/working_droid_factory/sevi_defo.lua")
---includeFile("custom_screenplays/instances/working_droid_factory/working_droid_factory_instance.lua")
---includeFile("custom_screenplays/instances/working_droid_factory/wdfactorySpawnMaps.lua")
 
 
 --Endor
@@ -152,9 +109,9 @@ includeFile("custom_screenplays/kashyyyk/kashyyykthreeleveltwoteleporter.lua")
 includeFile("custom_screenplays/mustafar/hk_army_teleport.lua")
 includeFile("custom_screenplays/mustafar/hk_army_return.lua")
 includeFile("custom_screenplays/mustafar/hk_army.lua")
---includeFile("custom_screenplays/mustafar/sherkar_teleport.lua")
---includeFile("custom_screenplays/mustafar/sherkar_return.lua")
---includeFile("custom_screenplays/mustafar/sherkar_cave.lua")
+includeFile("custom_screenplays/mustafar/sherkar_teleport.lua")
+includeFile("custom_screenplays/mustafar/sherkar_return.lua")
+includeFile("custom_screenplays/mustafar/sherkar_cave.lua")
 
 
 
@@ -162,7 +119,7 @@ includeFile("custom_screenplays/mustafar/hk_army.lua")
 
 --NalHutta
 includeFile("custom_screenplays/nalhutta/nalhutta_huttcity.lua")
---includeFile("custom_screenplays/nalhutta/nalhutta_volcano.lua")
+includeFile("custom_screenplays/nalhutta/nalhutta_volcano.lua")
 
 
 
