6626485e6ec20e00d8a1a43779b808c4eb3744b1
TOXIC <thrax989@yahoo.com>
2017-10-23 19:31:28 -0700

    [Added] diner test 2

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/deed/player_house_deed/diner_house_deed.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/deed/player_house_deed/diner_house_deed.lua
@@ -1,5 +1,7 @@
 object_tangible_deed_player_house_deed_diner_house_deed = object_tangible_deed_player_house_deed_shared_diner_house_deed:new {
-
+	templateType = STRUCTUREDEED,
+	placeStructureComponent = "PlaceStructureComponent",
+	generatedObjectTemplate = "object/building/player/diner_house.iff",
 }
 
 ObjectTemplates:addTemplate(object_tangible_deed_player_house_deed_diner_house_deed, "object/tangible/deed/player/house/deed/diner_house_deed.iff")
