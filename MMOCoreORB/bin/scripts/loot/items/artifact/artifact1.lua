9878c57893b2474803f750fd508b490bf717a05e
TOXIC <thrax989@yahoo.com>
2017-11-14 15:18:37 -0800

    [Updated] Artifact create

--- MMOCoreORB/bin/scripts/loot/items/artifact/artifact1.lua
+++ MMOCoreORB/bin/scripts/loot/items/artifact/artifact1.lua
@@ -1,8 +1,8 @@
 artifact1 = {
 	minimumLevel = 0,
 	maximumLevel = -1,
-	customObjectName = "Artifact 1",
-	directObjectTemplate = "object/tangible/loot/npc/loot/answering_machine_generic.iff",
+	customObjectName = "Artifact Crate",
+	directObjectTemplate = "object/tangible/item/loot_crates/base_crate.iff",
 	craftingValues = {
 	},
 	customizationStringNames = {},
