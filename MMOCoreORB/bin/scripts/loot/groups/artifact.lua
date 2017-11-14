a8b4d291696824f5edfa46ad8502f0a15255fda9
TOXIC <thrax989@yahoo.com>
2017-11-14 15:17:54 -0800

    [Updated] Artifact loot group

--- MMOCoreORB/bin/scripts/loot/groups/artifact.lua
+++ MMOCoreORB/bin/scripts/loot/groups/artifact.lua
@@ -5,17 +5,7 @@ artifact = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "junk", weight = 9999990},
-		{itemTemplate = "artifact1", weight = 1},
-		{itemTemplate = "artifact2", weight = 1},
-		{itemTemplate = "artifact3", weight = 1},
-		{itemTemplate = "artifact4", weight = 1},
-		{itemTemplate = "artifact5", weight = 1},
-		{itemTemplate = "artifact6", weight = 1},
-		{itemTemplate = "artifact7", weight = 1},
-		{itemTemplate = "artifact8", weight = 1},
-		{itemTemplate = "artifact9", weight = 1},
-		{itemTemplate = "artifact10", weight = 1},
+		{itemTemplate = "artifact1", weight = 10000000},
 	}
 }
 
