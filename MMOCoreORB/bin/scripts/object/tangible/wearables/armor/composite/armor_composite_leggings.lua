9684a72ea27c2cd54c2516805b01681d3f8231c2
TOXIC <thrax989@yahoo.com>
2017-11-19 21:16:52 -0800

    [Fixed] 1 sever load error

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_leggings.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_leggings.lua
@@ -47,7 +47,6 @@ object_tangible_wearables_armor_composite_armor_composite_leggings = object_tang
 
 	objectMenuComponent = "ArmorObjectMenuComponent",
 
-	playerRaces = { "object/creature/player/bothan_male.iff",
 	playerRaces = { "object/creature/player/abyssin_male.iff",
 				"object/creature/player/abyssin_female.iff",
 				"object/mobile/vendor/abyssin_male.iff",
