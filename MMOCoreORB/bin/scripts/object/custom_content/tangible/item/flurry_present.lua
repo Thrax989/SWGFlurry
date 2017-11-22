9fa3e4b49c4e5d1d5bd0c9c2315eaf8c03602454
slayersg <lorellian@comcast.net>
2017-11-22 00:26:03 -0500

    Diamond and Xmas Loot groups and items,
    [Fixed] Mobiles
    
    Change-Id: I29a294603193d0d50026355a8ba894f5e118a1b5

---
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/item/flurry_present.lua
@@ -0,0 +1,4 @@
+object_tangible_item_loot_crates_flurry_present = object_tangible_item_loot_crates_shared_flurry_present:new {
+		objectMenuComponent = "CollectionflurrypresentMenuComponent",
+}
+ObjectTemplates: addTemplate(object_tangible_item_loot_crates_flurry_present, "object/tangible/item/loot_crates/flurry_present.iff")
