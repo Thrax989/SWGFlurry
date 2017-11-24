5178d7871bd93c6663521203e6027db9e6162c53
TOXIC <thrax989@yahoo.com>
2017-11-24 04:03:20 -0800

    [Update]d Artifact broken loot

--- MMOCoreORB/bin/scripts/loot/items/artifact/artifactreward35.lua
+++ MMOCoreORB/bin/scripts/loot/items/artifact/artifactreward35.lua
@@ -2,7 +2,7 @@ artifactreward35 = {
 	minimumLevel = 0,
 	maximumLevel = -1,
 	customObjectName = "Artifact 35",
-	directObjectTemplate = "object/tangible/collection/col_gcw_city_reb_4.iff",
+	directObjectTemplate = "object/tangible/collection/col_tatooine_vistas_marker_01.iff",
 	craftingValues = {
 	},
 	customizationStringNames = {},
