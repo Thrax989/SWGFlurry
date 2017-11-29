9990bddfad39642cae626b7bbbf7bcfa9e0829e2
TOXIC <thrax989@yahoo.com>
2017-11-28 23:06:36 -0800

    [Added] Custom teleport points for new boss item

--- MMOCoreORB/src/server/zone/objects/tangible/components/BossMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/BossMenuComponent.cpp
@@ -30,14 +30,14 @@ void BossMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, ObjectM
 		menuResponse->addRadialMenuItem(216, 3, "Mini Boss 4"); //MINI BOSS ENCOUNTER 4
 		menuResponse->addRadialMenuItem(217, 3, "Mini Boss 5"); //MINI BOSS ENCOUNTER 5
 		menuResponse->addRadialMenuItem(220, 3, "Heroic Boss Instances");
-		menuResponse->addRadialMenuItemToRadialID(220, 221, 3, "BOSS 1"); //BOSS ENCOUNTER 1
-		menuResponse->addRadialMenuItemToRadialID(220, 222, 3, "BOSS 2"); //BOSS ENCOTUNER 2
-		menuResponse->addRadialMenuItemToRadialID(220, 223, 3, "BOSS 3"); //BOSS ENCOUNTER 3
-		menuResponse->addRadialMenuItemToRadialID(220, 224, 3, "BOSS 4"); //BOSS ENCOUNTER 4
-		menuResponse->addRadialMenuItemToRadialID(220, 225, 3, "BOSS 5"); //BOSS ENCOUNTER 5
-		menuResponse->addRadialMenuItemToRadialID(220, 226, 3, "BOSS 6"); //BOSS ENCOUNTER 6
-		menuResponse->addRadialMenuItemToRadialID(220, 227, 3, "BOSS 7"); //BOSS ENCOUNTER 7
-		menuResponse->addRadialMenuItemToRadialID(220, 228, 3, "BOSS 8"); //BOSS ENCOUNTER 8
+		menuResponse->addRadialMenuItemToRadialID(220, 221, 3, "Exar Kun Tomb"); //BOSS ENCOUNTER 1
+		menuResponse->addRadialMenuItemToRadialID(220, 222, 3, "Avatar Platform"); //BOSS ENCOTUNER 2
+		menuResponse->addRadialMenuItemToRadialID(220, 223, 3, "Hoth Echo Base"); //BOSS ENCOUNTER 3
+		menuResponse->addRadialMenuItemToRadialID(220, 224, 3, "IG-88"); //BOSS ENCOUNTER 4
+		menuResponse->addRadialMenuItemToRadialID(220, 225, 3, "Imperial Star Destroyer"); //BOSS ENCOUNTER 5
+		menuResponse->addRadialMenuItemToRadialID(220, 226, 3, "Myyydril"); //BOSS ENCOUNTER 6
+		menuResponse->addRadialMenuItemToRadialID(220, 227, 3, "NightSister Queen"); //BOSS ENCOUNTER 7
+		menuResponse->addRadialMenuItemToRadialID(220, 228, 3, "Rogue Star Destroyer"); //BOSS ENCOUNTER 8
 }
 int BossMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* creature, byte selectedID) const {
 	if (selectedID == 213) {
@@ -67,42 +67,42 @@ int BossMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, Creature
 	}
 	if (selectedID == 221) {
 		creature->sendSystemMessage("Prepair for the boss fight!.");
-		creature->switchZone("corellia", 0, 0, 0);
+		creature->switchZone("dungeon2", -11.5, 0.2, -121.8, 14200872)
 		sceneObject->destroyObjectFromWorld(true);
 	}
 	if (selectedID == 222) {
 		creature->sendSystemMessage("Prepair for the boss fight!.");
-		creature->switchZone("corellia", 0, 0, 0);
+		creature->switchZone("dungeon2", 6193, 250, -5978, 0)
 		sceneObject->destroyObjectFromWorld(true);
 	}
 	if (selectedID == 223) {
 		creature->sendSystemMessage("Prepair for the boss fight!.");
-		creature->switchZone("corellia", 0, 0, 0);
+		creature->switchZone("dungeon2", 89.1, -14.7, 300.1, 14201134)
 		sceneObject->destroyObjectFromWorld(true);
 	}
 	if (selectedID == 224) {
 		creature->sendSystemMessage("Prepair for the boss fight!.");
-		creature->switchZone("corellia", 0, 0, 0);
+		creature->switchZone("dungeon2", 0.1, 0.0, 42.2, 14200863)
 		sceneObject->destroyObjectFromWorld(true);
 	}
 	if (selectedID == 225) {
 		creature->sendSystemMessage("Prepair for the boss fight!.");
-		creature->switchZone("corellia", 0, 0, 0);
+		creature->switchZone("dungeon2", 13.9, 173.8, 14.6, 14201198)
 		sceneObject->destroyObjectFromWorld(true);
 	}
 	if (selectedID == 226) {
 		creature->sendSystemMessage("Prepair for the boss fight!.");
-		creature->switchZone("corellia", 0, 0, 0);
+		creature->switchZone("kashyyyk", -3.8, -1.1, -4.2, 281474997391073)
 		sceneObject->destroyObjectFromWorld(true);
 	}
 	if (selectedID == 227) {
 		creature->sendSystemMessage("Prepair for the boss fight!.");
-		creature->switchZone("corellia", 0, 0, 0);
+		creature->switchZone("dungeon2", 19.3, 0.1, 0.5, 14201104)
 		sceneObject->destroyObjectFromWorld(true);
 	}
 	if (selectedID == 228) {
 		creature->sendSystemMessage("Prepair for the boss fight!.");
-		creature->switchZone("corellia", 0, 0, 0);
+		creature->switchZone("dungeon1", 7, 172.3, 300, 4336867)
 		sceneObject->destroyObjectFromWorld(true);
 	}
 	return 0;
