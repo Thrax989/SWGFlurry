cd32a1584e09f9346d545602661a47780d198f12
TOXIC <thrax989@yahoo.com>
2017-12-01 21:41:21 -0800

    [Added] Wampa baby missions to Hoth

--- MMOCoreORB/bin/scripts/mobile/custom_content/lair/creature_lair/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/custom_content/lair/creature_lair/serverobjects.lua
@@ -1,4 +1,5 @@
 includeFile("custom_content/lair/creature_lair/taanab_nerf_lair.lua")
 includeFile("custom_content/lair/creature_lair/taanab_nerf_grassland_lair.lua")
 includeFile("custom_content/lair/creature_lair/taanab_ambrian_staga_lair.lua")
+includeFile("custom_content/lair/creature_lair/hoth_wampa_lair.lua")
 
