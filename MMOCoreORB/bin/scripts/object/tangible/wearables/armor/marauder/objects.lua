843df0eaae996415d9722d6f7e7fbc2b457df5ef
Weezy <Weezyyy@users.noreply.github.com>
2017-12-03 04:36:08 +1100

    Update objects.lua

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/marauder/objects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/marauder/objects.lua
@@ -1636,14 +1636,12 @@ ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_marauder_share
 
 object_tangible_wearables_armor_marauder_shared_armor_marauder_s02_gloves = SharedTangibleObjectTemplate:new {
 	clientTemplateFileName = "object/tangible/wearables/armor/marauder/shared_armor_marauder_s02_gloves.iff"
-
 }
 
 ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_marauder_shared_armor_marauder_s02_gloves, "object/tangible/wearables/armor/marauder/shared_armor_marauder_s02_gloves.iff")
 
 object_tangible_wearables_armor_marauder_shared_armor_marauder_s02_helmet = SharedTangibleObjectTemplate:new {
 	clientTemplateFileName = "object/tangible/wearables/armor/marauder/shared_armor_marauder_s02_helmet.iff"
-
 }
 
 ObjectTemplates:addClientTemplate(object_tangible_wearables_armor_marauder_shared_armor_marauder_s02_helmet, "object/tangible/wearables/armor/marauder/shared_armor_marauder_s02_helmet.iff")
