3aaa436a575779ddb9b978c3c8dd7f18659502d7
Weezy <Weezyyy@users.noreply.github.com>
2017-12-03 03:53:18 +1100

    Add files via upload
    loot group for marauder schems

---
+++ MMOCoreORB/bin/scripts/loot/groups/marauder_armor_schems.lua
@@ -0,0 +1,23 @@
+marauder_armor_schems = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "maraud_chest_plate_quest_schematic", weight = 1000000},
+		{itemTemplate = "maraud_bicep_l_schematic", weight = 1000000},
+		{itemTemplate = "maraud_bicep_r_schematic", weight = 1000000},
+		{itemTemplate = "maraud_boots_schematic", weight = 1000000},
+		{itemTemplate = "maraud_bracer_l_schematic", weight = 1000000},
+		{itemTemplate = "maraud_bracer_r_schematic", weight = 1000000},
+		{itemTemplate = "maraud_chest_plate_schematic", weight = 1000000},
+		{itemTemplate = "maraud_gloves_schematic", weight = 1000000},
+		{itemTemplate = "maraud_leggings_schematic", weight = 1000000},
+		{itemTemplate = "maraud_helmet_schematic", weight = 1000000}
+	},
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootGroupTemplate("marauder_armor_schems", marauder_armor_schems)
