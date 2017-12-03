a7c8dd70719ad21cc52677564958dbcc28f37613
Weezy <Weezyyy@users.noreply.github.com>
2017-12-03 20:47:22 +1100

    Add files via upload
    updated stats and loot groups

--- MMOCoreORB/bin/scripts/mobile/yavin4/yavin_world_boss/custom_exar_kun_boss.lua
+++ MMOCoreORB/bin/scripts/mobile/yavin4/yavin_world_boss/custom_exar_kun_boss.lua
@@ -1,18 +1,17 @@
 custom_exar_kun_boss = Creature:new {
-	--objectName = "@mob/creature_names:",
-	customName = "<<Shard of Exar Kun>>",
+	customName = "The Infamous Exar Kun",
 	socialGroup = "",
 	pvpFaction = "",
 	faction = "",
 	level = 300,
-	chanceHit = 45.00,
+	chanceHit = 35.00,
 	damageMin = 1750,
 	damageMax = 3750,
 	baseXp = 13273,
-	baseHAM = 1000000,
-	baseHAMmax = 1500000,
+	baseHAM = 750000,
+	baseHAMmax = 1000000,
 	armor = 3,
-	resists = {60,60,60,60,60,60,60,60,45},
+	resists = {25,65,25,80,80,80,25,35,45},
 	meatType = "",
 	meatAmount = 0,
 	hideType = "",
@@ -29,69 +28,80 @@ custom_exar_kun_boss = Creature:new {
 	scale = 1.5,
 	templates = {"object/mobile/costume_exar_kun_cultist.iff"},
 	lootGroups = {
-		{
-			groups = {
-				{group = "power_crystals", chance = 5000000},
-				{group = "armor_attachments", chance = 2500000},
-				{group = "clothing_attachments", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "power_crystals", chance = 5000000},
-				{group = "armor_attachments", chance = 2500000},
-				{group = "clothing_attachments", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
 		 {
 			groups = {
 				{group = "exar_kun_ls_group", chance = 10000000}
 			},
 			lootChance = 10000000
 		},
-		{
+		 {
 			groups = {
-				{group = "power_crystals", chance = 5000000},
-				{group = "armor_attachments", chance = 2500000},
-				{group = "clothing_attachments", chance = 2500000}
+				{group = "power_crystals", chance = 10000000}
 			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "power_crystals", chance = 5000000},
-				{group = "armor_attachments", chance = 2500000},
-				{group = "clothing_attachments", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "nge_house_loot_deed_01", chance = 5000000},
-				{group = "nge_house_loot_deed_windowed", chance = 2500000},
-				{group = "g_command_console", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "crafted_rugs", chance = 5000000},
-				{group = "g_banner_imperial", chance = 2500000},
-				{group = "g_banner_rebel", chance = 2500000}
-			},
-			lootChance = 10000000
+			lootChance = 9000000
 		},
 		  {
 			groups = {
-				{group = "crystal_kuns_blood", chance = 10000000},
+				{group = "crystal_kuns_blood", chance = 10000000}
 				
 		},
-			lootChance = 10000000
+			lootChance = 9000000
 		  },
-		 
+		{
+			groups = {
+				{group = "armor_attachments", chance = 10000000}
 		},
+			lootChance = 9000000
+		},
+		{
+			groups = {
+				{group = "clothing_attachments", chance = 10000000}
+		},
+			lootChance = 9000000
+		},
+		{
+			groups = {
+				{group = "tierone", chance = 10000000}
+		},
+			lootChance = 7500000
+		},
+		{
+			groups = {
+				{group = "tiertwo", chance = 10000000}
+		},
+			lootChance = 3500000
+		},
+		{
+			groups = {
+				{group = "tierthree", chance = 10000000}
+		},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "tiertwo", chance = 10000000}
+		},
+			lootChance = 3500000
+		},
+		{
+			groups = {
+				{group = "tierthree", chance = 10000000}
+		},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "tiertwo", chance = 10000000}
+		},
+			lootChance = 3500000
+		},
+		{
+			groups = {
+				{group = "tierthree", chance = 10000000}
+		},
+			lootChance = 1000000
+		},
+},
 	weapons = {"dark_jedi_weapons_gen4"},
 	conversationTemplate = "",
 	attacks = merge(forcepowermaster,tkamaster,lightsabermaster,brawlermaster,forcewielder)
