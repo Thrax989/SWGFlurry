de9d690182f44d1275b3feeb10b64e7d161e4267
TOXIC <thrax989@yahoo.com>
2017-12-14 05:14:48 -0800

    [Added] No Trade to xp Tokens

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/veteran_reward/exp_buff_item.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/veteran_reward/exp_buff_item.lua
@@ -1,4 +1,5 @@
 object_tangible_veteran_reward_exp_buff_item = object_tangible_veteran_reward_shared_exp_buff_item:new {
 	objectMenuComponent = "XpMenuComponent",
+			noTrade = 1
 }
 ObjectTemplates:addTemplate(object_tangible_veteran_reward_exp_buff_item, "object/tangible/veteran_reward/exp_buff_item.iff")
