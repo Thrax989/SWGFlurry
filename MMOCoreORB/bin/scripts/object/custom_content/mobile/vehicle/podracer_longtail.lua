f567af725d39e23cd64655e7910717d29fdc7866
slayersg <lorellian@comcast.net>
2017-12-15 13:06:08 -0500

    [Update] Mechanic Update
    Change-Id: I862d161958df4edf35b6373dba775ef82d20b8d0

--- MMOCoreORB/bin/scripts/object/custom_content/mobile/vehicle/podracer_longtail.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/mobile/vehicle/podracer_longtail.lua
@@ -1,4 +1,7 @@
 object_mobile_vehicle_podracer_longtail = object_mobile_vehicle_shared_podracer_longtail:new {
+templateType = VEHICLE,
+	decayRate = 50, -- Damage tick per decay cycle
+	decayCycle = 100 -- Time in seconds per cycle
 
 }
 
--- MMOCoreORB/bin/scripts/object/custom_content/mobile/vehicle/podracer_mawhonic.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/mobile/vehicle/podracer_mawhonic.lua
@@ -1,4 +1,7 @@
 object_mobile_vehicle_podracer_mawhonic = object_mobile_vehicle_shared_podracer_mawhonic:new {
+templateType = VEHICLE,
+	decayRate = 50, -- Damage tick per decay cycle
+	decayCycle = 100 -- Time in seconds per cycle
 
 }
 
