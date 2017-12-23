7b9da7f60c96cdb5a1db6c61ee96d4748f881b1c
TOXIC <thrax989@yahoo.com>
2017-12-22 19:39:51 -0800

    [Added] New Appearance Swap Terminal

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/terminal/terminal_quad_screen.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/terminal/terminal_quad_screen.lua
@@ -1,1 +1,18 @@
-object_tangible_terminal_terminal_quad_screen = object_tangible_terminal_shared_terminal_quad_screen:new {}ObjectTemplates:addTemplate(object_tangible_terminal_terminal_quad_screen, "object/tangible/terminal/terminal_quad_screen.iff")
+object_tangible_terminal_terminal_quad_screen = object_tangible_terminal_shared_terminal_quad_screen:new {
+	gameObjectType = 16400,
+
+	maxCondition = 0,
+
+	customName = "Character Appearance Service",
+
+	templateType = CHARACTERBUILDERTERMINAL,
+
+	itemList = {
+		        "Character Appearances",
+		   {
+			"Normal Character Appearance (100 Credits)", "switch_normal_loadout",
+			"Royal Guard Appearance (100 Credits)", "royal_guard_appearance",
+		   },
+	}
+}
+ObjectTemplates:addTemplate(object_tangible_terminal_terminal_quad_screen, "object/tangible/terminal/terminal_quad_screen.iff")
