ffe112b4cb5d259387f09e4da4d8b99f91482557
TOXIC <thrax989@yahoo.com>
2017-12-24 10:36:52 -0800

    [Updated] Teleports

--- MMOCoreORB/bin/scripts/screenplays/custom_screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/custom_screenplays/screenplays.lua
@@ -1,10 +1,10 @@
 --Custom Content
 
 --[[
-TODo List..........
+--TODo List..........
 
 
-]]
+]]--
 
 --Events
 
@@ -14,10 +14,10 @@ TODo List..........
 --Corellia
 
 --Coruscant
---includeFile("custom_screenplays/coruscant/coruscant.lua")
+includeFile("custom_screenplays/coruscant/coruscant.lua")
 
 --Dagobah
---includeFile("custom_screenplays/dagobah/dagobah_yoda_house.lua")
+includeFile("custom_screenplays/dagobah/dagobah_yoda_house.lua")
 
 --Dantooine
 
@@ -28,75 +28,75 @@ TODo List..........
 --Dromund Kaas
 
 --Dungeon
---includeFile("custom_screenplays/dungeon/exarkunteleporter.lua")
---includeFile("custom_screenplays/dungeon/exarkunreturn.lua")
---includeFile("custom_screenplays/dungeon/nightsisterteleporter.lua")
---includeFile("custom_screenplays/dungeon/nightsisterreturn.lua")
---includeFile("custom_screenplays/dungeon/igteleporter.lua")
---includeFile("custom_screenplays/dungeon/igreturn.lua")
---includeFile("custom_screenplays/dungeon/avatarteleporter.lua")
---includeFile("custom_screenplays/dungeon/avatarreturn.lua")
---includeFile("custom_screenplays/dungeon/avatarplatform.lua")
---includeFile("custom_screenplays/dungeon/isdteleporter.lua")
---includeFile("custom_screenplays/dungeon/isdreturn.lua")
---includeFile("custom_screenplays/dungeon/echobaseteleporter.lua")
---includeFile("custom_screenplays/dungeon/echobasereturn.lua")
---includeFile("custom_screenplays/dungeon/myyydrilteleporter.lua")
---includeFile("custom_screenplays/dungeon/myyydrilreturn.lua")
---includeFile("custom_screenplays/dungeon/necrosisspawn.lua")
---includeFile("custom_screenplays/dungeon/imperialstardestroyer.lua")
---includeFile("custom_screenplays/dungeon/exarkuntomb.lua")
---includeFile("custom_screenplays/dungeon/exarkuntemple.lua")
---includeFile("custom_screenplays/dungeon/ig88.lua")
---includeFile("custom_screenplays/dungeon/hothechobase.lua")
---includeFile("custom_screenplays/dungeon/hothechobaseelevators.lua")
+includeFile("custom_screenplays/dungeon/exarkunteleporter.lua")
+includeFile("custom_screenplays/dungeon/exarkunreturn.lua")
+includeFile("custom_screenplays/dungeon/nightsisterteleporter.lua")
+includeFile("custom_screenplays/dungeon/nightsisterreturn.lua")
+includeFile("custom_screenplays/dungeon/igteleporter.lua")
+includeFile("custom_screenplays/dungeon/igreturn.lua")
+includeFile("custom_screenplays/dungeon/avatarteleporter.lua")
+includeFile("custom_screenplays/dungeon/avatarreturn.lua")
+includeFile("custom_screenplays/dungeon/avatarplatform.lua")
+includeFile("custom_screenplays/dungeon/isdteleporter.lua")
+includeFile("custom_screenplays/dungeon/isdreturn.lua")
+includeFile("custom_screenplays/dungeon/echobaseteleporter.lua")
+includeFile("custom_screenplays/dungeon/echobasereturn.lua")
+includeFile("custom_screenplays/dungeon/myyydrilteleporter.lua")
+includeFile("custom_screenplays/dungeon/myyydrilreturn.lua")
+includeFile("custom_screenplays/dungeon/necrosisspawn.lua")
+includeFile("custom_screenplays/dungeon/imperialstardestroyer.lua")
+includeFile("custom_screenplays/dungeon/exarkuntomb.lua")
+includeFile("custom_screenplays/dungeon/exarkuntemple.lua")
+includeFile("custom_screenplays/dungeon/ig88.lua")
+includeFile("custom_screenplays/dungeon/hothechobase.lua")
+includeFile("custom_screenplays/dungeon/hothechobaseelevators.lua")
 
 --Endor
 
 --Factional
 
 --Hoth
---includeFile("custom_screenplays/hoth/hoth_echo_base.lua")
---includeFile("custom_screenplays/hoth/hoth_imp_base.lua")
---includeFile("custom_screenplays/hoth/hoth_starport.lua")
---includeFile("custom_screenplays/hoth/hoth_wampa_cave.lua")
---includeFile("custom_screenplays/hoth/hoth_echoturrets.lua")
+includeFile("custom_screenplays/hoth/hoth_echo_base.lua")
+includeFile("custom_screenplays/hoth/hoth_imp_base.lua")
+includeFile("custom_screenplays/hoth/hoth_starport.lua")
+includeFile("custom_screenplays/hoth/hoth_wampa_cave.lua")
+includeFile("custom_screenplays/hoth/hoth_echoturrets.lua")
 
 --Instances
 
 --Kashyyyk
 --Kashyyyk Teleporters
---includeFile("custom_screenplays/kashyyyk/kachirho.lua")
---includeFile("custom_screenplays/kashyyyk/kashyyykgroundleveloneteleporter.lua")
---includeFile("custom_screenplays/kashyyyk/kashyyykgroundleveltwoteleporter.lua")
---includeFile("custom_screenplays/kashyyyk/kashyyykgroundlevelthreeteleporter.lua")
---includeFile("custom_screenplays/kashyyyk/kashyyykonelevelgroundteleporter.lua")
---includeFile("custom_screenplays/kashyyyk/kashyyykoneleveltwoteleporter.lua")
---includeFile("custom_screenplays/kashyyyk/kashyyykonelevelthreeteleporter.lua")
---includeFile("custom_screenplays/kashyyyk/kashyyyktwolevelgroundteleporter.lua")
---includeFile("custom_screenplays/kashyyyk/kashyyyktwoleveloneteleporter.lua")
---includeFile("custom_screenplays/kashyyyk/kashyyyktwolevelthreeteleporter.lua")
---includeFile("custom_screenplays/kashyyyk/kashyyykthreelevelgroundteleporter.lua")
---includeFile("custom_screenplays/kashyyyk/kashyyykthreeleveloneteleporter.lua")
---includeFile("custom_screenplays/kashyyyk/kashyyykthreeleveltwoteleporter.lua")
+includeFile("custom_screenplays/kashyyyk/kachirho.lua")
+includeFile("custom_screenplays/kashyyyk/kashyyykgroundleveloneteleporter.lua")
+includeFile("custom_screenplays/kashyyyk/kashyyykgroundleveltwoteleporter.lua")
+includeFile("custom_screenplays/kashyyyk/kashyyykgroundlevelthreeteleporter.lua")
+includeFile("custom_screenplays/kashyyyk/kashyyykonelevelgroundteleporter.lua")
+includeFile("custom_screenplays/kashyyyk/kashyyykoneleveltwoteleporter.lua")
+includeFile("custom_screenplays/kashyyyk/kashyyykonelevelthreeteleporter.lua")
+includeFile("custom_screenplays/kashyyyk/kashyyyktwolevelgroundteleporter.lua")
+includeFile("custom_screenplays/kashyyyk/kashyyyktwoleveloneteleporter.lua")
+includeFile("custom_screenplays/kashyyyk/kashyyyktwolevelthreeteleporter.lua")
+includeFile("custom_screenplays/kashyyyk/kashyyykthreelevelgroundteleporter.lua")
+includeFile("custom_screenplays/kashyyyk/kashyyykthreeleveloneteleporter.lua")
+includeFile("custom_screenplays/kashyyyk/kashyyykthreeleveltwoteleporter.lua")
 
 --Lok
 
 --Mandalore
 
 --Mustafar
---includeFile("custom_screenplays/mustafar/hk_army_teleport.lua")
---includeFile("custom_screenplays/mustafar/hk_army_return.lua")
---includeFile("custom_screenplays/mustafar/hk_army.lua")
---includeFile("custom_screenplays/mustafar/sherkar_teleport.lua")
---includeFile("custom_screenplays/mustafar/sherkar_return.lua")
---includeFile("custom_screenplays/mustafar/sherkar_cave.lua")
+includeFile("custom_screenplays/mustafar/hk_army_teleport.lua")
+includeFile("custom_screenplays/mustafar/hk_army_return.lua")
+includeFile("custom_screenplays/mustafar/hk_army.lua")
+includeFile("custom_screenplays/mustafar/sherkar_teleport.lua")
+includeFile("custom_screenplays/mustafar/sherkar_return.lua")
+includeFile("custom_screenplays/mustafar/sherkar_cave.lua")
 
 --Naboo
 
 --NalHutta
---includeFile("custom_screenplays/nalhutta/nalhutta_huttcity.lua")
---includeFile("custom_screenplays/nalhutta/nalhutta_volcano.lua")
+includeFile("custom_screenplays/nalhutta/nalhutta_huttcity.lua")
+includeFile("custom_screenplays/nalhutta/nalhutta_volcano.lua")
 
 --POI
 
@@ -105,20 +105,20 @@ TODo List..........
 --Rori
 
 --Space Stations
---includeFile("custom_screenplays/spacestations/novaorion.lua")
---includeFile("custom_screenplays/spacestations/novaorion_travel_eis.lua")
---includeFile("custom_screenplays/spacestations/novaorion_travel_coro.lua")
---includeFile("custom_screenplays/spacestations/novaorion_travel_theed.lua")
+includeFile("custom_screenplays/spacestations/novaorion.lua")
+includeFile("custom_screenplays/spacestations/novaorion_travel_eis.lua")
+includeFile("custom_screenplays/spacestations/novaorion_travel_coro.lua")
+includeFile("custom_screenplays/spacestations/novaorion_travel_theed.lua")
 
 --Taanab
---includeFile("custom_screenplays/taanab/taanab_cave.lua")
---includeFile("custom_screenplays/taanab/taanab_downed_ship.lua")
---includeFile("custom_screenplays/taanab/taanab_great_herd.lua")
---includeFile("custom_screenplays/taanab/taanab_hex_farms.lua")
---includeFile("custom_screenplays/taanab/taanab_mine.lua")
---includeFile("custom_screenplays/taanab/taanab_pandath.lua")
---includeFile("custom_screenplays/taanab/taanab_starhunterstation.lua")
---includeFile("custom_screenplays/taanab/taanab_static_spawns.lua")
+includeFile("custom_screenplays/taanab/taanab_cave.lua")
+includeFile("custom_screenplays/taanab/taanab_downed_ship.lua")
+includeFile("custom_screenplays/taanab/taanab_great_herd.lua")
+includeFile("custom_screenplays/taanab/taanab_hex_farms.lua")
+includeFile("custom_screenplays/taanab/taanab_mine.lua")
+includeFile("custom_screenplays/taanab/taanab_pandath.lua")
+includeFile("custom_screenplays/taanab/taanab_starhunterstation.lua")
+includeFile("custom_screenplays/taanab/taanab_static_spawns.lua")
 
 --Talus
 
