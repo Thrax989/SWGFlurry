4195031a25f07a6dea486c4e301ceb8830318a76
TOXIC <thrax989@yahoo.com>
2017-12-24 12:12:47 -0800

    [Removed] NPC from echo base

--- MMOCoreORB/bin/scripts/screenplays/custom_screenplays/hoth/hoth_echo_base.lua
+++ MMOCoreORB/bin/scripts/screenplays/custom_screenplays/hoth/hoth_echo_base.lua
@@ -29,35 +29,6 @@ function EchoBaseScreenPlay:spawnMobiles()
 
 
 --Imp forces
-
-spawnMobile("hoth", "at_at", 1800, -4198, 84.7, 5625, -96, 0)
-spawnMobile("hoth", "at_at", 1800, -4166, 84.7, 5504.3, -89, 0)
-spawnMobile("hoth", "at_at", 1800, -4156.8, 87.8, 5913.4, -128, 0)
-
-spawnMobile("hoth", "at_st", 300, -4317.8, 81.6, 5734, -103, 0)
-spawnMobile("hoth", "at_st", 300, -4283.8, 84.7, 5517.1, -84, 0)
-
-spawnMobile("hoth", "snowtrooper", 300, -4270, 84.7, 5668, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4270, 84.7, 5672, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4270, 84.7, 5676, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4270, 84.7, 5680, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4270, 84.7, 5684, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4270, 84.7, 5688, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4270, 84.7, 5692, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4270, 84.7, 5696, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4270, 84.7, 5700, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4270, 84.7, 5704, -90, 0)
-
-spawnMobile("hoth", "snowtrooper", 300, -4265, 84.7, 5668, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4265, 84.7, 5672, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4265, 84.7, 5676, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4265, 84.7, 5680, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4265, 84.7, 5684, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4265, 84.7, 5688, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4265, 84.7, 5692, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4265, 84.7, 5696, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4265, 84.7, 5700, -90, 0)
-spawnMobile("hoth", "snowtrooper", 300, -4265, 84.7, 5704, -90, 0)
 	
 	
 
