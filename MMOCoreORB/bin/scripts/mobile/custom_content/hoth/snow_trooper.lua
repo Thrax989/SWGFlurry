9185af777a91676d00cf52df19c01a6e33af824f
TOXIC <thrax989@yahoo.com>
2017-12-24 12:09:13 -0800

    [Updated] Hoth npc default loot table for factions

--- MMOCoreORB/bin/scripts/mobile/custom_content/hoth/snow_trooper.lua
+++ MMOCoreORB/bin/scripts/mobile/custom_content/hoth/snow_trooper.lua
@@ -31,17 +31,47 @@ snowtrooper = Creature:new {
 	lootGroups = {
 		{
 			groups = {
-				{group = "junk", chance = 3500000},
-				{group = "rifles", chance = 1200000},
-				{group = "pistols", chance = 1200000},
-				{group = "melee_weapons", chance = 1200000},
-				{group = "carbines", chance = 1200000},
-				{group = "clothing_attachments", chance = 250000},
-				{group = "armor_attachments", chance = 250000},
-				{group = "stormtrooper_common", chance = 700000},
-				{group = "wearables_common", chance = 500000}
-			}
-		}
+				{group = "junk", chance = 10000000},
+		},
+			lootChance = 500000 
+
+		},
+		{
+			groups = {
+				{group = "g_imperial_credit", chance = 10000000}
+		},
+			lootChance = 200000 
+		},
+		{
+			groups = {
+				{group = "artifact", chance = 10000000}			
+		},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "tierone", chance = 10000000}
+		},
+			lootChance = 750000
+		},
+		{
+			groups = {
+				{group = "tiertwo", chance = 10000000}
+		},
+			lootChance = 100000
+		},
+		{
+			groups = {
+				{group = "tierthree", chance = 10000000}
+		},
+			lootChance = 50000
+		},
+		{
+			groups = {
+				{group = "imppoints", chance = 10000000}
+		},
+			lootChance = 100000 
+		},
 	},
 	weapons = {"stormtrooper_weapons"},
 	conversationTemplate = "",
