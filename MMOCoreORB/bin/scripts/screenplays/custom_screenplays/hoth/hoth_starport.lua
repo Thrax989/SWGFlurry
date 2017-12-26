c48357bbf074c68392fc9097dadc1824fdd9a1e7
TOXIC <thrax989@yahoo.com>
2017-12-26 10:55:25 -0500

    [Fixed] Missions Terms on Hoth

--- MMOCoreORB/bin/scripts/screenplays/custom_screenplays/hoth/hoth_starport.lua
+++ MMOCoreORB/bin/scripts/screenplays/custom_screenplays/hoth/hoth_starport.lua
@@ -9,12 +9,13 @@ registerScreenPlay("HothStarportScreenPlay", true)
 function HothStarportScreenPlay:start()
 	if (isZoneEnabled("hoth")) then
 		self:spawnMobiles()
+		self:spawnSceneObjects()
 	end
 end
 
 function HothStarportScreenPlay:spawnSceneObjects()
---spawns the mission terminal
-spawnSceneObject("hoth", "object/tangible/terminal/terminal_mission_imperial.iff", -3.53275, 0, -1987.58, 0, 0.00932471, 0, 0.999957, 0)
+
+spawnSceneObject("hoth", "object/tangible/terminal/terminal_mission.iff", -3.53275, 0, -1987.58, 0, 0.00932471, 0, 0.999957, 0)
 end
 
 function HothStarportScreenPlay:spawnMobiles()
