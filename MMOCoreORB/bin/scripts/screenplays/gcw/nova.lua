74db351795ec72b53263f97d5248394fe33b5256
TOXIC <thrax989@yahoo.com>
2017-12-25 19:16:52 -0800

    [Updated] Effects one last time when entering teleport radius

--- MMOCoreORB/bin/scripts/screenplays/gcw/nova.lua
+++ MMOCoreORB/bin/scripts/screenplays/gcw/nova.lua
@@ -42,7 +42,6 @@ local player = LuaCreatureObject(pPlayer)
 			--player:broadcastToServer("\\#00E604" .. player:getFirstName() .. "\\#63C8F9 Has entered the nova Boss Teleport Zone!")
 			player:sendSystemMessage("You have entered the Nova Boss Teleport Area.")
 			player:playEffect("clienteffect/sm_end_of_the_line.cef", "")
-			player:playEffect("clienteffect/cbt_hit_ship_shield_event_hvy.cef", "")
 			player:playEffect("clienteffect/droid_effect_dry_ice.cef", "")
 			end
 		return 0
