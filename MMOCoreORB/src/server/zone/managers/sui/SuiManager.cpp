8296d9762b28cf8e634dcb422110b5589d8f1b0d
TOXIC <thrax989@yahoo.com>
2018-01-03 14:47:39 -0800

    [Updated] Player city port

--- MMOCoreORB/src/server/zone/managers/sui/SuiManager.cpp
+++ MMOCoreORB/src/server/zone/managers/sui/SuiManager.cpp
@@ -903,7 +903,24 @@ void SuiManager::handleCharacterBuilderSelectItem(CreatureObject* player, SuiBox
  					player->subtractCashCredits(1000);
 					box->setForceCloseDistance(5.f);
 			        }
-				} else if (templatePath == "pc_remnant_travel") {
+			} else if (templatePath == "pc_wraith_travel") {
+				if (!player->isInCombat() && player->getCashCredits() < 999) {
+		                ManagedReference<SuiMessageBox*> box = new SuiMessageBox(player, SuiWindowType::CITY_ADMIN_CONFIRM_UPDATE_TYPE);
+		                box->setPromptTitle("Player City - Wraith");
+		                box->setPromptText("Travel Coast 1,000 credits. (Cash)");
+		                box->setOkButton(true, "@cancel");
+		                box->setUsingObject(player);
+		                player->getPlayerObject()->addSuiBox(box);
+		                player->sendMessage(box->generateMessage());
+			        }
+				if (!player->isInCombat() && player->getCashCredits() > 999) {
+		                ManagedReference<SuiMessageBox*> box = new SuiMessageBox(player, SuiWindowType::CITY_ADMIN_CONFIRM_UPDATE_TYPE);
+					player->sendSystemMessage("Thank you for your travels.");
+ 				        player->switchZone("dathomir", -3550, 0, 705);
+ 					player->subtractCashCredits(1000);
+					box->setForceCloseDistance(5.f);
+			        }
+			} else if (templatePath == "pc_remnant_travel") {
 				if (!player->isInCombat() && player->getCashCredits() < 999) {
 		                ManagedReference<SuiMessageBox*> box = new SuiMessageBox(player, SuiWindowType::CITY_ADMIN_CONFIRM_UPDATE_TYPE);
 		                box->setPromptTitle("Player City - Remnant");
