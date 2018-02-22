52acd0e74a60934e83e698ba74962246a832ae3b
TOXIC <thrax989@yahoo.com>
2018-02-21 19:34:16 -0800

    [Updated] Tusken weapon groups

--- MMOCoreORB/bin/scripts/mobile/weapon/groups/tusken_weapons.lua
+++ MMOCoreORB/bin/scripts/mobile/weapon/groups/tusken_weapons.lua
@@ -1,7 +1,8 @@
 tusken_weapons = {
 	"object/weapon/ranged/rifle/rifle_tusken.iff",
 	"object/weapon/melee/baton/baton_gaderiffi.iff",
-	"object/weapon/ranged/carbine/carbine_laser.iff"
+	"object/weapon/ranged/carbine/carbine_laser.iff",
+	"object/weapon/melee/special/vibroknuckler.iff"
 
 }
 
