7883def17be471c7680c72333797449be0cf5aba
TOXIC <thrax989@yahoo.com>
2018-02-22 08:02:58 -0800

    [Added] Intimidate formula for combat manager for jedi debuff when intimidated in encounters

--- MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
+++ MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -1525,9 +1525,13 @@ int CombatManager::getHitChance(TangibleObject* attacker, CreatureObject* target
 
 		// saber block is special because it's just a % chance to block based on the skillmod
 		if (def == "saber_block") {
-			if (!attacker->isTurret() && (weapon->getAttackType() == SharedWeaponObjectTemplate::RANGEDATTACK) && ((System::random(100)) < targetCreature->getSkillMod(def)))
-				return RICOCHET;
-			else return HIT;
+			int block_mod = targetCreature->getSkillMod(def);
+            	if (targetCreature->isIntimidated()) {
+                	block_mod = (block_mod / 2);
+            	}
+            	if (!attacker->isTurret() && (weapon->getAttackType() == SharedWeaponObjectTemplate::RANGEDATTACK) && ((System::random(100)) < block_mod))
+                return RICOCHET;
+            	else return HIT;
 		}
 
 		targetDefense = getDefenderSecondaryDefenseModifier(targetCreature);
