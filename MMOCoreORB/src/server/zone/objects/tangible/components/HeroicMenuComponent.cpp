6dd47937dcc5fadfb4e2feddf1a9dbc3b76255e6
TOXIC <thrax989@yahoo.com>
2018-02-26 10:50:58 -0500

    [Fixed] Server build error

--- MMOCoreORB/src/server/zone/objects/tangible/components/HeroicMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/HeroicMenuComponent.cpp
@@ -20,6 +20,7 @@
 #include "server/chat/ChatManager.h"
 #include "server/zone/packets/player/PlayMusicMessage.h"
 #include "server/zone/packets/object/ObjectMenuResponse.h"
+#include "server/zone/objects/building/BuildingObject.h"
 
 void HeroicMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, ObjectMenuResponse* menuResponse, CreatureObject* player) const {
 
@@ -30,16 +31,21 @@ void HeroicMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, Objec
 	if (building == NULL) {
 	menuResponse->addRadialMenuItem(20, 3, "Open Heroic Crate");
 	}
+
 }
 
 int HeroicMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* creature, byte selectedID) const {
 		ManagedReference<BuildingObject*> building = cast<BuildingObject*>(creature->getRootParent());
-	
-	// If outside dispaly menu options, if inside a building show nothing.	
+
+	// If outside dispaly menu options, if inside a building show nothing.
 	if (building == NULL) {
 	if (selectedID != 20)
 		return 0;
 
+	if (!sceneObject->isASubChildOf(creature))
+		return 0;
+
+	ManagedReference<PlayerObject*> ghost = creature->getPlayerObject();
  	ManagedReference<SceneObject*> inventory = creature->getSlottedObject("inventory");
  	ManagedReference<LootManager*> lootManager = creature->getZoneServer()->getLootManager();
 	lootManager->createLoot(inventory, "lootcollectiontierHeroic", 300);
