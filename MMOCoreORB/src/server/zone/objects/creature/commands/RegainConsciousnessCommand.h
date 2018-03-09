1265337cd5938cfdc0f3d82157f7ec11c6b647e2
TOXIC <thrax989@yahoo.com>
2018-03-09 15:19:36 -0800

    [Updated[ Regain Consciousness Command to check when there is no force

--- MMOCoreORB/src/server/zone/objects/creature/commands/RegainConsciousnessCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RegainConsciousnessCommand.h
@@ -8,6 +8,9 @@
 #include "server/zone/ZoneServer.h"
 #include "server/zone/managers/player/PlayerManager.h"
 #include "server/zone/objects/creature/buffs/PrivateBuff.h"
+#include "server/zone/objects/creature/buffs/PrivateSkillMultiplierBuff.h"
+
+#include "JediQueueCommand.h"
 
 class RegainConsciousnessCommand : public JediQueueCommand {
 public:
@@ -26,7 +29,17 @@ public:
 			if(res != SUCCESS)
 				return res;
 
-			doForceCost(creature);
+			//Check for Force Cost.
+			ManagedReference<PlayerObject*> ghost = creature->getPlayerObject();
+
+			if (ghost != NULL && ghost->getForcePower() < forceCost) {
+				creature->sendSystemMessage("@jedi_spam:no_force_power"); //"You do not have enough Force Power to perform that action.
+				return GENERALERROR;
+			}
+
+			// Force Cost.
+			if (ghost != NULL)
+				ghost->setForcePower(ghost->getForcePower() - forceCost);
 
 			// Cut Force Regen in Half for 30 Minutes.
 			ManagedReference<PrivateSkillMultiplierBuff *> regenDebuff = new PrivateSkillMultiplierBuff(creature, STRING_HASHCODE("private_force_regen_debuff"), 60*30, BuffType::JEDI);
