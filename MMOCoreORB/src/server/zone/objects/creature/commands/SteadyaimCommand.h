9e26ea141907023592aea6643b121398911b7ecd
TOXIC <thrax989@yahoo.com>
2018-03-09 14:31:38 -0800

    [Added] Distance check for steady aim

--- MMOCoreORB/src/server/zone/objects/creature/commands/SteadyaimCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/SteadyaimCommand.h
@@ -76,6 +76,9 @@ public:
 			if (member == NULL || !member->isPlayerCreature())
 				continue;
 
+			if (!checkDistance(leader, member, 200))
+				continue;
+
 			if (!isValidGroupAbilityTarget(leader, member, false))
 				continue;
 
