00782159a5e95ee7d127715833b4582101d5449f
TOXIC <thrax989@yahoo.com>
2018-03-10 18:11:36 -0800

    [Renamed] FRS check

--- MMOCoreORB/src/server/zone/objects/creature/commands/ForceArmor2Command.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ForceArmor2Command.h
@@ -40,9 +40,9 @@ public:
 			return;
 
 		// TODO: Force Rank modifiers.
-		int enhSkills = playerObject->numSpecificSkills(creo, "force_rank_");
-		float enhMod = enhSkills * .025;
-		int forceCost = param * (0.5 - enhMod);
+		int frsSkills = playerObject->numSpecificSkills(creo, "force_rank_");
+		float frsMod = frsSkills * .025;
+		int forceCost = param * (0.5 - frsMod);
 		if (playerObject->getForcePower() <= forceCost) { // Remove buff if not enough force.
 			Buff* buff = creo->getBuff(BuffCRC::JEDI_FORCE_ARMOR_2);
 			if (buff != NULL) {
