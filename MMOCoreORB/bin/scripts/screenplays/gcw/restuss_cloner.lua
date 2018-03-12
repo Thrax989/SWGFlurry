3f600ac7c9085fb26f55a4f105d7959aab78d30f
TOXIC <thrax989@yahoo.com>
2018-03-12 03:18:02 -0400

    [Updated[ rori travel location

--- MMOCoreORB/bin/scripts/screenplays/gcw/restuss_cloner.lua
+++ MMOCoreORB/bin/scripts/screenplays/gcw/restuss_cloner.lua
@@ -18,7 +18,7 @@ end
 
 function restussClonerScreenPlay:spawnSceneObjects()
 spawnSceneObject("rori", "object/building/military/outpost_cloning_facility.iff", 5276.94, 79.4459, 6093.22, -0.999848, 0, 0, 0.0174522, 0)
-spawnSceneObject("rori", "object/tangible/terminal/terminal_gcw_publish_gift.iff", 5328.47, 79.4361, 6138.81, 0, 0.742713, 0, 0.66961, 0)
+spawnSceneObject("rori", "object/tangible/terminal/terminal_gcw_publish_gift.iff", 5286.06, 79.0008, 6101.61, 0, 0.999644, 0, -0.0266684, 0)
 spawnSceneObject("rori", "object/tangible/terminal/terminal_bank.iff", 5185.61, 80, 5584.07, 0, math.rad(90))
 spawnSceneObject("rori", "object/tangible/terminal/terminal_bazaar.iff", 5203, 80, 5584.36, 0, math.rad(-90))
 end
