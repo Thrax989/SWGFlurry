887cc5c819e869c49d245efa9592bbcbbd831193
TOXIC <thrax989@yahoo.com>
2018-03-12 14:46:34 -0700

    [Disabled] Logging commands

--- MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
+++ MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -915,18 +915,18 @@ float CombatManager::applyDamageModifiers(CreatureObject* attacker, WeaponObject
 		ManagedReference<PlayerObject*> attackerGhost = attacker->getPlayerObject();
 		float forcePower = 1.f;
 		if(attackerGhost != NULL && attackerGhost->getJediState() == 4) {
-			info("Light Force Power Jedi : " + String::valueOf(attacker->getSkillMod("force_power_light")), true );
+			//info("Light Force Power Jedi : " + String::valueOf(attacker->getSkillMod("force_power_light")), true );
 			forcePower = (float)attacker->getSkillMod("force_power_light") / 100;
 	} 	else if (attackerGhost !=NULL &&attackerGhost->getJediState() == 8) {
-			info("Dark Force Power Jedi : " + String::valueOf(attacker->getSkillMod("force_power_dark")), true );
+			//info("Dark Force Power Jedi : " + String::valueOf(attacker->getSkillMod("force_power_dark")), true );
 			forcePower = (float)attacker->getSkillMod("force_power_dark") / 100;
 	}
 	
-		info("Force power increase is: " + String::valueOf(forcePower), true);
-		info("Old Damage: " + String::valueOf(damage), true);
+		//info("Force power increase is: " + String::valueOf(forcePower), true);
+		//info("Old Damage: " + String::valueOf(damage), true);
 		int frsDamage = damage * forcePower;
 		damage += frsDamage;
-		info("New Damage: " + String::valueOf(damage), true);
+		//info("New Damage: " + String::valueOf(damage), true);
 	}
 
 	damage += attacker->getSkillMod("private_damage_bonus");
@@ -1155,7 +1155,7 @@ int CombatManager::getArmorReduction(TangibleObject* attacker, WeaponObject* wea
 			<< rawDamage
 			<< "; = "
 			<< dmgAbsorbed;
-			info(dmgAbsorbedInfo, true);
+			//info(dmgAbsorbedInfo, true);
 		}
 	} else {
 		float jediBuffDamage = 0;
