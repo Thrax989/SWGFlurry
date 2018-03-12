41cba9a4a002342f7b61c5cc5f3a8e846f3a0ae5
TOXIC <thrax989@yahoo.com>
2018-03-11 22:21:02 -0400

    [Added] GST to rori pvp area

--- MMOCoreORB/bin/scripts/screenplays/gcw/restuss_cloner.lua
+++ MMOCoreORB/bin/scripts/screenplays/gcw/restuss_cloner.lua
@@ -17,7 +17,8 @@ function restussClonerScreenPlay:start()
 end
 
 function restussClonerScreenPlay:spawnSceneObjects()
---spawnSceneObject("rori", "object/building/military/outpost_cloning_facility.iff", 5276.94, 79.4459, 6093.22, -0.999848, 0, 0, 0.0174522, 0)
+spawnSceneObject("rori", "object/building/military/outpost_cloning_facility.iff", 5276.94, 79.4459, 6093.22, -0.999848, 0, 0, 0.0174522, 0)
+spawnSceneObject("rori", "object/tangible/terminal/terminal_gcw_publish_gift.iff", 5300.52, 78.2746, 6135.97, 0, 0.899309, -0, -0.437313, 0)
 spawnSceneObject("rori", "object/tangible/terminal/terminal_bank.iff", 5185.61, 80, 5584.07, 0, math.rad(90))
 spawnSceneObject("rori", "object/tangible/terminal/terminal_bazaar.iff", 5203, 80, 5584.36, 0, math.rad(-90))
 end
