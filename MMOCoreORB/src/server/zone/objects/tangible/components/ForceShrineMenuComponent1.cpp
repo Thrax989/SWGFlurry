d71074fd4d42e0daebcf0d68e4a64fca80bfd4b7
TOXIC <thrax989@yahoo.com>
2018-03-13 03:16:45 -0400

    [Added] FRS check to force players to complete the knight Trails before re entering FRS

--- MMOCoreORB/src/server/zone/objects/tangible/components/ForceShrineMenuComponent1.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/ForceShrineMenuComponent1.cpp
@@ -30,7 +30,8 @@ void ForceShrineMenuComponent1::fillObjectMenuResponse(SceneObject* sceneObject,
 	if (player->hasSkill("force_title_jedi_rank_02")) {
 		menuResponse->addRadialMenuItem(214, 3, "Robe Replacement"); // Get Robes
 	}
-	if ((ghost->getJediState() >= 2 && ghost->getSpentJediSkillPoints() > 235) || ghost->getJediState() >=4) {
+
+	if (player->hasSkill("force_title_jedi_rank_03")) {
 		menuResponse->addRadialMenuItem(215, 3, "Force Ranking");
 		if (ghost->getJediState() == 2 && ghost->getSpentJediSkillPoints() > 235) {
 			menuResponse->addRadialMenuItemToRadialID(215, 216, 3, "Join Sith Order"); // Join Sith
