75813d4e5f084dd42e0794325bda74beee3583cc
TOXIC <thrax989@yahoo.com>
2018-03-18 02:09:05 +0000

    [Fixed[ Buged enclave dark

--- MMOCoreORB/bin/scripts/screenplays/jedi/yavin4_dark_enclave.lua
+++ MMOCoreORB/bin/scripts/screenplays/jedi/yavin4_dark_enclave.lua
@@ -149,7 +149,9 @@ function Yavin4DarkEnclaveScreenPlay:spawnObjects()
 	end
 
         --FRS Shrines
-        spawnSceneObject("yavin4", "object/tangible/quest/col_jedi_robi_light_jedi_controller.iff", 28.2963, -13.4244, -15.6518, 3435628, 0.861191, 0, -0.508281, 0)     
+        spawnSceneObject("yavin4", "object/tangible/quest/col_jedi_robi_light_jedi_controller.iff", 17.0156, -43.4244, -37.6354, 3435634, 0.998624, 0, -0.0524352, 0)
+        spawnSceneObject("yavin4", "object/tangible/quest/col_jedi_robi_light_jedi_controller.iff", -16.3213, -43.4244, -37.6518, 3435634, 0.99741, 0, -0.0719286, 0)
+    
 end
 
 function Yavin4DarkEnclaveScreenPlay:notifyEnteredEnclave(pArea, pLifeform)
