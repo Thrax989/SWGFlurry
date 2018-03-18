6d11dc624fe7be0ca40f18ca2045cdc9bfe3afa4
TOXIC <thrax989@yahoo.com>
2018-03-18 02:10:07 +0000

    [Fixed[ Light enclave

--- MMOCoreORB/bin/scripts/screenplays/jedi/yavin4_light_enclave.lua
+++ MMOCoreORB/bin/scripts/screenplays/jedi/yavin4_light_enclave.lua
@@ -1,66 +1,64 @@
 local ObjectManager = require("managers.object.object_manager")
---Dark Jedi Enclave (5068, 310)
-Yavin4DarkEnclaveScreenPlay = ScreenPlay:new {
+--Light Jedi Enclave (-5575, 4910)
+Yavin4LightEnclaveScreenPlay = ScreenPlay:new {
 	numberOfActs = 1,
 
 	cellIds = {
-	-- thm_yavn_jedi_enclave_dark
-		outside = 3435626,
-		ramp1 = 3435627,
-		landing1 = 3435628,
-		ramp2 = 3435629,
-		landing2 = 3435630,
-		ramp3 = 3435631,
-		landing3 = 3435632,
-		foyer = 3435633,
-		antechamber = 3435634,
-		cloning = 3435635,
-		arenahall = 3435636,
-		mainroom = 3435643,
-		hallwayb0 = 3435648,
-		hallwaya0 = 3435649,
-		hallwaya1 = 3435637,
-		hallwayb1 = 3435644,
-		council1 = 3435650,
-		hallwayb2 = 3435645,
-		hallwayb3 = 3435646,
-		council2 = 3435651,
-		council3 = 3435652,
-		hallwaya2 = 3435638,
-		council4 = 3435653,
-		hallwaya3 = 3435639,
-		hallwaya4 = 3435640,
-		hallwayb4 = 3435647,
-		chamberramp = 3435641,
-		chamber = 3435642,
+	-- thm_yavn_jedi_enclave_light
+		outside = 8525417,
+		entrancehall1 = 8525444,
+		entrancehall2 = 8525443,
+		entrancehall3 = 8525442,
+		lobby = 8525439,
+		traininghall = 8525440,
+		trainingroom = 8525441,
+		auditorium = 8525418,
+		curvehall1 = 8525434,
+		curvehall2 = 8525435,
+		curvehall3 = 8525436,
+		curvehall4 = 8525437,
+		curvehall5 = 8525438,
+		tier1hall1 = 8525421,
+		tier1room = 8525420,
+		tier1hall2 = 8525419,
+		tier2hall1 = 8525424,
+		tier2room = 8525423,
+		tier2hall2 = 8525422,
+		tier3hall1 = 8525433,
+		tier3room = 8525432,
+		tier3hall2 = 8525431,
+		tier4hall1 = 8525430,
+		tier4room = 8525429,
+		tier4hall2 = 8525428,
+		tier5hall1 = 8525427,
+		tier5room = 8525426,
+		tier5hall2 = 8525425,
 
 	},
 
 	targetItems = {
-		{ "object/tangible/terminal/terminal_dark_enclave_voting.iff",
-			"object/tangible/terminal/terminal_dark_enclave_challenge.iff" },
-		{ "object/tangible/furniture/jedi/frn_all_dark_throne.iff" },
+		{ "object/tangible/terminal/terminal_light_enclave_voting.iff",
+			"object/tangible/terminal/terminal_light_enclave_challenge.iff" },
+		{ "object/tangible/furniture/jedi/frn_all_light_throne.iff" },
 	},
 
 	doorData = {
-		{ worldX = 5055.4, worldZ = 74.5, worldY = 308.0, cellAccess = 3435627 }, -- Entrance
-		{ worldX = 5019.8, worldZ = 31.6, worldY = 310.1, cellAccess = 3435636 }, -- arenahall (to mainroom)
-		{ worldX = 5011.3, worldZ = 35.8, worldY = 337.5, cellAccess = 3435644 }, -- hallway1 (to council1)
-		{ worldX = 4982.5, worldZ = 37.3, worldY = 333.0, cellAccess = 3435645 }, -- hallwaya2 (to council2)
-		{ worldX = 5011.5, worldZ = 35.7, worldY = 282.9, cellAccess = 3435637 }, -- hallwaya1 (to council3)
-		{ worldX = 4984.6, worldZ = 36.8, worldY = 287.0, cellAccess = 3435638 }, -- hallwaya2 (to council4)
-		{ worldX = 4971.0, worldZ = 37.6, worldY = 291.3, cellAccess = 3435638 }, -- mainroom/hallwaya2 (to council3,4,chamber)
-		{ worldX = 4958.0, worldZ = 39.6, worldY = 291.0, cellAccess = 3435640 }, -- hallwaya4 (to chamber)
-		{ worldX = 4957.9, worldZ = 39.6, worldY = 329.4, cellAccess = 3435647 }, -- hallwayb4 (to chamber)
+		{ worldX = -5575.9, worldZ = 87.8, worldY = 4911.7, cellAccess = 8525444 }, -- Entrance
+		{ worldX = -5565.0, worldZ = 63.7, worldY = 4933.6, cellAccess = 8525441 }, -- training room
+		{ worldX = -5530.7, worldZ = 65.9, worldY = 4878.5, cellAccess = 8525420 }, -- tier1room
+		{ worldX = -5496.8, worldZ = 65.9, worldY = 4878.5, cellAccess = 8525423 }, -- tier2room
+		{ worldX = -5530.8, worldZ = 65.9, worldY = 4945.2, cellAccess = 8525432 }, -- tier3room
+		{ worldX = -5496.6, worldZ = 65.9, worldY = 4945.3, cellAccess = 8525429 }, -- tier4room
+		{ worldX = -5476.3, worldZ = 65.9, worldY = 4911.8, cellAccess = 8525426 }, -- tier5room
 	},
 
 	states = {
 		2,--Enclave eligible
-		4,--Enforcer rank
-		8,--Templar rank
-		16,--Oppressor rank
-		32,--Dark Council Member
-		64,--Dark Council Leader
+		4,--Sentinel rank
+		8,--Consular rank
+		16,--Arbiter rank
+		32,--Council Member
+		64,--Council Leader
 		128--has cloned to Enclave
 	},
 
@@ -69,10 +67,10 @@ Yavin4DarkEnclaveScreenPlay = ScreenPlay:new {
 		{ unlock = "", lock = "@pvp_rating:enclave_deny_pet_entry"} -- "As you are not permitted to bring any pets or hirelings into the enclave. They have been returned to your data pad for you."
 	},
 
-	buildingID = 3435626,
+	buildingID = 8525417,
 
-	forceEnergy = { x = 5087.7, z = 79.7, y = 306.2 },
-	forcePush = { x = 0.1, z = -43.4, y = -31.1 },
+	forceField = { x = -5574.5, z = 87.8, y = 4900.2 },
+	forceRoom = { x = -0.0, z = -15.1, y = 24.4 },
 
 	sensitive = "force_title_jedi_novice",
 	initiate = "force_title_jedi_rank_01",
@@ -80,81 +78,71 @@ Yavin4DarkEnclaveScreenPlay = ScreenPlay:new {
 	knight = "force_title_jedi_rank_03",
 	guardian = "force_title_jedi_rank_04",
 	master = "force_title_jedi_master",
-	rankings = "force_rank_dark",
-	member = "force_rank_dark_novice",
-	enforcerone = "force_rank_dark_rank_01",
-	enforcertwo = "force_rank_dark_rank_02",
-	enforcerthree = "force_rank_dark_rank_03",
-	enforcerfour = "force_rank_dark_rank_04",
-	templarone = "force_rank_dark_rank_05",
-	templartwo = "force_rank_dark_rank_06",
-	templarthree = "force_rank_dark_rank_07",
-	oppressorone = "force_rank_dark_rank_08",
-	oppressortwo = "force_rank_dark_rank_09",
-	councilmember = "force_rank_dark_rank_10",
-	councilleader = "force_rank_dark_master",
+	rankings = "force_rank_light",
+	member = "force_rank_light_novice",
+	sentinelone = "force_rank_light_rank_01",
+	sentineltwo = "force_rank_light_rank_02",
+	sentinelthree = "force_rank_light_rank_03",
+	sentinelfour = "force_rank_light_rank_04",
+	consularone = "force_rank_light_rank_05",
+	consulartwo = "force_rank_light_rank_06",
+	consularthree = "force_rank_light_rank_07",
+	arbiterone = "force_rank_light_rank_08",
+	arbitertwo = "force_rank_light_rank_09",
+	councilmember = "force_rank_light_rank_10",
+	councilleader = "force_rank_light_master",
 }
 
-registerScreenPlay("Yavin4DarkEnclaveScreenPlay", true)
+registerScreenPlay("Yavin4LightEnclaveScreenPlay", true)
 
-function Yavin4DarkEnclaveScreenPlay:start()
+function Yavin4LightEnclaveScreenPlay:start()
 	if (isZoneEnabled("yavin4")) then
 
 		self:spawnObjects()
 	end
 end
 
-function Yavin4DarkEnclaveScreenPlay:spawnObjects()
+function Yavin4LightEnclaveScreenPlay:spawnObjects()
 	local spawnedSceneObject = LuaSceneObject(nil)
 	local spawnedPointer
 
-	local pActiveArea = spawnActiveArea("yavin4", "object/active_area.iff", 5055.4, 74.5, 308.0, 178, 3435627)
+	local pActiveArea = spawnActiveArea("yavin4", "object/active_area.iff", -5575.9, 87.8, 4911.7, 5, 8525444)
 	if pActiveArea ~= nil then
-		createObserver(ENTEREDAREA, "Yavin4DarkEnclaveScreenPlay", "notifyEnteredEnclave", pActiveArea)
+		createObserver(ENTEREDAREA, "Yavin4LightEnclaveScreenPlay", "notifyEnteredEnclave", pActiveArea)
 	end
 
-	local pTieroneArea = spawnActiveArea("yavin4", "object/active_area.iff", 5011.3, 35.8, 337.5, -107, 3435644)
+	local pTieroneArea = spawnActiveArea("yavin4", "object/active_area.iff", -5530.7, 65.9, 4878.5, -155, 8525420)
 	if pTieroneArea ~= nil then
-		createObserver(ENTEREDAREA, "Yavin4DarkEnclaveScreenPlay", "notifyEnteredTierOneRoom", pTieroneArea)
+		createObserver(ENTEREDAREA, "Yavin4LightEnclaveScreenPlay", "notifyEnteredTierOneRoom", pTieroneArea)
 	end
 
-	local pTiertwoArea = spawnActiveArea("yavin4", "object/active_area.iff", 4982.5, 37.3, 333.0, -104, 3435645)
+	local pTiertwoArea = spawnActiveArea("yavin4", "object/active_area.iff", -5496.8, 65.9, 4878.5, 150, 8525423)
 	if pTiertwoArea ~= nil then
-		createObserver(ENTEREDAREA, "Yavin4DarkEnclaveScreenPlay", "notifyEnteredTierTwoRoom", pTiertwoArea)
+		createObserver(ENTEREDAREA, "Yavin4LightEnclaveScreenPlay", "notifyEnteredTierTwoRoom", pTiertwoArea)
 	end
 
-	local pTierthreeArea = spawnActiveArea("yavin4", "object/active_area.iff", 5011.5, 35.7, 282.9, -86, 3435637)
+	local pTierthreeArea = spawnActiveArea("yavin4", "object/active_area.iff", -5530.8, 65.9, 4945.2, -33, 8525432)
 	if pTierthreeArea ~= nil then
-		createObserver(ENTEREDAREA, "Yavin4DarkEnclaveScreenPlay", "notifyEnteredTierThreeRoom", pTierthreeArea)
+		createObserver(ENTEREDAREA, "Yavin4LightEnclaveScreenPlay", "notifyEnteredTierThreeRoom", pTierthreeArea)
 	end
 
-	local pTierfourArea = spawnActiveArea("yavin4", "object/active_area.iff", 4984.6, 36.8, 287.0, -83, 3435638)
+	local pTierfourArea = spawnActiveArea("yavin4", "object/active_area.iff", -5496.6, 65.9, 4945.3, 25, 8525429)
 	if pTierfourArea ~= nil then
-		createObserver(ENTEREDAREA, "Yavin4DarkEnclaveScreenPlay", "notifyEnteredTierFourRoom", pTierfourArea)
+		createObserver(ENTEREDAREA, "Yavin4LightEnclaveScreenPlay", "notifyEnteredTierFourRoom", pTierfourArea)
 	end
 
-	local pTiertfcArea = spawnActiveArea("yavin4", "object/active_area.iff", 4971.0, 37.6, 291.3, -179, 3435638)
-	if pTiertfcArea ~= nil then
-		createObserver(ENTEREDAREA, "Yavin4DarkEnclaveScreenPlay", "notifyEnteredTierThreeFourChmbrRoom", pTiertfcArea)
-	end
-
-	local pTierfiveArea = spawnActiveArea("yavin4", "object/active_area.iff", 4958.0, 39.6, 291.0, -85, 3435640)
+	local pTierfiveArea = spawnActiveArea("yavin4", "object/active_area.iff", -5476.3, 65.9, 4911.8, 80, 8525426)
 	if pTierfiveArea ~= nil then
-		createObserver(ENTEREDAREA, "Yavin4DarkEnclaveScreenPlay", "notifyEnteredTierFiveRoom", pTierfiveArea)
+		createObserver(ENTEREDAREA, "Yavin4LightEnclaveScreenPlay", "notifyEnteredTierFiveRoom", pTierfiveArea)
 	end
 
-	local pTierfivetooArea = spawnActiveArea("yavin4", "object/active_area.iff", 4957.9, 39.6, 329.4, -103, 3435647)
-	if pTierfivetooArea ~= nil then
-		createObserver(ENTEREDAREA, "Yavin4DarkEnclaveScreenPlay", "notifyEnteredTierFiveTooRoom", pTierfivetooArea)
-	end
+        --FRS shriens
+	spawnSceneObject("yavin4", "object/tangible/quest/col_jedi_robi_light_jedi_controller.iff", 8.21562, -18.8883, 32.1547, 8525439, 0.996097, 0, -0.0882692, 0)
+        spawnSceneObject("yavin4", "object/tangible/quest/col_jedi_robi_light_jedi_controller.iff", -8.1815, -18.8883, 32.1397, 8525439, 0.998702, 0, -0.0509423, 0)
 
-        --FRS Shrines
-        spawnSceneObject("yavin4", "object/tangible/quest/col_jedi_robi_light_jedi_controller.iff", 17.0156, -43.4244, -37.6354, 3435634, 0.998624, 0, -0.0524352, 0)
-        spawnSceneObject("yavin4", "object/tangible/quest/col_jedi_robi_light_jedi_controller.iff", -16.3213, -43.4244, -37.6518, 3435634, 0.99741, 0, -0.0719286, 0)
-    
 end
 
-function Yavin4DarkEnclaveScreenPlay:notifyEnteredEnclave(pArea, pLifeform)
+function Yavin4LightEnclaveScreenPlay:notifyEnteredEnclave(pArea, pLifeform)
 	return ObjectManager.withCreatureObject(pLifeform, function(lifeform)
 	if (pLifeform == nil) then
 		return 0
@@ -162,16 +150,16 @@ function Yavin4DarkEnclaveScreenPlay:notifyEnteredEnclave(pArea, pLifeform)
 
 	if (SceneObject(pLifeform):isPlayerCreature()) then
 
-		if (not CreatureObject(pLifeform):hasSkill(self.knight) or not CreatureObject(pLifeform):isImperial()) then
+		if (not CreatureObject(pLifeform):hasSkill(self.knight) or not CreatureObject(pLifeform):isRebel()) then
 			CreatureObject(pLifeform):sendSystemMessage("@pvp_rating:enclave_deny_entry")
-			CreatureObject(pLifeform):teleport(self.forceEnergy.x, self.forceEnergy.z, self.forceEnergy.y, 0)
+			CreatureObject(pLifeform):teleport(self.forceField.x, self.forceField.z, self.forceField.y, 0)
 		else
-			CreatureObject(pLifeform):sendSystemMessage("The Darkside is within you.")
+			CreatureObject(pLifeform):sendSystemMessage("The Force is with you.")
 		end
 	end
 
 	if (SceneObject(pLifeform):isAiAgent() and AiAgent(pLifeform):isPet()) then
-		local pPetowner = CreatureObject(pLifeform):getOwner()
+	local pPetowner = CreatureObject(pLifeform):getOwner()
 		AiAgent(pLifeform):clearCombatState()
 		AiAgent(pLifeform):doDespawn()
 		CreatureObject(pPetowner):sendSystemMessage("@pvp_rating:enclave_deny_pet_entry")
@@ -186,98 +174,70 @@ function Yavin4DarkEnclaveScreenPlay:notifyEnteredEnclave(pArea, pLifeform)
     end)
 end
 
-function Yavin4DarkEnclaveScreenPlay:notifyEnteredTierOneRoom(pArea, pJedi)
+function Yavin4LightEnclaveScreenPlay:notifyEnteredTierOneRoom(pArea, pJedi)
 	return ObjectManager.withCreaturePlayerObject(pJedi, function(playerObject)
 
-	if (not CreatureObject(pJedi):hasSkill(self.enforcerfour)) then
+	if (not CreatureObject(pJedi):hasSkill(self.sentinelfour)) then
 		CreatureObject(pJedi):sendSystemMessage("@pvp_rating:enclave_deny_entry")
-		CreatureObject(pJedi):teleport(self.forcePush.x, self.forcePush.z, self.forcePush.y, 3435634)
+		CreatureObject(pJedi):teleport(self.forceRoom.x, self.forceRoom.z, self.forceRoom.y, 8525418)
 	else
-		CreatureObject(pJedi):sendSystemMessage("The Dark Council seduces you.")
+		CreatureObject(pJedi):sendSystemMessage("The Council welcomes you.")
 	end
 
 	return 0
     end)
 end
 
-function Yavin4DarkEnclaveScreenPlay:notifyEnteredTierTwoRoom(pArea, pJedi)
+function Yavin4LightEnclaveScreenPlay:notifyEnteredTierTwoRoom(pArea, pJedi)
 	return ObjectManager.withCreaturePlayerObject(pJedi, function(playerObject)
 
-	if (not CreatureObject(pJedi):hasSkill(self.templarthree)) then
+	if (not CreatureObject(pJedi):hasSkill(self.consularthree)) then
 		CreatureObject(pJedi):sendSystemMessage("@pvp_rating:enclave_deny_entry")
-		CreatureObject(pJedi):teleport(self.forcePush.x, self.forcePush.z, self.forcePush.y, 3435634)
+		CreatureObject(pJedi):teleport(self.forceRoom.x, self.forceRoom.z, self.forceRoom.y, 8525418)
 	else
-		CreatureObject(pJedi):sendSystemMessage("The Dark Council seduces you further.")
+		CreatureObject(pJedi):sendSystemMessage("The Council welcomes you further.")
 	end
 
 	return 0
     end)
 end
 
-function Yavin4DarkEnclaveScreenPlay:notifyEnteredTierThreeRoom(pArea, pJedi)
+function Yavin4LightEnclaveScreenPlay:notifyEnteredTierThreeRoom(pArea, pJedi)
 	return ObjectManager.withCreaturePlayerObject(pJedi, function(playerObject)
 
-	if (not CreatureObject(pJedi):hasSkill(self.oppressortwo)) then
+	if (not CreatureObject(pJedi):hasSkill(self.arbitertwo)) then
 		CreatureObject(pJedi):sendSystemMessage("@pvp_rating:enclave_deny_entry")
-		CreatureObject(pJedi):teleport(self.forcePush.x, self.forcePush.z, self.forcePush.y, 3435634)
+		CreatureObject(pJedi):teleport(self.forceRoom.x, self.forceRoom.z, self.forceRoom.y, 8525418)
 	else
-		CreatureObject(pJedi):sendSystemMessage("The Dark Council seduces you even further.")
+		CreatureObject(pJedi):sendSystemMessage("The Council welcomes you even further.")
 	end
 
 	return 0
     end)
 end
 
-function Yavin4DarkEnclaveScreenPlay:notifyEnteredTierThreeFourChmbrRoom(pArea, pJedi)
-	return ObjectManager.withCreaturePlayerObject(pJedi, function(playerObject)
-
-	if (not CreatureObject(pJedi):hasSkill(self.oppressortwo)) then
-		CreatureObject(pJedi):sendSystemMessage("@pvp_rating:enclave_deny_entry")
-		CreatureObject(pJedi):teleport(self.forcePush.x, self.forcePush.z, self.forcePush.y, 3435634)
-	else
-		CreatureObject(pJedi):sendSystemMessage("The Dark Council seduces you even further.")
-	end
-
-	return 0
-    end)
-end
-
-function Yavin4DarkEnclaveScreenPlay:notifyEnteredTierFourRoom(pArea, pJedi)
+function Yavin4LightEnclaveScreenPlay:notifyEnteredTierFourRoom(pArea, pJedi)
 	return ObjectManager.withCreaturePlayerObject(pJedi, function(playerObject)
 
 	if (not CreatureObject(pJedi):hasSkill(self.councilmember)) then
 		CreatureObject(pJedi):sendSystemMessage("@pvp_rating:enclave_deny_entry")
-		CreatureObject(pJedi):teleport(self.forcePush.x, self.forcePush.z, self.forcePush.y, 3435634)
+		CreatureObject(pJedi):teleport(self.forceRoom.x, self.forceRoom.z, self.forceRoom.y, 8525418)
 	else
-		CreatureObject(pJedi):sendSystemMessage("The Dark Council seduces you onto a greater path.")
+		CreatureObject(pJedi):sendSystemMessage("The Council welcomes you into a higher tier.")
 	end
 
 	return 0
     end)
 end
 
-function Yavin4DarkEnclaveScreenPlay:notifyEnteredTierFiveRoom(pArea, pJedi)
+function Yavin4LightEnclaveScreenPlay:notifyEnteredTierFiveRoom(pArea, pJedi)
 	return ObjectManager.withCreaturePlayerObject(pJedi, function(playerObject)
 
 	if (not CreatureObject(pJedi):hasSkill(self.councilleader)) then
 		CreatureObject(pJedi):sendSystemMessage("@pvp_rating:enclave_deny_entry")
-		CreatureObject(pJedi):teleport(self.forcePush.x, self.forcePush.z, self.forcePush.y, 3435634)
+		CreatureObject(pJedi):teleport(self.forceRoom.x, self.forceRoom.z, self.forceRoom.y, 8525418)
 	else
-		CreatureObject(pJedi):sendSystemMessage("The Dark Council beckons you, Sith Lord.")
-	end
-
-	return 0
-    end)
-end
-
-function Yavin4DarkEnclaveScreenPlay:notifyEnteredTierFiveTooRoom(pArea, pJedi)
-	return ObjectManager.withCreaturePlayerObject(pJedi, function(playerObject)
-
-	if (not CreatureObject(pJedi):hasSkill(self.councilleader)) then
-		CreatureObject(pJedi):sendSystemMessage("@pvp_rating:enclave_deny_entry")
-		CreatureObject(pJedi):teleport(self.forcePush.x, self.forcePush.z, self.forcePush.y, 3435634)
-	else
-		CreatureObject(pJedi):sendSystemMessage("The Dark Council beckons you, Sith Lord.")
+		CreatureObject(pJedi):sendSystemMessage("The Council welcomes you, Master Jedi.")
 	end
 
 	return 0
