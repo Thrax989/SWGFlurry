93b8f89bc052d85aa4795f439283a7240720bd6f
TOXIC <thrax989@yahoo.com>
2018-03-27 00:07:27 -0700

    [Added] Custom Beast pets for Beast Master

--- MMOCoreORB/bin/scripts/object/custom_content/intangible/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/intangible/serverobjects.lua
@@ -4,3 +4,4 @@
 includeFile("custom_content/intangible/pet/serverobjects.lua")
 includeFile("custom_content/intangible/ship/serverobjects.lua")
 includeFile("custom_content/intangible/vehicle/serverobjects.lua")
+includeFile("custom_content/intangible/beast/serverobjects.lua")
