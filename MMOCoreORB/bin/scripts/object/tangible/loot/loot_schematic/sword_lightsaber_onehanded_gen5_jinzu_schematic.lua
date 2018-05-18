c18db7b8520ae3f8f89b7dfb690b93b7378a2399
TOXIC <thrax989@yahoo.com>
2018-05-17 23:28:41 -0700

    [Updated[ 5th gen schematic to be used by gray jedi

--- MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/sword_lightsaber_onehanded_gen5_jinzu_schematic.lua
+++ MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/sword_lightsaber_onehanded_gen5_jinzu_schematic.lua
@@ -3,7 +3,8 @@ object_tangible_loot_loot_schematic_sword_lightsaber_onehanded_gen5_jinzu_schema
 templateType = LOOTSCHEMATIC,
 	objectMenuComponent = "LootSchematicMenuComponent",
 	attributeListComponent = "LootSchematicAttributeListComponent",
-	requiredSkill = "force_discipline_light_saber_master",
+	requiredSkill = { "force_discipline_light_saber_master",
+					"combat_jedi_novice" },
 	targetDraftSchematic = "object/draft_schematic/weapon/lightsaber/lightsaber_onehanded_gen5_jinzu.iff",
 	targetUseCount = 1,
 	noTrade = 1
