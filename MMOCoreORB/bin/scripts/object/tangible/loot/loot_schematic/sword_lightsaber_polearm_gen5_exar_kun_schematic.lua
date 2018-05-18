9e029ad50235b3fcce4d25162c7b60edb0f96e02
TOXIC <thrax989@yahoo.com>
2018-05-17 23:28:35 -0700

    [Updated[ 5th gen schematic to be used by gray jedi

--- MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/sword_lightsaber_polearm_gen5_exar_kun_schematic.lua
+++ MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/sword_lightsaber_polearm_gen5_exar_kun_schematic.lua
@@ -3,7 +3,8 @@ object_tangible_loot_loot_schematic_sword_lightsaber_polearm_gen5_exar_kun_schem
 templateType = LOOTSCHEMATIC,
 	objectMenuComponent = "LootSchematicMenuComponent",
 	attributeListComponent = "LootSchematicAttributeListComponent",
-	requiredSkill = "force_discipline_light_saber_master",
+	requiredSkill = { "force_discipline_light_saber_master",
+					"combat_jedi_novice" },
 	targetDraftSchematic = "object/draft_schematic/weapon/lightsaber/lightsaber_polearm_gen5_exar_kun.iff",
 	targetUseCount = 1,
 	noTrade = 1
