185dd7d6e11da570d8d0ed73d8b3d4c7bd4e97c3
TOXIC <thrax989@yahoo.com>
2018-05-18 07:34:55 +0000

    [Fixed] Bh Terminals to properly allow Melee Bounty Hunters to pull player jedi missions

--- MMOCoreORB/src/server/zone/managers/mission/MissionManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/mission/MissionManagerImplementation.cpp
@@ -163,7 +163,7 @@ void MissionManagerImplementation::handleMissionListRequest(MissionTerminal* mis
 	}
 
 	if (missionTerminal->isBountyTerminal()) {
-		if (!player->hasSkill("combat_bountyhunter_novice") || !player->hasSkill("combat_meleebountyhunter_novice")) {
+		if (!player->hasSkill("combat_bountyhunter_novice") && !player->hasSkill("combat_meleebountyhunter_novice")) {
 			player->sendSystemMessage("@mission/mission_generic:not_bounty_hunter_terminal");
 			return;
 		}
@@ -957,7 +957,7 @@ void MissionManagerImplementation::randomizeGenericSurveyMission(CreatureObject*
 }
 
 void MissionManagerImplementation::randomizeGenericBountyMission(CreatureObject* player, MissionObject* mission, const uint32 faction, Vector<ManagedReference<PlayerBounty*>>* potentialTargets) {
-	if (!player->hasSkill("combat_bountyhunter_novice") || !player->hasSkill("combat_meleebountyhunter_novice")) {
+	if (!player->hasSkill("combat_bountyhunter_novice") && !player->hasSkill("combat_meleebountyhunter_novice")) {
 		player->sendSystemMessage("@mission/mission_generic:not_bounty_hunter_terminal");
 		return;
 	}
