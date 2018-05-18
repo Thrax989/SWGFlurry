f513b45fa1ce96cd2e55953442c3e53d1e812bc6
TOXIC <thrax989@yahoo.com>
2018-05-18 00:09:18 +0000

    [Added] Melee bounty hunters the ability to pull jedi missions from bounty hunter terminals

--- MMOCoreORB/src/server/zone/managers/mission/MissionManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/mission/MissionManagerImplementation.cpp
@@ -957,7 +957,7 @@ void MissionManagerImplementation::randomizeGenericSurveyMission(CreatureObject*
 }
 
 void MissionManagerImplementation::randomizeGenericBountyMission(CreatureObject* player, MissionObject* mission, const uint32 faction, Vector<ManagedReference<PlayerBounty*>>* potentialTargets) {
-	if (!player->hasSkill("combat_bountyhunter_novice")) {
+	if (!player->hasSkill("combat_bountyhunter_novice") || !player->hasSkill("combat_meleebountyhunter_novice")) {
 		player->sendSystemMessage("@mission/mission_generic:not_bounty_hunter_terminal");
 		return;
 	}
@@ -970,19 +970,12 @@ void MissionManagerImplementation::randomizeGenericBountyMission(CreatureObject*
 
 	int level = 1;
 	int randomTexts = 25;
-	if (player->hasSkill("combat_bountyhunter_investigation_03")) {
+	if (player->hasSkill("combat_bountyhunter_investigation_03") || player->hasSkill("combat_meleebountyhunter_investigation_03")) {
 		level = 3;
-	} else if (player->hasSkill("combat_bountyhunter_investigation_01")) {
+	} else if (player->hasSkill("combat_bountyhunter_investigation_01") || player->hasSkill("combat_meleebountyhunter_investigation_03")) {
 		level = 2;
 		randomTexts = 50;
 	}
-
-	if (player->hasSkill("combat_meleebountyhunter_investigation_03")) {
-		level = 3;
-	} else if (player->hasSkill("combat_meleebountyhunter_investigation_01")) {
-		level = 2;
-		randomTexts = 50;
-	}	
 
 	NameManager* nm = processor->getNameManager();
 
