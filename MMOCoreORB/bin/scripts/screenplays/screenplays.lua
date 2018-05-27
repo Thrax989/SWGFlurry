2dae0c3c345f66cd6b63ad37e6b4c7672619476d
TOXIC <thrax989@yahoo.com>
2018-05-27 05:48:20 -0700

    [Updated] Exar Kun Temple test [WIP]

--- MMOCoreORB/bin/scripts/screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/screenplays.lua
@@ -109,7 +109,7 @@ includeFile("junk_dealer/junk_dealer_conv_handler.lua")
 
 -- Dungeons
 includeFile("dungeon/ig88/ig88_boss.lua")
-includeFile("dungeon/exar_kun/Exar_Kun.lua")
+includeFile("dungeon/exar_kun_temple/Exar_Kun.lua")
 includeFile("dungeon/death_watch_bunker/deathWatchBunker.lua")
 includeFile("dungeon/death_watch_bunker/deathWatchContainerComponents.lua")
 includeFile("dungeon/death_watch_bunker/deathWatchMenuComponents.lua")
