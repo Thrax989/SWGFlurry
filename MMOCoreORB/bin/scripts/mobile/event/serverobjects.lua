c877e5fb8b8f7d40c2e1ff10fc1845b7f6aac64e
TOXIC <thrax989@yahoo.com>
2018-06-12 23:33:58 -0700

    [Updated] Custom mobiles

--- MMOCoreORB/bin/scripts/mobile/event/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/event/serverobjects.lua
@@ -4,4 +4,12 @@ includeFile("event/life_day/kkatamk.lua")
 includeFile("event/life_day/oraalarri.lua")
 includeFile("event/life_day/radrrl.lua")
 includeFile("event/life_day/tebeurra.lua")
-
+--CUSTOM
+includeFile("event/event_bolma.lua")
+includeFile("event/event_baz_nitch.lua")
+includeFile("event/event_dewback.lua")
+includeFile("event/event_peko_peko.lua")
+includeFile("event/event_shear_mite.lua")
+includeFile("event/event_torton.lua")
+includeFile("event/event_tybis.lua")
+includeFile("event/event_kaadu.lua")
