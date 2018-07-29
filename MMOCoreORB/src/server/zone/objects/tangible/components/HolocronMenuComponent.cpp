6a9b8df06b465dd2084571e5e9de87fa173df4b2
TOXIC <thrax989@yahoo.com>
2018-07-29 10:36:57 +0000

    [Added] Re Wrote HoloCron Menu Component Menu Selection to work with Perma Death System **Testing**

--- MMOCoreORB/src/server/zone/objects/tangible/components/HolocronMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/HolocronMenuComponent.cpp
@@ -1,113 +1,70 @@
 /*
- * HolocronMenuComponent.cpp
- *
- *  Created on: 01/23/2012
- *      Author: xyborn
- */
+* HolocronMenuComponent.cpp
+*
+* Created on: 02/29/2018
+*	 Author: TOXIC
+*  
+*/
 
 #include "HolocronMenuComponent.h"
 #include "server/zone/objects/creature/CreatureObject.h"
 #include "server/zone/objects/scene/SceneObject.h"
 #include "server/zone/objects/player/PlayerObject.h"
-#include "server/zone/ZoneServer.h"
-#include "server/zone/managers/jedi/JediManager.h"
-#include "server/chat/ChatManager.h"
+#include "server/zone/packets/object/ObjectMenuResponse.h"
+#include "server/zone/objects/player/sui/messagebox/SuiMessageBox.h"
+#include "server/zone/managers/skill/SkillManager.h"
+#include "server/zone/managers/player/PlayerManager.h"
 #include "server/zone/packets/player/PlayMusicMessage.h"
-#include "server/zone/managers/visibility/VisibilityManager.h"
-//#include "server/zone/objects/player/sui/callbacks/BountyHuntSuiCallback.h"
-#include "server/zone/objects/player/sui/inputbox/SuiInputBox.h"
+#include "server/zone/managers/creature/CreatureManager.h"
+#include "server/zone/objects/region/CityRegion.h"
+#include "server/zone/ZoneServer.h"
+#include "server/chat/ChatManager.h"
+#include "server/zone/managers/jedi/JediManager.h"
 
+void HolocronMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, ObjectMenuResponse* menuResponse, CreatureObject* player) const {
 
+	TangibleObjectMenuComponent::fillObjectMenuResponse(sceneObject, menuResponse, player);
+	ManagedReference<PlayerObject*> ghost = player->getPlayerObject();
+
+	if (ghost->getJediState() >=2) {
+			menuResponse->addRadialMenuItem(213, 3, "Use Holocron"); // Use Holocron
+			menuResponse->addRadialMenuItemToRadialID(213, 214, 3, "Increase Jedi Lives"); // Increase Jedi Lives
+		}
+	}
 int HolocronMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* creature, byte selectedID) const {
-	
-	/*
-		@param If timer is up - Holocron will refil force bar and remove itself
-		@param If timer is down - Holoron will not refil force bar and stay in your inventory
-		
-		If player has !cooldown then
-			refil force bar
-			galactic message (player, "used holocron")
-			player message "your visibility is: #"
-		else
-			player message "your still under cooldown"
-			player message "your visibility is: #"
-	*/
-	
-	if (selectedID != 20)
+	ManagedReference<PlayerObject*> ghost = creature->getPlayerObject();
+
+	if (ghost == NULL)
 		return 0;
 
-	if (!sceneObject->isASubChildOf(creature))
+	ZoneServer* zserv = creature->getZoneServer();
+
+	if (zserv == NULL)
 		return 0;
-
-	ManagedReference<PlayerObject*> playerObject = creature->getPlayerObject();
-	int jediVis1 = playerObject->getVisibility();
-	StringBuffer messageVis;
-	//put to 0 after test
-	int test = 0;
 	
-	if (playerObject->getAdminLevel() >= 6) {
-		test = 1;
+	if (selectedID == 213) {
+ 		if (!creature->hasSkill("force_title_jedi_novice") && (ghost->getJediState() >= 2)) {
+			JediManager::instance()->useItem(sceneObject, JediManager::ITEMHOLOCRON, creature);
+		}
 	}
-	
-	if (!creature->checkCooldownRecovery("used_holocron")) {
-		if (playerObject->getForcePower() >= playerObject->getForcePowerMax()) {
-			creature->sendSystemMessage("@jedi_spam:holocron_force_max");
-		} else {
-			creature->sendSystemMessage("@jedi_spam:holocron_no_effect");
-		}
-		
-		if (test == 1) {
-			creature->sendSystemMessage("This is the check for used_holocron");
-		}
-		
-		messageVis << "\\#00CC00 Your Visibility is at: " << jediVis1;
-		creature->sendSystemMessage(messageVis.toString());
+	if (selectedID == 214 && (ghost->getJediState() >= 2) && (creature->getScreenPlayState("jediLives") == 0)) {
+		int livesLeft = creature->getScreenPlayState("jediLives") + 1;
+		creature->setScreenPlayState("jediLives", livesLeft);
+		sceneObject->destroyObjectFromWorld(true);
+	}
+	if (selectedID == 214 && (ghost->getJediState() >= 2) && (creature->getScreenPlayState("jediLives") == 1)) {
+		int livesLeft = creature->getScreenPlayState("jediLives") + 1;
+		creature->setScreenPlayState("jediLives", livesLeft);
+		sceneObject->destroyObjectFromWorld(true);
+	}
+	if (selectedID == 214 && (ghost->getJediState() >= 2) && (creature->getScreenPlayState("jediLives") == 2)) {
+		int livesLeft = creature->getScreenPlayState("jediLives") + 1;
+		creature->setScreenPlayState("jediLives", livesLeft);
+		sceneObject->destroyObjectFromWorld(true);
+	}
+	if (selectedID == 214 && (ghost->getJediState() >= 2) && (creature->getScreenPlayState("jediLives") == 3)) {
+		creature->sendSystemMessage("You are at your maximum amount of Jedi lives"); // You are at your maximum amount of Jedi Lives
 		return 0;
 	}
-
-	if (playerObject != NULL && playerObject->getJediState() >= 2) {
-		//No matter what, display your visibilty if you're a jedi
-		messageVis << "\\#00CC00 Your Visibility is at: " << jediVis1;
-		creature->sendSystemMessage(messageVis.toString());
-		if (test == 1) {
-			creature->sendSystemMessage("Your jedi state is >= 2");
-		}
-		//You're a jedi, and not on cooldown && forceFull ? fillForce : FullForceString
-		if (playerObject->getForcePower() < playerObject->getForcePowerMax()) {
-			//Refil force + Message player
-			creature->sendSystemMessage("@jedi_spam:holocron_force_replenish");
-			playerObject->setForcePower(playerObject->getForcePowerMax(), true);
-			//Set cooldown
-			creature->addCooldown("used_holocron", 1 * 1800000); //3,600,000 = 1 hr
-			//Destroy object
-			sceneObject->destroyObjectFromWorld(true);
-			//Music + Effect
-			creature->playEffect("clienteffect/pl_force_absorb_hit.cef");
-			PlayMusicMessage* pmm = new PlayMusicMessage("sound/music_become_light_jedi.snd");
-  			playerObject->sendMessage(pmm);
-			//Broadcast to Server
- 			Zone* zone = creature->getZone();
- 			String playerName = creature->getFirstName();
-  			StringBuffer zBroadcast;
-  			zBroadcast << "\\#00E604" << playerName << " \\#63C8F9 Has Used A Holocron";
- 			creature->getZoneServer()->getChatManager()->broadcastGalaxy(NULL, zBroadcast.toString());
-			if (test == 1) {
-				creature->sendSystemMessage("your force has been refilled");
-			}
-		} else {
-			//You have max force
-			creature->sendSystemMessage("@jedi_spam:holocron_force_max");
-			if (test == 1) {
-				creature->sendSystemMessage("You're full force, can't add");
-			}
-		}
-	} else {
-		//You're not a jedi yet
-		JediManager::instance()->useItem(sceneObject, JediManager::ITEMHOLOCRON, creature);
-		if (test == 1) {
-			creature->sendSystemMessage("You're not a jedi");
-		}
-	}
-
 	return 0;
 }
