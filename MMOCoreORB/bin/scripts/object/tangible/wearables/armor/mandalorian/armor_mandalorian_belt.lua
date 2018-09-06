005a2b662ad1947298d241875b745c7628df4367
TOXIC <thrax989@yahoo.com>
2018-09-05 23:21:38 -0700

    [Added] All wearables can be worn by all races

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/mandalorian/armor_mandalorian_belt.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/mandalorian/armor_mandalorian_belt.lua
@@ -42,7 +42,10 @@
 
 
 object_tangible_wearables_armor_mandalorian_armor_mandalorian_belt = object_tangible_wearables_armor_mandalorian_shared_armor_mandalorian_belt:new {
+
 	templateType = ARMOROBJECT,
+
+	objectMenuComponent = "ArmorObjectMenuComponent",
 
 	certificationsRequired = { "combat_bountyhunter_master",
 					"combat_commando_master",
@@ -50,7 +53,36 @@ object_tangible_wearables_armor_mandalorian_armor_mandalorian_belt = object_tang
 					"outdoors_ranger_master" },
 
 	playerRaces = { "object/creature/player/bothan_male.iff",
+				"object/creature/player/smc_female.iff",
+				"object/creature/player/smc_male.iff",
+				"object/creature/player/togruta_female.iff",
+				"object/creature/player/togruta_male.iff",
+				"object/creature/player/weequay_male.iff",
+				"object/creature/player/weequay_female.iff",
+				"object/creature/player/nautolan_male.iff",
+				"object/creature/player/nautolan_female.iff",
+				"object/creature/player/nightsister_female.iff",
+				"object/creature/player/nightsister_male.iff",
+				"object/creature/player/nikto_male.iff",
+				"object/creature/player/nikto_female.iff",
+				"object/creature/player/quarren_male.iff",
+				"object/creature/player/quarren_female.iff",
+				"object/creature/player/ishi_tib_male.iff",
+				"object/creature/player/ishi_tib_female.iff",
+				"object/creature/player/gran_male.iff",
+				"object/creature/player/gran_female.iff",
+				"object/creature/player/gotal_male.iff",
+				"object/creature/player/gotal_female.iff",
+				"object/creature/player/aqualish_female.iff",
+				"object/creature/player/aqualish_male.iff",
+				"object/creature/player/bith_female.iff",
+				"object/creature/player/bith_male.iff",
+				"object/creature/player/chiss_female.iff",
+				"object/creature/player/chiss_male.iff",
+				"object/creature/player/devaronian_male.iff",
+				"object/creature/player/devaronian_female.iff",
 				"object/creature/player/bothan_female.iff",
+				"object/creature/player/bothan_male.iff",
 				"object/creature/player/human_male.iff",
 				"object/creature/player/human_female.iff",
 				"object/creature/player/ithorian_male.iff",
