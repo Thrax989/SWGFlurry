395edc001bf07fcfadf47765b9d67a264fd80ac6
TOXIC <thrax989@yahoo.com>
2018-11-24 09:29:56 +0000

    [Added] Basic Galactic Travel System

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/terminal/terminal_gcw_publish_gift.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/terminal/terminal_gcw_publish_gift.lua
@@ -1,3 +1,109 @@
 object_tangible_terminal_terminal_gcw_publish_gift = object_tangible_terminal_shared_terminal_gcw_publish_gift:new {
+	gameObjectType = 16400,
+
+	maxCondition = 0,
+
+	customName = "Galactic Travel Service",
+
+	templateType = CHARACTERBUILDERTERMINAL,
+
+	itemList = {
+		        "corellia",
+		   {
+			"Bela Vistal Shuttleport A (1,000 Credits)", "corellia_bela_vistal_a_shuttleport_travel",
+			"Bela Vistal Shuttleport B (1,000 Credits)", "corellia_bela_vistal_b_shuttleport_travel",
+			"Coronet Starport (1,000 Credits)", "corellia_coronet_starport_travel",
+			"Coronet Shuttle A (1,000 Credits)", "corellia_coronet_a_shuttle_travel",
+			"Coronet Shuttle B (1,000 Credits)", "corellia_coronet_b_shuttle_travel",
+			"Doaba Guerfel Shuttleport (1,000 Credits)", "doaba_guerfel_shuttleport_travel",
+			"Doaba Guerfel Starport (1,000 Credits)", "doaba_guerfel_starport_travel",
+			"Kor Vella Shuttleport (1,000 Credits)", "kor_vella_shuttleport_travel",
+			"Kor Vella Starport (1,000 Credits)", "kor_vella_starport_travel",
+			"Tyrena Shuttle A (1,000 Credits)", "tyrena_a_shuttle_travel",
+			"Tyrena Shuttle B (1,000 Credits)", "tyrena_b_shuttle_travel",
+			"Tyrena Starport (1,000 Credits)", "tyrena_starport_travel",
+			"Vreni Island Shuttle (1,000 Credits)", "vreni_island_shuttle_travel",
+		   },
+			"dantooine",
+		   {
+			"Dantooine Agro Outpost Starport (1,000 Credits)", "dantooine_agro_outpost_starport_travel",
+			"Dantooine Imperial Outpost Starport (1,000 Credits)", "dantooine_imperial_outpost_starport_travel",
+			"Dantooine Mining Outpost Starport (1,000 Credits)", "dantooine_mining_outpost_startport_travel",
+		   },
+			"dathomir",
+		   {
+			"Dathomir Trade Outpost Starport (1,000 Credits)", "dathomir_trade_outpost_starport_travel",
+			"Dathomir Science Outpost Starport (1,000 Credits)", "dathomir_science_outpost_starport_travel",
+			"Dathomir Village Shuttleport (1,000 Credits)", "dathomir_village_shuttleport_travel",
+		   },
+		  	"lok",
+		   {
+			"Nym's Stronghold Starport (1,000 Credits)", "nyms_stronghold_starport_travel",
+		   },
+			"naboo",
+		   {
+			"Dee'ja Peak Shuttleport (1,000 Credits)", "deeja_peak_shuttleport_travel",
+			"Kaadara Shuttleport (1,000 Credits)", "kaadar_shuttleport_travel",
+			"Kaadara Starport (1,000 Credits)", "kaadara_starport_travel",
+			"Keren Shuttleport (1,000 Credits)", "keren_shuttleport_travel",
+			"Keren Shuttleport South (1,000 Credits)", "karen_shuttleport_south_travel",
+			"Keren Starport (1,000 Credits)", "keren_starport_travel",
+			"Moenia Starport (1,000 Credits)", "moemia_starport_travel",
+			"The Lake Retreat Shuttleport (1,000 Credits)", "the_lake_retreat_shuttleport_travel",
+			"Theed Shuttleport A (1,000 Credits)", "theed_shuttleport_a_travel",
+			"Theed Shuttleport B (1,000 Credits)", "theed_shuttleport_b_travel",
+			"Theed Shuttleport C (1,000 Credits)", "theed_shuttleport_c_travel",
+			"Theed Starport (1,000 Credits)", "theed_starport_travel",
+		   },
+		 	"rori",
+		   {
+			"Narmle Shuttleport (1,000 Credits)", "narmel_shuttleport_travel",
+			"Narmle Starport (1,000 Credits)", "narmel_starport_travel",
+			"Rebel Outpost Shuttleport (1,000 Credits)", "rebel_outpost_shuttleport_travel",
+			"Restuss Shuttleport (1,000 Credits)", "restuss_shuttleport_travel",
+			"Restuss Starport (1,000 Credits)", "restuss_starport_travel",
+		   },
+		 	"talus",
+		   {
+			"Talus Dearic Shuttleport (1,000 Credits)", "talus_dearic_shuttleport_travel",
+			"Talus Dearic Starport (1,000 Credits)", "talus_dearic_starport_travel",
+			"Talus Nashal Shuttleport (1,000 Credits)", "talus_nashal_shuttleport_travel",
+			"Talus Imperial Outpost (1,000 Credits)", "talus_imperial_outpost_travel",
+
+		   },
+		 	"tatooine",
+		   {
+			"Anchorhead Shuttleport (1,000 Credits)", "anchorhead_shuttle_travel",
+			"Bestine Shuttleport (1,000 Credits)", "bestine_shuttle_travel",
+			"Bestine Starport (1,000 Credits)", "bestine_starport_travel",
+			"Mos Eisley Shuttleport (1,000 Credits)", "mos_eisley_shuttleport_travel",
+			"Mos Entha Shuttle A (1,000 Credits)", "mos_entha_shuttle_a_travel",
+			"Mos Entha Shuttle B (1,000 Credits)", "mos_entha_shuttle_b_travel",
+			"Mos Entha Spaceport (1,000 Credits)", "mos_entha_spaceport_travel",
+			"Mos Espa Shuttle East (1,000 Credits)", "mos_espa_shuttleport_east_travel",
+			"Mos Espa Shuttle South (1,000 Credits)", "mos_espa_shuttleport_south_travel",
+			"Mos Espa Shuttle West (1,000 Credits)", "mos_espa_shuttleport_west_travel",
+			"Mos Espa Starport (1,000 Credits)", "mos_espa_starport_travel",
+		   },
+		 	"yavin4",
+		   {
+			"Yavin IV Imperial Outpost (1,000 Credits)", "yavin_iv_imperial_outpost_travel",
+			"Yavin IV Labor Outpost (1,000 Credits)", "yavin_iv_labor_outpost_travel",
+		   },
+		 	"endor",
+		   {
+			"Smuggler Outpost (1,000 Credits)", "smuggler_outpost_travel",
+			"Research Outpost (1,000 Credits)", "research_outpost_travel",
+		   },
+		 	"Jedi Enclave",
+		   {
+			"Light Jedi Enclave (1,000 Credits)", "light_enclave_travel",
+			"Dark Jedi Enclave (1,000 Credits)", "dark_enclave_travel",
+		   },
+		 	"Master Politician",
+		   {
+			"Master Politician Requires (10,000,000 Credits)", "citypolitician",
+		   },
+	}
 }
 ObjectTemplates:addTemplate(object_tangible_terminal_terminal_gcw_publish_gift, "object/tangible/terminal/terminal_gcw_publish_gift.iff")
