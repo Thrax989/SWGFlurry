3f321d1491f9e8fa9f883a9499267fcab026d51f
TOXIC <thrax989@yahoo.com>
2018-12-06 16:18:43 -0800

    [Added] GTS Terminals to Player Shuttles Tatooine style

--- MMOCoreORB/bin/scripts/object/building/player/city/shuttleport_tatooine.lua
+++ MMOCoreORB/bin/scripts/object/building/player/city/shuttleport_tatooine.lua
@@ -45,7 +45,7 @@ object_building_player_city_shuttleport_tatooine = object_building_player_city_s
 	planetMapCategory = "shuttleport",
 	lotSize = 0,
 	baseMaintenanceRate = 0,
-	allowedZones = {"dantooine", "lok", "tatooine"},
+	allowedZones = {"corellia", "dantooine", "lok", "naboo", "rori", "talus", "tatooine", "yavin4", "dathomir", "endor"},
 	length = 5,
 	width = 5,
 	cityRankRequired = 4,
@@ -57,8 +57,9 @@ object_building_player_city_shuttleport_tatooine = object_building_player_city_s
 	childObjects = {
 		{templateFile = "object/tangible/terminal/terminal_player_structure.iff", x = -8.5, z = 1.5, y = 14, ox = 0, oy = 0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1},
 		{templateFile = "object/tangible/terminal/terminal_travel.iff", x = -7, z = 0.9, y = 13.5, ox = 0, oy = 0, oz = 0, ow = 1, cellid = -1, containmentType = -1},
-		{templateFile = "object/tangible/travel/ticket_collector/ticket_collector.iff", x = 8, z = 0.9, y = 13.5, ox = 0, oy = 0, oz = 0, ow = 1, cellid = -1, containmentType = -1},
-		{templateFile = "object/creature/npc/theme_park/player_shuttle.iff", x = 0, z = 0.6, y = 0, ox = 0, oy = 1, oz = 0, ow = 0, cellid = -1, containmentType = -1}
+		{templateFile = "object/tangible/travel/ticket_collector/ticket_collector.iff", x = 6, z = 0.9, y = 13.5, ox = 0, oy = 0, oz = 0, ow = 1, cellid = -1, containmentType = -1},
+		{templateFile = "object/creature/npc/theme_park/player_shuttle.iff", x = 0, z = 0.6, y = 0, ox = 0, oy = 1, oz = 0, ow = 0, cellid = -1, containmentType = -1},
+		{templateFile = "object/tangible/terminal/terminal_gcw_publish_gift.iff", x = 8.75, z = 0.9, y = 14.5, ox = 0, oy = 0, oz = 0, ow = 1, cellid = -1, containmentType = -1},
 	}
 }
 
