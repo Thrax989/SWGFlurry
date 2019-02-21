fdc1b3d364235b1f871da316e3011ca382de3124
TOXIC <thrax989@yahoo.com>
2019-02-21 09:13:36 -0800

    Revert "Revert "Revert "[Updated] Travel Tickets to allow Traveling into Cells"""
    This reverts commit 3b6f3fc9ad71e02af47bd535b64671dc2cd2be92.

--- MMOCoreORB/src/server/zone/objects/creature/commands/BoardShuttleCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/BoardShuttleCommand.h
@@ -165,11 +165,6 @@ public:
 		Coordinate p;
 		float x;
 		float y;
-		int cellid = 0;
-		
-		if(arrivalPoint->getCellID() != 0){
-			cellid = arrivalPoint->getCellID();
-		}
 
 		p.initializePosition(arrivalPoint->getArrivalPosition());
 
@@ -198,12 +193,8 @@ public:
 			x = p.getPositionX() + sin(dirRadians) * distance;
 			y = p.getPositionY() + cos(dirRadians) * distance;
 		}
-		
-		if(cellid != 0){
-			creature->switchZone(arrivalZone->getZoneName(), x, p.getPositionZ(), y, cellid);
-		}else{
-			creature->switchZone(arrivalZone->getZoneName(), x, p.getPositionZ(), y, 0);
-		}
+
+		creature->switchZone(arrivalZone->getZoneName(), x, p.getPositionZ(), y, 0);
 
 		// Update the nearest mission for group waypoint for both the arrival and departure planet.
 		if (creature->isGrouped()) {
