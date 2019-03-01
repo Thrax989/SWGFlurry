cace1828cc5d9dbcbd18ace7e9f57403ab89f226
TOXIC <thrax989@yahoo.com>
2019-03-01 11:59:11 -0800

    [Added] All races to all weapons

--- MMOCoreORB/bin/scripts/object/weapon/melee/polearm/crafted_saber/sword_lightsaber_polearm_gen5_exar_kun.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/polearm/crafted_saber/sword_lightsaber_polearm_gen5_exar_kun.lua
@@ -250,8 +250,8 @@ object_weapon_melee_polearm_crafted_saber_sword_lightsaber_polearm_gen5_exar_kun
 	experimentalWeights = {1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1},
 	experimentalGroupTitles = {"null", "null", "expDamage", "expDamage", "expDamage", "expDamage", "expEffeciency", "expEffeciency", "expEffeciency", "expEffeciency"},
 	experimentalSubGroupTitles = {"null", "null", "mindamage", "maxdamage", "attackspeed", "woundchance", "forcecost", "attackhealthcost", "attackactioncost", "attackmindcost"},
-	experimentalMin = {0, 0, 225, 305, 5.1, 25, 58, 60, 95, 45},
-	experimentalMax = {0, 0, 235, 325, 4.8, 50, 52, 45, 65, 40},
+	experimentalMin = {0, 0, 225, 305, 5.1, 25, 55, 60, 95, 45},
+	experimentalMax = {0, 0, 235, 325, 4.8, 50, 48, 45, 65, 40},
 	experimentalPrecision = {0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
 	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1},
 }
