e495a4b5ba14756dbba68a2247cb8f63d0c668ae
TOXIC <thrax989@yahoo.com>
2019-05-07 12:05:05 -0700

    [Updated] TransferItemArmorCommand

--- MMOCoreORB/src/server/zone/objects/creature/commands/TransferItemArmorCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/TransferItemArmorCommand.h
@@ -26,11 +26,13 @@ public:
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
+		/*
 		creature->info("transfer item armor");
 
 		StringBuffer infoMsg;
 		infoMsg << "target: 0x" << hex << target << " arguments" << arguments.toString();
 		creature->info(infoMsg.toString());
+		*/
 
 		StringTokenizer tokenizer(arguments.toString());
 
@@ -171,4 +173,3 @@ public:
 };
 
 #endif //TRANSFERITEMARMORCOMMAND_H_
-
