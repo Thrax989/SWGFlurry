ad261765562a1f1d9bbcb4aae08bd3ee519fc562
TOXIC <thrax989@yahoo.com>
2019-09-22 17:08:48 -0400

    [Updated] Holocron Menu

--- MMOCoreORB/src/server/zone/objects/tangible/components/HolocronMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/HolocronMenuComponent.cpp
@@ -204,19 +204,19 @@ int HolocronMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, Crea
 	}
 	//Unlock Gray Jedi
 	if (selectedID == 220 && (ghost->getJediState() >= 2) && (creature->getScreenPlayState("jediLives") == 0)) {
-		        ManagedReference<SuiMessageBox*> box = new SuiMessageBox(player, SuiWindowType::CITY_ADMIN_CONFIRM_UPDATE_TYPE);
-				player->sendSystemMessage("You Have Unlocked Gray Jedi");
-				int livesLeft = player->getScreenPlayState("jediLives") + 3;
-				player->setScreenPlayState("jediLives", livesLeft);
+		        ManagedReference<SuiMessageBox*> box = new SuiMessageBox(creature, SuiWindowType::CITY_ADMIN_CONFIRM_UPDATE_TYPE);
+				creature->sendSystemMessage("You Have Unlocked Gray Jedi");
+				int livesLeft = creature->getScreenPlayState("jediLives") + 3;
+				creature->setScreenPlayState("jediLives", livesLeft);
 				int jediVis1 = ghost->getVisibility();
 				box->setPromptTitle("Gray Jedi Progress");
 				StringBuffer promptText;
-				String playerName = player->getFirstName();
-				promptText << "\\#00ff00 " << playerName << " Has " << "\\#000000 " << "(" << "\\#ffffff " << player->getScreenPlayState("jediLives") << "\\#000000 " << ")" << "\\#00ff00 " << " Gray Jedi Lives" << endl;
+				String playerName = creature->getFirstName();
+				promptText << "\\#00ff00 " << playerName << " Has " << "\\#000000 " << "(" << "\\#ffffff " << creature->getScreenPlayState("jediLives") << "\\#000000 " << ")" << "\\#00ff00 " << " Gray Jedi Lives" << endl;
 				promptText << "\\#ffffff " << playerName << "\\#00ff00 Your Visibility is at: " << jediVis1;
 				box->setPromptText(promptText.toString());
 				ghost->addSuiBox(box);
-				player->sendMessage(box->generateMessage());
+				creature->sendMessage(box->generateMessage());
 				SkillManager::instance()->awardSkill("combat_jedi_novice", creature, true, true, true);
 				box->setForceCloseDistance(5.f);
 		return 0;
