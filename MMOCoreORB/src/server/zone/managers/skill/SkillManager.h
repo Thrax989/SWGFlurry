ba7fead82e0c4b7cd4efa42129bbda66d1a08984
TOXIC <thrax989@yahoo.com>
2019-10-08 19:43:21 -0700

    [Updated] Skill Manager .h

--- MMOCoreORB/src/server/zone/managers/skill/SkillManager.h
+++ MMOCoreORB/src/server/zone/managers/skill/SkillManager.h
@@ -70,7 +70,9 @@ public:
 
 	void addAbilities(PlayerObject* ghost, const Vector<String>& abilityNames, bool notifyClient = true);
 	void removeAbilities(PlayerObject* ghost, const Vector<String>& abilityNames, bool notifyClient = true);
-
+	void awardResetSkills(CreatureObject* creature);
+	void awardForceFromSkills(CreatureObject* creature);
+	int getSpecificSkillCount(CreatureObject* creature, const String& skill);
 	bool awardSkill(const String& skillName, CreatureObject* creature, bool notifyClient = true, bool awardRequiredSkills = false, bool noXpRequired = false);
 	void awardDraftSchematics(Skill* skill, PlayerObject* ghost, bool notifyClient = true);
 
