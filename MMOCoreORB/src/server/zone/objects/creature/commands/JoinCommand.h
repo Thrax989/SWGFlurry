438c5e076edfbb87b1cf5df7f9121278702e7233
TOXIC <thrax989@yahoo.com>
2019-10-10 22:26:59 -0700

    [Updated] JoinCommand.h

--- MMOCoreORB/src/server/zone/objects/creature/commands/JoinCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/JoinCommand.h
@@ -25,11 +25,10 @@ public:
 
 		GroupManager* groupManager = GroupManager::instance();
 		groupManager->joinGroup(creature);
-
+		creature->playEffect("clienteffect/death_trooper_anti_virus.cef", "");
 		return SUCCESS;
 	}
 
 };
 
 #endif //JOINCOMMAND_H_
-
