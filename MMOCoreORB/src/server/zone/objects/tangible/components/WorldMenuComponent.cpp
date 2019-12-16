c8587d5a252a2e52264d424a27c89f25b0230ad5
TOXIC <thrax989@yahoo.com>
2019-12-15 22:28:42 -0800

    [Updated] World Boss Component Build Error

--- MMOCoreORB/src/server/zone/objects/tangible/components/WorldMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/WorldMenuComponent.cpp
@@ -5,12 +5,23 @@
  *      Author: TOXIC
  */
 
-#include "server/zone/objects/creature/CreatureObject.h"
+
 #include "WorldMenuComponent.h"
+#include "server/zone/objects/creature/CreatureObject.h"
 #include "server/zone/objects/scene/SceneObject.h"
-#include "server/zone/managers/loot/LootManager.h"
-#include "server/zone/Zone.h"
+#include "server/zone/objects/player/PlayerObject.h"
 #include "server/zone/ZoneServer.h"
+#include "server/zone/managers/jedi/JediManager.h"
+#include "server/chat/ChatManager.h"
+#include "server/zone/packets/player/PlayMusicMessage.h"
+#include "server/zone/managers/visibility/VisibilityManager.h"
+#include "server/zone/objects/player/sui/inputbox/SuiInputBox.h"
+#include "server/zone/managers/loot/LootManager.h"
+#include "server/zone/managers/player/PlayerManager.h"
+#include "server/chat/ChatManager.h"
+#include "server/zone/packets/player/PlayMusicMessage.h"
+#include "server/zone/packets/object/ObjectMenuResponse.h"
+#include "server/zone/objects/building/BuildingObject.h"
 
 void WorldMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, ObjectMenuResponse* menuResponse, CreatureObject* player) const {
 
