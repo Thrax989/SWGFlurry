cacc0ab8272fa78dfcd7c51f2bd25b80736134f4
TOXIC <thrax989@yahoo.com>
2019-12-15 21:18:06 -0800

    [Updated] Wolrd Boss Crate

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/item/loot_crates/base_crate_02.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/item/loot_crates/base_crate_02.lua
@@ -1,4 +1,4 @@
 object_tangible_item_loot_crates_base_crate_02 = object_tangible_item_loot_crates_shared_base_crate_02:new {
-
+        objectMenuComponent = "WorldMenuComponent",
 }
 ObjectTemplates: addTemplate(object_tangible_item_loot_crates_base_crate_02, "object/tangible/item/loot_crates/base_crate_02.iff")
