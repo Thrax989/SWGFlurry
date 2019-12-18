08d40bbb75ee1671bbac53ca09d28c2743b71247
TOXIC <thrax989@yahoo.com>
2019-12-18 10:41:32 -0800

    [Created] Vehicle 16 Item Template

--- MMOCoreORB/bin/scripts/loot/items.lua
+++ MMOCoreORB/bin/scripts/loot/items.lua
@@ -1290,6 +1290,7 @@ includeFile("items/vehicle/vehicle12.lua")
 includeFile("items/vehicle/vehicle13.lua")
 includeFile("items/vehicle/vehicle14.lua")
 includeFile("items/vehicle/vehicle15.lua")
+includeFile("items/vehicle/vehicle16.lua")
 
 --Rare Vehicle Deeds
 includeFile("items/vehicle/rarevehicle1.lua")
