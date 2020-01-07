25c476a6bd148a470c79e0c0dd3692b0d7894913
TOXIC <thrax989@yahoo.com>
2020-01-06 16:27:26 -0800

    [Fixed] Robe Template Generation

--- MMOCoreORB/bin/scripts/loot/custom_loot/items/wearables/robe5.lua
+++ MMOCoreORB/bin/scripts/loot/custom_loot/items/wearables/robe5.lua
@@ -1,4 +1,4 @@
-robe1 = {
+robe5 = {
   minimumLevel = 0,
   maximumLevel = -1,
   customObjectName = "",
@@ -12,4 +12,4 @@ robe1 = {
   junkMaxValue = 110
 }
 
-addLootItemTemplate("robe1", robe1)
+addLootItemTemplate("robe5", robe5)
