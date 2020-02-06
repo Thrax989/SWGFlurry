b53c63748f2cca2103360b379f387af611ecd03b
TOXIC <thrax989@yahoo.com>
2020-02-05 23:53:34 -0800

    [Updated] Ig88 Dungeon ***

--- MMOCoreORB/bin/scripts/screenplays/dungeon/ig88/ig88_platform.lua
+++ MMOCoreORB/bin/scripts/screenplays/dungeon/ig88/ig88_platform.lua
@@ -96,7 +96,7 @@ local x2 = boss:getPositionX()
 local y2 = boss:getPositionY()
 
 local distance = ((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1))
-local maxDistance = 100 --Max distance you can fight the boss is 100 meeters, you must be within range to fight the boss. Resets to full health if you fail the check.
+local maxDistance = 75 --Max distance you can fight the boss is 75 meeters, you must be within range to fight the boss. Resets to full health if you fail the check.
 if distance > (maxDistance * maxDistance) then
       forcePeace(pBoss)
       forcePeace(pBoss)
