b6daa8454fff148ef87542701daefe734f4d2825
TOXIC <thrax989@yahoo.com>
2020-02-06 11:22:01 -0800

    [Updated] Faction Selection  At Creation

--- MMOCoreORB/src/server/zone/objects/player/sui/SuiWindowType.h
+++ MMOCoreORB/src/server/zone/objects/player/sui/SuiWindowType.h
@@ -273,7 +273,9 @@ public:
 		SHUTTLE_BEACON					   = 1039,
 		FIELD_FACTION_CHANGE			   = 1040,
 		GOTO_LIST						   = 1041,
-		FS_EXP_CONVERT 					   = 1042
+		FS_EXP_CONVERT 					   = 1042,
+		FACTION_SELECT						   = 1043
+
 	};
 };
 
