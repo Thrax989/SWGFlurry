66ddc212cf13fba4491ec1ea3a0b972f1c7109d3
TOXIC <thrax989@yahoo.com>
2020-02-07 15:43:42 -0800

    [Updated] Nightsister Dungeon Loot Groups

--- MMOCoreORB/bin/scripts/mobile/custom_content/dungeon/ig_88/ig_88_boss.lua
+++ MMOCoreORB/bin/scripts/mobile/custom_content/dungeon/ig_88/ig_88_boss.lua
@@ -27,7 +27,44 @@ ig_88_boss = Creature:new {
 	scale = 1.3,
 
 	templates = {"object/mobile/ig_88_rocket.iff"},
-	lootGroups = {},
+	lootGroups = {
+		{
+			groups = {
+				{group = "clothing_attachments", chance = 10000000},
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "armor_attachments", chance = 10000000},
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "weapons_all", chance = 10000000},
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "tierone", chance = 1500000},
+				{group = "tiertwo", chance = 3500000},
+				{group = "tierthree", chance = 2500000},
+				{group = "tierdiamond", chance = 2500000},
+			},
+			lootChance = 2000000
+		},
+		{
+			groups = {
+				{group = "tierone", chance = 1500000},
+				{group = "tiertwo", chance = 3500000},
+				{group = "tierthree", chance = 2500000},
+				{group = "tierdiamond", chance = 2500000},
+			},
+			lootChance = 2000000
+		}
+	},
 	weapons = {"tusken_weapons"},
 	reactionStf = "@npc_reaction/slang",
 	attacks = merge(commandomaster,marksmanmaster,tkamaster,brawlermaster,fencermaster,swordsmanmaster,pikemanmaster,riflemanmaster,pistoleermaster)
