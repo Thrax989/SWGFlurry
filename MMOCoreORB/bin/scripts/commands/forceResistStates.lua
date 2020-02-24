0d008fc54d91f7681af65160575e51276afa2d5f
sygnum <jtrobbinsfl@gmail.com>
2020-02-24 16:57:45 -0500

    Force Resist States Duration adjusted 30min

--- MMOCoreORB/bin/scripts/commands/forceResistStates.lua
+++ MMOCoreORB/bin/scripts/commands/forceResistStates.lua
@@ -44,7 +44,7 @@
 ForceResistStatesCommand = {
 	name = "forceresiststates",
 	forceCost = 250,
-	duration = 900,
+	duration = 1800,
 	--animationCRC = hashCode()
 	clientEffect = "clienteffect/pl_force_resist_states_self.cef",
 	frsLightBuffModifier = 0.35,
