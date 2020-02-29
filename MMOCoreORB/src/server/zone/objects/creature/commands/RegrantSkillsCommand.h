124f30db9ec944d502bee31aa1a424ea1d989236
Sygnum <jtrobbinsfl@gmail.com>
2020-02-28 20:44:12 -0500

    Modified: Reworked regrantskills command (ty, mikeface)

--- MMOCoreORB/src/server/zone/objects/creature/commands/RegrantSkillsCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RegrantSkillsCommand.h
@@ -7,6 +7,8 @@
 
 #include "server/zone/objects/scene/SceneObject.h"
 #include "server/zone/managers/skill/SkillManager.h"
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>         // std::chrono::seconds
 
 class RegrantSkillsCommand : public QueueCommand {
 public:
@@ -16,12 +18,88 @@ public:
 	}
 
 	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
-
 		if (!checkStateMask(creature))
 			return INVALIDSTATE;
 
-		if (!checkInvalidLocomotions(creature))
+		if (!checkInvalidLocomotions(creature)) 
 			return INVALIDLOCOMOTION;
+
+
+		// This command only works on the player executing the command
+
+		Locker locker(creature);
+
+		SkillManager* skillManager = SkillManager::instance();
+		const SkillList* skillList = creature->getSkillList();
+
+		if (skillList == nullptr) 
+			return GENERALERROR;
+			
+/*	
+		if (!creature->checkCooldownRecovery("regrantSkills")) {
+                    creature->sendSystemMessage("You cannot have your skill regranted more than once per 12 hours.");
+                    return GENERALERROR;
+                }
+		creature->updateCooldownTimer("regrantSkills", 43200000);
+*/
+		String skillName = "";
+		Vector<String> listOfNames;
+		skillList->getStringList(listOfNames);
+		SkillList copyOfList;
+		copyOfList.loadFromNames(listOfNames);
+
+
+		for (int i = 0; i < copyOfList.size(); i++) {
+			Skill* skill = copyOfList.get(i);
+			String skillName = skill->getSkillName();
+
+			if (!skillName.beginsWith("admin") && (skillName.contains("master"))) {
+				skillManager->surrenderSkill(skillName, creature, true);
+			}
+		}
+		for (int i = 0; i < copyOfList.size(); i++) {
+			Skill* skill = copyOfList.get(i);
+			String skillName = skill->getSkillName();
+			if (!skillName.beginsWith("admin") && (skillName.contains("04"))) {
+				skillManager->surrenderSkill(skillName, creature, true);
+			}
+		}
+		for (int i = 0; i < copyOfList.size(); i++) {
+			Skill* skill = copyOfList.get(i);
+			String skillName = skill->getSkillName();
+			if (!skillName.beginsWith("admin") && (skillName.contains("03"))) {
+				skillManager->surrenderSkill(skillName, creature, true);
+			}
+		}
+		for (int i = 0; i < copyOfList.size(); i++) {
+			Skill* skill = copyOfList.get(i);
+			String skillName = skill->getSkillName();
+			if (!skillName.beginsWith("admin") && (skillName.contains("02"))) {
+				skillManager->surrenderSkill(skillName, creature, true);
+			}
+		}
+		for (int i = 0; i < copyOfList.size(); i++) {
+			Skill* skill = copyOfList.get(i);
+			String skillName = skill->getSkillName();
+			if (!skillName.beginsWith("admin") && (skillName.contains("01"))) {
+				skillManager->surrenderSkill(skillName, creature, true);
+			}
+		}
+		for (int i = 0; i < copyOfList.size(); i++) {
+			Skill* skill = copyOfList.get(i);
+			String skillName = skill->getSkillName();
+			if (!skillName.beginsWith("admin") && (skillName.contains("novice"))) {
+				skillManager->surrenderSkill(skillName, creature, true);
+			}
+		}
+		for (int i = 0; i < copyOfList.size(); i++) {
+			Skill* skill = copyOfList.get(i);
+			String skillName = skill->getSkillName();
+			if (!skillName.beginsWith("admin")) {
+				bool skillGranted = skillManager->awardSkill(skillName, creature, true, true, true);
+				creature->sendSystemMessage("Regranting SKill: " + skillName);
+			}
+		}
 
 		return SUCCESS;
 	}
