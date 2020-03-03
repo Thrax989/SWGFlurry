2ca66a2388d0d53578560c754058410ac961b55b
Sygnum <jtrobbinsfl@gmail.com>
2020-03-02 21:48:46 -0500

    Updated: krayt_pearl to pearls_flawless

--- MMOCoreORB/bin/scripts/mobile/tatooine/juvenile_canyon_krayt_dragon.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/juvenile_canyon_krayt_dragon.lua
@@ -34,7 +34,7 @@ juvenile_canyon_krayt_dragon = Creature:new {
 	        	groups = {
 				{group = "krayt_tissue_common", chance = 2500000},
 				{group = "krayt_dragon_common", chance = 3500000},
-				{group = "armor_all", chance = 2000000},
+				{group = "pearl_flawless", chance = 2000000},
 				{group = "weapons_all", chance = 2000000},
 			},
 			lootChance = 3440000
