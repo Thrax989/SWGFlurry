14d4914abd3f13946445b56de175c0a2cfb83b9c
Sygnum <jtrobbinsfl@gmail.com>
2020-03-27 09:57:44 -0400

    [Update] Adjusted Event Bosses, fixed bugged HAM dispaly

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/event/event_carax.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/event/event_carax.lua
@@ -1,14 +1,14 @@
 event_carax = Creature:new {
 	customName = "Carax Nightwraith",
-	socialGroup = "event",
+	socialGroup = "mercenary",
 	faction = "",
 	level = 300,
 	chanceHit = 50.0,
 	damageMin = 3000,
 	damageMax = 6000,
-	baseXp = 28549,
-	baseHAM = 9500000,
-	baseHAMmax = 9900000,
+	baseXp = 128549,
+	baseHAM = 3900000,
+	baseHAMmax = 4000000,
 	armor = 3,
 	resists = {195,195,195,195,195,195,195,195,195},
 	meatType = "meat_carnivore",
@@ -30,9 +30,39 @@ event_carax = Creature:new {
 	lootGroups = {
 		{
 			groups = {
-				{group = "goggles_all", chance = 10000000},
+				{group = "g_named_crystals", chance = 10000000},
 			},
-			lootChance = 1000000
+			lootChance = 5000000
+		},
+		{
+			groups = {
+				{group = "g_named_crystals", chance = 10000000},
+			},
+			lootChance = 5000000
+		},
+		{
+			groups = {
+				{group = "g_cloak_of_hate", chance = 10000000},
+			},
+			lootChance = 500000
+		},
+		{
+			groups = {
+				{group = "capes", chance = 10000000},
+			},
+			lootChance = 1500000
+		},
+		{
+			groups = {
+				{group = "g_cloak_of_hate", chance = 10000000},
+			},
+			lootChance = 1500000
+		},
+		{
+			groups = {
+				{group = "capes", chance = 10000000},
+			},
+			lootChance = 1500000
 		},
 		{
 			groups = {
@@ -42,63 +72,69 @@ event_carax = Creature:new {
 		},
 		{
 			groups = {
+				{group = "goggles_all", chance = 10000000},
+			},
+			lootChance = 2000000
+		},
+		{
+			groups = {
 				{group = "neck_crafter", chance = 10000000},
 			},
-			lootChance = 1000000
+			lootChance = 2000000
 		},
 		{
 			groups = {
 				{group = "non_jedi_ring_crafter_second", chance = 10000000},
 			},
-			lootChance = 1000000
+			lootChance = 2000000
 		},
 		{
 			groups = {
 				{group = "non_jedi_ring_crafter", chance = 10000000},
 			},
-			lootChance = 1000000
+			lootChance = 2000000
 		},
 		{
 			groups = {
 				{group = "non_jedi_rings_ranged", chance = 10000000},
 			},
-			lootChance = 1000000
+			lootChance = 2000000
 		},
 		{
 			groups = {
 				{group = "non_jedi_rings", chance = 10000000},
 			},
-			lootChance = 1000000
+			lootChance = 2000000
 		},
 		{
 			groups = {
 				{group = "jedi_earings", chance = 10000000},
 			},
-			lootChance = 1000000
+			lootChance = 2000000
 		},
 		{
 			groups = {
 				{group = "jedi_bracelets", chance = 10000000},
 			},
-			lootChance = 1000000
+			lootChance = 2000000
 		},
 		{
 			groups = {
 				{group = "jedi_neck", chance = 10000000},
 			},
-			lootChance = 1000000
+			lootChance = 2000000
 		},
 		{
 			groups = {
 				{group = "jedi_rings", chance = 10000000},
 			},
-			lootChance = 1000000
+			lootChance = 2000000
 		},
 		{
 			groups = {
 				{group = "g_necklaces", chance = 10000000},
 			},
-			lootChance = 1000000
+			lootChance = 2000000
 		},
 		{
 			groups = {
