0ac65f796dd4719f27b18027c057f529537d11f9
bigtap <24537455+bigtap@users.noreply.github.com>
2020-04-07 18:37:55 -0700

    New world boss

---
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/worldboss/worldboss_6.lua
@@ -0,0 +1,174 @@
+worldboss_6 = Creature:new {
+	customName = "\\#00ff00<<< Malevolent Gurk >>> \\#ff0000[lvl 300]",
+	socialGroup = "gurk",
+	faction = "",
+	level = 300,
+	chanceHit = 50.0,
+	damageMin = 3000,
+	damageMax = 6000,
+	baseXp = 28549,
+	baseHAM = 990000,
+	baseHAMmax = 1000000,
+	armor = 3,
+	resists = {75,75,75,75,75,75,75,75,75},
+	meatType = "meat_herbivore",
+	meatAmount = 1500,
+	hideType = "hide_leathery",
+	hideAmount = 1500,
+	boneType = "bone_mammal",
+	boneAmount = 1500,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 30,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = PACK + HERD + KILLER,
+	optionsBitmask = AIENABLED,
+	diet = CARNIVORE,
+
+	templates = {"object/mobile/recluse_gurk_king.iff"},
+	hues = { 16, 17, 18, 19, 20, 21, 22, 23 },
+	scale = 2.0,
+	lootGroups = {
+		{
+			groups = {
+				{group = "goggles_all", chance = 10000000},
+			},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "nge_all", chance = 10000000},
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "neck_crafter", chance = 10000000},
+			},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "non_jedi_ring_crafter_second", chance = 10000000},
+			},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "non_jedi_ring_crafter", chance = 10000000},
+			},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "non_jedi_rings_ranged", chance = 10000000},
+			},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "non_jedi_rings", chance = 10000000},
+			},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "jedi_earings", chance = 10000000},
+			},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "jedi_bracelets", chance = 10000000},
+			},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "jedi_neck", chance = 10000000},
+			},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "jedi_rings", chance = 10000000},
+			},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "g_necklaces", chance = 10000000},
+			},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "clothing_attachments", chance = 10000000},
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "armor_attachments", chance = 10000000},
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "weapons_all", chance = 10000000},
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "worldbosscrate", chance = 10000000},
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "vehicledeedsnormal", chance = 10000000},
+			},
+			lootChance = 5000000
+		},
+		{
+			groups = {
+				{group = "vehicledeedsrare", chance = 10000000},
+			},
+			lootChance = 2500000
+		},
+		{
+			groups = {
+				{group = "tierone", chance = 1500000},
+				{group = "tiertwo", chance = 3500000},
+				{group = "tierthree", chance = 2500000},
+				{group = "tierdiamond", chance = 2500000},
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "tierone", chance = 1500000},
+				{group = "tiertwo", chance = 3500000},
+				{group = "tierthree", chance = 2500000},
+				{group = "tierdiamond", chance = 2500000},
+			},
+			lootChance = 10000000
+		}
+	},
+	weapons = {},
+	conversationTemplate = "",
+	attacks = {
+		{"posturedownattack",""},
+                {"creatureareacombo","stateAccuracyBonus=100"},
+		{"creatureareaknockdown","stateAccuracyBonus=100"},
+		{"posturedownattack","stateAccuracyBonus=100"},
+		{"creatureareacombo","stateAccuracyBonus=100"},
+                {"creatureareadisease",""},
+		{"creatureareapoison",""},
+		{"creatureareableeding",""},
+		{"blindattack",""}
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(worldboss_6, "worldboss_6")
