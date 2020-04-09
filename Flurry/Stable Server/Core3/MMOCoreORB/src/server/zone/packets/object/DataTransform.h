e7143f1bc32740cd251aae8f7f0e93a822963601
TOXIC <thrax989@yahoo.com>
2020-04-08 18:37:01 -0700

    [Updated] Title Limitations to only Admins

--- Flurry/Stable Server/Core3/MMOCoreORB/src/server/zone/packets/object/DataTransform.h
+++ Flurry/Stable Server/Core3/MMOCoreORB/src/server/zone/packets/object/DataTransform.h
@@ -193,7 +193,7 @@ public:
 		ValidatedPosition pos;
 		pos.update(object);
 
-		if (!ghost->hasGodMode()) {
+		if (!ghost->getAdminLevel() == 15) {
 			SceneObject* inventory = object->getSlottedObject("inventory");
 
 			if (inventory != nullptr && inventory->getCountableObjectsRecursive() > inventory->getContainerVolumeLimit() + 1) {
