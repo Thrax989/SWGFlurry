e59f7adc269ae49477765529ac431e889c24a5ad
Sygnum <jtrobbinsfl@gmail.com>
2020-04-13 10:27:40 -0400

    [Fixed] Typo loot group

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/mandalore/PykeSyndicateCriminal.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/mandalore/PykeSyndicateCriminal.lua
@@ -38,7 +38,7 @@ pyke_syndicate_criminal = Creature:new {
 		},
 		{
 			groups = {
-				{group = "mandalorian_armor", chance = 10000000},
+				{group = "mandalore_armor", chance = 10000000},
 			},
 			lootChance = 500000
 		},
