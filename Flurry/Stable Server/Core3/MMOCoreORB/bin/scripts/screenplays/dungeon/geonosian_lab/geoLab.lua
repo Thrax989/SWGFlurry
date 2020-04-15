263ba9af599ffd76c7d6bdc6b53be43c57b5db07
TOXIC <thrax989@yahoo.com>
2020-04-14 19:10:11 -0700

    [Updated] Geo Cave Boss

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/dungeon/geonosian_lab/geoLab.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/dungeon/geonosian_lab/geoLab.lua
@@ -420,7 +420,7 @@ function GeonosianLab:spawnMobiles()
 	spawnMobile("yavin4", "cavern_spider",180,13.4,-22.0,-337.3,-179,1627822)
 
 	-- largeendcave (1627823)
-	spawnMobile("yavin4", "acklayboss",3600,101.1,-34.3,-321.6,-136,1627823, true) --Randomized respawn
+	spawnMobile("yavin4", "acklay",3600,101.1,-34.3,-321.6,-136,1627823, true) --Randomized respawn
 	spawnMobile("yavin4", "enhanced_kwi",180,48.0,-34.0,-334.4,0,1627823)
 	spawnMobile("yavin4", "cavern_spider",180,91.2,-33.9,-347.9,5,1627823)
 	spawnMobile("yavin4", "enhanced_kliknik",180,98.0,-34.1,-334.4,-53,1627823)
