555e06d964e457da52324880344177f1c1a21d07
TOXIC <thrax989@yahoo.com>
2020-04-24 14:34:55 -0700

    [Added] Craftable Rebel Assault GCW Armor Added Craftable Rebel Battle Armor Added Craftable Rebel Marine Armor Added Craftable Rebel Snow
    ADDED ALL REMAINING CRAFTED ARMOR

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/armor/armor_appearance_rebel_specforce_utility_belt.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/armor/armor_appearance_rebel_specforce_utility_belt.lua
@@ -1,10 +1,8 @@
 object_draft_schematic_armor_armor_appearance_rebel_specforce_utility_belt = object_draft_schematic_armor_shared_armor_appearance_rebel_specforce_utility_belt:new {
 
-   templateType = DRAFTSCHEMATIC,
+   customObjectName = "Armor Belt",
 
-   customObjectName = "",
-
-   craftingToolTab = 2, -- (See DraftSchematicObjectTemplate.h)
+   craftingToolTab = 2, -- (See DraftSchemticImplementation.h)
    complexity = 40, 
    size = 4, 
 
@@ -14,6 +12,7 @@ object_draft_schematic_armor_armor_appearance_rebel_specforce_utility_belt = obj
    assemblySkill = "armor_assembly", 
    experimentingSkill = "armor_experimentation", 
    customizationSkill = "armor_customization", 
+   disableFactoryRun = true,
 
    customizationOptions = {},
    customizationStringNames = {},
@@ -22,7 +21,7 @@ object_draft_schematic_armor_armor_appearance_rebel_specforce_utility_belt = obj
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
    ingredientTitleNames = {"auxilary_coverage", "body", "liner", "hardware_and_attachments", "binding_and_reinforcement", "padding", "armor", "load_bearing_harness", "reinforcement"},
    ingredientSlotType = {0, 0, 0, 0, 0, 0, 1, 1, 1},
-   resourceTypes = {"hide_leathery_lok", "hide_scaley", "fiberplast_corellia", "metal", "petrochem_inert_polymer", "hide_wooly", "object/tangible/component/armor/shared_armor_segment_padded.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
+   resourceTypes = {"hide_leathery_lok", "hide_scaley", "fiberplast_corellia", "metal", "petrochem_inert_polymer", "hide_wooly", "object/tangible/component/armor/armor_segment_ris_acklay.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
    resourceQuantities = {10, 10, 5, 5, 3, 2, 1, 1, 1},
    contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100},
 
