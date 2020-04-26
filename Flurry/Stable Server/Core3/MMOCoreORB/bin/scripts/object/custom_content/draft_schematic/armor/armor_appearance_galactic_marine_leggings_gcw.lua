c08c445aae4352f671ace1499f2752d85d30cdfd
TOXIC <thrax989@yahoo.com>
2020-04-26 14:35:08 -0700

    [Fixed] More Server Load Errors with Craftable Items

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/armor/armor_appearance_galactic_marine_leggings_gcw.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/armor/armor_appearance_galactic_marine_leggings_gcw.lua
@@ -2,7 +2,7 @@ object_draft_schematic_armor_armor_appearance_galactic_marine_leggings_gcw = obj
 
    templateType = DRAFTSCHEMATIC,
 
-   customObjectName = "Composite Armor Pants",
+   customObjectName = "Armor Pants",
 
    craftingToolTab = 2, -- (See DraftSchematicObjectTemplate.h)
    complexity = 45, 
