99f497d9f9ecc122e757cb539dbd7df6a3b9e042
TOXIC <thrax989@yahoo.com>
2020-04-27 09:23:56 -0700

    [Fixed] Bugged GCW Battle Marine Armor

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/armor/armor_appearance_galactic_marine_chest_gcw.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/armor/armor_appearance_galactic_marine_chest_gcw.lua
@@ -27,7 +27,7 @@ object_draft_schematic_armor_armor_appearance_galactic_marine_chest_gcw = object
    contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100},
 
 
-   targetTemplate = "object/tangible/wearables/armor/galactic_marine/armor_galactic_marine_chest_gcw.iff",
+   targetTemplate = "object/tangible/wearables/armor/galactic_marine/armor_galactic_marine_chest_plate_gcw.iff",
 
    additionalTemplates = {
               }
