d271c38f66785a25e494d9e9c6f2e170c8bd2d18
TOXIC <thrax989@yahoo.com>
2020-04-27 18:59:28 -0700

    [Fixed] Bone Armor To Act Like Comp when Crafting

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_bone_bicep_l.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_bone_bicep_l.lua
@@ -48,27 +48,26 @@ object_draft_schematic_clothing_clothing_armor_bone_bicep_l = object_draft_schem
    customObjectName = "Bone Armor Left Bicep",
 
    craftingToolTab = 2, -- (See DraftSchematicObjectTemplate.h)
-   complexity = 14, 
-   size = 3, 
+   complexity = 45, 
+   size = 4, 
 
-   xpType = "crafting_general", 
-   xp = 130, 
+   xpType = "crafting_clothing_armor", 
+   xp = 420, 
 
-   assemblySkill = "general_assembly", 
-   experimentingSkill = "general_experimentation", 
+   assemblySkill = "armor_assembly", 
+   experimentingSkill = "armor_experimentation", 
    customizationSkill = "armor_customization", 
 
    customizationOptions = {2},
    customizationStringNames = {"/private/index_color_1"},
-   customizationDefaults = {7},
+   customizationDefaults = {0},
 
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
    ingredientTitleNames = {"auxilary_coverage", "body", "liner", "hardware_and_attachments", "binding_and_reinforcement", "padding", "armor", "load_bearing_harness", "reinforcement"},
    ingredientSlotType = {0, 0, 0, 0, 0, 0, 1, 1, 1},
-   resourceTypes = {"bone", "hide", "hide", "metal", "petrochem_inert", "hide", "object/tangible/component/armor/shared_armor_segment_bone.iff", "object/tangible/component/clothing/shared_fiberplast_panel.iff", "object/tangible/component/clothing/shared_fiberplast_panel.iff"},
-   resourceQuantities = {15, 15, 15, 10, 5, 5, 1, 1, 1},
+   resourceTypes = {"ore_intrusive", "fuel_petrochem_solid_known", "fiberplast_naboo", "aluminum", "copper_beyrllius", "hide_wooly", "object/tangible/component/armor/shared_armor_segment_composite.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
+   resourceQuantities = {50, 50, 25, 30, 20, 20, 2, 1, 1},
    contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100},
-
 
    targetTemplate = "object/tangible/wearables/armor/bone/armor_bone_s01_bicep_l.iff",
 
