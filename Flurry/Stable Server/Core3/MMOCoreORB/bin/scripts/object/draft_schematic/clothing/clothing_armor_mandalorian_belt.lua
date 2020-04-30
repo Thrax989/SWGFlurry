c131bf0cb6ce6040113f1c941206bcc76882143a
TOXIC <thrax989@yahoo.com>
2020-04-29 21:09:39 -0400

    [Updated] Mandalorian Belts

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_mandalorian_belt.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_mandalorian_belt.lua
@@ -46,12 +46,12 @@ templateType = DRAFTSCHEMATIC,
 
    customObjectName = "Mandalorian Armor Belt",
 
-   craftingToolTab = 2, -- (See DraftSchematicObjectTemplate.h)
-   complexity = 20, 
+   craftingToolTab = 2, -- (See DraftSchemticImplementation.h)
+   complexity = 40, 
    size = 4, 
 
    xpType = "crafting_clothing_armor", 
-   xp = 40, 
+   xp = 80, 
 
    assemblySkill = "armor_assembly", 
    experimentingSkill = "armor_experimentation", 
@@ -61,12 +61,12 @@ templateType = DRAFTSCHEMATIC,
    customizationStringNames = {},
    customizationDefaults = {},
 
-   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
-   ingredientTitleNames = {"auxilary_coverage", "body", "liner"},
-   ingredientSlotType = {0, 0, 1},
-   resourceTypes = {"metal", "hide", "object/tangible/component/clothing/shared_fiberplast_panel.iff"},
-   resourceQuantities = {15, 4, 1},
-   contribution = {100, 100, 100},
+   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
+   ingredientTitleNames = {"auxilary_coverage", "body", "liner", "hardware_and_attachments", "binding_and_reinforcement", "padding", "armor", "load_bearing_harness", "reinforcement"},
+   ingredientSlotType = {0, 0, 0, 0, 0, 0, 1, 1, 1},
+   resourceTypes = {"hide_leathery_lok", "hide_scaley", "fiberplast_corellia", "metal", "petrochem_inert_polymer", "hide_wooly", "object/tangible/component/armor/armor_segment_ris.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
+   resourceQuantities = {10, 10, 5, 5, 3, 2, 1, 1, 1},
+   contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100},
 
 
    targetTemplate = "object/tangible/wearables/armor/mandalorian/armor_mandalorian_belt.iff",
