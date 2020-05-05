6d9f69a5b90610dbc22fe5e406b4c833fb622476
bigtap <24537455+bigtap@users.noreply.github.com>
2020-05-04 20:48:41 -0700

    Create baz_nitch_terror.lua

---
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/mandalore/baz_nitch_terror.lua
@@ -0,0 +1,51 @@
+baz_nitch_terror = Creature:new {
+	customName = "\\#00ff00<<< Baz Nitch Terror >>> \\#ff0000[lvl 100]",
+	socialGroup = "baz_nitch",
+	faction = "",
+	level = 100,
+	chanceHit = 50.0,
+	damageMin = 250,
+	damageMax = 500,
+	baseXp = 5000,
+	baseHAM = 6000,
+	baseHAMmax = 10000,
+	armor = 1,
+	resists = {20,20,20,20,20,20,20,20,10},
+	meatType = "meat_wild",
+	meatAmount = 5,
+	hideType = "hide_leathery",
+	hideAmount = 2,
+	boneType = "bone_mammal",
+	boneAmount = 2,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 30,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = PACK + HERD + KILLER,
+	optionsBitmask = AIENABLED,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/baz_nitch_hue.iff"},
+	hues = { 16, 17, 18, 19, 20, 21, 22, 23 },
+	controlDeviceTemplate = "object/intangible/pet/nuna_hue.iff",
+        scale = 1.2, 
+	lootGroups = {
+		{
+			groups = {
+				{group = "tierone", chance = 1500000},
+				{group = "tiertwo", chance = 3500000},
+				{group = "tierthree", chance = 2500000},
+				{group = "tierdiamond", chance = 2500000},
+			},
+			lootChance = 5000000
+		}
+},
+	weapons = {},
+	conversationTemplate = "",
+	attacks = {
+		{"creatureareableeding",""},
+		{"blindattack",""}
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(baz_nitch_terror, "baz_nitch_terror")
