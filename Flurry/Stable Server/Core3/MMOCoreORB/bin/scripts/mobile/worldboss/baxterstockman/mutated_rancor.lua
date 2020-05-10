140703bfedd5e1246fc5d2fc562cc8ee197088a6
Sygnum <jtrobbinsfl@gmail.com>
2020-05-10 01:39:32 -0400

    New World Boss

---
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/worldboss/baxterstockman/mutated_rancor.lua
@@ -0,0 +1,133 @@
+mutated_rancor = Creature:new {
+	customName = "Mutated Rancor",
+	randomNameType = NAME_GENERIC,
+	randomNameTag = true,
+	socialGroup = "mercenary",
+	faction = "",
+	level = 300,
+	chanceHit = 35.00,
+	damageMin = 1250,
+	damageMax = 2800,
+	baseXp = 2500000,
+	baseHAM = 400000,
+	baseHAMmax = 600000,
+	armor = 3,
+	resists = {95,95,95,95,95,95,95,95,95},
+	meatType = "meat_carnivore",
+	meatAmount = 2500,
+	hideType = "hide_leathery",
+	hideAmount = 2500,
+	boneType = "bone_mammal",
+	boneAmount = 2500,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 10,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = PACK + KILLER + STALKER,
+	optionsBitmask = AIENABLED,
+	diet = CARNIVORE,
+	scale = 2.0,
+
+	templates = {"object/mobile/shared_bm_mutated_rancor.iff"},
+  hues = { 16, 17, 18, 19, 20, 21, 22, 23 },
+  
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 5000000},
+				{group = "armor_all", chance = 2500000},
+				{group = "weapons_all", chance = 2500000}
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "junk", chance = 5000000},
+				{group = "armor_all", chance = 2500000},
+				{group = "weapons_all", chance = 2500000}
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "junk", chance = 5000000},
+				{group = "armor_all", chance = 2500000},
+				{group = "weapons_all", chance = 2500000}
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "junk", chance = 5000000},
+				{group = "armor_all", chance = 2500000},
+				{group = "weapons_all", chance = 2500000}
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "junk", chance = 5000000},
+				{group = "armor_all", chance = 2500000},
+				{group = "weapons_all", chance = 2500000}
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "junk", chance = 5000000},
+				{group = "armor_all", chance = 2500000},
+				{group = "weapons_all", chance = 2500000}
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "junk", chance = 5000000},
+				{group = "armor_all", chance = 2500000},
+				{group = "weapons_all", chance = 2500000}
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "junk", chance = 5000000},
+				{group = "armor_all", chance = 2500000},
+				{group = "weapons_all", chance = 2500000}
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "junk", chance = 5000000},
+				{group = "armor_all", chance = 2500000},
+				{group = "weapons_all", chance = 2500000}
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "wearables_all", chance = 5000000},
+				{group = "loot_kit_parts", chance = 2500000},
+				{group = "tailor_components", chance = 2500000}
+			},
+			lootChance = 10000000
+		},
+		},
+	weapons = {"creature_spit_heavy_flame"},
+	conversationTemplate = "",
+	attacks = {
+		{"creatureareacombo","StateAccuracyBonus=100"},
+		{"creatureareaknockdown","StateAccuracyBonus=100"},
+		{"knockdownattack","KnockdownChance=100"},
+		{"creatureareaknockdown","KnockdownChance=100"},
+		{"dizzyattack","DizzyChance=100"},
+		{"stunattack","StunChance=100"},
+		{"mildpoison","PoisonChance=100"},
+		{"intimidationattack","IntimidationChance=100"},
+		{"mediumpoison","PoisonChance=100"},
+		{"creatureareapoison","PoisonChance=100"},
+		{"strongpoison","PoisonChance=100"},
+		{"creatureareaattack","StateAccuracyBonus=100"}
+	}
+}
+CreatureTemplates:addCreatureTemplate(mutated_rancor, "mutated_rancor")
