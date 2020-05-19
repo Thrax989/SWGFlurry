b2937e169e939edc13f4c452a26be497310c9fdc
TOXIC <thrax989@yahoo.com>
2020-05-18 22:06:16 -0700

    [Updated] Error

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/events/worldboss_four.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/events/worldboss_four.lua
@@ -99,7 +99,6 @@ function worldboss_fourScreenplay:KillBoss(pBoss)
 	if SceneObject(pBoss) then
 		print("Unlooted World Boss One Destroyed")
 		SceneObject(pBoss):destroyObjectFromWorld()
-		SceneObject(pBoss):destroyObjectFromDatabase()
 	end
 	return 0
 end
