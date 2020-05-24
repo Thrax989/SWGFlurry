67c819436ffba29f6bc0eb832bd04f07e16c4819
TOXIC <thrax989@yahoo.com>
2020-05-24 11:48:58 -0700

    [Updated] Build Error

--- Flurry/Stable Server/Core3/MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
+++ Flurry/Stable Server/Core3/MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
@@ -1367,7 +1367,7 @@ void PlayerManagerImplementation::killPlayer(TangibleObject* attacker, CreatureO
 				if (!CombatManager::instance()->areInDuel(attackerCreature, player)) {
 				//If not in duel
 				ghost->updatePvpDeaths();
-				attackerGhost->updatePvPKillCount();
+				updatePvPKillCount(attackerCreature);
 				//Award Faction Points
 				FactionManager::instance()->awardPvpFactionPoints(attackerCreature, player);
 				if (!attackerCreature->hasBountyMissionFor(player) && !player->hasBountyMissionFor(attackerCreature))
