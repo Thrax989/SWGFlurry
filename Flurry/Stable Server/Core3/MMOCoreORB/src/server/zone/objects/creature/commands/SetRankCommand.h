a9e97a14f73f73b07529ae8088e504e4cf75a043
TOXIC <thrax989@yahoo.com>
2020-06-09 22:00:47 -0700

    [Added] Client Effect Command for Testing Effects

--- Flurry/Stable Server/Core3/MMOCoreORB/src/server/zone/objects/creature/commands/SetRankCommand.h
+++ Flurry/Stable Server/Core3/MMOCoreORB/src/server/zone/objects/creature/commands/SetRankCommand.h
@@ -21,9 +21,37 @@ public:
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
+		if( !creature->isPlayerCreature() )
+			return GENERALERROR;
+
+		StringTokenizer args(arguments.toString());
+
+		ManagedReference<PlayerObject*> ghost = creature->getPlayerObject();
+
+		if (ghost == NULL || !ghost->isPrivileged()) {
+			creature->sendSystemMessage("@error_message:insufficient_permissions"); //You do not have sufficient permissions to perform the requested action.
+			return INSUFFICIENTPERMISSION;
+		}
+
+		if( !args.hasMoreTokens() ){
+			creature->sendSystemMessage("format is /SetRankCommand clienteffect/int_camshake_light.cef");
+			return INVALIDPARAMETERS;
+		}
+		//Translate String
+		String clienteffect;
+		args.getStringToken(clienteffect);
+
+		//Search for CreatureObject
+		ManagedReference<SceneObject*> targetObject = server->getZoneServer()->getObject(creature->getTargetID());
+		ManagedReference<CreatureObject*> targetCreature = NULL;
+		if (targetObject != NULL && targetObject->isCreature()) {
+			targetCreature = cast<CreatureObject*>( targetObject.get());
+			targetCreature->playEffect(clienteffect, "head");
+		} else {
+			creature->playEffect(clienteffect, "head");
+		}
 		return SUCCESS;
 	}
-
 };
 
 #endif //SETRANKCOMMAND_H_
