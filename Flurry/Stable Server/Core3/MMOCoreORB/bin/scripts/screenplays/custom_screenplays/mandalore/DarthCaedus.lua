8e9e3ed8d7f6650d45289afbd9e69aa22dcb03e2
TOXIC <thrax989@yahoo.com>
2020-06-16 13:03:48 -0700

    [Updated] DarhCaedus

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/custom_screenplays/mandalore/DarthCaedus.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/custom_screenplays/mandalore/DarthCaedus.lua
@@ -1,76 +1,75 @@
 --/////////////////////////////////////////////////////////
---//		  World Boss Spawn System					//
---//			Created By Sygnum 04/13/20				//
+--//		    Boss Spawn System					//
+--//			Created By TOXIC:6/11/2020				//
 --////////////////////////////////////////////////////////
---//		Change your world boss under WORLDBOSS		//
---//Spawn Points Will Determain The Boss Spawn location //
+
 --////////////////////////////////////////////////////////
---//		Current World Boss Planet Mandalore			//
---//		Current World Boss Type CREATURE			//
+--//		Current Boss Planet Dungeon2			//
+--//		Current Boss Type NPC			//
 --///////////////////////////////////////////////////////
-DarthCaedusScreenplay = ScreenPlay:new {
+DarthCaedus = ScreenPlay:new {
 	numberOfActs = 1,
   	planet = "mandalore",
 }
-registerScreenPlay("DarthCaedusScreenplay", true)
+registerScreenPlay("DarthCaedus", true)
 -----------------------------
---Start World Boss ScreenPlay
+--Start DarthCaedus ScreenPlay
 -----------------------------
-function DarthCaedusScreenplay:start()
+function DarthCaedus:start()
 	if (isZoneEnabled(self.planet)) then
 		self:spawnMobiles()
-		print("Darth Caedus has Loaded")
+		print("DarthCaedus Loaded")
 	end
 end
 -----------------------
---The Boss Has Spawned
+--DarthCaedus Has Spawned
 -----------------------
-function DarthCaedusScreenplay:spawnMobiles()
-		local pBoss = spawnMobile("mandalore", "darth_caedus", -1, -91.1, -100.7, -95.8, 170, 8566162)--Spawn World Boss
+function DarthCaedus:spawnMobiles()
+		local pBoss = spawnMobile("mandalore", "darth_caedus", -1, -91.1, -100.7, -95.8, 170, 8566162)--Spawn DarthCaedus
 		local creature = CreatureObject(pBoss)
-		print("Darth Caedus has Spawned")
-		createObserver(OBJECTDESTRUCTION, "DarthCaedusScreenplay", "bossDead", pBoss)--World Boss Has Died Trigger Respawn Function
+		print("DarthCaedus Spawned")
+		createObserver(OBJECTDESTRUCTION, "DarthCaedus", "bossDead", pBoss)--DarthCaedus Has Died Trigger Respawn Function
 end
 ---------------------------------------------------------------
---The Boss Has Died Respawn WorldBoss With A New Dynamic Spawn
+--DarthCaedus Has Died Respawn DarthCaedus With A New Dynamic Spawn
 ---------------------------------------------------------------
-function DarthCaedusScreenplay:bossDead(pBoss, pPlayer)
+function DarthCaedus:bossDead(pBoss, pPlayer)
 	local player = LuaCreatureObject(pPlayer)
-	player:broadcastToServer("\\#63C8F9 Darth Caedus Has Died!")
-	player:broadcastToServer("\\#63C8F9 Darth Caedus Will Respawn In 3 Hours")
-	player:broadcastToDiscord("Darth Caedus Has Died!")
-	player:broadcastToDiscord("Darth Caedus Will Respawn In 3 Hours")
-	print("Darth Caedus Has Died")
+	player:broadcastToServer("\\#63C8F9 DarthCaedus Has Died!")
+	player:broadcastToServer("\\#63C8F9 DarthCaedus Will Respawn In 3 Hours")
+	player:broadcastToDiscord("DarthCaedus Has Died!")
+	player:broadcastToDiscord("DarthCaedus Will Respawn In 3 Hours")
+	print("DarthCaedus Has Died")
 	local creature = CreatureObject(pBoss)
-	createEvent(120 * 1000, "DarthCaedusScreenplay", "KillBoss", pBoss, "")--Despawn Corpse
-	createEvent(10800 * 1000, "DarthCaedusScreenplay", "KillSpawn", pBoss, "")--Respawn Boss In 3 Hours
-	createEvent(10797 * 1000, "DarthCaedusScreenplay", "KillSpawnCast", pBoss, "")--Broadcast Respawn
-	createEvent(10798 * 1000, "DarthCaedusScreenplay", "KillSpawnCast1", pBoss, "")--Broadcast Respawn 3
-	createEvent(10799 * 1000, "DarthCaedusScreenplay", "KillSpawnCast2", pBoss, "")--Broadcast Respawn 2
-	createEvent(10800 * 1000, "DarthCaedusScreenplay", "KillSpawnCast3", pBoss, "")--Broadcast Respawn 1
+	createEvent(120 * 1000, "DarthCaedus", "KillBoss", pBoss, "")--Despawn Corpse
+	createEvent(10800 * 1000, "DarthCaedus", "KillSpawn", pBoss, "")--Respawn Boss In 3 Hours
+	createEvent(10797 * 1000, "DarthCaedus", "KillSpawnCast", pBoss, "")--Broadcast Respawn
+	createEvent(10798 * 1000, "DarthCaedus", "KillSpawnCast1", pBoss, "")--Broadcast Respawn 3
+	createEvent(10799 * 1000, "DarthCaedus", "KillSpawnCast2", pBoss, "")--Broadcast Respawn 2
+	createEvent(10800 * 1000, "DarthCaedus", "KillSpawnCast3", pBoss, "")--Broadcast Respawn 1
 	return 0
 end
 -----------------------
---Respawn World Boss
+--Respawn DarthCaedus Boss
 -----------------------
-function DarthCaedusScreenplay:KillSpawn()
-		local pBoss = spawnMobile("mandalore", "darth_caedus", -1, -91.1, -100.7, -95.8, 170, 8566162)--Spawn WorldBoss After Death 3 Hour Timer
+function DarthCaedus:KillSpawn()
+		local pBoss = spawnMobile("mandalore", "darth_caedus", -1, -91.1, -100.7, -95.8, 170, 8566162)--Spawn DarthCaedus After Death 3 Hour Timer
 		local creature = CreatureObject(pBoss)
-		print("Darth Caedus has Spawned")
-		createObserver(OBJECTDESTRUCTION, "DarthCaedusScreenplay", "bossDead", pBoss)
+		print("DarthCaedus Respawned")
+		createObserver(OBJECTDESTRUCTION, "DarthCaedus", "bossDead", pBoss)
 end
 -----------------------
 --Broadcast Respawn
 -----------------------
-function DarthCaedusScreenplay:KillSpawnCast(pPlayer)
+function DarthCaedus:KillSpawnCast(pPlayer)
 		local player = LuaCreatureObject(pPlayer)
-		player:broadcastToServer("\\#63C8F9 Darth Caedus Respawning In ..")
-		player:broadcastToDiscord("Darth Caedus Respawning In ..")
+		player:broadcastToServer("\\#63C8F9 DarthCaedus Respawning In ..")
+		player:broadcastToDiscord("DarthCaedus Respawning In ..")
 end
 -----------------------
 --Broadcast Respawn 3
 -----------------------
-function DarthCaedusScreenplay:KillSpawnCast1(pPlayer)
+function DarthCaedus:KillSpawnCast1(pPlayer)
 		local player = LuaCreatureObject(pPlayer)
 		player:broadcastToServer("\\#63C8F9 3")
 		player:broadcastToDiscord("3")
@@ -78,7 +77,7 @@ end
 -----------------------
 --Broadcast Respawn 2
 -----------------------
-function DarthCaedusScreenplay:KillSpawnCast2(pPlayer)
+function DarthCaedus:KillSpawnCast2(pPlayer)
 		local player = LuaCreatureObject(pPlayer)
 		player:broadcastToServer("\\#63C8F9 2")
 		player:broadcastToDiscord("2")
@@ -86,18 +85,18 @@ end
 -----------------------
 --Broadcast Respawn 1
 -----------------------
-function DarthCaedusScreenplay:KillSpawnCast3(pPlayer)
+function DarthCaedus:KillSpawnCast3(pPlayer)
 		local player = LuaCreatureObject(pPlayer)
 		player:broadcastToServer("\\#63C8F9 1")
 		player:broadcastToDiscord("1")
 end
 -----------------------------------------------------------------------------
---The Boss Has Died Without Being Looted, "Abandon" Destroy NPC, Destroy Loot
+--DarthCaedus Has Died Without Being Looted, "Abandon" Destroy NPC, Destroy Loot
 -----------------------------------------------------------------------------
-function DarthCaedusScreenplay:KillBoss(pBoss)
+function DarthCaedus:KillBoss(pBoss)
 	dropObserver(pBoss, OBJECTDESTRUCTION)
 	if SceneObject(pBoss) then
-		print("Unlooted Darth Caedus Destroyed")
+		print("DarthCaedus Destroyed")
 		SceneObject(pBoss):destroyObjectFromWorld()
 	end
 	return 0
