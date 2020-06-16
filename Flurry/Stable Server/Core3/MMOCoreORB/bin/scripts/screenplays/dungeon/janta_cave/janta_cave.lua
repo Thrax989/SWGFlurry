f01904287b37201461298ad486a6fd3d11423659
TOXIC <thrax989@yahoo.com>
2020-06-16 12:48:22 -0700

    [Updated] Janta Cave Encounter

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/dungeon/janta_cave/janta_cave.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/dungeon/janta_cave/janta_cave.lua
@@ -1,202 +1,85 @@
-  --------------------------------------
---   Creator : TOXIC
---   Date : 1/24/2020
---------------------------------------
-janta_cave = ScreenPlay:new {
-  numberOfActs = 1,
-  questString = "janta_cave",
-  questdata = Object:new {
-  activePlayerName = "initial",
-  }
-}
--------------------------------------------------------------
---   Register Screenplay to planet dantooine janta cave
--------------------------------------------------------------
-spHelper = require("screenplayHelper")
-registerScreenPlay("janta_cave", true)
-local ObjectManager = require("managers.object.object_manager")  --print("Object manager loaded for Janta Boss")
---------------------------------------
---   Initialize screenplay
---------------------------------------
-function janta_cave:start()
-if (isZoneEnabled("dantooine")) then
-	self:spawnMobiles()
-	self:spawnSceneObjects()
-  end
-end
+--/////////////////////////////////////////////////////////
+--//		    Boss Spawn System					//
+--//			Created By TOXIC:6/11/2020				//
+--////////////////////////////////////////////////////////
 
+--////////////////////////////////////////////////////////
+--//		Current Boss Planet Dungeon2			//
+--//		Current Boss Type NPC			//
+--///////////////////////////////////////////////////////
+janta_caveScreenplay = ScreenPlay:new {
+	numberOfActs = 1,
+  	planet = "dantooine",
+}
+registerScreenPlay("janta_caveScreenplay", true)
+-----------------------------
+--Start JantaKing ScreenPlay
+-----------------------------
+function janta_caveScreenplay:start()
+	if (isZoneEnabled(self.planet)) then
+		self:spawnMobiles()
+		self:spawnSceneObjects()
+		print("JantaKing Loaded")
+	end
+end
+--------------------------------------------------
+--   Terminal
+--------------------------------------------------
 function janta_cave:spawnSceneObjects()
 spawnSceneObject("dantooine", "object/tangible/terminal/terminal_quad_screen.iff", -40.4491, 0.61331, 29.3437, 14200816, 1, 0, 0, 0)
 end
 --------------------------------------------------
 --   spawn mobiles for dungeon
 --------------------------------------------------
-function janta_cave:spawnMobiles()
--------------------------------------------------------------------------
---  Spawn a NPC as a swtich once killed, triggers boss observer to spawn
--------------------------------------------------------------------------
-local pBoss = spawnMobile("dantooine", "janta_chieftain", 10800, -91.6196, -100.898, -99.5903, 165, 529333)--3 hour respawn to start the boss
-	print("Spawning Janta Boss")
-if (pBoss ~= nil ) then
-    createObserver(OBJECTDESTRUCTION, "janta_cave", "notifyTriggerDead", pBoss)
+-----------------------
+--JantaKing Has Spawned
+-----------------------
+function janta_caveScreenplay:spawnMobiles()
+		local pBoss = spawnMobile("dantooine", "janta_cave_boss", -1, -91.6196, -100.898, -99.5903, 165, 529333)--Spawn JantaKing
+		local creature = CreatureObject(pBoss)
+		print("JantaKing Spawned")
+		createObserver(OBJECTDESTRUCTION, "janta_caveScreenplay", "bossDead", pBoss)--JantaKing Has Died Trigger Respawn Function
 end
-    writeData("janta_cave:spawnState",0)
-    return 0
-end
------------------------------------------
---  Notify trigger is dead to spawn Boss
------------------------------------------
-function janta_cave:notifyTriggerDead(pBoss, pPlayer)
-local pBoss = spawnMobile("dantooine", "janta_cave_boss", -1, -91.6196, -100.898, -99.5903, 165, 529333)
-    print("Spawning Janta Boss")
+---------------------------------------------------------------
+--JantaKing Has Died Respawn JantaKing With A New Dynamic Spawn
+---------------------------------------------------------------
+function janta_caveScreenplay:bossDead(pBoss, pPlayer)
+	local player = LuaCreatureObject(pPlayer)
+	player:broadcastToServer("\\#63C8F9 JantaKing Has Died!")
+	player:broadcastToServer("\\#63C8F9 JantaKing Will Respawn In 3 Hours")
+	player:broadcastToDiscord("JantaKing Has Died!")
+	player:broadcastToDiscord("JantaKing Will Respawn In 3 Hours")
+	print("JantaKing Has Died")
 	local creature = CreatureObject(pBoss)
-    CreatureObject(pPlayer):playEffect("clienteffect/sm_end_of_the_line.cef", "")
-    ObjectManager.withCreatureObject(pBoss, function(oBoss)
-    writeData("janta_cave:spawnState", 1)
-    writeData("janta_cave", oBoss:getObjectID())
-    spatialChat(pBoss, "Intruder Alert Activating Defense Systems")
-    createObserver(DAMAGERECEIVED,"janta_cave","boss_damage", pBoss)
-    createObserver(OBJECTDESTRUCTION, "janta_cave", "Restart", pBoss)
-end)
-    return 0
+	createEvent(120 * 1000, "janta_caveScreenplay", "KillBoss", pBoss, "")--Despawn Corpse
+	createEvent(10800 * 1000, "janta_caveScreenplay", "KillSpawn", pBoss, "")--Respawn Boss In 3 Hours
+	createEvent(10797 * 1000, "janta_caveScreenplay", "KillSpawnCast", pBoss, "")--Broadcast Respawn
+	createEvent(10798 * 1000, "janta_caveScreenplay", "KillSpawnCast1", pBoss, "")--Broadcast Respawn 3
+	createEvent(10799 * 1000, "janta_caveScreenplay", "KillSpawnCast2", pBoss, "")--Broadcast Respawn 2
+	createEvent(10800 * 1000, "janta_caveScreenplay", "KillSpawnCast3", pBoss, "")--Broadcast Respawn 1
+	return 0
 end
------------------------------------------
---  Notify trigger broadcast respawning
------------------------------------------
-function janta_cave:Restart(pPlayer, pBoss)
-    print("Starting Boss Broadcast Scripts")
-	createEvent(1 * 1000, "janta_cave", "Restartstates", pPlayer, "")--Restart Janta Boss States
-	createEvent(1 * 1000, "janta_cave", "BroadcastRespawn", pPlayer, "")--Broadcast 3 Hour Respawn
-	createEvent(300 * 1000, "janta_cave", "KillBoss", pPlayer, "")--Clean Up Dead Corpse
-	createEvent(10795 * 1000, "janta_cave", "KillSpawnCast", pPlayer, "")--Broadcast Respawn
-	createEvent(10798 * 1000, "janta_cave", "KillSpawnCast1", pPlayer, "")--Broadcast Respawn 3
-	createEvent(10799 * 1000, "janta_cave", "KillSpawnCast2", pPlayer, "")--Broadcast Respawn 2
-	createEvent(10800 * 1000, "janta_cave", "KillSpawnCast3", pPlayer, "")--Broadcast Respawn 1
-    return 0
-end
---------------------------------------
---   Player, Boss Functions
---------------------------------------
-function janta_cave:boss_damage(pBoss, pPlayer, onespawn, twospawn, threespawn, fourspawn, fivespawn, sixspawn, sevenspawn, eightspawn, ninespawn, player, pMember)
-local player = LuaCreatureObject(pPlayer)
-local boss = LuaCreatureObject(pBoss)
-createEvent(10800 * 1000, "janta_cave", "Remove", pBoss, "")
---------------------------------------
---   Range and health checks for boss
---------------------------------------
-if (boss ~= nil) then
-local heal = 999999
-local bossHealth = boss:getHAM(0)
-local bossAction = boss:getHAM(3)
-local bossMind = boss:getHAM(6)
-local bossMaxHealth = boss:getMaxHAM(0)
-local bossMaxAction = boss:getMaxHAM(3)
-local bossMaxMind = boss:getMaxHAM(6)
---------------------------------------
---  90% health check
---------------------------------------
-if (((bossHealth <= (bossMaxHealth * 0.9)) or (bossAction <= (bossMaxAction * 0.9)) or (bossMind <= (bossMaxMind * 0.9))) and readData("janta_cave:spawnState") == 1) then
-      spatialChat(pBoss, "Boss Current Health = 90%")
-      spatialChat(pBoss, "You fools..")
-      writeData("janta_cave:spawnState",2)
-		end
---------------------------------------
---  80% health check
---------------------------------------
-if (((bossHealth <= (bossMaxHealth * 0.8)) or (bossAction <= (bossMaxAction * 0.8)) or (bossMind <= (bossMaxMind * 0.8))) and readData("janta_cave:spawnState") == 2) then
-      spatialChat(pBoss, "Boss Current Health = 80%")
-      writeData("janta_cave:spawnState",3)
-		end
---------------------------------------
---  70% health check
---------------------------------------
-if (((bossHealth <= (bossMaxHealth * 0.7)) or (bossAction <= (bossMaxAction * 0.7)) or (bossMind <= (bossMaxMind * 0.7))) and readData("janta_cave:spawnState") == 3) then
-      spatialChat(pBoss, "Boss Current Health = 70%")
-      writeData("janta_cave:spawnState",4)
-		end	
---------------------------------------
---  60% health check
---------------------------------------
-if (((bossHealth <= (bossMaxHealth * 0.6)) or (bossAction <= (bossMaxAction * 0.6)) or (bossMind <= (bossMaxMind * 0.6))) and readData("janta_cave:spawnState") == 4) then
-      spatialChat(pBoss, "Boss Current Health = 60%")
-      writeData("janta_cave:spawnState",5)
-		end
---------------------------------------
---  50% health check
---------------------------------------
-if (((bossHealth <= (bossMaxHealth * 0.5)) or (bossAction <= (bossMaxAction * 0.5)) or (bossMind <= (bossMaxMind * 0.5))) and readData("janta_cave:spawnState") == 5) then
-      spatialChat(pBoss, "Boss Current Health = 50%")
-      writeData("janta_cave:spawnState",6)
-		end
---------------------------------------
---  40% health check
---------------------------------------
-if (((bossHealth <= (bossMaxHealth * 0.4)) or (bossAction <= (bossMaxAction * 0.4)) or (bossMind <= (bossMaxMind * 0.4))) and readData("janta_cave:spawnState") == 6) then
-      spatialChat(pBoss, "Boss Current Health = 40%")
-      writeData("janta_cave:spawnState",7)
-		end
---------------------------------------
---  30% health check
---------------------------------------
-if (((bossHealth <= (bossMaxHealth * 0.3)) or (bossAction <= (bossMaxAction * 0.3)) or (bossMind <= (bossMaxMind * 0.3))) and readData("janta_cave:spawnState") == 7) then
-      spatialChat(pBoss, "Boss Current Health = 30%")
-      writeData("janta_cave:spawnState",8)
-		end
---------------------------------------
---  20% health check
---------------------------------------
-if (((bossHealth <= (bossMaxHealth * 0.2)) or (bossAction <= (bossMaxAction * 0.2)) or (bossMind <= (bossMaxMind * 0.2))) and readData("janta_cave:spawnState") == 8) then
-      spatialChat(pBoss, "Boss Current Health = 20%")
-      writeData("janta_cave:spawnState",9)
-		end
---------------------------------------
---  10% health check
---------------------------------------
-if (((bossHealth <= (bossMaxHealth * 0.1)) or (bossAction <= (bossMaxAction * 0.1)) or (bossMind <= (bossMaxMind * 0.1))) and readData("janta_cave:spawnState") == 9) then
-      CreatureObject(pPlayer):sendSystemMessage("Enemy Wave Starting!")
-      spatialChat(pBoss, "Boss Current Health = 10%")
-      writeData("janta_cave:spawnState",10)
-      local ninespawn = spawnMobile("dantooine", "janta_chieftain", 0, -91.6196, -100.898, -99.5903, 165, 529333)
-      local ninespawn = spawnMobile("dantooine", "janta_chieftain", 0, -91.6196, -100.898, -99.5903, 165, 529333)
-      ObjectManager.withCreatureObject(ninespawn, function(ofirstTime)
-      writeData("countadd", ofirstTime:getObjectID())
-      ofirstTime:engageCombat(pPlayer)
-			end)
-		end
---------------------------------------------------------------------------------
---   Check that the boss has died, Broadcast server wide, set state for players
---------------------------------------------------------------------------------
-if (((bossHealth <= (bossMaxHealth * 0.001)) or (bossAction <= (bossMaxAction * 0.001)) or (bossMind <= (bossMaxMind * 0.001))) and readData("janta_cave:spawnState") == 10) then
-      spatialChat(pBoss, "We shall meet again uggggh!.")
-      spatialChat(pBoss, "You have 5 minuets to loot my body befor it disappears.")
-            writeData("janta_cave:spawnState",11)
-        end
-      end
-   return 0
-end
-----------------------------
---Broadcast Initial Respawn
-----------------------------
-function janta_cave:BroadcastRespawn(pPlayer)
-		local player = LuaCreatureObject(pPlayer)
-		player:broadcastToServer("\\#63C8F9 Janta Boss Has Died!")
-		player:broadcastToDiscord("Janta Boss Has Died!")
-		player:broadcastToServer("\\#63C8F9 Janta Boss Respawning In 3 Hours")
-		player:broadcastToDiscord("Janta Boss Respawning In 3 Hours")
-    	print("Starting Boss Respawn Broadcast Message")
+-----------------------
+--Respawn JantaKing Boss
+-----------------------
+function janta_caveScreenplay:KillSpawn()
+		local pBoss = spawnMobile("dantooine", "janta_cave_boss", -1, -91.6196, -100.898, -99.5903, 165, 529333)--Spawn JantaKing After Death 3 Hour Timer
+		local creature = CreatureObject(pBoss)
+		print("JantaKing Respawned")
+		createObserver(OBJECTDESTRUCTION, "janta_caveScreenplay", "bossDead", pBoss)
 end
 -----------------------
 --Broadcast Respawn
 -----------------------
-function janta_cave:KillSpawnCast(pPlayer)
+function janta_caveScreenplay:KillSpawnCast(pPlayer)
 		local player = LuaCreatureObject(pPlayer)
-		player:broadcastToServer("\\#63C8F9 Janta Boss Respawning In ..")
-		player:broadcastToDiscord("Janta Boss Respawning In ..")
+		player:broadcastToServer("\\#63C8F9 JantaKing Respawning In ..")
+		player:broadcastToDiscord("JantaKing Respawning In ..")
 end
 -----------------------
 --Broadcast Respawn 3
 -----------------------
-function janta_cave:KillSpawnCast1(pPlayer)
+function janta_caveScreenplay:KillSpawnCast1(pPlayer)
 		local player = LuaCreatureObject(pPlayer)
 		player:broadcastToServer("\\#63C8F9 3")
 		player:broadcastToDiscord("3")
@@ -204,7 +87,7 @@ end
 -----------------------
 --Broadcast Respawn 2
 -----------------------
-function janta_cave:KillSpawnCast2(pPlayer)
+function janta_caveScreenplay:KillSpawnCast2(pPlayer)
 		local player = LuaCreatureObject(pPlayer)
 		player:broadcastToServer("\\#63C8F9 2")
 		player:broadcastToDiscord("2")
@@ -212,49 +95,19 @@ end
 -----------------------
 --Broadcast Respawn 1
 -----------------------
-function janta_cave:KillSpawnCast3(pPlayer)
+function janta_caveScreenplay:KillSpawnCast3(pPlayer)
 		local player = LuaCreatureObject(pPlayer)
 		player:broadcastToServer("\\#63C8F9 1")
 		player:broadcastToDiscord("1")
-    	print("Janta Boss Is Respawning")
 end
 -----------------------------------------------------------------------------
---The Boss Has Died Without Being Looted, "Abandon" Destroy NPC, Destroy Loot
+--JantaKing Has Died Without Being Looted, "Abandon" Destroy NPC, Destroy Loot
 -----------------------------------------------------------------------------
-function janta_cave:KillBoss(pBoss)
+function janta_caveScreenplay:KillBoss(pBoss)
 	dropObserver(pBoss, OBJECTDESTRUCTION)
 	if SceneObject(pBoss) then
-		print("Unlooted Janta Boss Destroyed")
+		print("JantaKing Destroyed")
 		SceneObject(pBoss):destroyObjectFromWorld()
-		return 0
 	end
-end
-----------------------------
---Remove Boss After 3 hours
-----------------------------
-function janta_cave:Remove(pBoss)
-
-	if SceneObject(pBoss == nil) then
-		return
-	end
-
-	if SceneObject(pBoss) then
-		print("Janta Boss Removed")
-		SceneObject(pBoss):destroyObjectFromWorld()
-		dropObserver(pBoss, OBJECTDESTRUCTION)
-		dropObserver(pBoss, DAMAGERECEIVED)
-		forcePeace(pBoss)
-		forcePeace(pBoss)
-		forcePeace(pBoss)
-		forcePeace(pBoss)
-		forcePeace(pBoss)
-		forcePeace(pBoss)
-		end
-		return 0
-end
-----------------------------
---Reset Player Boss States
-----------------------------
-function janta_cave:Restartstates(pPlayer)
-   writeData("janta_cave:spawnState", 0)
+	return 0
 end
