25c7fe6aff3ea030e5d9529d9ae069c86f633b82
TOXIC <thrax989@yahoo.com>
2020-06-18 22:33:17 -0400

    Revert "Sygnum test server changes"

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/custom_content/mobile/dressed_meatlump_captain_05.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/custom_content/mobile/dressed_meatlump_captain_05.lua
@@ -20,11 +20,7 @@ dressed_meatlump_captain_05 = Creature:new {
 	boneType = "",
 	boneAmount = 0,
 	milk = 0,
-<<<<<<< HEAD
 	tamingChance = 0.0,
-=======
-	tamingChance = 0.00,
->>>>>>> 49dba15ebfe4217b8d3cb3ced3b2bd5d52e08bf9
 	ferocity = 0,
 	pvpBitmask = NONE,
 	creatureBitmask = NONE,
--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/custom_content/mobile/kashyyyk_bantha.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/custom_content/mobile/kashyyyk_bantha.lua
@@ -1,8 +1,7 @@
 kashyyyk_bantha = Creature:new {
-	--customName = "a Kashyyyk Bantha",
-  objectName = "@mob/creature_names:kashyyyk_bantha",
+	customName = "a Kashyyyk Bantha",
 	socialGroup = "townsperson",
-	-- faction = "townsperson",
+	faction = "townsperson",
   level = 40,
 	chanceHit = 1,
 	damageMin = 380,
