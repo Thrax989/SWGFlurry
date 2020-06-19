41d52cd815a4ce079dd536c97ebb55c7ecf9a97c
TOXIC <thrax989@yahoo.com>
2020-06-19 15:17:27 -0700

    [Updated] Screen Plays

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/custom_screenplays/mandalore/SC87.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/custom_screenplays/mandalore/SC87.lua
@@ -34,20 +34,11 @@ end
 ---------------------------------------------------------------
 --The Boss Has Died Respawn WorldBoss With A New Dynamic Spawn
 ---------------------------------------------------------------
-function SC87Screenplay:bossDead(pBoss, pPlayer)
-	local player = LuaCreatureObject(pPlayer)
-	--player:broadcastToServer("\\#63C8F9 SC87 Has Died!")
-	--player:broadcastToServer("\\#63C8F9 SC87 Will Respawn In 3 Hours")
-	player:broadcastToDiscord("SC87 Has Died!")
-	player:broadcastToDiscord("SC87 Will Respawn In 3 Hours")
+function SC87Screenplay:bossDead(pBoss)
 	print("SC87 Has Died")
 	local creature = CreatureObject(pBoss)
 	createEvent(120 * 1000, "SC87Screenplay", "KillBoss", pBoss, "")--Despawn Corpse
 	createEvent(10800 * 1000, "SC87Screenplay", "KillSpawn", pBoss, "")--Respawn Boss In 3 Hours
-	createEvent(10797 * 1000, "SC87Screenplay", "KillSpawnCast", pBoss, "")--Broadcast Respawn
-	createEvent(10798 * 1000, "SC87Screenplay", "KillSpawnCast1", pBoss, "")--Broadcast Respawn 3
-	createEvent(10799 * 1000, "SC87Screenplay", "KillSpawnCast2", pBoss, "")--Broadcast Respawn 2
-	createEvent(10800 * 1000, "SC87Screenplay", "KillSpawnCast3", pBoss, "")--Broadcast Respawn 1
 	return 0
 end
 -----------------------
@@ -58,38 +49,6 @@ function SC87Screenplay:KillSpawn()
 		local creature = CreatureObject(pBoss)
 		print("SC87 has Spawned")
 		createObserver(OBJECTDESTRUCTION, "SC87Screenplay", "bossDead", pBoss)
-end
------------------------
---Broadcast Respawn
------------------------
-function SC87Screenplay:KillSpawnCast(pPlayer)
-		local player = LuaCreatureObject(pPlayer)
-		--player:broadcastToServer("\\#63C8F9 SC87 Respawning In ..")
-		player:broadcastToDiscord("SC87 Respawning In ..")
-end
------------------------
---Broadcast Respawn 3
------------------------
-function SC87Screenplay:KillSpawnCast1(pPlayer)
-		local player = LuaCreatureObject(pPlayer)
-		--player:broadcastToServer("\\#63C8F9 3")
-		player:broadcastToDiscord("3")
-end
------------------------
---Broadcast Respawn 2
------------------------
-function SC87Screenplay:KillSpawnCast2(pPlayer)
-		local player = LuaCreatureObject(pPlayer)
-		--player:broadcastToServer("\\#63C8F9 2")
-		player:broadcastToDiscord("2")
-end
------------------------
---Broadcast Respawn 1
------------------------
-function SC87Screenplay:KillSpawnCast3(pPlayer)
-		local player = LuaCreatureObject(pPlayer)
-		--player:broadcastToServer("\\#63C8F9 1")
-		player:broadcastToDiscord("1")
 end
 -----------------------------------------------------------------------------
 --The Boss Has Died Without Being Looted, "Abandon" Destroy NPC, Destroy Loot
--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/custom_screenplays/mandalore/ToborroTheHutt.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/custom_screenplays/mandalore/ToborroTheHutt.lua
@@ -34,20 +34,11 @@ end
 ---------------------------------------------------------------
 --The Boss Has Died Respawn WorldBoss With A New Dynamic Spawn
 ---------------------------------------------------------------
-function ToborroTheHuttScreenplay:bossDead(pBoss, pPlayer)
-	local player = LuaCreatureObject(pPlayer)
-	--player:broadcastToServer("\\#63C8F9 Toborro The Hutt Has Died!")
-	--player:broadcastToServer("\\#63C8F9 Toborro The Hutt Will Respawn In 3 Hours")
-	player:broadcastToDiscord("Toborro The Hutt Has Died!")
-	player:broadcastToDiscord("Toborro The Hutt Will Respawn In 3 Hours")
+function ToborroTheHuttScreenplay:bossDead(pBoss)
 	print("Toborro The Hutt Has Died")
 	local creature = CreatureObject(pBoss)
 	createEvent(120 * 1000, "ToborroTheHuttScreenplay", "KillBoss", pBoss, "")--Despawn Corpse
 	createEvent(10800 * 1000, "ToborroTheHuttScreenplay", "KillSpawn", pBoss, "")--Respawn Boss In 3 Hours
-	createEvent(10797 * 1000, "ToborroTheHuttScreenplay", "KillSpawnCast", pBoss, "")--Broadcast Respawn
-	createEvent(10798 * 1000, "ToborroTheHuttScreenplay", "KillSpawnCast1", pBoss, "")--Broadcast Respawn 3
-	createEvent(10799 * 1000, "ToborroTheHuttScreenplay", "KillSpawnCast2", pBoss, "")--Broadcast Respawn 2
-	createEvent(10800 * 1000, "ToborroTheHuttScreenplay", "KillSpawnCast3", pBoss, "")--Broadcast Respawn 1
 	return 0
 end
 -----------------------
@@ -58,38 +49,6 @@ function ToborroTheHuttScreenplay:KillSpawn()
 		local creature = CreatureObject(pBoss)
 		print("Toborro The Hutt has Spawned")
 		createObserver(OBJECTDESTRUCTION, "ToborroTheHuttScreenplay", "bossDead", pBoss)
-end
------------------------
---Broadcast Respawn
------------------------
-function ToborroTheHuttScreenplay:KillSpawnCast(pPlayer)
-		local player = LuaCreatureObject(pPlayer)
-		--player:broadcastToServer("\\#63C8F9 Toborro The Hutt Respawning In ..")
-		player:broadcastToDiscord("Toborro The Hutt Respawning In ..")
-end
------------------------
---Broadcast Respawn 3
------------------------
-function ToborroTheHuttScreenplay:KillSpawnCast1(pPlayer)
-		local player = LuaCreatureObject(pPlayer)
-		--player:broadcastToServer("\\#63C8F9 3")
-		player:broadcastToDiscord("3")
-end
------------------------
---Broadcast Respawn 2
------------------------
-function ToborroTheHuttScreenplay:KillSpawnCast2(pPlayer)
-		local player = LuaCreatureObject(pPlayer)
-		--player:broadcastToServer("\\#63C8F9 2")
-		player:broadcastToDiscord("2")
-end
------------------------
---Broadcast Respawn 1
------------------------
-function ToborroTheHuttScreenplay:KillSpawnCast3(pPlayer)
-		local player = LuaCreatureObject(pPlayer)
-		--player:broadcastToServer("\\#63C8F9 1")
-		player:broadcastToDiscord("1")
 end
 -----------------------------------------------------------------------------
 --The Boss Has Died Without Being Looted, "Abandon" Destroy NPC, Destroy Loot
