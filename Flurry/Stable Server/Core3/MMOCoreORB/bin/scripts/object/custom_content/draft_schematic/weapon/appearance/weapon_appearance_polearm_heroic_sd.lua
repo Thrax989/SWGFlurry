47515829768e8ee76aaacad6cd74caa2e91a3de4
TOXIC <thrax989@yahoo.com>
2020-07-10 13:09:37 -0700

    [Fixed] All Melee NGE Craftable Weapon Templates

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/weapon/appearance/weapon_appearance_polearm_heroic_sd.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/weapon/appearance/weapon_appearance_polearm_heroic_sd.lua
@@ -27,7 +27,7 @@ object_draft_schematic_weapon_appearance_weapon_appearance_polearm_heroic_sd = o
    contribution = {100, 100, 100, 100, 100},
 
 
-   targetTemplate = "object/weapon/melee/polearm/polearm_vibro_axe.iff",
+   targetTemplate = "object/weapon/melee/polearm/polearm_heroic_sd.iff",
 
    additionalTemplates = {
              }
