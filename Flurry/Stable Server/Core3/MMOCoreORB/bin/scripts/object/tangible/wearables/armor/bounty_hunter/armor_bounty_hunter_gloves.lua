6e3c231ae95741291287a00ac04aa127ffe23b2c
Sygnum <jtrobbinsfl@gmail.com>
2020-07-11 17:23:44 -0400

    [Modified] Fixed missing species from new custom armors.

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/tangible/wearables/armor/bounty_hunter/armor_bounty_hunter_gloves.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/tangible/wearables/armor/bounty_hunter/armor_bounty_hunter_gloves.lua
@@ -183,7 +183,6 @@ object_tangible_wearables_armor_bounty_hunter_armor_bounty_hunter_gloves = objec
 				"object/creature/player/zabrak_female.iff" ,
 				"object/mobile/vendor/zabrak_male.iff",
 				"object/mobile/vendor/zabrak_female.iff" },
-
 	
 	-- Damage types in WeaponObject
 	vulnerability = STUN,
