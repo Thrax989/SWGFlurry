7fc7a9e488132674db3325ba2659394b1fdb830b
Sygnum <jtrobbinsfl@gmail.com>
2020-07-19 14:42:27 -0400

    [Modified] Added Cyberdyne to GTS player cities

--- Flurry/Stable Server/Core3/MMOCoreORB/src/server/zone/managers/sui/SuiManager.cpp
+++ Flurry/Stable Server/Core3/MMOCoreORB/src/server/zone/managers/sui/SuiManager.cpp
@@ -2590,7 +2590,29 @@ void SuiManager::handleCharacterBuilderSelectItem(CreatureObject* player, SuiBox
 						Locker clocker(currentCity, player);
 						currentCity->addToCityTreasury(1000);
 						}
-			        }			       
+			        }	
+                    } else if (templatePath == "pc_cyberdyne_travel") {
+				if (!player->isInCombat() && player->getBankCredits() < 4999) {
+		                ManagedReference<SuiMessageBox*> box = new SuiMessageBox(player, SuiWindowType::CITY_ADMIN_CONFIRM_UPDATE_TYPE);
+		                box->setPromptTitle("Cyberdyne");
+		                box->setPromptText("Travel Coast 5,000 credits. (Bank)");
+		                box->setOkButton(true, "@cancel");
+		                box->setUsingObject(player);
+		                player->getPlayerObject()->addSuiBox(box);
+		                player->sendMessage(box->generateMessage());
+			        }
+				if (!player->isInCombat() && player->getBankCredits() > 4999) {
+		                ManagedReference<SuiMessageBox*> box = new SuiMessageBox(player, SuiWindowType::CITY_ADMIN_CONFIRM_UPDATE_TYPE);
+		                ManagedReference<CityRegion*> currentCity = player->getCityRegion().get();
+						player->sendSystemMessage("Thank you for your travels.");
+ 				        player->switchZone("Dathomir", -5060, 78, -5015);
+						player->subtractBankCredits(5000);
+						box->setForceCloseDistance(5.f);
+						if(currentCity != nullptr && !currentCity->isClientRegion()) {
+						Locker clocker(currentCity, player);
+						currentCity->addToCityTreasury(1000);
+						}
+			        }			                     
 //GRAY JEDI HOLOCRON QUEST END CHAPTER
 			} else if (templatePath == "Gray_Chapter_31") {
 				if (!player->isInCombat() && player->hasSkill("force_title_jedi_novice") && (ghost->getSkillPoints() == 250) && player->getScreenPlayState("jediQuest") >= 31) {
