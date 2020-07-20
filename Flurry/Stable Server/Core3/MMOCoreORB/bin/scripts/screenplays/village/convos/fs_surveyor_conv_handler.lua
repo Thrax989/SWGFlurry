9c0c78c110841ed8d5406b1cc92a74c1e23b7eb7
TOXIC <thrax989@yahoo.com>
2020-07-20 11:30:37 -0700

    [Updated] Survey FS Village Task To Work For Gray And Normal Jedi

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/village/convos/fs_surveyor_conv_handler.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/village/convos/fs_surveyor_conv_handler.lua
@@ -9,11 +9,9 @@ function villageSurveyorConvoHandler:getInitialScreen(pPlayer, pNpc, pConvTempla
 
 	if (phase ~= 2 and phase ~= 3) then
 		return convoTemplate:getScreen("intro_wrong_phase")
-	elseif (not CreatureObject(pPlayer):hasSkill("crafting_artisan_novice")) then
-		return convoTemplate:getScreen("intro_no_skill")
-	elseif (phase == 2 and VillageJediManagerCommon.hasUnlockedBranch(pPlayer, "force_sensitive_heightened_senses_surveying")) then
+	elseif (phase == 2) then
 		return convoTemplate:getScreen("intro_completed_current_phase")
-	elseif (phase == 3 and VillageJediManagerCommon.hasUnlockedBranch(pPlayer, "force_sensitive_heightened_senses_luck")) then
+	elseif (phase == 3) then
 		return convoTemplate:getScreen("intro_completed_current_phase")
 	end
 
