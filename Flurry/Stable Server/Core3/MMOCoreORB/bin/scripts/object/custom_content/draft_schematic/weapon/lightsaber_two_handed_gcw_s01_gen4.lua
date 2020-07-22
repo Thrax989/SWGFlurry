57854b8d4eb64a2a4fbc32f3718d31697a26a35f
TOXIC <thrax989@yahoo.com>
2020-07-22 13:59:17 -0700

    [Updated] 5th Generation Crating Sabers

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/weapon/lightsaber_two_handed_gcw_s01_gen4.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/weapon/lightsaber_two_handed_gcw_s01_gen4.lua
@@ -2,7 +2,7 @@ object_draft_schematic_weapon_lightsaber_two_handed_gcw_s01_gen4 = object_draft_
 
    templateType = DRAFTSCHEMATIC,
 
-   customObjectName = "Two-Handed Fourth Generation Lightsaber",
+   customObjectName = "Two-Handed Fifth Generation Lightsaber",
 
    craftingToolTab = 2048, -- (See DraftSchematicObjectTemplate.h)
    complexity = 19, 
