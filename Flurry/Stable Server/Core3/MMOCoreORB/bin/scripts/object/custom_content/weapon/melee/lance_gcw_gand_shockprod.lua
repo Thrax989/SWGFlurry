2df5e9a1448ea98e6ab6dede319b2d519bec0e0f
bigtap <24537455+bigtap@users.noreply.github.com>
2020-07-22 21:38:05 -0700

    Update lance_gcw_gand_shockprod.lua

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/melee/lance_gcw_gand_shockprod.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/melee/lance_gcw_gand_shockprod.lua
@@ -230,7 +230,7 @@ object_weapon_melee_polearm_lance_gcw_gand_shockprod = object_weapon_melee_polea
 	attackType = MELEEATTACK,
 
 	-- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, LIGHTSABER
-	damageType = KINETIC,
+	damageType = ELECTRICITY,
 
 	-- NONE, LIGHT, MEDIUM, HEAVY
 	armorPiercing = HEAVY,
