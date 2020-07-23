327ad9d7b43a341aaa8bdf40d9910a28e13350b0
bigtap <24537455+bigtap@users.noreply.github.com>
2020-07-22 21:46:52 -0700

    Update lance_trando.lua

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/melee/lance_trando.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/melee/lance_trando.lua
@@ -230,7 +230,7 @@ object_weapon_melee_polearm_ep3_lance_trando = object_weapon_melee_polearm_ep3_s
 	attackType = MELEEATTACK,
 
 	-- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, LIGHTSABER
-	damageType = KINETIC,
+	damageType = BLAST,
 
 	-- NONE, LIGHT, MEDIUM, HEAVY
 	armorPiercing = HEAVY,
