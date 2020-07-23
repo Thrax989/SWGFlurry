6e6d5a420e95581973d9c74cfca336a866e0fdde
bigtap <24537455+bigtap@users.noreply.github.com>
2020-07-22 21:49:21 -0700

    Update lance_kashyyk.lua

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/melee/lance_kashyyk.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/melee/lance_kashyyk.lua
@@ -230,7 +230,7 @@ object_weapon_melee_polearm_lance_kashyyk = object_weapon_melee_polearm_shared_l
 	attackType = MELEEATTACK,
 
 	-- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, LIGHTSABER
-	damageType = KINETIC,
+	damageType = HEAT,
 
 	-- NONE, LIGHT, MEDIUM, HEAVY
 	armorPiercing = HEAVY,
