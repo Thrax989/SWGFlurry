3693c443efcfececf9029ef9291546d6a375af84
TOXIC <thrax989@yahoo.com>
2020-07-22 18:53:58 -0700

    [Fixed] FS2

--- Flurry/Stable Server/Core3/MMOCoreORB/src/server/zone/objects/creature/commands/ForceShield2Command.h
+++ Flurry/Stable Server/Core3/MMOCoreORB/src/server/zone/objects/creature/commands/ForceShield2Command.h
@@ -17,11 +17,11 @@ public:
 		skillMods.put("force_shield", 45);
 	}
 
-	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const override {
+	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
 		return doJediSelfBuffCommand(creature);
 	}
 
-	void handleBuff(SceneObject* creature, ManagedObject* object, int64 param) const override {
+	void handleBuff(SceneObject* creature, ManagedObject* object, int64 param) {
 		ManagedReference<CreatureObject*> player = creature->asCreatureObject();
 
 		if (player == nullptr)
@@ -35,7 +35,7 @@ public:
 		// Client Effect upon hit (needed)
 		player->playEffect("clienteffect/pl_force_shield_hit.cef", "");
 
-		int fCost = param * getFrsModifiedExtraForceCost(player, 0.03f);
+		int fCost = param * getFrsModifiedExtraForceCost(player, 0.3);
 		if (ghost->getForcePower() <= fCost) { // Remove buff if not enough force.
 			Buff* buff = player->getBuff(BuffCRC::JEDI_FORCE_SHIELD_2);
 			if (buff != nullptr) {
