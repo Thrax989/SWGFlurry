005497546f7e6effb17f8f9babd0e10f4c1e6225
bigtap <24537455+bigtap@users.noreply.github.com>
2020-07-22 21:46:16 -0700

    Update lance_kaminoan_generic.lua

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/melee/lance_kaminoan_generic.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/melee/lance_kaminoan_generic.lua
@@ -230,7 +230,7 @@ object_weapon_melee_polearm_lance_kaminoan_generic = object_weapon_melee_polearm
 	attackType = MELEEATTACK,
 
 	-- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, LIGHTSABER
-	damageType = KINETIC,
+	damageType = BLAST,
 
 	-- NONE, LIGHT, MEDIUM, HEAVY
 	armorPiercing = HEAVY,
