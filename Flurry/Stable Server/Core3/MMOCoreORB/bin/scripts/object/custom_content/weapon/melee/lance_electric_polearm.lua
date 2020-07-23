ea8d93e62a8c9b88bbd3feeb1bdfb053a62cde7b
bigtap <24537455+bigtap@users.noreply.github.com>
2020-07-22 21:37:19 -0700

    Update lance_electric_polearm.lua

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/melee/lance_electric_polearm.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/melee/lance_electric_polearm.lua
@@ -230,7 +230,7 @@ object_weapon_melee_polearm_lance_electric_polearm = object_weapon_melee_polearm
 	attackType = MELEEATTACK,
 
 	-- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, LIGHTSABER
-	damageType = KINETIC,
+	damageType = ELECTRICITY,
 
 	-- NONE, LIGHT, MEDIUM, HEAVY
 	armorPiercing = HEAVY,
