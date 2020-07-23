c7adc4e2cbca5adb4dcbe99b0f59e6bbd2c269d0
bigtap <24537455+bigtap@users.noreply.github.com>
2020-07-22 21:39:02 -0700

    Update lance_massassi.lua

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/melee/lance_massassi.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/melee/lance_massassi.lua
@@ -230,7 +230,7 @@ object_weapon_melee_polearm_lance_massassi = object_weapon_melee_polearm_shared_
 	attackType = MELEEATTACK,
 
 	-- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, LIGHTSABER
-	damageType = ACID,
+	damageType = KINETIC,
 
 	-- NONE, LIGHT, MEDIUM, HEAVY
 	armorPiercing = HEAVY,
