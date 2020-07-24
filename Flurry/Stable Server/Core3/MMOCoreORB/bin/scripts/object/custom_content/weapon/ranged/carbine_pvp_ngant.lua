1678a4c9a5bcbf07da3109fb6931bad1b04c8d34
bigtap <24537455+bigtap@users.noreply.github.com>
2020-07-23 18:14:34 -0700

    Update carbine_pvp_ngant.lua

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/ranged/carbine_pvp_ngant.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/ranged/carbine_pvp_ngant.lua
@@ -230,7 +230,7 @@ object_weapon_ranged_carbine_carbine_pvp_ngant = object_weapon_ranged_carbine_sh
 	attackType = RANGEDATTACK,
 
 	-- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, LIGHTSABER
-	damageType = ACID,
+	damageType = ELECTRICITY,
 
 	-- NONE, LIGHT, MEDIUM, HEAVY
 	armorPiercing = MEDIUM,
