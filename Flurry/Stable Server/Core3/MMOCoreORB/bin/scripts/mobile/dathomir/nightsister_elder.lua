e2dc91160d452a7d81403fa291e08c14435cc618
bigtap <24537455+bigtap@users.noreply.github.com>
2020-07-24 10:47:49 -0700

    Update nightsister_elder.lua

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/dathomir/nightsister_elder.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/dathomir/nightsister_elder.lua
@@ -6,8 +6,8 @@ nightsister_elder = Creature:new {
 	faction = "nightsister",
 	level = 300,
 	chanceHit = 27.25,
-	damageMin = 1520,
-	damageMax = 2750,
+	damageMin = 1820,
+	damageMax = 3150,
 	baseXp = 26654,
 	baseHAM = 321000,
 	baseHAMmax = 392000,
@@ -28,13 +28,7 @@ nightsister_elder = Creature:new {
 	diet = HERBIVORE,
 
 	templates = {"object/mobile/dressed_dathomir_nightsister_elder.iff"},
-	lootGroups = {
-		{
-			groups = {
-				{group = "jedi_comp_group", chance = 10000000},
-			},
-			lootChance = 200000
-		},		
+	lootGroups = {		
 		{
 			groups = {
 				{group = "power_crystals", chance = 10000000},
@@ -73,39 +67,33 @@ nightsister_elder = Creature:new {
 		},
 		{
 			groups = {
-				{group = "melee_weapons", chance = 10000000},
-			},
-			lootChance = 5000000
-		},
-		{
-			groups = {
-				{group = "rifles", chance = 10000000},
-			},
-			lootChance = 5000000
-		},
-		{
-			groups = {
 				{group = "wearables_scarce", chance = 10000000},
 			},
 			lootChance = 5000000
+		},	
+		{
+			groups = {
+				{group = "jedi_comp_group", chance = 10000000},
+			},
+			lootChance = 200000
 		},
 		{
 			groups = {
-				{group = "carbines", chance = 10000000},
+				{group = "tierone", chance = 1500000},
+				{group = "tiertwo", chance = 3500000},
+				{group = "tierthree", chance = 2500000},
+				{group = "tierdiamond", chance = 2500000},
 			},
-			lootChance = 5000000
+			lootChance = 4000000
 		},
 		{
 			groups = {
-				{group = "tailor_components", chance = 10000000},
+				{group = "tierone", chance = 1500000},
+				{group = "tiertwo", chance = 3500000},
+				{group = "tierthree", chance = 2500000},
+				{group = "tierdiamond", chance = 2500000},
 			},
-			lootChance = 5000000
-		},
-		{
-			groups = {
-				{group = "pistols", chance = 10000000},
-			},
-			lootChance = 5000000
+			lootChance = 4000000
 		},
 	},
 	weapons = {"mixed_force_weapons"},
