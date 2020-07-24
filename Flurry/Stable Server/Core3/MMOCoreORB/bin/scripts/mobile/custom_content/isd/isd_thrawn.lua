9b4556982ef832dfa69c9f966a95b9a001115bad
bigtap <24537455+bigtap@users.noreply.github.com>
2020-07-23 22:04:21 -0700

    Update isd_thrawn.lua

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/custom_content/isd/isd_thrawn.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/custom_content/isd/isd_thrawn.lua
@@ -34,6 +34,12 @@ isd_thrawn = Creature:new {
 	lootGroups = {
 		{
 			groups = {
+				{group = "jedi_comp_group", chance = 10000000},
+			},
+			lootChance = 1000000
+		},	
+		{
+			groups = {
 				{group = "g_named_crystals", chance = 10000000},
 			},
 			lootChance = 5000000
