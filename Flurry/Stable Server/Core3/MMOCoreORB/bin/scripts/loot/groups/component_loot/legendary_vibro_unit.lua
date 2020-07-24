4fc0e161e7d0d45a03e7b9e932ab23b9cb77ce8f
bigtap <24537455+bigtap@users.noreply.github.com>
2020-07-24 13:27:54 -0700

    Create legendary_vibro_unit.lua

---
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/loot/groups/component_loot/legendary_vibro_unit.lua
@@ -0,0 +1,10 @@
+legendary_vibro_unit = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "legendary_vibro_unit", weight = 10000000}
+	}
+}
+
+addLootGroupTemplate("legendary_vibro_unit", legendary_vibro_unit)
