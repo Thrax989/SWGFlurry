e902ba2df282e749e77903ea82331ac8688b03b7
bigtap <24537455+bigtap@users.noreply.github.com>
2020-07-24 10:39:03 -0700

    Update nightsister_elder.lua

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/dathomir/nightsister_elder.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/dathomir/nightsister_elder.lua
@@ -31,6 +31,12 @@ nightsister_elder = Creature:new {
 	lootGroups = {
 		{
 			groups = {
+				{group = "jedi_comp_group", chance = 10000000},
+			},
+			lootChance = 200000
+		},		
+		{
+			groups = {
 				{group = "power_crystals", chance = 10000000},
 			},
 			lootChance = 5000000
