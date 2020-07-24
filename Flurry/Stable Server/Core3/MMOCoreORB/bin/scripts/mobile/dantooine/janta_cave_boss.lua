b59b52030bd8b67a9142a07fc13305e934d7ff2c
bigtap <24537455+bigtap@users.noreply.github.com>
2020-07-23 21:49:49 -0700

    Update janta_cave_boss.lua

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/dantooine/janta_cave_boss.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/dantooine/janta_cave_boss.lua
@@ -32,9 +32,9 @@ janta_cave_boss = Creature:new {
 	lootGroups = {
 		{
 			groups = {
-				{group = "junk", chance = 10000000}				
-		},
-			lootChance = 10000000
+				{group = "jedi_comp_group", chance = 10000000},
+			},
+			lootChance = 1000000
 		},
 		{
 			groups = {
