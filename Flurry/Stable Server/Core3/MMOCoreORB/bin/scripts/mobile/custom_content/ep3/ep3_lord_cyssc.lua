98a7b09f1b93432b13cd0e87ccf835a68ba856c0
bigtap <24537455+bigtap@users.noreply.github.com>
2020-07-24 13:54:14 -0700

    Update ep3_lord_cyssc.lua

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/custom_content/ep3/ep3_lord_cyssc.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/custom_content/ep3/ep3_lord_cyssc.lua
@@ -31,6 +31,12 @@ ep3_lord_cyssc = Creature:new {
 	lootGroups = {
 		{
 			groups = {
+				{group = "legendary_comp_group", chance = 10000000},
+			},
+			lootChance = 1000000
+		},	
+		{
+			groups = {
 				{group = "jedi_comp_group", chance = 10000000},
 			},
 			lootChance = 1000000
