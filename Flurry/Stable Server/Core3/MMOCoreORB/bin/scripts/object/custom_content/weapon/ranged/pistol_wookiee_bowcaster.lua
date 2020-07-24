d9c6080c466819d82bc2831da24106b8ce9bea33
bigtap <24537455+bigtap@users.noreply.github.com>
2020-07-23 17:54:42 -0700

    Update pistol_wookiee_bowcaster.lua

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/ranged/pistol_wookiee_bowcaster.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/weapon/ranged/pistol_wookiee_bowcaster.lua
@@ -230,7 +230,7 @@ object_weapon_ranged_pistol_ep3_pistol_wookiee_bowcaster = object_weapon_ranged_
 	attackType = RANGEDATTACK,
 
 	-- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, LIGHTSABER
-	damageType = ENERGY,
+	damageType = BLAST,
 
 	-- NONE, LIGHT, MEDIUM, HEAVY
 	armorPiercing = LIGHT,
