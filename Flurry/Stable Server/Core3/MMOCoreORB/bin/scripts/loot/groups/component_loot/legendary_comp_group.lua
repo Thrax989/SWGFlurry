6d5825836dc8218a87f3524bf81fe1f998740d9e
Sygnum <jtrobbinsfl@gmail.com>
2020-07-24 22:21:47 -0400

    [Modfied] Loot groups now fixed

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/loot/groups/component_loot/legendary_comp_group.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/loot/groups/component_loot/legendary_comp_group.lua
@@ -3,11 +3,11 @@ legendary_comp_group = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "g_legendary_tissue", weight = 2000000},
-		{itemTemplate = "g_legendary_tooth", weight = 2000000},
-		{itemTemplate = "g_legendary_bones", weight = 2000000},
-		{itemTemplate = "g_legendary_bone_shards", weight = 2000000},
-		{itemTemplate = "g_legendary_vibro_unit", weight = 2000000}
+		{itemTemplate = "legendary_tissue", weight = 2000000},
+		{itemTemplate = "legendary_tooth", weight = 2000000},
+		{itemTemplate = "legendary_bones", weight = 2000000},
+		{itemTemplate = "legendary_bone_shards", weight = 2000000},
+		{itemTemplate = "legendary_vibro_unit", weight = 2000000}
 	}
 }
 
