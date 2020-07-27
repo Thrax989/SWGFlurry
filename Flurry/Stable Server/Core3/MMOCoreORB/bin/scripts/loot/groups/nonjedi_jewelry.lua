5bf8f4b9442753a840eaf09edef3847a42f56463
Sygnum <jtrobbinsfl@gmail.com>
2020-07-27 13:05:55 -0400

    [Modified] New Boss and Moble loot tables

---
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/loot/groups/nonjedi_jewelry.lua
@@ -0,0 +1,15 @@
+nonjedi_jewelry = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "neck_crafter", weight = 3333333},
+		{itemTemplate = "non_jedi_ring_crafter_second", weight = 3333333},
+		{itemTemplate = "non_jedi_ring_crafter", weight = 3333333},      
+		{itemTemplate = "non_jedi_rings_ranged", weight = 3333333},
+		{itemTemplate = "non_jedi_rings", weight = 3333333},
+		{itemTemplate = "g_necklaces", weight = 3333333}
+	}
+}
+
+addLootGroupTemplate("nonjedi_jewelry", nonjedi_jewelry)
