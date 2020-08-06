bed14acfa62489ffa723565c66233768680f7963
Sygnum <jtrobbinsfl@gmail.com>
2020-08-06 10:49:01 -0400

    [Modified] Increaded condition on loot vehicles

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/tangible/deed/vehicle_deed/koro2_speeder_deed.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/object/custom_content/tangible/deed/vehicle_deed/koro2_speeder_deed.lua
@@ -53,8 +53,8 @@ object_tangible_deed_vehicle_deed_koro2_speeder_deed = object_tangible_deed_vehi
 	experimentalWeights = {1, 1, 1},
 	experimentalGroupTitles = {"null", "null", "exp_durability"},
 	experimentalSubGroupTitles = {"null", "null", "hit_points"},
-	experimentalMin = {0, 0, 2000},
-	experimentalMax = {0, 0, 4500},
+	experimentalMin = {0, 0, 5000},
+	experimentalMax = {0, 0, 10000},
 	experimentalPrecision = {0, 0, 0},
 	experimentalCombineType = {0, 0, 1},
 }
