34bacc0f3f9f3472e810900ca20aa02a6c740b0f
TOXIC <thrax989@yahoo.com>
2020-08-07 10:13:57 -0700

    [Added] Cloning Option To Pvp Arena

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/events/pvp/pvp.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/screenplays/events/pvp/pvp.lua
@@ -20,4 +20,5 @@ function pvpScreenPlay:spawnSceneObjects()
 --Spawn PvP Terminal for 2v2 Imperial Vs Rebel
 	spawnSceneObject("tatooine", "object/tangible/terminal/systems_control_terminal.iff", 3373.25, 10.998, -5128.88, 0, -0.34202, 0, 0.939693, 0)
 	spawnSceneObject("tatooine", "object/tangible/terminal/systems_control_terminal.iff", 3415.67, 5, -5087.69, 0, 0.332035, 0, -0.943267, 0)
+	spawnSceneObject("tatooine", "object/tangible/terminal/crafting_assembly_terminal.iff", 3372.2, 10.248, -5128.7, 0, -0.398749, 0, 0.91706, 0)
 end
