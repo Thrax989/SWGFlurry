2537dcec6fde1f79a41e673ad5f93c1db9b1a345
TOXIC <thrax989@yahoo.com>
2020-08-10 16:04:52 -0700

    [Updated] Group Loot bonus Skill Mod Fix

--- Flurry/Stable Server/Core3/MMOCoreORB/src/server/zone/managers/group/GroupLootTask.h
+++ Flurry/Stable Server/Core3/MMOCoreORB/src/server/zone/managers/group/GroupLootTask.h
@@ -135,12 +135,12 @@ public:
 		int luck = player->getSkillMod("force_luck");
 
 		if (luck > 0)
-			lootCredits += (lootCredits * luck) * 2;
+			lootCredits += (lootCredits * luck) / 10;
 
 		int bonusluck = player->getSkillMod("luck");
 
 		if (bonusluck > 0)
-			lootCredits += (lootCredits * bonusluck) * 2;
+			lootCredits += (lootCredits * bonusluck) / 10;
 
 		Locker clocker(group, corpse);
 
