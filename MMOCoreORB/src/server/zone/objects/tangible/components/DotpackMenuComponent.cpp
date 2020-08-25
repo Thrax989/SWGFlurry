b5dfa712950e4e5dee87122c2d90e95791071082
TOXIC <thrax989@yahoo.com>
2020-08-24 23:00:41 -0700

    [Updated] Clean Up New Dot Component Packs

--- MMOCoreORB/src/server/zone/objects/tangible/components/DotpackMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/DotpackMenuComponent.cpp
@@ -4,51 +4,37 @@
  *  Created on: 08/24/2020
  *      Author: TOXIC
  */
-
 #include "DotpackMenuComponent.h"
 #include "server/zone/objects/creature/CreatureObject.h"
 #include "server/zone/objects/scene/SceneObject.h"
-#include "server/zone/objects/player/PlayerObject.h"
-#include "server/zone/ZoneServer.h"
-#include "server/zone/managers/jedi/JediManager.h"
-#include "server/chat/ChatManager.h"
-#include "server/zone/packets/player/PlayMusicMessage.h"
-#include "server/zone/managers/visibility/VisibilityManager.h"
-#include "server/zone/objects/player/sui/inputbox/SuiInputBox.h"
 #include "server/zone/managers/loot/LootManager.h"
-#include "server/zone/managers/player/PlayerManager.h"
-#include "server/chat/ChatManager.h"
-#include "server/zone/packets/player/PlayMusicMessage.h"
+#include "server/zone/Zone.h"
+#include "server/zone/ZoneServer.h"
 #include "server/zone/packets/object/ObjectMenuResponse.h"
-#include "server/zone/objects/building/BuildingObject.h"
 #include "server/zone/objects/group/GroupObject.h"
-
 void DotpackMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, ObjectMenuResponse* menuResponse, CreatureObject* player) const {
-
 	TangibleObjectMenuComponent::fillObjectMenuResponse(sceneObject, menuResponse, player);
-	ManagedReference<BuildingObject*> building = cast<BuildingObject*>(player->getRootParent());
-	// If outside dispaly menu options, if inside a building show nothing.
-	if (building == nullptr) {
 	menuResponse->addRadialMenuItem(20, 3, "Use Dot Pack [Clears Dots]");
-	}
 }
-
 int DotpackMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* creature, byte selectedID) const {
-	ManagedReference<BuildingObject*> building = cast<BuildingObject*>(creature->getRootParent());
-
-	// If outside dispaly menu options, if inside a building show nothing.
-	if (building == nullptr) {
-	if (selectedID != 20)
+	if (!sceneObject->isTangibleObject())
 		return 0;
 
-	ManagedReference<GroupObject*> group = creature->getGroup();
-	if (group == nullptr) {
-				creature->clearDots();
-				creature->playEffect("clienteffect/level_granted.cef", "");
-				sceneObject->destroyObjectFromWorld(true);
+	if (!creature->isPlayerCreature())
+		return 0;
+
+	if (!sceneObject->isASubChildOf(creature))
+		return 0;
+
+	if (selectedID == 20) {
+					ManagedReference<GroupObject*> group = creature->getGroup();
+				if (group == nullptr) {
+					creature->clearDots();
+					creature->playEffect("clienteffect/level_granted.cef", "");
+					sceneObject->destroyObjectFromWorld(true);
 				   }		
-		if (group != nullptr) {
-			for (int i = 0; i < group->getGroupSize(); i++) {
+				if (group != nullptr) {
+					for (int i = 0; i < group->getGroupSize(); i++) {
 					ManagedReference<CreatureObject*> groupedCreature = group->getGroupMember(i);
 					if (groupedCreature != nullptr && groupedCreature->isCreatureObject() && groupedCreature->isInRange(creature, 30.0f) && groupedCreature != creature) {
 					Locker locker(groupedCreature);
@@ -61,6 +47,7 @@ int DotpackMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, Creat
 			creature->playEffect("clienteffect/level_granted.cef", "");
 			sceneObject->destroyObjectFromWorld(true);
 		}
-	}		
-	return 0;
+		return 0;
+	}
+	return TangibleObjectMenuComponent::handleObjectMenuSelect(sceneObject, creature, selectedID);
 }
