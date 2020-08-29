23930312b24c32f3c42876aabe6fb756ba8dce96
bigtap <24537455+bigtap@users.noreply.github.com>
2020-08-28 19:50:57 -0700

    Update event_nsqueen.lua

--- Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/event/event_nsqueen.lua
+++ Flurry/Stable Server/Core3/MMOCoreORB/bin/scripts/mobile/event/event_nsqueen.lua
@@ -30,169 +30,165 @@ event_nsqueen = Creature:new {
 	lootGroups = {
 		{
 			groups = {
-				{group = "g_named_crystals", chance = 10000000},
+				{group = "boss_common", chance = 10000000},
+			},
+			lootChance = 10000000
+		},	
+		{
+			groups = {
+				{group = "boss_common", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "g_named_crystals", chance = 3500000},
-				{group = "color_crystals", chance = 4500000},
-				{group = "crystal_sunriders_destiny", chance = 1000000},
-				{group = "crystal_windus_guile", chance = 1000000},
+				{group = "boss_common", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "g_cloak_of_hate", chance = 10000000},
-			},
-			lootChance = 5000000
-		},
-		{
-			groups = {
-				{group = "capes", chance = 10000000},
-			},
-			lootChance = 5000000
-		},
-		{
-			groups = {
-				{group = "g_cloak_of_hate", chance = 10000000},
-			},
-			lootChance = 5000000
-		},
-		{
-			groups = {
-				{group = "capes", chance = 10000000},
+				{group = "boss_common", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "nge_all", chance = 10000000},
+				{group = "boss_common", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "goggles_all", chance = 10000000},
+				{group = "boss_common", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "neck_crafter", chance = 10000000},
+				{group = "boss_common", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "non_jedi_ring_crafter_second", chance = 10000000},
+				{group = "boss_common", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "non_jedi_ring_crafter", chance = 10000000},
+				{group = "boss_common", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "non_jedi_rings_ranged", chance = 10000000},
+				{group = "boss_common", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "non_jedi_rings", chance = 10000000},
+				{group = "boss_common", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "jedi_earings", chance = 10000000},
+				{group = "boss_rare", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "jedi_bracelets", chance = 10000000},
+				{group = "boss_rare", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "jedi_neck", chance = 10000000},
+				{group = "death_watch_bunker_ingredient_protective", chance = 5000000},
+ 				{group = "death_watch_bunker_ingredient_binary", chance = 5000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "jedi_rings", chance = 10000000},
+				{group = "death_watch_bunker_ingredient_protective", chance = 5000000},
+ 				{group = "death_watch_bunker_ingredient_binary", chance = 5000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "g_necklaces", chance = 10000000},
+				{group = "worldbosscrate", chance = 10000000},
+			},
+			lootChance = 10000000
+		},
+ 		{
+			groups = {
+				{group = "worldbosscrate", chance = 10000000},
+			},
+			lootChance = 10000000
+		},
+ 		{
+			groups = {
+				{group = "worldbosscrate", chance = 10000000},
+			},
+			lootChance = 10000000
+		},   
+		{
+			groups = {
+				{group = "worldbosscrate", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "clothing_attachments", chance = 10000000},
+				{group = "boss_rare", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "armor_attachments", chance = 10000000},
+				{group = "tierone", chance = 1000000},
+				{group = "tiertwo", chance = 1000000},
+				{group = "tierthree", chance = 3000000},
+				{group = "tierdiamond", chance = 5000000},
+			},
+			lootChance = 10000000
+		},    
+		{
+			groups = {
+				{group = "boss_rare", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "weapons_all", chance = 10000000},
+				{group = "tierone", chance = 1000000},
+				{group = "tiertwo", chance = 1000000},
+				{group = "tierthree", chance = 3000000},
+				{group = "tierdiamond", chance = 5000000},
+			},
+			lootChance = 10000000
+		},    
+		{
+			groups = {
+				{group = "tierone", chance = 1000000},
+				{group = "tiertwo", chance = 1000000},
+				{group = "tierthree", chance = 3000000},
+				{group = "tierdiamond", chance = 5000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "tierone", chance = 1500000},
-				{group = "tiertwo", chance = 3500000},
-				{group = "tierthree", chance = 2500000},
-				{group = "tierdiamond", chance = 2500000},
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "vehicledeedsnormal", chance = 10000000},
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "vehicledeedsrare", chance = 10000000},
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "tierone", chance = 1500000},
-				{group = "tiertwo", chance = 3500000},
-				{group = "tierthree", chance = 2500000},
-				{group = "tierdiamond", chance = 2500000},
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "tierone", chance = 1500000},
-				{group = "tiertwo", chance = 3500000},
-				{group = "tierthree", chance = 2500000},
-				{group = "tierdiamond", chance = 2500000},
+				{group = "tierone", chance = 1000000},
+				{group = "tiertwo", chance = 1000000},
+				{group = "tierthree", chance = 3000000},
+				{group = "tierdiamond", chance = 5000000},
 			},
 			lootChance = 10000000
 		}
