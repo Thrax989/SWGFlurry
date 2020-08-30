421e7cf072709ecde655d0bf6da402c1285de17d
Sygnum <jtrobbinsfl@gmail.com>
2020-08-29 21:44:21 -0400

    [Modified] MIssed one

--- MMOCoreORB/bin/scripts/loot/groups/boss_dungeon/boss_rare.lua
+++ MMOCoreORB/bin/scripts/loot/groups/boss_dungeon/boss_rare.lua
@@ -10,7 +10,7 @@ boss_rare = {
 		{groupTemplate = "weapon_component_advanced", weight = 955555}, 
 		{groupTemplate = "fire_breathing_spider", weight = 955555},   
 		{groupTemplate = "krayt_tissue_rare", weight = 955555},
-		{itemTemplate = "krayt_pearls_flawless", weight = 955555},    
+		{groupTemplate = "krayt_pearls_flawless", weight = 955555},    
 		{groupTemplate = "g_named_crystals", weight = 955555},
 		{groupTemplate = "g_cloak_of_hate", weight = 955555},  
 		{groupTemplate = "jedi_jewelry", weight = 955555},      
