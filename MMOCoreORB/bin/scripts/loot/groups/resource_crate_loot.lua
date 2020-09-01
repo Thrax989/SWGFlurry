c29f0e16e05e370b203e0c0d19fb7473deb00869
TOXIC <thrax989@yahoo.com>
2020-08-31 17:08:40 -0700

    [Added] Resource Deed / Resource Crate Loot Group

---
+++ MMOCoreORB/bin/scripts/loot/groups/resource_crate_loot.lua
@@ -0,0 +1,10 @@
+resource_crate_loot = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "resource_crate", weight = 10000000}
+	}
+}
+
+addLootGroupTemplate("resource_crate_loot", resource_crate_loot)
