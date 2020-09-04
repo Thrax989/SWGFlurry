268748b665a7ada27a2617c1b8e8fc1ec171f725
TOXIC <thrax989@yahoo.com>
2020-09-04 11:06:52 -0700

    [Updated] Custom Name

--- MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/clothing/clothing_hat_imperial_command.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/clothing/clothing_hat_imperial_command.lua
@@ -2,7 +2,7 @@ object_draft_schematic_clothing_clothing_hat_imperial_command = object_draft_sch
 
    templateType = DRAFTSCHEMATIC,
 
-   customObjectName = "",
+   customObjectName = "Imperial Hat",
 
    craftingToolTab = 8, -- (See DraftSchematicObjectTemplate.h)
    complexity = 30, 
