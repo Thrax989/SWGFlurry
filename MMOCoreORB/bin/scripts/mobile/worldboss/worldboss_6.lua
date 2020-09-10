5aad9bae208e234f8b5265de709cbd4382033a7b
bigtap <24537455+bigtap@users.noreply.github.com>
2020-09-09 22:17:44 -0700

    boss adjustments

--- MMOCoreORB/bin/scripts/mobile/worldboss/worldboss_6.lua
+++ MMOCoreORB/bin/scripts/mobile/worldboss/worldboss_6.lua
@@ -3,14 +3,14 @@ worldboss_6 = Creature:new {
 	socialGroup = "gurk",
 	faction = "",
 	level = 300,
-	chanceHit = 50.0,
-	damageMin = 3000,
-	damageMax = 6000,
+	chanceHit = 80.0,
+	damageMin = 4500,
+	damageMax = 6500,
 	baseXp = 28549,
-	baseHAM = 990000,
-	baseHAMmax = 1000000,
+	baseHAM = 400000,
+	baseHAMmax = 500000,
 	armor = 3,
-	resists = {75,75,75,75,75,75,75,75,75},
+	resists = {160,160,160,140,160,160,160,160,150},
 	meatType = "meat_herbivore",
 	meatAmount = 1500,
 	hideType = "hide_leathery",
@@ -33,7 +33,7 @@ worldboss_6 = Creature:new {
 			groups = {
 				{group = "boss_common", chance = 10000000},
 			},
-			lootChance = 1000000
+			lootChance = 10000000
 		},	
 		{
 			groups = {
@@ -97,31 +97,31 @@ worldboss_6 = Creature:new {
 		},
 		{
 			groups = {
-				{group = "boss_common", chance = 10000000},
+				{group = "boss_rare", chance = 10000000},
 			},
 			lootChance = 5000000
 		},
 		{
 			groups = {
-				{group = "boss_common", chance = 10000000},
+				{group = "boss_rare", chance = 10000000},
 			},
 			lootChance = 5000000
 		},
 		{
 			groups = {
-				{group = "clothing_attachments", chance = 10000000},
+				{group = "tierthree", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "armor_attachments", chance = 10000000},
+				{group = "tierthree", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "weapons_all", chance = 10000000},
+				{group = "tierdiamond", chance = 10000000},
 			},
 			lootChance = 10000000
 		},
--- MMOCoreORB/bin/scripts/mobile/worldboss/worldboss_8.lua
+++ MMOCoreORB/bin/scripts/mobile/worldboss/worldboss_8.lua
@@ -32,13 +32,13 @@ worldboss_8 = Creature:new {
 			groups = {
 				{group = "boss_common", chance = 10000000},
 			},
-			lootChance = 1000000
+			lootChance = 10000000
 		},		
 		{
 			groups = {
 				{group = "boss_rare", chance = 10000000},
 			},
-			lootChance = 1000000
+			lootChance = 10000000
 		},
 		{
 			groups = {
@@ -54,21 +54,21 @@ worldboss_8 = Creature:new {
 		},
 		{
 			groups = {
-				{group = "boss_common", chance = 10000000},
+				{group = "boss_rare", chance = 10000000},
 		},
-			lootChance = 500000
+			lootChance = 5000000
 		},
 		{
 			groups = {
-				{group = "boss_common", chance = 10000000},
+				{group = "boss_rare", chance = 10000000},
 		},
-			lootChance = 1500000
+			lootChance = 5000000
 		},
 		{
 			groups = {
 				{group = "boss_common", chance = 10000000}			
 		},
-			lootChance = 10000000
+			lootChance = 5000000
 		},
 		{
 			groups = {
@@ -78,7 +78,7 @@ worldboss_8 = Creature:new {
 		},
 		{
 			groups = {
-				{group = "tiertwo", chance = 10000000}
+				{group = "boss_common", chance = 10000000}
 		},
 			lootChance = 10000000
 		},
@@ -90,19 +90,7 @@ worldboss_8 = Creature:new {
 		},
 		{
 			groups = {
-				{group = "tiertwo", chance = 10000000}
-		},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "tierthree", chance = 10000000}
-		},
-			lootChance = 10000000
-		},
-                {
-			groups = {
-				{group = "tiertwo", chance = 10000000}
+				{group = "boss_rare", chance = 10000000}
 		},
 			lootChance = 10000000
 		},
@@ -114,7 +102,19 @@ worldboss_8 = Creature:new {
 		},
                 {
 			groups = {
-				{group = "tiertwo", chance = 10000000}
+				{group = "tierdiamond", chance = 10000000}
+		},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "tierthree", chance = 10000000}
+		},
+			lootChance = 10000000
+		},
+                {
+			groups = {
+				{group = "tierdiamond", chance = 10000000}
 		},
 			lootChance = 10000000
 		},
