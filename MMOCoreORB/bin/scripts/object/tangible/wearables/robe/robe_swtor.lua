0a654fcdd773001f1501a839ffbdc6190b534808
bigtap <24537455+bigtap@users.noreply.github.com>
2020-09-13 22:43:17 -0700

    Update robe_swtor.lua

--- MMOCoreORB/bin/scripts/object/tangible/wearables/robe/robe_swtor.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/robe/robe_swtor.lua
@@ -235,11 +235,7 @@ object_tangible_wearables_robe_robe_swtor = object_tangible_wearables_robe_share
 	experimentalCombineType = {0, 0, 4, 4},
 				skillMods = {
 				    {"jedi_force_power_max", 250},
-				    {"jedi_force_power_regen", 25},
-					{"resistance_bleeding", 25},
-					{"jedi_state_defense", 25},
-					{"melee_defense", 25},
-					{"ranged_defense", 25},
+				    {"jedi_force_power_regen", 10},
 				},
 
 
