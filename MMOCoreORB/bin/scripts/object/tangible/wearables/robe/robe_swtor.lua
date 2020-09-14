d0daac1a95f2658bc7a3fa7c89b2aa29396395f5
TOXIC <thrax989@yahoo.com>
2020-09-13 18:08:33 -0700

    [Updated] Robe Base Mods

--- MMOCoreORB/bin/scripts/object/tangible/wearables/robe/robe_swtor.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/robe/robe_swtor.lua
@@ -235,7 +235,11 @@ object_tangible_wearables_robe_robe_swtor = object_tangible_wearables_robe_share
 	experimentalCombineType = {0, 0, 4, 4},
 				skillMods = {
 				    {"jedi_force_power_max", 250},
-				    {"jedi_force_power_regen", 10}
+				    {"jedi_force_power_regen", 10},
+					{"resistance_bleeding", 25},
+					{"slope_move", 25},
+					{"melee_defense", 25},
+					{"ranged_defense", 25},
 				},
 
 
