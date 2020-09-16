69a62111608f76266a6b8d7a1193c69939389c6c
christian <thrax989@yahoo.com>
2020-09-16 15:22:44 -0700

    [Updated] Typos

--- MMOCoreORB/bin/scripts/object/building/heroic/objects.lua
+++ MMOCoreORB/bin/scripts/object/building/heroic/objects.lua
@@ -1,154 +1,154 @@
-object_building_heroic_shared_hoth_ion = :new {
+object_building_heroic_shared_hoth_ion = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_hoth_ion.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_hoth_ion, "object/building/heroic/shared_hoth_ion.iff")
 
-object_building_heroic_shared_dark_tower_b = :new {
+object_building_heroic_shared_dark_tower_b = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_dark_tower_b.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_dark_tower_b, "object/building/heroic/shared_dark_tower_b.iff")
 
-object_building_heroic_shared_moncal_tower_e = :new {
+object_building_heroic_shared_moncal_tower_e = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_moncal_tower_e.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_moncal_tower_e, "object/building/heroic/shared_moncal_tower_e.iff")
 
-object_building_heroic_shared_hoth_rebel_transport = :new {
+object_building_heroic_shared_hoth_rebel_transport = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_hoth_rebel_transport.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_hoth_rebel_transport, "object/building/heroic/shared_hoth_rebel_transport.iff")
 
-object_building_heroic_shared_dark_tower_e = :new {
+object_building_heroic_shared_dark_tower_e = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_dark_tower_e.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_dark_tower_e, "object/building/heroic/shared_dark_tower_e.iff")
 
-object_building_heroic_shared_bespin_huge_tower = :new {
+object_building_heroic_shared_bespin_huge_tower = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_bespin_huge_tower.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_bespin_huge_tower, "object/building/heroic/shared_bespin_huge_tower.iff")
 
-object_building_heroic_shared_bespin_tower_g = :new {
+object_building_heroic_shared_bespin_tower_g = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_bespin_tower_g.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_bespin_tower_g, "object/building/heroic/shared_bespin_tower_g.iff")
 
-object_building_heroic_shared_cloud_city = :new {
+object_building_heroic_shared_cloud_city = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_cloud_city.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_cloud_city, "object/building/heroic/shared_cloud_city.iff")
 
-object_building_heroic_shared_hoth_generator = :new {
+object_building_heroic_shared_hoth_generator = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_hoth_generator.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_hoth_generator, "object/building/heroic/shared_hoth_generator.iff")
 
-object_building_heroic_shared_bespin_tower_b = :new {
+object_building_heroic_shared_bespin_tower_b = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_bespin_tower_b.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_bespin_tower_b, "object/building/heroic/shared_bespin_tower_b.iff")
 
-object_building_heroic_shared_bespin_tower_e = :new {
+object_building_heroic_shared_bespin_tower_e = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_bespin_tower_e.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_bespin_tower_e, "object/building/heroic/shared_bespin_tower_e.iff")
 
-object_building_heroic_shared_echo_base = :new {
+object_building_heroic_shared_echo_base = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_echo_base.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_echo_base, "object/building/heroic/shared_echo_base.iff")
 
-object_building_heroic_shared_ig88_factory_arena = :new {
+object_building_heroic_shared_ig88_factory_arena = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_ig88_factory_arena.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_ig88_factory_arena, "object/building/heroic/shared_ig88_factory_arena.iff")
 
-object_building_heroic_shared_hoth_generator_destroyed = :new {
+object_building_heroic_shared_hoth_generator_destroyed = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_hoth_generator_destroyed.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_hoth_generator_destroyed, "object/building/heroic/shared_hoth_generator_destroyed.iff")
 
-object_building_heroic_shared_moncal_tower_d = :new {
+object_building_heroic_shared_moncal_tower_d = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_moncal_tower_d.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_moncal_tower_d, "object/building/heroic/shared_moncal_tower_d.iff")
 
-object_building_heroic_shared_dark_tower_c = :new {
+object_building_heroic_shared_dark_tower_c = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_dark_tower_c.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_dark_tower_c, "object/building/heroic/shared_dark_tower_c.iff")
 
-object_building_heroic_shared_cloud_platform = :new {
+object_building_heroic_shared_cloud_platform = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_cloud_platform.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_cloud_platform, "object/building/heroic/shared_cloud_platform.iff")
 
-object_building_heroic_shared_bespin_tower_a = :new {
+object_building_heroic_shared_bespin_tower_a = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_bespin_tower_a.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_bespin_tower_a, "object/building/heroic/shared_bespin_tower_a.iff")
 
-object_building_heroic_shared_bespin_tower_f = :new {
+object_building_heroic_shared_bespin_tower_f = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_bespin_tower_f.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_bespin_tower_f, "object/building/heroic/shared_bespin_tower_f.iff")
 
-object_building_heroic_shared_ptower = :new {
+object_building_heroic_shared_ptower = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_ptower.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_ptower, "object/building/heroic/shared_ptower.iff")
 
-object_building_heroic_shared_dark_tower_d = :new {
+object_building_heroic_shared_dark_tower_d = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_dark_tower_d.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_dark_tower_d, "object/building/heroic/shared_dark_tower_d.iff")
 
-object_building_heroic_shared_golan_laser_battery = :new {
+object_building_heroic_shared_golan_laser_battery = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_golan_laser_battery.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_golan_laser_battery, "object/building/heroic/shared_golan_laser_battery.iff")
 
-object_building_heroic_shared_dark_tower_a = :new {
+object_building_heroic_shared_dark_tower_a = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_dark_tower_a.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_dark_tower_a, "object/building/heroic/shared_dark_tower_a.iff")
 
-object_building_heroic_shared_axkva_min_lair = :new {
+object_building_heroic_shared_axkva_min_lair = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_axkva_min_lair.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_axkva_min_lair, "object/building/heroic/shared_axkva_min_lair.iff")
 
-object_building_heroic_shared_moncal_tower_f = :new {
+object_building_heroic_shared_moncal_tower_f = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_moncal_tower_f.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_moncal_tower_f, "object/building/heroic/shared_moncal_tower_f.iff")
 
-object_building_heroic_shared_exar_kun_tomb = :new {
+object_building_heroic_shared_exar_kun_tomb = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_exar_kun_tomb.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_exar_kun_tomb, "object/building/heroic/shared_exar_kun_tomb.iff")
 
-object_building_heroic_shared_dark_huge_tower = :new {
+object_building_heroic_shared_dark_huge_tower = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_dark_huge_tower.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_dark_huge_tower, "object/building/heroic/shared_dark_huge_tower.iff")
 
-object_building_heroic_shared_bespin_tower_c = :new {
+object_building_heroic_shared_bespin_tower_c = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_bespin_tower_c.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_bespin_tower_c, "object/building/heroic/shared_bespin_tower_c.iff")
 
-object_building_heroic_shared_frozen_tauntaun = :new {
+object_building_heroic_shared_frozen_tauntaun = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_frozen_tauntaun.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_frozen_tauntaun, "object/building/heroic/shared_frozen_tauntaun.iff")
 
-object_building_heroic_shared_bespin_tower_d = :new {
+object_building_heroic_shared_bespin_tower_d = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_bespin_tower_d.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_bespin_tower_d, "object/building/heroic/shared_bespin_tower_d.iff")
 
-object_building_heroic_shared_dark_tower_f = :new {
+object_building_heroic_shared_dark_tower_f = SharedBuildingObjectTemplate:new {
     clientTemplateFileName = "object/building/heroic/shared_dark_tower_f.iff"
 }
 ObjectTemplates:addClientTemplate(object_building_heroic_shared_dark_tower_f, "object/building/heroic/shared_dark_tower_f.iff")
