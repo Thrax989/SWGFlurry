d6bea00dbbe1aedb982d0525733dd7164d6962ab
christian <thrax989@yahoo.com>
2020-09-17 11:15:35 -0700

    [Updated] More Server Object

--- MMOCoreORB/bin/scripts/object/intangible/vehicle/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/intangible/vehicle/serverobjects.lua
@@ -1,55 +1,82 @@
---Copyright (C) 2010 <SWGEmu>
-
-
---This File is part of Core3.
-
---This program is free software; you can redistribute 
---it and/or modify it under the terms of the GNU Lesser 
---General Public License as published by the Free Software
---Foundation; either version 2 of the License, 
---or (at your option) any later version.
-
---This program is distributed in the hope that it will be useful, 
---but WITHOUT ANY WARRANTY; without even the implied warranty of 
---MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
---See the GNU Lesser General Public License for
---more details.
-
---You should have received a copy of the GNU Lesser General 
---Public License along with this program; if not, write to
---the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-
---Linking Engine3 statically or dynamically with other modules 
---is making a combined work based on Engine3. 
---Thus, the terms and conditions of the GNU Lesser General Public License 
---cover the whole combination.
-
---In addition, as a special exception, the copyright holders of Engine3 
---give you permission to combine Engine3 program with free software 
---programs or libraries that are released under the GNU LGPL and with 
---code included in the standard release of Core3 under the GNU LGPL 
---license (or modified versions of such code, with unchanged license). 
---You may copy and distribute such a system following the terms of the 
---GNU LGPL for Engine3 and the licenses of the other code concerned, 
---provided that you include the source code of that other code when 
---and as the GNU LGPL requires distribution of source code.
-
---Note that people who make modified versions of Engine3 are not obligated 
---to grant this special exception for their modified versions; 
---it is their choice whether to do so. The GNU Lesser General Public License 
---gives permission to release a modified version without this exception; 
---this exception also makes it possible to release a modified version 
-
-
-
---Children folder includes
-
--- Server Objects
-includeFile("intangible/vehicle/jetpack_pcd.lua")
 includeFile("intangible/vehicle/landspeeder_av21_pcd.lua")
-includeFile("intangible/vehicle/landspeeder_x31_pcd.lua")
-includeFile("intangible/vehicle/landspeeder_x34_pcd.lua")
 includeFile("intangible/vehicle/speederbike_flash_pcd.lua")
-includeFile("intangible/vehicle/speederbike_pcd.lua")
 includeFile("intangible/vehicle/speederbike_swoop_pcd.lua")
+includeFile("intangible/vehicle/organa_speeder_pcd.lua")
+includeFile("intangible/vehicle/barc_speeder_pcd.lua")
+includeFile("intangible/vehicle/fg_8t8_podracer_pcd.lua")
+includeFile("intangible/vehicle/barc_speeder_imperial_pcd.lua")
+includeFile("intangible/vehicle/atst_pcd.lua")
+includeFile("intangible/vehicle/podracer_longtail_pcd.lua")
+includeFile("intangible/vehicle/air2_swoop_speeder_pcd.lua")
+includeFile("intangible/vehicle/walker_at_rt_camo_pcd.lua")
+includeFile("intangible/vehicle/tcg_merr_sonn_jt12_jetpack_pcd.lua")
+includeFile("intangible/vehicle/jetpack_pcd.lua")
+includeFile("intangible/vehicle/light_bending_barc.lua")
+includeFile("intangible/vehicle/grievous_wheel_bike.lua")
+includeFile("intangible/vehicle/tcg_8_single_pod_airspeeder.lua")
+includeFile("intangible/vehicle/landspeeder_v35_pcd.lua")
+includeFile("intangible/vehicle/temp_walker1_pcd.lua")
+includeFile("intangible/vehicle/speeder_stap_pcd.lua")
+includeFile("intangible/vehicle/pod_racer_light_bending.lua")
+includeFile("intangible/vehicle/basilisk_war_droid.lua")
+includeFile("intangible/vehicle/tcg_8_single_pod_airspeeder_pcd.lua")
+includeFile("intangible/vehicle/pod_racer_balta_podracer_pcd.lua")
+includeFile("intangible/vehicle/pod_racer_ipg_longtail_pcd.lua")
+includeFile("intangible/vehicle/mustafar_panning_droid_pcd.lua")
+includeFile("intangible/vehicle/landspeeder_tantive4_pcd.lua")
+includeFile("intangible/vehicle/swamp_speeder_pcd.lua")
+includeFile("intangible/vehicle/landspeeder_x31_pcd.lua")
+includeFile("intangible/vehicle/atxt_pcd.lua")
+includeFile("intangible/vehicle/flare_s_swoop_crafted_pcd.lua")
+includeFile("intangible/vehicle/stap_speeder_pcd.lua")
+includeFile("intangible/vehicle/basilisk_war_droid_pcd.lua")
+includeFile("intangible/vehicle/snowspeeder_pcd.lua")
+includeFile("intangible/vehicle/atpt_pcd.lua")
+includeFile("intangible/vehicle/balta_podracer_pcd.lua")
+includeFile("intangible/vehicle/walker_at_rt_reg_pcd.lua")
+includeFile("intangible/vehicle/pod_racer_one_pcd.lua")
+includeFile("intangible/vehicle/walker_at_st_walker_pcd.lua")
+includeFile("intangible/vehicle/tcg_8_air_speeder_pcd.lua")
+includeFile("intangible/vehicle/pod_racer_two_pcd.lua")
+includeFile("intangible/vehicle/tcg_8_air_speeder.lua")
+includeFile("intangible/vehicle/snowspeeder.lua")
+includeFile("intangible/vehicle/speeder_ric_920_pcd.lua")
+includeFile("intangible/vehicle/walker_at_xt_pcd.lua")
+includeFile("intangible/vehicle/podracer_anakin_pcd.lua")
+includeFile("intangible/vehicle/xj6_air_speeder_pcd.lua")
+includeFile("intangible/vehicle/landspeeder_xp38_pcd.lua")
+includeFile("intangible/vehicle/landspeeder_x34_pcd.lua")
+includeFile("intangible/vehicle/landspeeder_tantive4_adv_pcd.lua")
+includeFile("intangible/vehicle/tcg_hk47_jetpack_pcd.lua")
+includeFile("intangible/vehicle/mechno_chair_pcd.lua")
+includeFile("intangible/vehicle/landspeeder_av21_pcd.lua")
+includeFile("intangible/vehicle/koro2_speeder_pcd.lua")
+includeFile("intangible/vehicle/landspeeder_usv5_s02_pcd.lua")
+includeFile("intangible/vehicle/pod_racer_longtail_pcd.lua")
+includeFile("intangible/vehicle/speederbike_pcd.lua")
+includeFile("intangible/vehicle/landspeeder_organa_pcd.lua")
+includeFile("intangible/vehicle/flare_s_swoop_pcd.lua")
+includeFile("intangible/vehicle/mustafar_panning_droid.lua")
+includeFile("intangible/vehicle/landspeeder_usv5_pcd.lua")
+includeFile("intangible/vehicle/air2_swoop_pcd.lua")
+includeFile("intangible/vehicle/atat_pcd.lua")
 includeFile("intangible/vehicle/vehicle_pcd_base.lua")
+includeFile("intangible/vehicle/a1_deluxe_floater_pcd.lua")
+includeFile("intangible/vehicle/senate_pod_pcd.lua")
+includeFile("intangible/vehicle/landspeeder_lava_skiff_pcd.lua")
+includeFile("intangible/vehicle/hover_chair_pcd.lua")
+includeFile("intangible/vehicle/hoth_at_st_pcd.lua")
+includeFile("intangible/vehicle/hoverlifter_speeder_crafted_pcd.lua")
+includeFile("intangible/vehicle/podracer_gasgano_pcd.lua")
+includeFile("intangible/vehicle/geonosian_speeder_pcd.lua")
+includeFile("intangible/vehicle/sith_speeder_pcd.lua")
+includeFile("intangible/vehicle/landspeeder_desert_skiff_pcd.lua")
+includeFile("intangible/vehicle/tcg_at_pt_walker_pcd.lua")
+includeFile("intangible/vehicle/tcg_military_transport_pcd.lua")
+includeFile("intangible/vehicle/military_transport_pcd.lua")
+includeFile("intangible/vehicle/podracer_mawhonic_pcd.lua")
+includeFile("intangible/vehicle/tcg_republic_gunship_pcd.lua")
+includeFile("intangible/vehicle/hoverlifter_speeder_pcd.lua")
+includeFile("intangible/vehicle/landspeeder_ab1_pcd.lua")
+includeFile("intangible/vehicle/barc_speeder_rebel_pcd.lua")
+includeFile("intangible/vehicle/gift_stap_pcd.lua")
