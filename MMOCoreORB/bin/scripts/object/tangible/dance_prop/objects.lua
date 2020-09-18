247e4d70bc035f6af2f5d89da6e2b6c6b640387b
TOXIC <thrax989@yahoo.com>
2020-09-18 10:52:55 -0700

    [Updated] Server Objects

--- MMOCoreORB/bin/scripts/object/tangible/dance_prop/objects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/dance_prop/objects.lua
@@ -2,4 +2,55 @@ object_tangible_dance_prop_shared_prop_base = SharedTangibleObjectTemplate:new {
 	clientTemplateFileName = "object/tangible/dance_prop/shared_prop_base.iff"
 }
 
+object_tangible_dance_prop_shared_prop_glowstick_r_s04 = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/dance_prop/shared_prop_glowstick_r_s04.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_dance_prop_shared_prop_glowstick_r_s04, "object/tangible/dance_prop/shared_prop_glowstick_r_s04.iff")
+
+object_tangible_dance_prop_shared_prop_glowstick_r_s03 = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/dance_prop/shared_prop_glowstick_r_s03.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_dance_prop_shared_prop_glowstick_r_s03, "object/tangible/dance_prop/shared_prop_glowstick_r_s03.iff")
+
+object_tangible_dance_prop_shared_prop_glowstick_l_s04 = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/dance_prop/shared_prop_glowstick_l_s04.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_dance_prop_shared_prop_glowstick_l_s04, "object/tangible/dance_prop/shared_prop_glowstick_l_s04.iff")
+
+object_tangible_dance_prop_shared_prop_glowstick_l_s03 = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/dance_prop/shared_prop_glowstick_l_s03.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_dance_prop_shared_prop_glowstick_l_s03, "object/tangible/dance_prop/shared_prop_glowstick_l_s03.iff")
+
+object_tangible_dance_prop_shared_prop_glowstick_r = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/dance_prop/shared_prop_glowstick_r.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_dance_prop_shared_prop_glowstick_r, "object/tangible/dance_prop/shared_prop_glowstick_r.iff")
+
+object_tangible_dance_prop_shared_prop_glowstick_r_s05 = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/dance_prop/shared_prop_glowstick_r_s05.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_dance_prop_shared_prop_glowstick_r_s05, "object/tangible/dance_prop/shared_prop_glowstick_r_s05.iff")
+
+object_tangible_dance_prop_shared_prop_glowstick_l = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/dance_prop/shared_prop_glowstick_l.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_dance_prop_shared_prop_glowstick_l, "object/tangible/dance_prop/shared_prop_glowstick_l.iff")
+
+object_tangible_dance_prop_shared_prop_glowstick_r_s02 = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/dance_prop/shared_prop_glowstick_r_s02.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_dance_prop_shared_prop_glowstick_r_s02, "object/tangible/dance_prop/shared_prop_glowstick_r_s02.iff")
+
+object_tangible_dance_prop_shared_prop_glowstick_l_s05 = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/dance_prop/shared_prop_glowstick_l_s05.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_dance_prop_shared_prop_glowstick_l_s05, "object/tangible/dance_prop/shared_prop_glowstick_l_s05.iff")
+
+object_tangible_dance_prop_shared_prop_glowstick_l_s02 = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/dance_prop/shared_prop_glowstick_l_s02.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_dance_prop_shared_prop_glowstick_l_s02, "object/tangible/dance_prop/shared_prop_glowstick_l_s02.iff")
+
+
 ObjectTemplates:addClientTemplate(object_tangible_dance_prop_shared_prop_base, "object/tangible/dance_prop/shared_prop_base.iff")
