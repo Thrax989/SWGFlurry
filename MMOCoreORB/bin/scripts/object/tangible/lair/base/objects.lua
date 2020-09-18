55515630796636ac7526b1075f0b71b7362b7320
TOXIC <thrax989@yahoo.com>
2020-09-18 13:42:32 -0700

    [Added] Server Objects

--- MMOCoreORB/bin/scripts/object/tangible/lair/base/objects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/lair/base/objects.lua
@@ -15439,3 +15439,69 @@ object_tangible_lair_base_shared_trash_dark = SharedTangibleObjectTemplate:new {
 }
 
 ObjectTemplates:addClientTemplate(object_tangible_lair_base_shared_trash_dark, "object/tangible/lair/base/shared_trash_dark.iff")
+
+object_tangible_lair_base_shared_lair_lava_beetle = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/lair/base/shared_lair_lava_beetle.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_lair_base_shared_lair_lava_beetle, "object/tangible/lair/base/shared_lair_lava_beetle.iff")
+
+object_tangible_lair_base_shared_lair_tulrus = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/lair/base/shared_lair_tulrus.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_lair_base_shared_lair_tulrus, "object/tangible/lair/base/shared_lair_tulrus.iff")
+
+object_tangible_lair_base_shared_lair_web = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/lair/base/shared_lair_web.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_lair_base_shared_lair_web, "object/tangible/lair/base/shared_lair_web.iff")
+
+object_tangible_lair_base_shared_lair_blistmok = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/lair/base/shared_lair_blistmok.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_lair_base_shared_lair_blistmok, "object/tangible/lair/base/shared_lair_blistmok.iff")
+
+object_tangible_lair_base_shared_poi_all_lair_ice_shelter_small = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/lair/base/shared_poi_all_lair_ice_shelter_small.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_lair_base_shared_poi_all_lair_ice_shelter_small, "object/tangible/lair/base/shared_poi_all_lair_ice_shelter_small.iff")
+
+object_tangible_lair_base_shared_lair_mouf = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/lair/base/shared_lair_mouf.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_lair_base_shared_lair_mouf, "object/tangible/lair/base/shared_lair_mouf.iff")
+
+object_tangible_lair_base_shared_lair_walluga = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/lair/base/shared_lair_walluga.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_lair_base_shared_lair_walluga, "object/tangible/lair/base/shared_lair_walluga.iff")
+
+object_tangible_lair_base_shared_poi_all_lair_ice_shelter_large = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/lair/base/shared_poi_all_lair_ice_shelter_large.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_lair_base_shared_poi_all_lair_ice_shelter_large, "object/tangible/lair/base/shared_poi_all_lair_ice_shelter_large.iff")
+
+object_tangible_lair_base_shared_lair_uller = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/lair/base/shared_lair_uller.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_lair_base_shared_lair_uller, "object/tangible/lair/base/shared_lair_uller.iff")
+
+object_tangible_lair_base_shared_lair_lava_monster = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/lair/base/shared_lair_lava_monster.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_lair_base_shared_lair_lava_monster, "object/tangible/lair/base/shared_lair_lava_monster.iff")
+
+object_tangible_lair_base_shared_lair_xandank = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/lair/base/shared_lair_xandank.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_lair_base_shared_lair_xandank, "object/tangible/lair/base/shared_lair_xandank.iff")
+
+object_tangible_lair_base_shared_lair_lava_flea = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/lair/base/shared_lair_lava_flea.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_lair_base_shared_lair_lava_flea, "object/tangible/lair/base/shared_lair_lava_flea.iff")
+
+object_tangible_lair_base_shared_lair_lava_lizard = SharedTangibleObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/lair/base/shared_lair_lava_lizard.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_lair_base_shared_lair_lava_lizard, "object/tangible/lair/base/shared_lair_lava_lizard.iff")
+
