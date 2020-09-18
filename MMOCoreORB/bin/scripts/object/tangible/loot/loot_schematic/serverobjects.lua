33a29391f960527644829c83c91bbf356ddcf651
christian <thrax989@yahoo.com>
2020-09-18 09:48:34 -0700

    [Added] Last Bit Of Tangible Server Includes

--- MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/serverobjects.lua
@@ -1,50 +1,3 @@
---Copyright (C) 2010 <SWGEmu>
-
-
---This File is part of Core3.
-
---This program is free software; you can redistribute 
---it and/or modify it under the terms of the GNU Lesser 
---General Public License as published by the Free Software
---Foundation; either version 2 of the License, 
---or (at your option) any later version.
-
---This program is distributed in the hope that it will be useful, 
---but WITHOUT ANY WARRANTY; without even the implied warranty of 
---MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
---See the GNU Lesser General Public License for
---more details.
-
---You should have received a copy of the GNU Lesser General 
---Public License along with this program; if not, write to
---the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-
---Linking Engine3 statically or dynamically with other modules 
---is making a combined work based on Engine3. 
---Thus, the terms and conditions of the GNU Lesser General Public License 
---cover the whole combination.
-
---In addition, as a special exception, the copyright holders of Engine3 
---give you permission to combine Engine3 program with free software 
---programs or libraries that are released under the GNU LGPL and with 
---code included in the standard release of Core3 under the GNU LGPL 
---license (or modified versions of such code, with unchanged license). 
---You may copy and distribute such a system following the terms of the 
---GNU LGPL for Engine3 and the licenses of the other code concerned, 
---provided that you include the source code of that other code when 
---and as the GNU LGPL requires distribution of source code.
-
---Note that people who make modified versions of Engine3 are not obligated 
---to grant this special exception for their modified versions; 
---it is their choice whether to do so. The GNU Lesser General Public License 
---gives permission to release a modified version without this exception; 
---this exception also makes it possible to release a modified version 
-
-
-
---Children folder includes
-
--- Server Objects
 includeFile("tangible/loot/loot_schematic/agitator_motor_schematic.lua")
 includeFile("tangible/loot/loot_schematic/assorted_fruit_schematic.lua")
 includeFile("tangible/loot/loot_schematic/bacta_tank_schematic.lua")
@@ -154,3 +107,54 @@ includeFile("tangible/loot/loot_schematic/vehicle_house_loot_schem.lua")
 includeFile("tangible/loot/loot_schematic/vipbunker_house_loot_schem.lua")
 includeFile("tangible/loot/loot_schematic/yoda_house_loot_schem.lua")
 includeFile("tangible/loot/loot_schematic/yt1300_house_loot_schem.lua")
+
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s01_gloves.lua")
+includeFile("tangible/loot/loot_schematic/invisible_bounty_hunter_helmet.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_neutral_clone_bracer_l.lua")
+includeFile("tangible/loot/loot_schematic/naboo_small_window_s01_house_loot_schem.lua")
+includeFile("tangible/loot/loot_schematic/tatooine_small_window_s01_house_loot_schem.lua")
+includeFile("tangible/loot/loot_schematic/armor_bounty_hunter_bracer_l.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s01_helmet.lua")
+includeFile("tangible/loot/loot_schematic/gen_medium_window_s01_house_loot_schem.lua")
+includeFile("tangible/loot/loot_schematic/armor_bounty_hunter_chest_plate.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s01_belt.lua")
+includeFile("tangible/loot/loot_schematic/invisible_infiltrator_helmet.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_neutral_clone_chest_plate.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_neutral_clone_boots.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s02_boots.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s01_bicep_l.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s02_gloves.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s02_belt.lua")
+includeFile("tangible/loot/loot_schematic/gen_large_window_s02_house_loot_schem.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s02_bicep_l.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s01_bracer_r.lua")
+includeFile("tangible/loot/loot_schematic/armor_bounty_hunter_bicep_r.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_neutral_clone_bicep_r.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s02_bracer_l.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s02_helmet.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s01_leggings.lua")
+includeFile("tangible/loot/loot_schematic/gen_small_window_s01_house_loot_schem.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s02_chest_plate.lua")
+includeFile("tangible/loot/loot_schematic/armor_bounty_hunter_gloves.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s02_leggings.lua")
+includeFile("tangible/loot/loot_schematic/armor_bounty_hunter_boots.lua")
+includeFile("tangible/loot/loot_schematic/armor_bounty_hunter_helmet.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s01_bracer_l.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s01_chest_plate.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s02_bicep_r.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_neutral_clone_belt.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_neutral_clone_bicep_l.lua")
+includeFile("tangible/loot/loot_schematic/armor_bounty_hunter_bicep_l.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s02_bracer_r.lua")
+includeFile("tangible/loot/loot_schematic/gen_medium_window_s02_house_loot_schem.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s01_bicep_r.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_neutral_clone_leggings.lua")
+includeFile("tangible/loot/loot_schematic/armor_bounty_hunter_leggings.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_neutral_clone_helmet.lua")
+includeFile("tangible/loot/loot_schematic/gen_small_window_s02_house_loot_schem.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_infiltrator_s01_boots.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_neutral_clone_gloves.lua")
+includeFile("tangible/loot/loot_schematic/gen_large_window_s01_house_loot_schem.lua")
+includeFile("tangible/loot/loot_schematic/invisible_ris_helmet.lua")
+includeFile("tangible/loot/loot_schematic/armor_appearance_neutral_clone_bracer_r.lua")
+includeFile("tangible/loot/loot_schematic/armor_bounty_hunter_bracer_r.lua")
