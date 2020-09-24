008b51702c1fdb783162652d5f03a8c32029c982
christian <thrax989@yahoo.com>
2020-09-24 14:47:40 -0700

    [Updated] Cybernetic Resource Requirements

--- MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/armor/cybernetic_appearance_s02_torso.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/armor/cybernetic_appearance_s02_torso.lua
@@ -20,10 +20,10 @@ object_draft_schematic_armor_cybernetic_appearance_s02_torso = object_draft_sche
    customizationDefaults = {0},
 
    ingredientTemplateNames = {"craft_droid_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
-   ingredientTitleNames = {"sensor_suite", "body", "liner", "hardware_and_attachments", "binding_and_reinforcement", "padding", "armor", "load_bearing_harness", "reinforcement"},
-   ingredientSlotType = {1, 0, 0, 0, 0, 0, 1, 1, 1},
-   resourceTypes = {"object/tangible/component/droid/shared_sensor_package_droid.iff", "fuel_petrochem_solid_known", "fiberplast_naboo", "aluminum", "copper_beyrllius", "hide_wooly", "object/tangible/component/armor/shared_armor_segment_composite.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
-   resourceQuantities = {1, 25, 15, 10, 10, 10, 1, 2, 1},
+   ingredientTitleNames = {"sensor_suite", "reinforcement", "liner", "hardware_and_attachments", "binding_and_reinforcement", "padding", "armor", "load_bearing_harness", "reinforcement"},
+   ingredientSlotType = {1, 1, 0, 0, 0, 0, 1, 1, 1},
+   resourceTypes = {"object/tangible/component/droid/shared_sensor_package_droid.iff", "object/tangible/loot/misc/shared_artifact_rare_s01.iff", "fiberplast_naboo", "aluminum", "copper_beyrllius", "metal", "object/tangible/component/armor/shared_armor_segment_composite.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
+   resourceQuantities = {1, 1, 15, 10, 10, 10, 1, 2, 1},
    contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100},
 
 
