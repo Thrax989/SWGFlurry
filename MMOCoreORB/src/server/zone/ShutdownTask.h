cd6141f5ab4851e199649a4c98dc0cd710590e53
TOXIC <thrax989@yahoo.com>
2020-10-08 13:08:59 -0700

    [Added] Test Connections String On Shutdown

--- MMOCoreORB/src/server/zone/ShutdownTask.h
+++ MMOCoreORB/src/server/zone/ShutdownTask.h
@@ -22,6 +22,8 @@ public:
 	}
 
 	void run() {
+		ManagedReference<PlayerObject*> ghost = player->getPlayerObject();
+		PlayerManager* playerManager = server->getZoneServer()->getPlayerManager();
 		--minutesRemaining;
 
 		String str = "Server will shutdown in " + String::valueOf(minutesRemaining) + " minutes";
@@ -33,6 +35,7 @@ public:
 
 		zoneServer->getChatManager()->broadcastGalaxy(nullptr, str);
 		zoneServer->getChatManager()->handleGeneralDiscordShutdownChat(nullptr, str);
+		str << "Connections Online: " << String::valueOf(player->getZoneServer()->getConnectionCount()) << endl;
 
 		if (minutesRemaining <= 0) {
 			ServerCore::getInstance()->signalShutdown();
