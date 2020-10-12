55cf0c0f372eabe3fbfc64b072964f1b628c0362
TOXIC <thrax989@yahoo.com>
2020-10-11 17:09:16 -0700

    [Updated] Boss Rare Rolls Removed Robes

--- MMOCoreORB/bin/scripts/loot/groups/boss_dungeon/boss_rare.lua
+++ MMOCoreORB/bin/scripts/loot/groups/boss_dungeon/boss_rare.lua
@@ -3,8 +3,8 @@ boss_rare = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{groupTemplate = "jedi_comp_group", weight = 200000},
-		{groupTemplate = "legendary_comp_group", weight = 800000},
+		{groupTemplate = "jedi_comp_group", weight = 600000},
+		{groupTemplate = "legendary_comp_group", weight = 955555},
 		{groupTemplate = "vehicledeedsrare", weight = 955560},
 		{groupTemplate = "chemistry_component_advanced", weight = 955555},
 		{groupTemplate = "weapon_component_advanced", weight = 955555}, 
@@ -12,10 +12,9 @@ boss_rare = {
 		{groupTemplate = "krayt_tissue_rare", weight = 955555},
 		{groupTemplate = "krayt_pearls_flawless", weight = 955555},    
 		{groupTemplate = "g_named_crystals", weight = 955555},
-		{groupTemplate = "g_cloak_of_hate", weight = 955555},  
 		{groupTemplate = "jedi_jewelry", weight = 955555},      
 		{groupTemplate = "weapons_all", weight = 200000},    
-		{groupTemplate = "nge_all", weight = 200000}   
+		{groupTemplate = "nge_all", weight = 600000}   
 		
 	}
 }
