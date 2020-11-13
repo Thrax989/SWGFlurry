9ff3ebf7a7f6ebdf4ff1706297dee6e387d72db7
christian <thrax989@yahoo.com>
2020-11-13 13:32:24 -0800

    [Added]19 New Geonosis NPC's

---
+++ MMOCoreORB/bin/scripts/mobile/geonosis/pit_droid.lua
@@ -0,0 +1,49 @@
+pit_droid = Creature:new {
+	customName = "Geonosis Battle Droid",
+	socialGroup = "",
+	faction = "",
+	level = 100,
+	chanceHit = 1.0,
+	damageMin = 500,
+	damageMax = 1500,
+	baseXp = 128549,
+	baseHAM = 100000,
+	baseHAMmax = 110000,
+	armor = 3,
+	resists = {90,90,90,90,90,90,90,90,50},--kinetic,energy,blast,heat,cold,electric,acid,stun,ls
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = KILLER,
+	optionsBitmask = AIENABLED,
+	diet = NONE,
+	scale = 1.0,
+	templates = {"object/mobile/pit_droid.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "", chance = 10000000},
+			},
+			lootChance = 10000000
+		},	
+		{
+			groups = {
+				{group = "", chance = 10000000},
+			},
+			lootChance = 10000000
+		}
+	},
+	weapons = {"pirate_weapons_heavy"},
+	conversationTemplate = "",
+	attacks = merge(bountyhuntermaster,marksmanmaster,brawlermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(pit_droid, "pit_droid")
+
---
+++ MMOCoreORB/bin/scripts/mobile/geonosis/security1_droid.lua
@@ -0,0 +1,49 @@
+security1_droid = Creature:new {
+	customName = "Geonosis Battle Droid",
+	socialGroup = "",
+	faction = "",
+	level = 100,
+	chanceHit = 1.0,
+	damageMin = 500,
+	damageMax = 1500,
+	baseXp = 128549,
+	baseHAM = 100000,
+	baseHAMmax = 110000,
+	armor = 3,
+	resists = {90,90,90,90,90,90,90,90,50},--kinetic,energy,blast,heat,cold,electric,acid,stun,ls
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = KILLER,
+	optionsBitmask = AIENABLED,
+	diet = NONE,
+	scale = 1.0,
+	templates = {"object/mobile/endor_ig88_security_battledroid_blue.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "", chance = 10000000},
+			},
+			lootChance = 10000000
+		},	
+		{
+			groups = {
+				{group = "", chance = 10000000},
+			},
+			lootChance = 10000000
+		}
+	},
+	weapons = {"pirate_weapons_heavy"},
+	conversationTemplate = "",
+	attacks = merge(bountyhuntermaster,marksmanmaster,brawlermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(security1_droid, "security1_droid")
+
