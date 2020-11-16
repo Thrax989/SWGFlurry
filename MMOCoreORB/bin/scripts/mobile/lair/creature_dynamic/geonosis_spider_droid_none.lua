9b6c44575f8891386d5947e47cd3834e6193762a
christian <thrax989@yahoo.com>
2020-11-15 21:23:35 -0800

    [Added] New Dynamic Spawns To Geonosis Hellfire , Spider, Sentry Droids.

---
+++ MMOCoreORB/bin/scripts/mobile/lair/creature_dynamic/geonosis_spider_droid_none.lua
@@ -0,0 +1,12 @@
+geonosis_spider_droid_none = Lair:new {
+	mobiles = {{"spider_droid",6}},
+	spawnLimit = 15,
+	buildingsVeryEasy = {},
+	buildingsEasy = {},
+	buildingsMedium = {},
+	buildingsHard = {},
+	buildingsVeryHard = {},
+	buildingType = "none"
+}
+
+addLairTemplate("geonosis_spider_droid_none", geonosis_spider_droid_none)
