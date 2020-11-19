302cd0ae403658bfd1eae32c50b54dfc30995f2a
christian <thrax989@yahoo.com>
2020-11-18 16:19:19 -0800

    [Updated] Create Template Object Call

--- MMOCoreORB/bin/scripts/object/mobile/hologram/objects.lua
+++ MMOCoreORB/bin/scripts/object/mobile/hologram/objects.lua
@@ -1,174 +1,174 @@
-object_mobile_hologram_shared_tauntaun = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_tauntaun = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_tauntaun.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_tauntaun, "object/mobile/hologram/shared_tauntaun.iff")
 
-object_mobile_hologram_shared_atat = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_atat = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_atat.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_atat, "object/mobile/hologram/shared_atat.iff")
 
-object_mobile_hologram_shared_sullustan_female = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_sullustan_female = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_sullustan_female.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_sullustan_female, "object/mobile/hologram/shared_sullustan_female.iff")
 
-object_mobile_hologram_shared_ithorian_male = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_ithorian_male = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_ithorian_male.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_ithorian_male, "object/mobile/hologram/shared_ithorian_male.iff")
 
-object_mobile_hologram_shared_sullustan_male = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_sullustan_male = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_sullustan_male.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_sullustan_male, "object/mobile/hologram/shared_sullustan_male.iff")
 
-object_mobile_hologram_shared_twilek_female = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_twilek_female = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_twilek_female.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_twilek_female, "object/mobile/hologram/shared_twilek_female.iff")
 
-object_mobile_hologram_shared_wookiee_female = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_wookiee_female = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_wookiee_female.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_wookiee_female, "object/mobile/hologram/shared_wookiee_female.iff")
 
-object_mobile_hologram_shared_rancor = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_rancor = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_rancor.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_rancor, "object/mobile/hologram/shared_rancor.iff")
 
-object_mobile_hologram_shared_probe_droid = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_probe_droid = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_probe_droid.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_probe_droid, "object/mobile/hologram/shared_probe_droid.iff")
 
-object_mobile_hologram_shared_trandoshan_male = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_trandoshan_male = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_trandoshan_male.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_trandoshan_male, "object/mobile/hologram/shared_trandoshan_male.iff")
 
-object_mobile_hologram_shared_squall = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_squall = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_squall.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_squall, "object/mobile/hologram/shared_squall.iff")
 
-object_mobile_hologram_shared_ewok = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_ewok = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_ewok.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_ewok, "object/mobile/hologram/shared_ewok.iff")
 
-object_mobile_hologram_shared_atat_hoth = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_atat_hoth = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_atat_hoth.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_atat_hoth, "object/mobile/hologram/shared_atat_hoth.iff")
 
-object_mobile_hologram_shared_ins444 = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_ins444 = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_ins444.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_ins444, "object/mobile/hologram/shared_ins444.iff")
 
-object_mobile_hologram_shared_moncal_female = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_moncal_female = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_moncal_female.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_moncal_female, "object/mobile/hologram/shared_moncal_female.iff")
 
-object_mobile_hologram_shared_twilek_male = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_twilek_male = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_twilek_male.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_twilek_male, "object/mobile/hologram/shared_twilek_male.iff")
 
-object_mobile_hologram_shared_ithorian_female = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_ithorian_female = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_ithorian_female.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_ithorian_female, "object/mobile/hologram/shared_ithorian_female.iff")
 
-object_mobile_hologram_shared_bothan_female = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_bothan_female = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_bothan_female.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_bothan_female, "object/mobile/hologram/shared_bothan_female.iff")
 
-object_mobile_hologram_shared_bothan_male = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_bothan_male = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_bothan_male.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_bothan_male, "object/mobile/hologram/shared_bothan_male.iff")
 
-object_mobile_hologram_shared_peko_peko = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_peko_peko = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_peko_peko.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_peko_peko, "object/mobile/hologram/shared_peko_peko.iff")
 
-object_mobile_hologram_shared_moncal_male = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_moncal_male = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_moncal_male.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_moncal_male, "object/mobile/hologram/shared_moncal_male.iff")
 
-object_mobile_hologram_shared_zabrak_female = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_zabrak_female = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_zabrak_female.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_zabrak_female, "object/mobile/hologram/shared_zabrak_female.iff")
 
-object_mobile_hologram_shared_bearded_jax = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_bearded_jax = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_bearded_jax.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_bearded_jax, "object/mobile/hologram/shared_bearded_jax.iff")
 
-object_mobile_hologram_shared_corellian_slice_hound = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_corellian_slice_hound = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_corellian_slice_hound.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_corellian_slice_hound, "object/mobile/hologram/shared_corellian_slice_hound.iff")
 
-object_mobile_hologram_shared_human_male = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_human_male = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_human_male.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_human_male, "object/mobile/hologram/shared_human_male.iff")
 
-object_mobile_hologram_shared_rodian_male = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_rodian_male = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_rodian_male.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_rodian_male, "object/mobile/hologram/shared_rodian_male.iff")
 
-object_mobile_hologram_shared_trandoshan_female = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_trandoshan_female = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_trandoshan_female.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_trandoshan_female, "object/mobile/hologram/shared_trandoshan_female.iff")
 
-object_mobile_hologram_shared_human_female = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_human_female = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_human_female.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_human_female, "object/mobile/hologram/shared_human_female.iff")
 
-object_mobile_hologram_shared_veermok = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_veermok = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_veermok.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_veermok, "object/mobile/hologram/shared_veermok.iff")
 
-object_mobile_hologram_shared_rodian_female = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_rodian_female = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_rodian_female.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_rodian_female, "object/mobile/hologram/shared_rodian_female.iff")
 
-object_mobile_hologram_shared_quenker = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_quenker = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_quenker.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_quenker, "object/mobile/hologram/shared_quenker.iff")
 
-object_mobile_hologram_shared_cle004 = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_cle004 = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_cle004.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_cle004, "object/mobile/hologram/shared_cle004.iff")
 
-object_mobile_hologram_shared_wookiee_male = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_wookiee_male = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_wookiee_male.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_wookiee_male, "object/mobile/hologram/shared_wookiee_male.iff")
 
-object_mobile_hologram_shared_jawa = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_jawa = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_jawa.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_jawa, "object/mobile/hologram/shared_jawa.iff")
 
-object_mobile_hologram_shared_zabrak_male = SharedTangibleObjectTemplate:new {
+object_mobile_hologram_shared_zabrak_male = SharedCreatureObjectTemplate:new {
     clientTemplateFileName = "object/mobile/hologram/shared_zabrak_male.iff"
 }
 ObjectTemplates:addClientTemplate(object_mobile_hologram_shared_zabrak_male, "object/mobile/hologram/shared_zabrak_male.iff")
