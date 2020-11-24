9aafdd2353ea2e053f6311c1bb2b22e9c1581b71
christian <thrax989@yahoo.com>
2020-11-24 15:07:49 -0800

    [Added] 36 New Beast Master BE Pets

---
+++ MMOCoreORB/bin/scripts/mobile/be/be_1.lua
@@ -0,0 +1,36 @@
+be_1 = Creature:new {
+	customName = "BE",
+	socialGroup = "",
+	faction = "",
+	level = 5,
+	chanceHit = 0.2,
+	damageMin = 30,
+	damageMax = 40,
+	baseXp = 30,
+	baseHAM = 45,
+	baseHAMmax = 55,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,0,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = HERD,
+	optionsBitmask = AIENABLED,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/beast_master/bm_bane_back.iff"},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "",
+	attacks = {
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(be_1, "be_1")
---
+++ MMOCoreORB/bin/scripts/mobile/be/be_32.lua
@@ -0,0 +1,36 @@
+be_32 = Creature:new {
+	customName = "BE",
+	socialGroup = "",
+	faction = "",
+	level = 5,
+	chanceHit = 0.2,
+	damageMin = 30,
+	damageMax = 40,
+	baseXp = 30,
+	baseHAM = 45,
+	baseHAMmax = 55,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,0,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = HERD,
+	optionsBitmask = AIENABLED,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/beast_master/bm_pangrack.iff"},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "",
+	attacks = {
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(be_32, "be_32")
