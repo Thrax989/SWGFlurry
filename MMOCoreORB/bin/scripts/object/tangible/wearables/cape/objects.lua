1f3db959b11d5566d150587cc0e122eac5ee6826
christian <thrax989@yahoo.com>
2020-11-24 15:21:27 -0800

    [Updated] s01 Cape

--- MMOCoreORB/bin/scripts/object/tangible/wearables/cape/objects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/cape/objects.lua
@@ -1,12 +1,3 @@
-object_tangible_wearables_cape_shared_cape_rebel_01 = SharedTangibleObjectTemplate:new {
-	clientTemplateFileName = "object/tangible/wearables/cape/shared_cape_rebel_01.iff"
-}
-ObjectTemplates:addClientTemplate(object_tangible_wearables_cape_shared_cape_rebel_01, "object/tangible/wearables/cape/shared_cape_rebel_01.iff")
---*********************************************************************************
-object_tangible_wearables_cape_shared_cape_imperial_01 = SharedTangibleObjectTemplate:new {
-	clientTemplateFileName = "object/tangible/wearables/cape/shared_cape_imperial_01.iff"
-}
-ObjectTemplates:addClientTemplate(object_tangible_wearables_cape_shared_cape_imperial_01, "object/tangible/wearables/cape/shared_cape_imperial_01.iff")
 --*********************************************************************************
 object_tangible_wearables_cape_shared_cape_s01 = SharedTangibleObjectTemplate:new {
     clientTemplateFileName = "object/tangible/wearables/cape/shared_cape_s01.iff"
--- MMOCoreORB/bin/scripts/object/tangible/wearables/cape/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/cape/serverobjects.lua
@@ -1,3 +1,1 @@
-includeFile("tangible/wearables/cape/cape_imperial_01.lua")
-includeFile("tangible/wearables/cape/cape_rebel_01.lua")
 includeFile("tangible/wearables/cape/cape_s01.lua")
