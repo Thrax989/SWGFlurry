d96bb6d764460dca645776618419309befed199d
TOXIC <thrax989@yahoo.com>
2020-11-28 14:00:42 -0800

    [Added] FRS Selection Floor Port To GTS Terminals

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/terminal/terminal_gcw_publish_gift.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/terminal/terminal_gcw_publish_gift.lua
@@ -173,6 +173,10 @@ object_tangible_terminal_terminal_gcw_publish_gift = object_tangible_terminal_sh
 		   {
 			"Teleport To Boss Instance Teleport Room (5,000 Credits)", "teleportroom",
 		   },
+		 	"FRS Selection Room",
+		   {
+			"Teleport To FRS Selection Room (5,000 Credits)", "frs_floor",
+		   },
 		 	"PvP Zone Rori Restuss",
 		   {
 			"Teleport To PvP Zone Rori Restuss (5,000 Credits)", "restuss_pvp_travel",
