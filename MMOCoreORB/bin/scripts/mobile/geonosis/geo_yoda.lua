9efc5ed8da6bb831443e39aafc95712eafaa5f59
christian <thrax989@yahoo.com>
2020-11-28 12:45:05 -0800

    [Added] Yoda To Geonosis

---
+++ MMOCoreORB/bin/scripts/mobile/geonosis/geo_yoda.lua
@@ -0,0 +1,75 @@
+geo_yoda = Creature:new {
+	customName = "Yoda",
+	socialGroup = "",
+	faction = "",
+	level = 291,
+	chanceHit = 27.25,
+	damageMin = 1800,
+	damageMax = 3310,
+	baseXp = 27849,
+	baseHAM = 321000,
+	baseHAMmax = 392000,
+	armor = 3,
+	resists = {90,90,90,90,90,90,90,90,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = KILLER + STALKER,
+	optionsBitmask = AIENABLED,
+	diet = HERBIVORE,
+	scale = 1.0,
+	templates = {"object/mobile/yoda.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "jedi_comp_group", chance = 10000000},
+			},
+			lootChance = 500000
+		},	
+		{
+			groups = {
+				{group = "holocron_dark", chance = 2000000},
+				{group = "holocron_light", chance = 2000000},
+				{group = "power_crystals", chance = 1000000},
+				{group = "armor_attachments", chance = 2000000},
+				{group = "clothing_attachments", chance = 2000000},
+				{group = "dark_jedi_common", chance = 1000000}
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "tierone", chance = 1500000},
+				{group = "tiertwo", chance = 3500000},
+				{group = "tierthree", chance = 2500000},
+				{group = "tierdiamond", chance = 2500000},
+			},
+			lootChance = 5000000
+		},
+		{
+			groups = {
+				{group = "vehicledeedsrare", chance = 10000000},
+			},
+			lootChance = 1000000
+		},
+		{
+			groups = {
+				{group = "nge_all", chance = 10000000},
+			},
+			lootChance = 10000000
+		}
+	},
+	weapons = {""},
+	conversationTemplate = "",
+	attacks = merge(forcepowermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(geo_yoda, "geo_yoda")
+
