4ff91b53fca94d22f79744007a908015df73de36
christian <thrax989@yahoo.com>
2020-11-29 21:35:52 -0800

    Revert "[Added] Multi Passenger Vehicle Test"
    This reverts commit 5719e0676c60543bec833b2b46115b500968fa82.

--- MMOCoreORB/bin/scripts/object/mobile/vehicle/landspeeder_av21.lua
+++ MMOCoreORB/bin/scripts/object/mobile/vehicle/landspeeder_av21.lua
@@ -44,9 +44,7 @@
 object_mobile_vehicle_landspeeder_av21 = object_mobile_vehicle_shared_landspeeder_av21:new {
 	templateType = VEHICLE,
 	decayRate = 15, -- Damage tick per decay cycle
-	decayCycle = 600, -- Time in seconds per cycle
-	passengerCapacity = 1,
-	passengerSeatString = "av21",
+	decayCycle = 600 -- Time in seconds per cycle
 }
 
 ObjectTemplates:addTemplate(object_mobile_vehicle_landspeeder_av21, "object/mobile/vehicle/landspeeder_av21.iff")
