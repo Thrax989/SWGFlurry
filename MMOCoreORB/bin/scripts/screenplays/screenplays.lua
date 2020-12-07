dcbb8f96f8aa6117961869170e8836cfc3f92ffb
christian <thrax989@yahoo.com>
2020-12-06 19:09:08 -0800

    [Disabled] Unfinished Lab

--- MMOCoreORB/bin/scripts/screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/screenplays.lua
@@ -186,7 +186,7 @@ includeFile("dungeon/ig88/ig88_platform.lua")
 --custom general grievous platform
 includeFile("dungeon/general_grievous/generalgrievous_platform.lua")
 --custom geonosis lab dungeon
-includeFile("dungeon/geonosis/geonosis_lab.lua")
+--includeFile("dungeon/geonosis/geonosis_lab.lua")
 -- Village
 includeFile("village/village_community_crafting.lua")
 includeFile("village/village_gm_sui.lua")
