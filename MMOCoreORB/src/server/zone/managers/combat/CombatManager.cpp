5ccdcd283b9c5c74c3bbc1e2441938e2a96c8f74
TOXIC <thrax989@yahoo.com>
2020-12-07 11:49:12 -0800

    [Updated] PvP Reduction

--- MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
+++ MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -1660,7 +1660,7 @@ float CombatManager::calculateDamage(CreatureObject* attacker, WeaponObject* wea
 
 	// PvP Damage Reduction.
 	if (attacker->isPlayerCreature() && defender->isPlayerCreature() && !data.isForceAttack())
-		damage *= 0.50;
+		damage *= 0.20;
 
 	if (damage < 1) damage = 1;
 
