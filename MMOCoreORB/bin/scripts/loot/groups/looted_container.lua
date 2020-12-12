013035c92275c4560cc5e7dd7bddad84644bdd2d
TOXIC <thrax989@yahoo.com>
2020-12-12 10:50:30 -0800

    [Updated] Locked Container Rolls Removed Some Junk

--- MMOCoreORB/bin/scripts/loot/groups/looted_container.lua
+++ MMOCoreORB/bin/scripts/loot/groups/looted_container.lua
@@ -6,18 +6,10 @@ looted_container = {
 		-- Junk/Misc Items (25% chance)
 		-- Common
 		{itemTemplate = "locked_briefcase", weight = 900000},
-		{itemTemplate = "camera", weight = 100000},
-		{itemTemplate = "corrupt_datadisk", weight = 100000},
-		{itemTemplate = "corsec_id_badge", weight = 100000},
-		{itemTemplate = "damaged_datapad", weight = 100000},
 		{itemTemplate = "decorative_bowl", weight = 100000},
 		{itemTemplate = "decorative_shisa", weight = 100000},
 		{itemTemplate = "dermal_analyzer", weight = 100000},
-		{itemTemplate = "dud_firework_grey", weight = 100000},
-		{itemTemplate = "dud_firework_red", weight = 100000},
 		{itemTemplate = "empty_cage", weight = 100000},
-		{itemTemplate = "expensive_basket", weight = 100000},
-		{itemTemplate = "expired_ticket", weight = 100000},
 		{itemTemplate = "hyperdrive_part", weight = 100000},
 		{itemTemplate = "ledger", weight = 100000},
 		{itemTemplate = "locked_container", weight = 100000},
@@ -27,8 +19,6 @@ looted_container = {
 		{itemTemplate = "recorded_image_1", weight = 100000},
 		{itemTemplate = "broken_decryptor", weight = 100000},
 		{itemTemplate = "recording_rod", weight = 100000},
-		{itemTemplate = "slave_collar", weight = 100000},
-		{itemTemplate = "used_ticket", weight = 100000},
 		{itemTemplate = "worklight", weight = 100000},
 		{itemTemplate = "force_color_crystal", weight = 1000000},
 		{itemTemplate = "force_power_crystal", weight = 100000},
@@ -43,10 +33,10 @@ looted_container = {
 		{groupTemplate = "outbreak", weight = 1000000},
 		-- Clothing (25% chance)
 		{groupTemplate = "wearables_all", weight = 1000000},
-		-- Dot System (3.5% chance)
-		{groupTemplate = "dotsystem", weight = 350000},
-		-- Dot System (4.5% chance)
-		{groupTemplate = "dot_cleanse", weight = 450000},
+		-- Dot System (8.5% chance)
+		{groupTemplate = "dotsystem", weight = 850000},
+		-- Dot System (9.5% chance)
+		{groupTemplate = "dot_cleanse", weight = 950000},
 	}
 }
 
