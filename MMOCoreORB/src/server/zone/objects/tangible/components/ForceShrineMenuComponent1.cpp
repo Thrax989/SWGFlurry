9a4a71bb0c647c1464f0a0505d18d2c1bc120ebd
TOXIC <thrax989@yahoo.com>
2020-12-13 12:28:31 -0800

    [Updated] FRS Shrine Jedi State Checks

--- MMOCoreORB/src/server/zone/objects/tangible/components/ForceShrineMenuComponent1.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/ForceShrineMenuComponent1.cpp
@@ -27,14 +27,20 @@ void ForceShrineMenuComponent1::fillObjectMenuResponse(SceneObject* sceneObject,
 
 	if (ghost->getJediState() >=2) {
 			menuResponse->addRadialMenuItem(213, 3, "Jedi Visibility"); // Visibility
+		}
+	if (ghost->getJediState() == 8 || ghost->getJediState() == 4)) {
 			menuResponse->addRadialMenuItem(215, 3, "Force Ranking");
+		}
+	if (ghost->getJediState() == 8) {
 			menuResponse->addRadialMenuItemToRadialID(215, 216, 3, "Join Sith Order"); // Join Sith
+		}
+	if (ghost->getJediState() == 4) {
 			menuResponse->addRadialMenuItemToRadialID(215, 217, 3, "Join Jedi Order"); // Join Jedi
 		}
-		if (ghost->getJediState() == 8) {
+	if (ghost->getJediState() == 8) {
 			menuResponse->addRadialMenuItemToRadialID(215, 218, 3, "Leave Sith Order"); // Leave Sith
 		}
-		if (ghost->getJediState() == 4) {
+	if (ghost->getJediState() == 4) {
 			menuResponse->addRadialMenuItemToRadialID(215, 219, 3, "Leave Jedi Order"); // Leave Jedi
 		}
 	}
