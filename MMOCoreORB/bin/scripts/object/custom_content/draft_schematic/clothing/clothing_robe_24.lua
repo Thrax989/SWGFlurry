1ea41a03f6f466667c3f66d23cd9f65217900a6f
bigtap <24537455+bigtap@users.noreply.github.com>
2020-12-20 15:19:30 -0800

    BE option to Jedi robes Max 8 stats

--- MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/clothing/clothing_robe_24.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/draft_schematic/clothing/clothing_robe_24.lua
@@ -19,12 +19,12 @@ object_draft_schematic_clothing_clothing_robe_24 = object_draft_schematic_clothi
    customizationStringNames = {},
    customizationDefaults = {},
 
-   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n"},
-   ingredientTitleNames = {"primary_pocket", "reinforcement", "strap_pad", "power_cell_socket", "power_cell_brackets", "enhanced_cooling_mechanism"},
-   ingredientSlotType = {0, 1, 1, 1, 1, 1},
-   resourceTypes = {"fiberplast", "object/tangible/loot/misc/shared_artifact_rare_s01.iff", "object/tangible/component/clothing/shared_leather_heavy_duty.iff", "object/tangible/furniture/all/frn_all_holocron_cube.iff", "object/tangible/furniture/all/frn_all_holocron_triangle.iff", "object/tangible/furniture/all/frn_all_holocron_dode.iff"},
-   resourceQuantities = {2000, 1, 1, 1, 1, 1},
-   contribution = {100, 100, 100, 100, 100, 100},
+   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
+   ingredientTitleNames = {"primary_pocket", "reinforcement", "strap_pad", "power_cell_socket", "power_cell_brackets", "enhanced_cooling_mechanism", "binding_and_reinforcement", "binding_and_hardware"},
+   ingredientSlotType = {0, 1, 1, 1, 1, 1, 3, 3},
+   resourceTypes = {"fiberplast", "object/tangible/loot/misc/shared_artifact_rare_s01.iff", "object/tangible/component/clothing/shared_clothing_treatment_crystalline.iff", "object/tangible/furniture/all/frn_all_holocron_cube.iff", "object/tangible/furniture/all/frn_all_holocron_triangle.iff", "object/tangible/furniture/all/frn_all_holocron_dode.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
+   resourceQuantities = {2000, 1, 1, 1, 1, 1, 1, 1},
+   contribution = {100, 100, 100, 100, 100, 100, 100, 100},
 
 
    targetTemplate = "object/tangible/wearables/robe/robe_jedi_grey_s02.iff",
