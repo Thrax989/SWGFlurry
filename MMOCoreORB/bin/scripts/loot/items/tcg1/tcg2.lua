f928ab6f4fb785ad7ff1129b902be35287f93898
christian <thrax989@yahoo.com>
2020-12-27 16:08:40 -0800

    [Added] 7 New TCG Series 1 Consumable Loot Items

---
+++ MMOCoreORB/bin/scripts/loot/items/tcg1/tcg2.lua
@@ -0,0 +1,12 @@
+tcg2 = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Lepese Dictionary",
+	directObjectTemplate = "object/tangible/tcg/series1/consumable_lepese_dictionary.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("tcg2", tcg2)
---
+++ MMOCoreORB/bin/scripts/loot/items/tcg1/tcg5.lua
@@ -0,0 +1,12 @@
+tcg5 = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Nuna Ball Adertisment",
+	directObjectTemplate = "object/tangible/tcg/series1/consumable_nuna_ball_advertisement.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("tcg5", tcg5)
