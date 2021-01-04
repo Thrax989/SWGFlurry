c80326fdd75b3ab2fd453533aee677a2c267a4c4
christian <thrax989@yahoo.com>
2021-01-04 12:20:34 -0800

    [Added] 7 New TCG Lootable Items

---
+++ MMOCoreORB/bin/scripts/loot/items/tcg1/tcg13.lua
@@ -0,0 +1,12 @@
+tcg13 = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Dooku Bust ",
+	directObjectTemplate = "object/tangible/tcg/series1/decorative_dooku_bust.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("tcg13", tcg13)
---
+++ MMOCoreORB/bin/scripts/loot/items/tcg1/tcg18.lua
@@ -0,0 +1,12 @@
+tcg18 = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Indoor Garden",
+	directObjectTemplate = "object/tangible/tcg/series1/decorative_indoor_garden_01.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("tcg18", tcg18)
