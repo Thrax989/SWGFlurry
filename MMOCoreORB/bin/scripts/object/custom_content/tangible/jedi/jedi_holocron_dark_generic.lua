dfa3f5999178a16fc7df521744b3e1d9aa481ebc
TOXIC <thrax989@yahoo.com>
2021-01-18 11:55:49 -0800

    [Added] New Dark Jedi FRS XP Item

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/jedi/jedi_holocron_dark_generic.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/jedi/jedi_holocron_dark_generic.lua
@@ -1,5 +1,6 @@
 object_tangible_jedi_jedi_holocron_dark_generic = object_tangible_jedi_shared_jedi_holocron_dark_generic:new {
 
+		objectMenuComponent = "DarkJediMenuComponent",
 }
 
 ObjectTemplates:addTemplate(object_tangible_jedi_jedi_holocron_dark_generic, "object/tangible/jedi/jedi_holocron_dark_generic.iff")
