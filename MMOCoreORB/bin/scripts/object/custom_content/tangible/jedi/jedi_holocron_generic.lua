18d27048724c44315810ed87efc324de87f645c7
TOXIC <thrax989@yahoo.com>
2021-01-18 11:55:57 -0800

    [Added] New Light Jedi FRS XP Item

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/jedi/jedi_holocron_generic.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/jedi/jedi_holocron_generic.lua
@@ -1,5 +1,6 @@
 object_tangible_jedi_jedi_holocron_generic = object_tangible_jedi_shared_jedi_holocron_generic:new {
 
+		objectMenuComponent = "LightJediMenuComponent",
 }
 
 ObjectTemplates:addTemplate(object_tangible_jedi_jedi_holocron_generic, "object/tangible/jedi/jedi_holocron_generic.iff")
