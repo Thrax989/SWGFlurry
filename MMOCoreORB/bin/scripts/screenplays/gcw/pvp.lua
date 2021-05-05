f9ca6b301e8235a360b6260a34a99a63966b6a13
christian <thrax989@yahoo.com>
2021-05-05 11:25:13 -0700

    [Updated] PvP Zone Checks

--- MMOCoreORB/bin/scripts/screenplays/gcw/pvp.lua
+++ MMOCoreORB/bin/scripts/screenplays/gcw/pvp.lua
@@ -41,7 +41,7 @@ function pvp:notifySpawnArea(pActiveArea, pMovingObject)
 			return 0
 		end
 		
-		if (player:isImperial() or player:isNeutral() or player:isRebel()) then
+		if (player:isImperial() or player:isRebel()) then
 			player:sendSystemMessage("You have left the pvp zone.")
 			player:playEffect("clienteffect/sm_end_of_the_line.cef", "")
 			player:playEffect("clienteffect/player_clone_compile.cef", "")
@@ -67,7 +67,7 @@ function pvp:notifySpawnAreaLeave(pActiveArea, pMovingObject)
 			return 0
 		end
 
-		if (player:isImperial() or player:isNeutral() or player:isRebel()) then
+		if (player:isImperial() or player:isRebel()) then
 			player:sendSystemMessage("You entered the pvp zone!")
 			player:setFactionStatus(2)
 			player:playEffect("clienteffect/sm_end_of_the_line.cef", "")
