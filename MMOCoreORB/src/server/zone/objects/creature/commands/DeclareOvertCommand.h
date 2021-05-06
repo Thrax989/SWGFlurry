72c8f1fcecd85418111e94a9324018be6844154a
TOXIC <thrax989@yahoo.com>
2021-05-06 10:28:59 -0700

    [Updated] /Dec command

--- MMOCoreORB/src/server/zone/objects/creature/commands/DeclareOvertCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/DeclareOvertCommand.h
@@ -31,12 +31,7 @@ public:
 			return GENERALERROR;
 		}
 
-		if (creature->getFaction() != 370444368){
-			creature->sendSystemMessage("You may not use this command if you are not a Rebel Or Imperial, You must be apart of the GCW.");
-			return GENERALERROR;
-		}
-
-		if (creature->getFaction() != 3679112276){
+		if (creature->getFaction() == 0){
 			creature->sendSystemMessage("You may not use this command if you are not a Rebel Or Imperial, You must be apart of the GCW.");
 			return GENERALERROR;
 		}
