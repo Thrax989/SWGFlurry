dbf4fd1897a7dbd49e70cb14f8609531702c3eac
TOXIC <thrax989@yahoo.com>
2021-05-05 21:46:11 -0700

    [Updated] GCW Command

--- MMOCoreORB/src/server/zone/objects/creature/commands/SetPvpCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/SetPvpCommand.h
@@ -43,12 +43,12 @@ public:
 		}
 
 		if (creature->getFaction() != 370444368){
-			creature->sendSystemMessage("You may not use this command if you are not a Rebel, You must be apart of the GCW.");
+			creature->sendSystemMessage("You may not use this command if you are not a Rebel Or Imperial, You must be apart of the GCW.");
 			return GENERALERROR;
 		}
 
 		if (creature->getFaction() != 3679112276){
-			creature->sendSystemMessage("You may not use this command if you are not a Imperial, You must be apart of the GCW.");
+			creature->sendSystemMessage("You may not use this command if you are not a Rebel Or Imperial, You must be apart of the GCW.");
 			return GENERALERROR;
 		}
 		
