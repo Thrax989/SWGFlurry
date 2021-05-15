488ec87d6f7ff4573abce7d92fa5539bca0430d1
Sygnum <jtrobbinsfl@gmail.com>
2021-05-14 23:39:06 -0400

    [Fixed] Typo in loot table

--- MMOCoreORB/bin/scripts/loot/groups/boss_dungeon/boss_common.lua
+++ MMOCoreORB/bin/scripts/loot/groups/boss_dungeon/boss_common.lua
@@ -8,7 +8,7 @@ boss_common = {
 		{groupTemplate = "capes", weight = 1111112},  
 		{itemTemplate = "krayt_pearls", weight = 1111111},      
 		{itemTemplate = "power_crystals", weight = 1111111},      
-		{groupTemplate = "color_crystal", weight = 1111111}, 
+		{groupTemplate = "color_crystals", weight = 1111111}, 
 		{groupTemplate = "nonjedi_jewelry", weight = 1111111},      
 		{itemTemplate = "clothing_attachments", weight = 1111111},
 		{itemTemplate = "armor_attachments", weight = 1111111}   
