86e21ecb8994418d6bf1e4cdb6e5908884763d19
christian <thrax989@yahoo.com>
2021-05-20 16:39:24 -0700

    Revert "[Updated] Crystal Properties Test"
    This reverts commit 7b823632ed4140dde097c0b9111ce6814a57f0b5.

--- MMOCoreORB/bin/scripts/object/tangible/component/weapon/lightsaber/lightsaber_refined_crystal_pack.lua
+++ MMOCoreORB/bin/scripts/object/tangible/component/weapon/lightsaber/lightsaber_refined_crystal_pack.lua
@@ -45,7 +45,7 @@ object_tangible_component_weapon_lightsaber_lightsaber_refined_crystal_pack = ob
 
 
 	numberExperimentalProperties = {1, 1, 1},
-	experimentalProperties = {"XX", "XX", "XX"},
+	experimentalProperties = {"OQ", "OQ", "OQ"},
 	experimentalWeights = {1, 1, 1},
 	experimentalGroupTitles = {"expDamage", "expDamage", "expEffeciency"},
 	experimentalSubGroupTitles = {"mindamage", "maxdamage", "forcecost"},
