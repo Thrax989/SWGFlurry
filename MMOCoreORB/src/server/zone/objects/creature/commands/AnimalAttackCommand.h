a2f10c44b673c4445bdd47bb25669e991f68f459
christian <thrax989@yahoo.com>
2021-05-22 11:39:05 -0700

    Revert "[Updated] Animal Attack Command"
    This reverts commit c11e9dd600ded3ec87a0e7c3b721e66b35577648.

--- MMOCoreORB/src/server/zone/objects/creature/commands/AnimalAttackCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/AnimalAttackCommand.h
@@ -5,7 +5,6 @@
 #ifndef ANIMALATTACKCOMMAND_H_
 #define ANIMALATTACKCOMMAND_H_
 
-#include "server/zone/objects/scene/SceneObject.h"
 #include "server/zone/objects/creature/commands/JediQueueCommand.h"
 
 class AnimalAttackCommand : public JediQueueCommand {
@@ -23,66 +22,6 @@ public:
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
-
-		if (creature->isInvisible()) {
-			return GENERALERROR;
-		}
-
-		CreatureObject* player = cast<CreatureObject*>(creature);
-
-		ZoneServer* zserv = creature->getZoneServer();
-		PlayerObject* jedi = creature->getPlayerObject();
-
-		// Fail if target is not a player...
-		ManagedReference<SceneObject*> object = server->getZoneServer()->getObject(target);
-
-		if (object == nullptr || !object->isCreatureObject())
-			return INVALIDTARGET;
-
-		CreatureObject* creatureTarget = cast<CreatureObject*>( object.get());
-		PlayerManager* playerManager = server->getPlayerManager();
-
-		if (creature != creatureTarget && !CollisionManager::checkLineOfSight(creature, creatureTarget)) {
-			creature->sendSystemMessage("You do not have a clear line of sight to the target.");
-			return INVALIDTARGET;
-		}
-
-		if (!creature->checkCooldownRecovery("animal")) {
-			StringIdChatParameter stringId;
-
-			Time* cdTime = creature->getCooldownTime("animal");
-
-			// Returns -time. Multiple by -1 to return positive.
-			int timeLeft = floor((float)cdTime->miliDifference() / 1000) *-1;
-
-			stringId.setStringId("@innate:equil_wait"); // You are still recovering from your last equilization. Command available in %DI seconds.
-			stringId.setDI(timeLeft);
-			creature->sendSystemMessage(stringId);
-			return GENERALERROR;
-		}
-
-		if (creatureTarget == nullptr)
-			return INVALIDTARGET;
-
-		Locker clocker(creatureTarget, creature);
-
-		ManagedReference<PlayerObject*> targetGhost = creatureTarget->getPlayerObject();
-		ManagedReference<PlayerObject*> playerObject = creature->getPlayerObject();
-
-		if (targetGhost == nullptr || playerObject == nullptr)
-			return GENERALERROR;
-
-		if (creature->getDistanceTo(creatureTarget) > 42.f){
-			creature->sendSystemMessage("You are out of range.");
-			return GENERALERROR;}
-
-		if (object->isCreatureObject() && creatureTarget->isAttackableBy(creature)) {
-			creatureTarget->setDizziedState(3);
-			creatureTarget->playEffect("clienteffect/droid_effect_dry_ice.cef", "");
-			creatureTarget->sendSystemMessage("An animal sized force wave hits your legs");
-			creatureTarget->setPosture(CreaturePosture::KNOCKEDDOWN);
-			creature->addCooldown("animal", 60 * 1000);
-		}
 
 		return SUCCESS;
 	}
