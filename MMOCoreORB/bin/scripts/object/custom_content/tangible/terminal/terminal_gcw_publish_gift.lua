e4a19af7f5411348ecccdadce761385dfe1b7cfa
Sygnum <jtrobbinsfl@gmail.com>
2021-05-25 01:36:04 -0400

    [Modified]Added REBS of HOTH to GTS Player cities.

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/terminal/terminal_gcw_publish_gift.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/terminal/terminal_gcw_publish_gift.lua
@@ -242,7 +242,7 @@ object_tangible_terminal_terminal_gcw_publish_gift = object_tangible_terminal_sh
           },  
             "Lok",
           {
-            "Crimson Thorn (5,000 Credits)", "pc_crimson_travel",
+      "Crimson Thorn (5,000 Credits)", "pc_crimson_travel",
 			"Binary (5,000 Credits)", "pc_binary_travel",
 			"Serenity (5,000 Credits)", "pc_serenity_travel"
           },
@@ -260,8 +260,10 @@ object_tangible_terminal_terminal_gcw_publish_gift = object_tangible_terminal_sh
           },
             "Hoth",
           {
-            "Indestine (5,000 Credits)", "pc_indestine_travel"
+            "Indestine (5,000 Credits)", "pc_indestine_travel",
+            "REBS of HOTH (5,000 Credits)", "pc_rebs_travel"
           },
+
             "Mandalore",
           {  
 			"Nevarro (5,000 Credits)", "pc_freedom_travel",
