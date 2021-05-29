7c02ddb58cbfbb4a3ea66d0ba2355b1eb8e2ad06
TOXIC <thrax989@yahoo.com>
2021-05-29 14:14:52 -0400

    [Removed] Testing

--- MMOCoreORB/src/server/zone/objects/tangible/component/lightsaber/LightsaberCrystalComponentImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/component/lightsaber/LightsaberCrystalComponentImplementation.cpp
@@ -287,19 +287,6 @@ void LightsaberCrystalComponentImplementation::fillAttributeList(AttributeListMe
 			alm->insertAttribute("crystal_owner", ownerName);
 		}
 
-	if ((player->getJediState() > 1 || player->isPrivileged()) && getColor() == 31) {
-		if (ownerID != 0 || player->isPrivileged()) {
-			alm->insertAttribute("mindamage", minimumDamage);
-			alm->insertAttribute("maxdamage", maximumDamage);
-			alm->insertAttribute("wpn_attack_speed", attackSpeed);
-			alm->insertAttribute("wpn_wound_chance", woundChance);
-			alm->insertAttribute("wpn_attack_cost_health", sacHealth);
-			alm->insertAttribute("wpn_attack_cost_action", sacAction);
-			alm->insertAttribute("wpn_attack_cost_mind", sacMind);
-			alm->insertAttribute("forcecost", (int)getForceCost());
-			}
-		}
-
 		if (getColor() != 31) {
 			StringBuffer str3;
 			str3 << "@jedi_spam:saber_color_" << getColor();
@@ -447,7 +434,6 @@ void LightsaberCrystalComponentImplementation::updateCrystal(int value){
 void LightsaberCrystalComponentImplementation::updateCraftingValues(CraftingValues* values, bool firstUpdate) {
 	int colorMax = values->getMaxValue("color");
 	int color = values->getCurrentValue("color");
-	String preTuneName = getCustomObjectName().toString();
 
 	if (colorMax != 31) {
 		int finalColor = Math::min(color, 31);
@@ -461,28 +447,6 @@ void LightsaberCrystalComponentImplementation::updateCraftingValues(CraftingValu
 	generateCrystalStats();
 
 	ComponentImplementation::updateCraftingValues(values, firstUpdate);
-
-	if (color == 31) {
-		setQuality(values->getCurrentValue("quality"));
-		setAttackSpeed(Math::getPrecision(values->getCurrentValue("attackspeed"), 2));
-
-		if (preTuneName == "Sunrider's Destiny")
-		{
-			setMinimumDamage(Math::min(values->getCurrentValue("mindamage"), 50.f));
-			setMaximumDamage(Math::min(values->getCurrentValue("maxdamage"), 55.f));
-		}
-		else
-		if (preTuneName == "Banes's Heart")
-		{
-			setMinimumDamage(Math::min(values->getCurrentValue("mindamage"), 60.f));
-			setMaximumDamage(Math::min(values->getCurrentValue("maxdamage"), 65.f));
-		}
-		else
-		{
-			setMinimumDamage(Math::min(values->getCurrentValue("mindamage"), 40.f));
-			setMaximumDamage(Math::min(values->getCurrentValue("maxdamage"), 40.f));
-		}
-	}
 }
 
 int LightsaberCrystalComponentImplementation::inflictDamage(TangibleObject* attacker, int damageType, float damage, bool destroy, bool notifyClient) {
