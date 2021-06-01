815b80b408b20540ea4091d4663bb9198d5a047a
TOXIC <thrax989@yahoo.com>
2021-06-01 12:01:32 -0700

    [Added] Test 24

--- MMOCoreORB/src/templates/creature/SharedCreatureObjectTemplate.h
+++ MMOCoreORB/src/templates/creature/SharedCreatureObjectTemplate.h
@@ -19,7 +19,7 @@ protected:
 	IntegerParam niche;
 	IntegerParam species;
 	IntegerParam race;
-
+	IntegerParam passengerCapacity;
 	VectorParam<FloatParam> acceleration;
 	VectorParam<FloatParam> speed;
 	VectorParam<FloatParam> turnRate;
@@ -117,6 +117,10 @@ public:
 
 	inline int getRace() const {
 		return race;
+	}
+
+	inline int getPassengerCapacity() const {
+		return passengerCapacity;
 	}
 
 	inline float getSlopeModAngle() const {
