a13584393392212def845baa0bc1e1937ad2e4f1
bigtap <24537455+bigtap@users.noreply.github.com>
2021-06-05 12:54:23 -0700

    Revert "exar sabers schems"
    This reverts commit dabcc9e11e5de9802f01f33f03be2da5fcd34a38.

--- MMOCoreORB/bin/scripts/object/draft_schematic/weapon/lightsaber/lightsaber_onehanded_gen5_exar_kun.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/weapon/lightsaber/lightsaber_onehanded_gen5_exar_kun.lua
@@ -20,12 +20,12 @@ templateType = DRAFTSCHEMATIC,
    customizationStringNames = {},
    customizationDefaults = {},
 
-   ingredientTemplateNames = {"craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n"},
-   ingredientTitleNames = {"emitter_shroud", "primary_crystal", "activator", "handgrip", "focusing_crystals", "power_field_insulator", "energizers", "power_cell_socket", "power_cell_brackets", "enhanced_cooling_mechanism", "primary_crystal"},
-   ingredientSlotType = {0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1},
-   resourceTypes = {"steel_duralloy", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_blackwing_pack.iff", "aluminum_titanium", "petrochem_inert_polymer", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_blackwing_pack.iff", "gas_inert_culsion", "copper_polysteel", "object/tangible/furniture/all/frn_all_holocron_cube.iff", "object/tangible/furniture/all/frn_all_holocron_triangle.iff", "object/tangible/furniture/all/frn_all_holocron_dode.iff", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_crystal_pack.iff"},
-   resourceQuantities = {4000, 1, 2200, 2800, 1, 2800, 2800, 1, 1, 1, 1},
-   contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100},
+   ingredientTemplateNames = {"craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n"},
+   ingredientTitleNames = {"emitter_shroud", "primary_crystal", "activator", "handgrip", "focusing_crystals", "power_field_insulator", "energizers", "power_cell_socket", "power_cell_brackets", "enhanced_cooling_mechanism"},
+   ingredientSlotType = {0, 1, 0, 0, 1, 0, 0, 1, 1, 1},
+   resourceTypes = {"steel_duralloy", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_blackwing_pack.iff", "aluminum_titanium", "petrochem_inert_polymer", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_blackwing_pack.iff", "gas_inert_culsion", "copper_polysteel", "object/tangible/furniture/all/frn_all_holocron_cube.iff", "object/tangible/furniture/all/frn_all_holocron_triangle.iff", "object/tangible/furniture/all/frn_all_holocron_dode.iff"},
+   resourceQuantities = {4000, 1, 2200, 2800, 1, 2800, 2800, 1, 1, 1},
+   contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100, 100},
 
 
    targetTemplate = "object/weapon/melee/sword/crafted_saber/sword_lightsaber_onehanded_gen5_exar_kun.iff",
