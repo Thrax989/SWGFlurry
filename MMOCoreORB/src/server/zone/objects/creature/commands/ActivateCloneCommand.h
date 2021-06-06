70cb7e6101ac351a5e025e445457486d487693a0
TOXIC <thrax989@yahoo.com>
2021-06-05 17:34:15 -0700

    [Added] Clone Test

--- MMOCoreORB/src/server/zone/objects/creature/commands/ActivateCloneCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ActivateCloneCommand.h
@@ -7,6 +7,11 @@
 
 #include "server/zone/ZoneServer.h"
 #include "server/zone/managers/player/PlayerManager.h"
+#include "server/zone/objects/player/sui/callbacks/FieldFactionChangeSuiCallback.h"
+#include "server/zone/objects/player/sui/messagebox/SuiMessageBox.h"
+#include "server/zone/objects/player/sui/inputbox/SuiInputBox.h"
+#include "server/zone/objects/player/sui/listbox/SuiListBox.h"
+#include "server/zone/objects/player/sui/SuiWindowType.h"
 
 class ActivateCloneCommand : public QueueCommand {
 public:
@@ -32,7 +37,19 @@ public:
 		if (!player->isDead())
 			return GENERALERROR;
 
-		PlayerManager* playerManager = server->getZoneServer()->getPlayerManager();
+		if (player->isDead() && !isCloning()) {
+
+			ManagedReference<SuiBox*> cloneBox = getSuiBoxFromWindowType(SuiWindowType::CLONE_REQUEST);
+			PlayerManager* playerManager = server->getZoneServer()->getPlayerManager();
+
+			if (cloneBox != nullptr) {
+				cloneBox->clearOptions();
+				sendMessage(cloneBox->generateMessage());
+			} else {
+				playerManager->getPlayerManager()->sendActivateCloneRequest(player);
+			}
+		}
+
 		playerManager->sendActivateCloneRequest(player);
 
 		return SUCCESS;
