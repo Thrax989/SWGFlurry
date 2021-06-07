e0d34c8504be81417d37aa190f9575f3814ed430
bigtap <24537455+bigtap@users.noreply.github.com>
2021-06-06 20:33:53 -0700

    Update clothing_armor_mandalorian_helmet.lua
    Helmet to match leggings resource quantities - Drelnor

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_mandalorian_helmet.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_mandalorian_helmet.lua
@@ -22,7 +22,7 @@ object_draft_schematic_clothing_clothing_armor_mandalorian_helmet = object_draft
    ingredientTitleNames = {"auxilary_coverage", "body", "liner", "hardware_and_attachments", "binding_and_reinforcement", "padding", "armor", "load_bearing_harness", "reinforcement"},
    ingredientSlotType = {0, 0, 0, 0, 0, 0, 1, 1, 1},
    resourceTypes = {"armophous_vendusii", "fuel_petrochem_solid_known", "fiberplast_talus", "aluminum_chromium", "copper_platinite", "hide_wooly_rori", "object/tangible/component/armor/shared_armor_segment_ris.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/loot/dungeon/death_watch_bunker/shared_emulsion_protection.iff"},
-   resourceQuantities = {175, 100, 85, 70, 60, 40, 1, 1, 1},
+   resourceQuantities = {175, 100, 85, 70, 40, 40, 1, 1, 1},
    contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100},
 
 
