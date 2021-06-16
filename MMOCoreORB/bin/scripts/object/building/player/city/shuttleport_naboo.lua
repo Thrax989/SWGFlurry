31e33bbcdaf6acc4e906446aa10299e4446f5f4c
christian <thrax989@yahoo.com>
2021-06-16 13:51:20 -0700

    [Added] Flatten Layer Test To Corellia, Naboo, Tatooine Shuttle Ports

--- MMOCoreORB/bin/scripts/object/building/player/city/shuttleport_naboo.lua
+++ MMOCoreORB/bin/scripts/object/building/player/city/shuttleport_naboo.lua
@@ -53,6 +53,7 @@ object_building_player_city_shuttleport_naboo = object_building_player_city_shar
 	cityMaintenanceBase = 25000,
 	gameObjectType = 4103,
 	abilityRequired = "place_shuttleport",
+	flattenLayer = true,
 	childObjects = {
 		{templateFile = "object/tangible/terminal/terminal_player_structure.iff", x = -8.5, z = 1.5, y = 14, ox = 0, oy = 0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1},
 		{templateFile = "object/tangible/terminal/terminal_travel.iff", x = -7, z = 0.9, y = 13.5, ox = 0, oy = 0, oz = 0, ow = 1, cellid = -1, containmentType = -1},
