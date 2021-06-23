cc41a90ce9bf91127bb898aab4ea7c4245f03869
Drelnor <83726742+Drelnor@users.noreply.github.com>
2021-06-23 09:46:39 -0600

    Player City GTS Update
    Added new Player City, Sanitarium located on Naboo to the GTS

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/terminal/terminal_gcw_publish_gift.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/terminal/terminal_gcw_publish_gift.lua
@@ -200,7 +200,8 @@ object_tangible_terminal_terminal_gcw_publish_gift = object_tangible_terminal_sh
            	--"Intas Minor (5,000 Credits)", "pc_intas_travel", 
             "Lafayette (5,000 Credits)", "pc_lafayette_travel",
             "Avalon Prime (5,000 Credits)", "pc_avalon_travel",
-	    "Flurrys Haven (5,000 Credits)", "pc_flurrys haven_travel"		
+	    	"Flurrys Haven (5,000 Credits)", "pc_flurrys haven_travel"
+			"Sanitarium (5,000 Credits)", "pc_sanitarium_travel"		
           },
             "Dantooine",
           {
