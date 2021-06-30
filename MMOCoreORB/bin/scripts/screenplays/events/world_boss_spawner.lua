8825439a58d8ac8ccb6d6210ba8b1e6b9977a6c8
TOXIC <thrax989@yahoo.com>
2021-06-29 22:34:55 -0400

    [Updated] BGH

--- MMOCoreORB/bin/scripts/screenplays/events/world_boss_spawner.lua
+++ MMOCoreORB/bin/scripts/screenplays/events/world_boss_spawner.lua
@@ -119,7 +119,7 @@ function WorldBossSpawner:respawnBoss(pOldBoss)
 
 			createEvent(30, "WorldBossSpawner", "setupBoss", pBoss, "")
 
-			self:spawnBigGameHunter(pBoss, zone)
+			--self:spawnBigGameHunter(pBoss, zone)
 
 			--print("World Boss: " .. bossObject.name .. " spawned at " .. spawnPoint[1] .. ", " .. spawnPoint[3] .. ", " .. zone)
 
