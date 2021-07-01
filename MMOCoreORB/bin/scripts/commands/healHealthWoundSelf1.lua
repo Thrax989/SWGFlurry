72a0f66bdac44ecb17c8c54ac7eae2c223ea2db8
TOXIC <thrax989@yahoo.com>
2021-07-01 15:02:12 -0700

    [Updated]

--- MMOCoreORB/bin/scripts/commands/healHealthWoundSelf1.lua
+++ MMOCoreORB/bin/scripts/commands/healHealthWoundSelf1.lua
@@ -46,7 +46,7 @@ HealHealthWoundSelf1Command = {
 	healWoundAmount = 250,
 	forceCost = 20,
 	forceCostMultiplier = 0.05,
-	defaultTime = 1,
+	defaultTime = 0,
 	woundAttributesToHeal = HEALTH_ATTRIBUTE,
 }
 
