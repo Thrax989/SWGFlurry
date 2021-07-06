51c48ba623d0f3fb39b0508c2146704e48949032
Drelnor <83726742+Drelnor@users.noreply.github.com>
2021-07-06 14:41:42 -0600

    Update lok_destroy_missions.lua
    Added dune kimogilla and giant dune kimogilla missions.

--- MMOCoreORB/bin/scripts/mobile/spawn/destroy_mission/lok_destroy_missions.lua
+++ MMOCoreORB/bin/scripts/mobile/spawn/destroy_mission/lok_destroy_missions.lua
@@ -152,6 +152,18 @@ lok_destroy_missions = {
 			maxDifficulty = 50,
 			size = 25,
 		},
+		{
+			lairTemplateName = "lok_kimogilla_dune_lair_neutral_medium",
+			minDifficulty = 55,
+			maxDifficulty = 100,
+			size = 25,
+		},
+		{
+			lairTemplateName = "lok_kimogilla_giant_dune_lair_neutral_medium",
+			minDifficulty = 60,
+			maxDifficulty = 200,
+			size = 25,
+		},
 	}
 }
 
