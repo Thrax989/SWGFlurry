1a6a642a3307997f6828d4786fe3aee9e34bea97
christian <thrax989@yahoo.com>
2021-07-08 08:15:43 -0700

    Revert "[Added] Updated Holo Grind Jedi Unlock System"
    This reverts commit 5ecf12413575547adbdd5cbb6645451c0ceb52fc.

--- MMOCoreORB/bin/scripts/managers/jedi/hologrind_jedi_manager.lua
+++ MMOCoreORB/bin/scripts/managers/jedi/hologrind_jedi_manager.lua
@@ -3,8 +3,8 @@ local ObjectManager = require("managers.object.object_manager")
 
 jediManagerName = "HologrindJediManager"
 
-NUMBEROFPROFESSIONSTOMASTER = 4
-MAXIMUMNUMBEROFPROFESSIONSTOSHOWWITHHOLOCRON = NUMBEROFPROFESSIONSTOMASTER - 1
+NUMBEROFPROFESSIONSTOMASTER = 6
+MAXIMUMNUMBEROFPROFESSIONSTOSHOWWITHHOLOCRON = NUMBEROFPROFESSIONSTOMASTER - 2
 
 HologrindJediManager = JediManager:new {
 	screenplayName = jediManagerName,
@@ -18,37 +18,49 @@ HologrindJediManager = JediManager:new {
 function HologrindJediManager:getGrindableProfessionList()
 	local grindableProfessions = {
 		-- String Id, badge number, profession name
-		{ "outdoors_creaturehandler_master", 	OUTDOORS_CREATUREHANDLER_MASTER  },
-		{ "outdoors_squadleader_master", 	OUTDOORS_SQUADLEADER_MASTER  },
-		{ "outdoors_bio_engineer_master", 	OUTDOORS_BIO_ENGINEER_MASTER  },
+		--{ "pilot_rebel_navy_corellia", 	PILOT_REBEL_NAVY_CORELLIA },
+		--{ "pilot_imperial_navy_corellia", 	PILOT_IMPERIAL_NAVY_CORELLIA },
+		--{ "pilot_neutral_corellia", 		PILOT_CORELLIA },
+		--{ "pilot_rebel_navy_tatooine", 	PILOT_REBEL_NAVY_TATOOINE },
+		--{ "pilot_imperial_navy_naboo", 	PILOT_IMPERIAL_NAVY_NABOO },
 		{ "crafting_architect_master", 		CRAFTING_ARCHITECT_MASTER  },
 		{ "crafting_armorsmith_master", 	CRAFTING_ARMORSMITH_MASTER  },
-		{ "crafting_tailor_master", 		CRAFTING_TAILOR_MASTER  },
-		{ "crafting_weaponsmith_master", 	CRAFTING_WEAPONSMITH_MASTER  },
-		{ "crafting_droidengineer_master", 	CRAFTING_DROIDENGINEER_MASTER  },
-		{ "crafting_chef_master", 		CRAFTING_CHEF_MASTER  },
-		{ "combat_1hsword_master", 		COMBAT_1HSWORD_MASTER  },
-		{ "combat_marksman_master", 		COMBAT_MARKSMAN_MASTER  },
+		{ "crafting_artisan_master", 		CRAFTING_ARTISAN_MASTER  },
+		{ "outdoors_bio_engineer_master", 	OUTDOORS_BIOENGINEER_MASTER  },
 		{ "combat_bountyhunter_master", 	COMBAT_BOUNTYHUNTER_MASTER  },
 		{ "combat_brawler_master", 		COMBAT_BRAWLER_MASTER  },
-		{ "combat_smuggler_master", 		COMBAT_SMUGGLER_MASTER  },
 		{ "combat_carbine_master", 		COMBAT_CARBINE_MASTER  },
+		{ "crafting_chef_master", 		CRAFTING_CHEF_MASTER  },
+		{ "science_combatmedic_master", 	SCIENCE_COMBATMEDIC_MASTER  },
 		{ "combat_commando_master", 		COMBAT_COMMANDO_MASTER  },
+		{ "outdoors_creaturehandler_master", 	OUTDOORS_CREATUREHANDLER_MASTER  },
+		{ "social_dancer_master", 		SOCIAL_DANCER_MASTER  },
+		{ "science_doctor_master", 		SCIENCE_DOCTOR_MASTER  },
+		{ "crafting_droidengineer_master", 	CRAFTING_DROIDENGINEER_MASTER  },
+		{ "social_entertainer_master", 		SOCIAL_ENTERTAINER_MASTER  },
+		{ "combat_1hsword_master", 		COMBAT_1HSWORD_MASTER  },
+		{ "social_imagedesigner_master", 	SOCIAL_IMAGEDESIGNER_MASTER  },
+		{ "combat_marksman_master", 		COMBAT_MARKSMAN_MASTER  },
+		{ "science_medic_master", 		SCIENCE_MEDIC_MASTER  },
+		{ "crafting_merchant_master", 		CRAFTING_MERCHANT_MASTER  },
+		{ "social_musician_master", 		SOCIAL_MUSICIAN_MASTER  },
 		{ "combat_polearm_master", 		COMBAT_POLEARM_MASTER  },
 		{ "combat_pistol_master", 		COMBAT_PISTOL_MASTER  },
-		{ "combat_rifleman_master", 		COMBAT_RIFLEMAN_MASTER  },
-		{ "combat_2hsword_master", 		COMBAT_2HSWORD_MASTER  },
-		{ "combat_unarmed_master", 		COMBAT_UNARMED_MASTER  },
+		--{ "social_politician_master", 	SOCIAL_POLITICIAN_MASTER  },
 		{ "outdoors_ranger_master", 		OUTDOORS_RANGER_MASTER  },
+		{ "combat_rifleman_master", 		COMBAT_RIFLEMAN_MASTER  },
 		{ "outdoors_scout_master", 		OUTDOORS_SCOUT_MASTER  },
+		--{ "crafting_shipwright", 		CRAFTING_SHIPWRIGHT },
+		{ "combat_smuggler_master", 		COMBAT_SMUGGLER_MASTER  },
 		{ "outdoors_squadleader_master", 	OUTDOORS_SQUADLEADER_MASTER  },
-		{ "science_medic_master", 		SCIENCE_MEDIC_MASTER  },
-		{ "science_combatmedic_master", 	SCIENCE_COMBATMEDIC_MASTER  },
-		{ "science_doctor_master", 		SCIENCE_DOCTOR_MASTER  },
-		{ "social_musician_master", 		SOCIAL_MUSICIAN_MASTER  },
-		{ "social_imagedesigner_master", 	SOCIAL_IMAGEDESIGNER_MASTER  },
-		{ "social_entertainer_master", 		SOCIAL_ENTERTAINER_MASTER  },
-		{ "social_dancer_master", 		SOCIAL_DANCER_MASTER  },
+		{ "combat_2hsword_master", 		COMBAT_2HSWORD_MASTER  },
+		{ "crafting_tailor_master", 		CRAFTING_TAILOR_MASTER  },
+		{ "crafting_weaponsmith_master", 	CRAFTING_WEAPONSMITH_MASTER  },
+		--{ "pilot_neutral_naboo", 		PILOT_NEUTRAL_NABOO },
+		--{ "pilot_neutral_tatooine", 		PILOT_TATOOINE },
+		--{ "pilot_imperial_navy_tatooine", 	PILOT_IMPERIAL_NAVY_TATOOINE },
+		{ "combat_unarmed_master", 		COMBAT_UNARMED_MASTER  },
+	--{ "pilot_rebel_navy_naboo", 		PILOT_REBEL_NAVY_NABOO }
 	}
 	return grindableProfessions
 end
@@ -59,58 +71,51 @@ end
 function HologrindJediManager:onPlayerCreated(pCreatureObject)
 	local skillList = self:getGrindableProfessionList()
 
-	return ObjectManager.withCreaturePlayerObject(pCreatureObject, function(playerObject)
-		local pGhost = CreatureObject(pCreatureObject):getPlayerObject()
+	local pGhost = CreatureObject(pCreatureObject):getPlayerObject()
 
-		if (pGhost == nil) then
-			return
-		end
+	if (pGhost == nil) then
+		return
+	end
 
-		for i = 1, NUMBEROFPROFESSIONSTOMASTER, 1 do
-			local numberOfSkillsInList = #skillList
-			local skillNumber = getRandomNumber(1, numberOfSkillsInList)
-			PlayerObject(pGhost):addHologrindProfession(skillList[skillNumber][2])
-			table.remove(skillList, skillNumber)
-		end
-	end)
+	for i = 1, NUMBEROFPROFESSIONSTOMASTER, 1 do
+		local numberOfSkillsInList = #skillList
+		local skillNumber = getRandomNumber(1, numberOfSkillsInList)
+		PlayerObject(pGhost):addHologrindProfession(skillList[skillNumber][2])
+		table.remove(skillList, skillNumber)
+	end
 end
 
 -- Check and count the number of mastered hologrind professions.
 -- @param pCreatureObject pointer to the creature object of the player which should get its number of mastered professions counted.
 -- @return the number of mastered hologrind professions.
 function HologrindJediManager:getNumberOfMasteredProfessions(pCreatureObject)
+	local pGhost = CreatureObject(pCreatureObject):getPlayerObject()
 
-	return ObjectManager.withCreaturePlayerObject(pCreatureObject, function(playerObject)
-		local pGhost = CreatureObject(pCreatureObject):getPlayerObject()
+	if (pGhost == nil) then
+		return 0
+	end
 
-		if (pGhost == nil) then
-			return 0
+	local professions = PlayerObject(pGhost):getHologrindProfessions()
+	local masteredNumberOfProfessions = 0
+	for i = 1, #professions, 1 do
+		if PlayerObject(pGhost):hasBadge(professions[i]) then
+			masteredNumberOfProfessions = masteredNumberOfProfessions + 1
 		end
-
-		local professions = playerObject:getHologrindProfessions()
-		local masteredNumberOfProfessions = 0
-		for i = 1, #professions, 1 do
-			if PlayerObject(pGhost):hasBadge(professions[i]) then
-				masteredNumberOfProfessions = masteredNumberOfProfessions + 1
-			end
-		end
-		return masteredNumberOfProfessions
-	end)
+	end
+	return masteredNumberOfProfessions
 end
 
 -- Check if the player is jedi.
 -- @param pCreatureObject pointer to the creature object of the player to check if he is jedi.
 -- @return returns if the player is jedi or not.
 function HologrindJediManager:isJedi(pCreatureObject)
-	return ObjectManager.withCreaturePlayerObject(pCreatureObject, function(playerObject)
-		local pGhost = CreatureObject(pCreatureObject):getPlayerObject()
+	local pGhost = CreatureObject(pCreatureObject):getPlayerObject()
 
-		if (pGhost == nil) then
-			return false
-		end
+	if (pGhost == nil) then
+		return false
+	end
 
-		return PlayerObject(pGhost):isJedi()
-	end)
+	return PlayerObject(pGhost):isJedi()
 end
 
 -- Sui window ok pressed callback function.
@@ -128,21 +133,14 @@ end
 -- Award skill and jedi status to the player.
 -- @param pCreatureObject pointer to the creature object of the player who unlocked jedi.
 function HologrindJediManager:awardJediStatusAndSkill(pCreatureObject)
-	ObjectManager.withCreaturePlayerObject(pCreatureObject, function(playerObject)
-		local pGhost = CreatureObject(pCreatureObject):getPlayerObject()
-		local firstName = CreatureObject(pCreatureObject):getFirstName()
-		local player = LuaCreatureObject(pCreatureObject)
+	local pGhost = CreatureObject(pCreatureObject):getPlayerObject()
 
+	if (pGhost == nil) then
+		return
+	end
 
-		if (pGhost == nil) then
-			return
-		end
-
-		awardSkill(pPlayer, "force_title_jedi_novice")
-		--PlayerObject(pGhost):setJediState(2)
-		--player:setScreenPlayState(3, "jediLives")
-		print(firstName, "has become a jedi")
-	end)
+	awardSkill(pCreatureObject, "force_title_jedi_novice")
+	PlayerObject(pGhost):setJediState(1)
 end
 
 -- Check if the player has mastered all hologrind professions and send sui window and award skills.
@@ -207,23 +205,19 @@ function HologrindJediManager:sendHolocronMessage(pCreatureObject)
 		CreatureObject(pCreatureObject):sendSystemMessage("@jedi_spam:holocron_quiet")
 		return true
 	else
-		ObjectManager.withCreatureAndPlayerObject(pCreatureObject, function(creatureObject, playerObject)
+		local pGhost = CreatureObject(pCreatureObject):getPlayerObject()
 
-			local pGhost = CreatureObject(pCreatureObject):getPlayerObject()
+		if (pGhost == nil) then
+			return false
+		end
 
-			if (pGhost == nil) then
-				return false
+		local professions = PlayerObject(pGhost):getHologrindProfessions()
+		for i = 1, #professions, 1 do
+			if not PlayerObject(pGhost):hasBadge(professions[i]) then
+				local professionText = self:getProfessionStringIdFromBadgeNumber(professions[i])
+				CreatureObject(pCreatureObject):sendSystemMessageWithTO("@jedi_spam:holocron_light_information", "@skl_n:" .. professionText)
 			end
-
-			local professions = PlayerObject(pGhost):getHologrindProfessions()
-			for i = 1, #professions, 1 do
-				if not PlayerObject(pGhost):hasBadge(professions[i]) then
-					local professionText = self:getProfessionStringIdFromBadgeNumber(professions[i])
-					CreatureObject(pCreatureObject):sendSystemMessageWithTO("@jedi_spam:holocron_light_information", "@skl_n:" .. professionText)
-				break
-				end
-			end
-		end)
+		end
 
 		return false
 	end
