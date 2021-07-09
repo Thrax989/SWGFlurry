5176b7674db9f9c8a91f33bd85269da477a88e02
TOXIC <thrax989@yahoo.com>
2021-07-09 09:55:31 -0700

    [Added] Test Shuttle Transition

--- MMOCoreORB/src/server/zone/objects/creature/commands/BoardShuttleCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/BoardShuttleCommand.h
@@ -196,9 +196,10 @@ public:
 
 		if (arrivalZone->getZoneName() == "dungeon2") {
 			creature->switchZone(arrivalZone->getZoneName(), 84.1568, 0.899999, -46.0048, 14200813);
-		}else{
+		} else if (arrivalPlanet == departurePlanet) {
+			creature->teleport(x, p.getPositionZ(), y, 0);
+		} else {
 			creature->switchZone(arrivalZone->getZoneName(), x, p.getPositionZ(), y, 0);
-		}
 
 		// Update the nearest mission for group waypoint for both the arrival and departure planet.
 		if (creature->isGrouped()) {
