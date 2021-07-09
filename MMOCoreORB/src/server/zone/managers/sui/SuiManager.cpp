4f4f94935a4370fa2b86810aeeb4176dced6ac5f
TOXIC <thrax989@yahoo.com>
2021-07-09 15:52:37 -0400

    [Added] 100,000,000 Credit Base Jedi Unlock Credit Sync To GTS Terms

--- MMOCoreORB/src/server/zone/managers/sui/SuiManager.cpp
+++ MMOCoreORB/src/server/zone/managers/sui/SuiManager.cpp
@@ -3533,19 +3533,19 @@ void SuiManager::handleCharacterBuilderSelectItem(CreatureObject* player, SuiBox
 				bluefrog->grantGlowyBadges(player);
 
 			} else if (templatePath == "unlock_jedi_initiate") {
-				if (!player->isInCombat() && player->getBankCredits() < 49999999) {
+				if (!player->isInCombat() && player->getBankCredits() < 99999999) {
 		                ManagedReference<SuiMessageBox*> box = new SuiMessageBox(player, SuiWindowType::CITY_ADMIN_CONFIRM_UPDATE_TYPE);
 		                box->setPromptTitle("Unlocking Jedi");
-		                box->setPromptText("Unlocking Jedi Cost 50,000,000 Credits. (Bank)");
+		                box->setPromptText("Unlocking Jedi Cost 100,000,000 Credits. (Bank)");
 		                box->setOkButton(true, "@cancel");
 		                box->setUsingObject(player);
 		                player->getPlayerObject()->addSuiBox(box);
 		                player->sendMessage(box->generateMessage());
 			        }
-				if (!player->isInCombat() && player->getBankCredits() > 49999999) {
+				if (!player->isInCombat() && player->getBankCredits() > 99999999) {
 		                ManagedReference<SuiMessageBox*> box = new SuiMessageBox(player, SuiWindowType::CITY_ADMIN_CONFIRM_UPDATE_TYPE);
 						player->sendSystemMessage("Thank you for purchasing a Jedi Unlock.");
-						bluefrog->grantGlowyBadges(player);					
+						bluefrog->grantJediInitiate(player);
 						player->subtractBankCredits(5000);
 						box->setForceCloseDistance(5.f);
 			        }
