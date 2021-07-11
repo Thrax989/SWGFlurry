ac87f1aa15aec2f2deea05554fd6771f5b56c061
TOXIC <thrax989@yahoo.com>
2021-07-11 12:08:19 -0700

    [Updated] Unarmed Lunge 2 Strip Function

--- MMOCoreORB/src/server/zone/objects/creature/commands/UnarmedLunge2Command.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/UnarmedLunge2Command.h
@@ -6,6 +6,7 @@
 #define UNARMEDLUNGE2COMMAND_H_
 
 #include "CombatQueueCommand.h"
+#include "server/zone/objects/scene/SceneObject.h"
 
 class UnarmedLunge2Command : public CombatQueueCommand {
 public:
@@ -22,6 +23,44 @@ public:
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
+
+		Reference<SceneObject*> object = server->getZoneServer()->getObject(target);
+		ManagedReference<CreatureObject*> creatureTarget = cast<CreatureObject*>( object.get());
+
+		if (creatureTarget == nullptr)
+			return GENERALERROR;
+
+		if (creature->getDistanceTo(object) > 20.f){
+			creature->sendSystemMessage("You are out of range.");
+			return GENERALERROR;
+		}		
+
+			PlayerManager* playerManager = server->getPlayerManager();
+
+		if (creature != creatureTarget && !CollisionManager::checkLineOfSight(creature, creatureTarget)) {
+			return GENERALERROR;
+		}
+
+		if (creature->isAttackableBy(creatureTarget) && creature->isInRange(creatureTarget, 20)) {
+
+		}else{
+				return INVALIDTARGET;
+		}
+
+		const bool hasFr1 = creatureTarget->hasBuff(BuffCRC::JEDI_FORCE_RUN_1);
+		const bool hasFr2 = creatureTarget->hasBuff(BuffCRC::JEDI_FORCE_RUN_2);
+		const bool hasFr3 = creatureTarget->hasBuff(BuffCRC::JEDI_FORCE_RUN_3);
+
+		if(hasFr1 || hasFr2 || hasFr3) {
+			creature->sendSystemMessage(creatureTarget->getFirstName() + "'s Force Run has been disrupted by your attack!");
+			creatureTarget->sendSystemMessage("Your Force Run has been disrupted by" + creature->getFirstName() + ".");
+
+			Locker lock(creatureTarget);
+			if (hasFr1) { creatureTarget->removeBuff(BuffCRC::JEDI_FORCE_RUN_1); }
+			if (hasFr2) { creatureTarget->removeBuff(BuffCRC::JEDI_FORCE_RUN_2); }
+			if (hasFr3) { creatureTarget->removeBuff(BuffCRC::JEDI_FORCE_RUN_3); }
+
+		}
 		return doCombatAction(creature, target);
 	}
 
