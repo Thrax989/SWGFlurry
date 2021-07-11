0cd17cfe59535729d149fa3414c778bc6e76ed8c
TOXIC <thrax989@yahoo.com>
2021-07-11 14:44:33 -0700

    [Fixed] Game Object Type

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/collection/deathtrooper_alpha_camera_01.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/collection/deathtrooper_alpha_camera_01.lua
@@ -1,5 +1,4 @@
 
 object_tangible_collection_deathtrooper_alpha_camera_01 = object_tangible_collection_shared_deathtrooper_alpha_camera_01:new {
-		objectMenuComponent = "EventpackMenuComponent",
-
+gameObjectType = 8211,}
 ObjectTemplates:addTemplate(object_tangible_collection_deathtrooper_alpha_camera_01, "object/tangible/collection/deathtrooper_alpha_camera_01.iff")
