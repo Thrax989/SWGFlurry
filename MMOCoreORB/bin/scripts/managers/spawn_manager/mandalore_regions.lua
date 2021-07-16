3cead5584fd19a2a7387ebef44175c6e5a15bb54
Sygnum <jtrobbinsfl@gmail.com>
2021-07-16 11:39:12 -0400

    [Added] Mythosaur Graveyard to No Build

--- MMOCoreORB/bin/scripts/managers/spawn_manager/mandalore_regions.lua
+++ MMOCoreORB/bin/scripts/managers/spawn_manager/mandalore_regions.lua
@@ -2,4 +2,5 @@ require("scripts.managers.spawn_manager.regions")
 
 mandalore_regions = {
 	{"world_spawner",0,0,{CIRCLE,-1},SPAWNAREA + WORLDSPAWNAREA,{"mandalore_world"},2048},
+  {"mythosaur", 1857, -1124, {CIRCLE, 800}, NOSPAWNAREA + NOBUILDZONEAREA}, --added no build
 }
