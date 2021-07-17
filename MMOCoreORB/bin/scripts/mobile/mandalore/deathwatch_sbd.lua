3a55a84ba8430758bd787754e9fb0d3169a1dfbe
Drelnor <83726742+Drelnor@users.noreply.github.com>
2021-07-17 11:51:53 -0600

    Deathwatch SBD
    Created new Mandalore creature, Deathwatch SBD.

---
+++ MMOCoreORB/bin/scripts/mobile/mandalore/deathwatch_sbd.lua
@@ -0,0 +1,77 @@
+deathwatch_sbd = Creature:new {
+	customName = "Deathwatch Super Battle Droid",
+	socialGroup = "death_watch",
+	faction = "",
+	level = 200,
+	chanceHit = 18,
+	damageMin = 1200,
+	damageMax = 2100,
+	baseXp = 19000,
+	baseHAM = 230000,
+	baseHAMmax = 230000,
+	armor = 2,
+	resists = {85,95,100,60,100,25,40,85,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = KILLER,
+	optionsBitmask = AIENABLED,
+	diet = NONE,
+	scale = 1.40,
+
+	templates = {
+		"object/mobile/death_watch_s_battle_droid.iff",
+		"object/mobile/death_watch_s_battle_droid_02.iff",
+		"object/mobile/death_watch_s_battle_droid_03.iff"},
+	lootGroups = {
+		    {
+			    groups = {
+				    {group = "wearables_uncommon", chance = 10000000}
+			  },
+			  lootChance = 5000000
+		    },
+		    {
+			    groups = {
+				    {group = "death_watch_bunker_lieutenants", chance = 10000000}
+			  },
+			  lootChance = 5000000
+		    },
+		    {
+			    groups = {
+				    {group = "jetpack_base", chance = 10000000}
+			  },
+			  lootChance = 5000000
+		    },
+		    {
+			    groups = {
+				    {group = "death_watch_bunker_ingredient_protective", chance = 5000000},
+ 				    {group = "death_watch_bunker_ingredient_binary", chance = 5000000},
+			  },
+			  lootChance = 500000
+		    },
+		    {
+			    groups = {
+				    {group = "death_watch_bunker_commoners", chance = 10000000}
+			  },
+			  lootChance = 5000000
+		    },
+		    {
+			    groups = {
+				    {group = "jedi_comp_group", chance = 10000000},
+			  },
+			  lootChance = 400000
+		    },
+	  },
+	conversationTemplate = "",
+	defaultWeapon = "object/weapon/ranged/droid/droid_droideka_ranged.iff",
+	defaultAttack = "attack"
+}
+
+CreatureTemplates:addCreatureTemplate(death_watch_s_battle_droid_alt, "death_watch_s_battle_droid_alt")
