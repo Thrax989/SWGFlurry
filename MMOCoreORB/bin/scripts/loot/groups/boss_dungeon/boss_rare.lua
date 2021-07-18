33b091011f83b57b8ab49388fed265113eb0e498
bigtap <24537455+bigtap@users.noreply.github.com>
2021-07-17 22:26:42 -0700

    jewelry back to boss and boss minions

--- MMOCoreORB/bin/scripts/loot/groups/boss_dungeon/boss_rare.lua
+++ MMOCoreORB/bin/scripts/loot/groups/boss_dungeon/boss_rare.lua
@@ -8,7 +8,7 @@ boss_rare = {
 		{groupTemplate = "vehicledeedsrare", weight = 955560},
 		{groupTemplate = "chemistry_component_advanced", weight = 955555},
 		{groupTemplate = "weapon_component_advanced", weight = 955555}, 
-		{groupTemplate = "fire_breathing_spider", weight = 955555},   
+		{groupTemplate = "jedi_jewelry", weight = 955555},   
 		{groupTemplate = "krayt_tissue_rare", weight = 955555},
 		{groupTemplate = "krayt_pearls_flawless", weight = 955555},    
 		{groupTemplate = "g_named_crystals", weight = 955555},
