1ef8b6197bbf04d456a23439c5b6c86db24f0079
bigtap <24537455+bigtap@users.noreply.github.com>
2021-07-17 22:56:58 -0700

    Update jedi_jewelry.lua
    MATH!

--- MMOCoreORB/bin/scripts/loot/groups/jedi_jewelry.lua
+++ MMOCoreORB/bin/scripts/loot/groups/jedi_jewelry.lua
@@ -3,10 +3,10 @@ jedi_jewelry = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{groupTemplate = "jedi_earings", weight = 3333333},
-		{groupTemplate = "jedi_bracelets", weight = 3333333},
-		{groupTemplate = "jedi_neck", weight = 3333333},      
-		{groupTemplate = "jedi_rings", weight = 3333333}
+		{groupTemplate = "jedi_earings", weight = 2500000},
+		{groupTemplate = "jedi_bracelets", weight = 2500000},
+		{groupTemplate = "jedi_neck", weight = 2500000},      
+		{groupTemplate = "jedi_rings", weight = 2500000}
 		}
 }
 
