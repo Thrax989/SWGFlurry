74f9fe5e7b9356c83a30618d9081fcf4cc3b6e1c
christian <thrax989@yahoo.com>
2021-07-24 20:30:22 -0700

    Revert "[Moved] FRS Checks From FRs Manager To Faction Manager"
    This reverts commit a73e81160eaed8e00e60e216714c382cc6948bdc.

--- MMOCoreORB/src/server/zone/managers/frs/FrsManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/frs/FrsManagerImplementation.cpp
@@ -1048,6 +1048,24 @@ int FrsManagerImplementation::calculatePvpExperienceChange(CreatureObject* attac
 		}
 	}
 
+	if (!isVictim){
+		Zone* zone = attacker->getZone();
+		String planetName = zone->getZoneName();
+		String attackerName = attacker->getFirstName();
+		String victimName = victim->getFirstName();
+		StringBuffer frsKillQuery, zBroadcast;
+		Vector3 worldPosition = attacker->getWorldPosition();
+		String name = " (" + String::valueOf((int)victim->getWorldPositionX()) + ", " + String::valueOf((int)victim->getWorldPositionZ()) + ", " + String::valueOf((int)victim->getWorldPositionY()) + ")";
+		zBroadcast << "\\#00cc99 " << attackerName << " Has Gained FRS From Killing " << "\\#00e604" << victimName << "\\#e60000 on Planet " << planetName; 
+		playerGhost->getZoneServer()->getChatManager()->broadcastGalaxy(nullptr, zBroadcast.toString());
+		ChatManager* chatManager = attacker->getZoneServer()->getChatManager();	
+		StringBuffer zGeneral;
+		zGeneral << " Has Gained FRS From Killing " << victimName << " on Planet " << planetName << name;
+		chatManager->handleGeneralChat(attacker, zGeneral.toString());
+		PlayerManager* playerManager = attacker->getZoneServer()->getPlayerManager();
+		attacker->playEffect("clienteffect/level_granted.cef", "");
+		playerManager->awardExperience(attacker, "force_rank_xp", 5000, true); // Award FRS XP
+	}
 	return xpChange;
 }
 
