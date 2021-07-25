bc3d910e5b9723c08d91a29ba37a1cb76d07a873
TOXIC <thrax989@yahoo.com>
2021-07-25 11:20:59 -0700

    [Added] Galactic Civil War Rank System [WORK IN PROGRESS] Rebel And Imperial Ranks With Skills And Bonuses

--- MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
@@ -2310,6 +2310,81 @@ int PlayerManagerImplementation::awardExperience(CreatureObject* player, const S
 	}
 
 
+//GCW Rank System REBEL|Imperial
+	if (xpType == "gcw_rank_xp") {
+		if (player->hasSkill("rebel_rank_gcw_novice") || player->hasSkill("imperial_rank_gcw_novice")) {
+			PlayerObject* ghost = player->getPlayerObject();
+			const SkillList* skillList = player->getSkillList();
+			int curExp = ghost->getExperience("gcw_rank_xp");
+			if (curExp < -15000) {
+				if (player->hasSkill("rebel_rank_gcw_novice")) {
+					while (player->hasSkill("rebel_rank_gcw_novice")) {
+						for (int i = 0; i < skillList->size(); ++i) {
+							Skill* skill = skillList->get(i);
+							if (skill->getSkillName().indexOf("rebel_rank_") != -1 && skill->getSkillName().indexOf("rebel_rank_gcw_novice") == -1) {
+								SkillManager::instance()->surrenderSkill(skill->getSkillName(), player, true);
+							}
+						}
+					}
+					String playerName = player->getFirstName();
+					StringBuffer zBroadcast;
+					zBroadcast << "\\#ffb90f" << playerName << " has left the \\#22b7f6Rebel GCW!";
+					ghost->getZoneServer()->getChatManager()->broadcastGalaxy(nullptr, zBroadcast.toString());
+				} else if (player->hasSkill("imperial_rank_gcw_novice")) {
+					while (player->hasSkill("imperial_rank_gcw_novice")) {
+						for (int i = 0; i < skillList->size(); ++i) {
+							Skill* skill = skillList->get(i);
+							if (skill->getSkillName().indexOf("gcw_rank_") != -1 && skill->getSkillName().indexOf("imperial_rank_gcw_novice") == -1)  {
+								SkillManager::instance()->surrenderSkill(skill->getSkillName(), player, true);
+							}
+						}
+					}
+					String playerName = player->getFirstName();
+					StringBuffer zBroadcast;
+					zBroadcast << "\\#ffb90f" << playerName << " has left the \\#e51b1bImperial GCW!";
+					ghost->getZoneServer()->getChatManager()->broadcastGalaxy(nullptr, zBroadcast.toString());
+				}
+			}
+			if (curExp < 10000) {
+				gcwSkillCheck(player, "novice", "rank_01");
+			}
+			if (curExp >= 10000 && curExp < 20000) {
+				gcwSkillCheck(player, "rank_01", "rank_02");
+			}
+			if (curExp >= 20000 && curExp < 30000) {
+				gcwSkillCheck(player, "rank_02", "rank_03");
+			}
+			if (curExp >= 30000 && curExp < 40000) {
+				gcwSkillCheck(player, "rank_03", "rank_04");
+			}
+			if (curExp >= 40000 && curExp < 60000) {
+				gcwSkillCheck(player, "rank_04", "rank_05");
+			}
+			if (curExp >= 60000 && curExp < 80000) {
+				gcwSkillCheck(player, "rank_05", "rank_06");
+			}
+			if (curExp >= 80000 && curExp < 100000) {
+				gcwSkillCheck(player, "rank_06", "rank_07");
+			}
+			if (curExp >= 100000 && curExp < 150000) {
+				gcwSkillCheck(player, "rank_07", "rank_08");
+			}
+			if (curExp >= 150000 && curExp < 200000) {
+				gcwSkillCheck(player, "rank_08", "rank_09");
+			}
+			if (curExp >= 200000 && curExp < 300000) {
+				gcwSkillCheck(player, "rank_09", "rank_10");
+			}
+			if (curExp >= 300000 && curExp < 500000) {
+				gcwSkillCheck(player, "rank_10", "master");
+			}
+			if (curExp >= 500000) {
+				gcwSkillCheck(player, "master", "master");
+			}
+		}
+	}
+//End Of GCW Rank System	
+
 //BH Rank System BHG
 	if (xpType == "bhg_rank_xp") {
 		if (player->hasSkill("bounty_hunters_guild_novice")) {
