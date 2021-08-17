0719b23fe8ccbe2ffa5ef35a8a940b8a3b116619
bigtap <24537455+bigtap@users.noreply.github.com>
2021-08-17 12:57:18 -0700

    Update turret_block_small.lua

--- MMOCoreORB/bin/scripts/object/weapon/ranged/turret/turret_block_small.lua
+++ MMOCoreORB/bin/scripts/object/weapon/ranged/turret/turret_block_small.lua
@@ -41,13 +41,13 @@
 --this exception also makes it possible to release a modified version 
 
 
-object_weapon_ranged_turret_turret_block_small = object_weapon_ranged_turret_shared_turret_block_large:new {
+object_weapon_ranged_turret_turret_block_small = object_weapon_ranged_turret_shared_turret_block_small:new {
 	-- HEAVYLIGHTNINGBEAMATTACK, HEAVYPARTICLEBEAMATTACK, HEAVYROCKETLAUNCHERATTACK, HEAVYLAUNCHERATTACK
 	attackType = RANGEDATTACK,
 	-- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, LIGHTSABER
 	damageType = BLAST,
 	-- NONE, LIGHT, MEDIUM, HEAVY
-	armorPiercing = HEAVY,
+	armorPiercing = MEDIUM,
 	-- See http://www.ocdsoft.com/files/accuracy.xls
 	creatureAccuracyModifiers = { "rifle_accuracy" },
 	creatureAimModifiers = { "rifle_aim", "aim" },
@@ -66,9 +66,9 @@ object_weapon_ranged_turret_turret_block_small = object_weapon_ranged_turret_sha
 	idealAccuracy = 100,
 	maxRange = 50,
 	maxRangeAccuracy = 100,
-	minDamage = 4000,
-	maxDamage = 8000,
-	attackSpeed = 1,
+	minDamage = 3000,
+	maxDamage = 7000,
+	attackSpeed = 3,
 	woundsRatio = 20,
 }
 
