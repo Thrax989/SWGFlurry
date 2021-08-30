44fad378ac7557cabddf8c98e4ab05e075051e6e
bigtap <24537455+bigtap@users.noreply.github.com>
2021-08-29 21:17:56 -0700

    Update medpack_cure_disease_c.lua
    to match poison C cure

--- MMOCoreORB/bin/scripts/object/tangible/medicine/crafted/medpack_cure_disease_c.lua
+++ MMOCoreORB/bin/scripts/object/tangible/medicine/crafted/medpack_cure_disease_c.lua
@@ -57,7 +57,7 @@ object_tangible_medicine_crafted_medpack_cure_disease_c = object_tangible_medici
 	experimentalGroupTitles = {"null", "null", "exp_effectiveness", "exp_charges", "expEaseOfUse", "null"},
 	experimentalSubGroupTitles = {"null", "null", "power", "charges", "skillmodmin", "hitpoints"},
 	experimentalMin = {0, 0, 10, 20, 30, 1000},
-	experimentalMax = {0, 0, 80, 40, 20, 1000},
+	experimentalMax = {0, 0, 450, 40, 20, 1000},
 	experimentalPrecision = {0, 0, 0, 0, 0, 0},
 	experimentalCombineType = {0, 0, 1, 1, 1, 4},
 }
