e1ee1f32f78f16a7091647d7e1d7053507b379ec
bigtap <24537455+bigtap@users.noreply.github.com>
2021-09-14 19:58:56 -0700

    Revert "[Updated]  atat helmets are now clothing objects"
    This reverts commit b1cbe9cb5e55fa50d5db614e117c1d544d543f43.

--- MMOCoreORB/bin/scripts/object/tangible/wearables/helmet/helmet_atat.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/helmet/helmet_atat.lua
@@ -224,8 +224,6 @@ object_tangible_wearables_helmet_helmet_atat = object_tangible_wearables_helmet_
 				"object/creature/player/zabrak_female.iff" ,
 				"object/mobile/vendor/zabrak_male.iff",
 				"object/mobile/vendor/zabrak_female.iff" },
-				gameObjectType = 16777226,
-        			clientGameObjectType = 16777226
 }
 
 ObjectTemplates:addTemplate(object_tangible_wearables_helmet_helmet_atat, "object/tangible/wearables/helmet/helmet_atat.iff")
