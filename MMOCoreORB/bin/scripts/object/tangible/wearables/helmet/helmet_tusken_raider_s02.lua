36051c87b18491e0cecec5300d9224de8c03d3bd
bigtap <24537455+bigtap@users.noreply.github.com>
2021-09-14 19:59:00 -0700

    Revert "[Updated]  tusken raider helmets are now clothing objects"
    This reverts commit 58bc01e22041e2629c41efe063dc87de7f02c2de.

--- MMOCoreORB/bin/scripts/object/tangible/wearables/helmet/helmet_tusken_raider_s02.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/helmet/helmet_tusken_raider_s02.lua
@@ -222,8 +222,6 @@ object_tangible_wearables_helmet_helmet_tusken_raider_s02 = object_tangible_wear
 				"object/creature/player/zabrak_female.iff" ,
 				"object/mobile/vendor/zabrak_male.iff",
 				"object/mobile/vendor/zabrak_female.iff" },
-				gameObjectType = 16777226,
-        			clientGameObjectType = 16777226
 }
 
 ObjectTemplates:addTemplate(object_tangible_wearables_helmet_helmet_tusken_raider_s02, "object/tangible/wearables/helmet/helmet_tusken_raider_s02.iff")
