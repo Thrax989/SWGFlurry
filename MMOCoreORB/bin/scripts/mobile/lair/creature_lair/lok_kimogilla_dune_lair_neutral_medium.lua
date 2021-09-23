5c312d2dbffd557401aee2fa8d11264d52007961
Drelnor <83726742+Drelnor@users.noreply.github.com>
2021-09-23 02:02:10 -0600

    Update lok_kimogilla_dune_lair_neutral_medium.lua
    Added bossMobiles enraged_kimogila

--- MMOCoreORB/bin/scripts/mobile/lair/creature_lair/lok_kimogilla_dune_lair_neutral_medium.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/creature_lair/lok_kimogilla_dune_lair_neutral_medium.lua
@@ -1,5 +1,6 @@
 lok_kimogilla_dune_lair_neutral_medium = Lair:new {
 	mobiles = {{"dune_kimogila",1}},
+	bossMobiles = {{"enraged_kimogila",1}},
 	spawnLimit = 15,
 	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_bones_large_evil_fire_small.iff"},
 	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_bones_large_evil_fire_small.iff"},
