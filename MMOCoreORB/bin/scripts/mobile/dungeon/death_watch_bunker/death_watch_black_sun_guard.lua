b2d57bc509d135924d7d6d3e344afcc03e3accc6
Voxxy <70905380+Voxxy-Flurry@users.noreply.github.com>
2021-09-27 20:46:30 +0200

    Black Sun Helmet added to DWB loot table.
    Adding 1% Helmet chance to DWB Black Sun Guard.
    
    Mirrored loot values from black_sun_guard.lua

--- MMOCoreORB/bin/scripts/mobile/dungeon/death_watch_bunker/death_watch_black_sun_guard.lua
+++ MMOCoreORB/bin/scripts/mobile/dungeon/death_watch_bunker/death_watch_black_sun_guard.lua
@@ -38,12 +38,6 @@ death_watch_black_sun_guard = Creature:new {
 		},
 		{
 			groups = {
-				{group = "death_watch_bunker_commoners", chance = 10000000}
-			},
-			lootChance = 5000000
-		},
-		{
-			groups = {
 				{group = "wearables_common", chance = 10000000}
 			},
 			lootChance = 5000000
@@ -65,6 +59,12 @@ death_watch_black_sun_guard = Creature:new {
 				{group = "junk", chance = 10000000}
 			},
 			lootChance = 5000000
+		},
+		{
+			groups = {
+				{group = "black_suns", chance = 10000000}
+			},
+			lootChance = 100000
 		},
 	},
 	weapons = {"pirate_weapons_heavy"},
