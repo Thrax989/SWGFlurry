2b44f76d9060cf39ab244f12fc57aa4b76ca0205
christian <thrax989@yahoo.com>
2021-11-30 00:04:32 -0800

    [Updated] TCG 1 And 2 Vendor list

--- MMOCoreORB/bin/scripts/screenplays/merchants/tcgonevendorData.lua
+++ MMOCoreORB/bin/scripts/screenplays/merchants/tcgonevendorData.lua
@@ -2,11 +2,11 @@ TcgoneVendorLogic = VendorLogic:new {
 	scriptName = "TcgoneVendorLogic",
 	currencies = {
 	--For Tokens: Displayed Name, full template string (without shared_), if applicable: ScreenPlayData string, ScreenPlayData key
-		{currency = "token", name = "Community Token", template = ".iff", ScreenPlayDataString = "vendorToken", ScreenPlayDataKey = "event_token"},
+		{currency = "token", name = "Community Token", template = "object/tangible/loot/token/event_token.iff", ScreenPlayDataString = "vendorToken", ScreenPlayDataKey = "event_token"},
 	},
 	--Displayed Name, full template string (without shared_), cost in {}, use the same structure as currencies
 	merchandise = {
-	{name = "TCG Items", template = "", cost = {10,}},
+	{name = "TCG Items", template = "object/tangible/tcg/series1/consumable_hans_hydrospanner.iff", cost = {10,}},
 	},
 }
 
