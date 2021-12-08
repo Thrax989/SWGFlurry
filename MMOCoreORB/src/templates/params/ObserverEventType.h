2245062cf48b2136f7bf38add7ae35ccb5176ddb
christian <thrax989@yahoo.com>
2021-12-08 15:01:59 -0800

    [Added] |More MVP COD\E

--- MMOCoreORB/src/templates/params/ObserverEventType.h
+++ MMOCoreORB/src/templates/params/ObserverEventType.h
@@ -90,6 +90,8 @@ public:
 		BHTEFCHANGED,				// ManagedObject arg1 = null, long arg2 = 0
 		FACTIONCHANGED,				// ManagedObject arg1 = null, long arg2 = 0
 		SLICED,						// ManagedObject arg1 = player doing slicing, long arg2 = result (1/0)
+		MOUNTED,
+		DISMOUNTED,
 	};
 };
 
