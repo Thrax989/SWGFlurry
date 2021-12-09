dfc4f507cbc2b20a940446ad34a4f563fdee3145
TOXIC <thrax989@yahoo.com>
2021-12-08 23:45:42 -0800

    [Updated] Holocron Component

--- MMOCoreORB/src/server/zone/objects/tangible/components/HolocronMenuComponent.h
+++ MMOCoreORB/src/server/zone/objects/tangible/components/HolocronMenuComponent.h
@@ -34,24 +34,9 @@ namespace creature {
 
 using namespace server::zone::objects::creature;
 
-namespace server {
-namespace zone {
-namespace objects {
-namespace player {
-	class PlayerObject;
-}
-}
-}
-}
-
-using namespace server::zone::objects::player;
-
 class HolocronMenuComponent : public TangibleObjectMenuComponent {
-
 public:
-	virtual int handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* player, byte selectedID) const;
-
-	virtual void fillObjectMenuResponse(SceneObject* sceneObject, ObjectMenuResponse* menuResponse, CreatureObject* player) const;
+	int handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* player, byte selectedID) const;
 };
 
 
