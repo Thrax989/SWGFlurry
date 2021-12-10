db49280bd4280dfec63c02d962b61ae440f25363
TOXIC <thrax989@yahoo.com>
2021-12-10 03:13:21 -0800

    [Updated] Visibility Decay System

--- MMOCoreORB/bin/scripts/managers/jedi/visibility_manager.lua
+++ MMOCoreORB/bin/scripts/managers/jedi/visibility_manager.lua
@@ -1,6 +1,7 @@
-maxVisibility = 8000      -- Maximum visibility
-termThreshold = 100      -- Visibility Jedi will show on terminals
-falloffThreshold = 50    -- Visibility jedi will disappear off terminals
-totalDecayTimeInDays = 21 -- Total time before visibility decays from cap
-tickRateInSeconds = 60*60 -- How often visibility will decay in seconds
+
+maxVisibility = 10000      -- Maximum visibility
+termThreshold = 5000      -- Visibility Jedi will show on terminals
+falloffThreshold = 2500    -- Visibility jedi will disappear off terminals
+totalDecayTimeInDays = 1 -- Total time before visibility decays from cap
+tickRateInSeconds = 30*30 -- How often visibility will decay in seconds
 pvpRatingDivisor = 18      -- Divisor for calculating visibility
