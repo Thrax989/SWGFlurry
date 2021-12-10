f29916970cccc71297228d4af9456c8f3bcdf06c
TOXIC <thrax989@yahoo.com>
2021-12-10 13:34:33 -0800

    [Updated] Vis Mod

--- MMOCoreORB/bin/scripts/commands/forceKnockdown1.lua
+++ MMOCoreORB/bin/scripts/commands/forceKnockdown1.lua
@@ -47,7 +47,7 @@ ForceKnockdown1Command = {
 	damageMultiplier = 0,
 	speed = 4.0,
 	forceCost = 75,
-	visMod = 25,
+	visMod = 1,
 	accuracySkillMod = "forceknockdown_accuracy",
 
 	stateEffects = {
