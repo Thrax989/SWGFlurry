d5ac770d00a0516f88d8472e1876a45ba7c287ac
TOXIC <thrax989@yahoo.com>
2021-12-10 13:36:54 -0800

    [Updated] Vis Mod

--- MMOCoreORB/bin/scripts/commands/saber2hHit1.lua
+++ MMOCoreORB/bin/scripts/commands/saber2hHit1.lua
@@ -50,7 +50,7 @@ Saber2hHit1Command = {
 	actionCostMultiplier = 0,
 	mindCostMultiplier = 0,
 	forceCostMultiplier = 1.0,
-	visMod = 25,
+	visMod = 1,
 
 	animation = "combo_2c", 
 	animType = GENERATE_INTENSITY,
