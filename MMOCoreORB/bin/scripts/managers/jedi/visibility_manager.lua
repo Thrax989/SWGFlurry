48c64f636abdc24829ceb088707a33550d43638e
christian <thrax989@yahoo.com>
2021-12-12 02:51:56 -0800

    Revert "[Updated] Visibility Decay System"
    This reverts commit db49280bd4280dfec63c02d962b61ae440f25363.

--- MMOCoreORB/bin/scripts/managers/jedi/visibility_manager.lua
+++ MMOCoreORB/bin/scripts/managers/jedi/visibility_manager.lua
@@ -1,7 +1,6 @@
-
-maxVisibility = 10000      -- Maximum visibility
-termThreshold = 5000      -- Visibility Jedi will show on terminals
-falloffThreshold = 2500    -- Visibility jedi will disappear off terminals
-totalDecayTimeInDays = 1 -- Total time before visibility decays from cap
-tickRateInSeconds = 30*30 -- How often visibility will decay in seconds
+maxVisibility = 8000      -- Maximum visibility
+termThreshold = 100      -- Visibility Jedi will show on terminals
+falloffThreshold = 50    -- Visibility jedi will disappear off terminals
+totalDecayTimeInDays = 21 -- Total time before visibility decays from cap
+tickRateInSeconds = 60*60 -- How often visibility will decay in seconds
 pvpRatingDivisor = 18      -- Divisor for calculating visibility
