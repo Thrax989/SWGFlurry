023333394559833247975656fef4f53d79be10a3
TOXIC <thrax989@yahoo.com>
2021-12-27 09:38:41 -0800

    [Updated] Wording

--- MMOCoreORB/bin/scripts/screenplays/dungeon/boba_fett/boba_fett.lua
+++ MMOCoreORB/bin/scripts/screenplays/dungeon/boba_fett/boba_fett.lua
@@ -1,11 +1,11 @@
 --/////////////////////////////////////////////////////////
---//		    Buba Fett Dungeon						//
---//			Created By TOXIC:12/27/2021				//
+--//		    Buba Fett Dungeon			//
+--//			Created By TOXIC:12/27/2021	//
 --////////////////////////////////////////////////////////
 
 --////////////////////////////////////////////////////////
---//		Current Boss Planet endor					//
---//		Current Boss Type NPC						//
+--//		Current Boss Planet endor		//
+--//		Current Boss Type NPC			//
 --///////////////////////////////////////////////////////
 boba_fett_dungeonScreenplay = ScreenPlay:new {
 				numberOfActs = 1,
@@ -53,139 +53,139 @@ function boba_fett_dungeonScreenplay:npcDamageObserver(bossObject, playerObject,
 --Boba Fett Boss 90% health
 ----------------------------
 				if (((health <= (maxHealth * 0.9)) or (action <= (maxAction * 0.9)) or (mind <= (maxMind * 0.9))) and readData("boba_fett_dungeonScreenplay:spawnState") == 0) then
-      			writeData("boba_fett_dungeonScreenplay:spawnState",1)
+      				writeData("boba_fett_dungeonScreenplay:spawnState",1)
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "bomb", playerObject, "")
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "grenadebomb", bossObject, "")
 				self:spawnSupport(playerObject)
-      			CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
+      				CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
    				CreatureObject(playerObject):playMusicMessage("sound/boba_fett_dungeon.snd")
-      			CreatureObject(bossObject):playEffect("clienteffect//mustafar/som_force_crystal_buff.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/mustafar/som_force_crystal_drain.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
-      			spatialChat(bossObject, "Boss Current Health = 90%")
+      				CreatureObject(bossObject):playEffect("clienteffect//mustafar/som_force_crystal_buff.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/mustafar/som_force_crystal_drain.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
+      				spatialChat(bossObject, "Boss Current Health = 90%")
 end
 ----------------------------
 --Boba Fett Boss 80% health
 ----------------------------
 				if (((health <= (maxHealth * 0.8)) or (action <= (maxAction * 0.8)) or (mind <= (maxMind * 0.8))) and readData("boba_fett_dungeonScreenplay:spawnState") == 1) then
-      			writeData("boba_fett_dungeonScreenplay:spawnState",2)
+      				writeData("boba_fett_dungeonScreenplay:spawnState",2)
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "bomb", playerObject, "")
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "grenadebomb", bossObject, "")
 				self:spawnSupport(playerObject)
-      			CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
-      			CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
-      			spatialChat(bossObject, "Boss Current Health = 80%")
+      				CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
+      				CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
+      				spatialChat(bossObject, "Boss Current Health = 80%")
 end
 ----------------------------
 --Boba Fett Boss 70% health
 ----------------------------
 				if (((health <= (maxHealth * 0.7)) or (action <= (maxAction * 0.7)) or (mind <= (maxMind * 0.7))) and readData("boba_fett_dungeonScreenplay:spawnState") == 2) then
-      			writeData("boba_fett_dungeonScreenplay:spawnState",3)
+      				writeData("boba_fett_dungeonScreenplay:spawnState",3)
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "bomb", playerObject, "")
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "grenadebomb", bossObject, "")
 				self:spawnSupport(playerObject)
-      			CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
-      			CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
-      			spatialChat(bossObject, "Boss Current Health = 70%")
+      				CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
+      				CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
+      				spatialChat(bossObject, "Boss Current Health = 70%")
 end
 ----------------------------
 --Boba Fett Boss 60% health
 ----------------------------
 				if (((health <= (maxHealth * 0.6)) or (action <= (maxAction * 0.6)) or (mind <= (maxMind * 0.6))) and readData("boba_fett_dungeonScreenplay:spawnState") == 3) then
-      			writeData("boba_fett_dungeonScreenplay:spawnState",4)
+      				writeData("boba_fett_dungeonScreenplay:spawnState",4)
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "bomb", playerObject, "")
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "grenadebomb", bossObject, "")
 				self:spawnSupport(playerObject)
-      			CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
-      			CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
-      			spatialChat(bossObject, "Boss Current Health = 60%")
+      				CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
+      				CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
+      				spatialChat(bossObject, "Boss Current Health = 60%")
 end
 ----------------------------
 --Boba Fett Boss 50% health
 ----------------------------
 				if (((health <= (maxHealth * 0.5)) or (action <= (maxAction * 0.5)) or (mind <= (maxMind * 0.5))) and readData("boba_fett_dungeonScreenplay:spawnState") == 4) then
-      			writeData("boba_fett_dungeonScreenplay:spawnState",5)
+      				writeData("boba_fett_dungeonScreenplay:spawnState",5)
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "bomb", playerObject, "")
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "grenadebomb", bossObject, "")
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "starthealing", bossObject, "")
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "npchealingmessage", bossObject, "")
 				self:spawnSupport(playerObject)
-      			CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
-      			CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/mustafar/som_force_crystal_drain.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/droid_effect_dry_ice.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/death_trooper_infection_03.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/combat_pt_electricalfield.cef", "")
-      			spatialChat(bossObject, "Boss Current Health = 50%")
+      				CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
+      				CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/mustafar/som_force_crystal_drain.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/droid_effect_dry_ice.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/death_trooper_infection_03.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/combat_pt_electricalfield.cef", "")
+      				spatialChat(bossObject, "Boss Current Health = 50%")
 end
 ----------------------------
 --Boba Fett Boss 40% health
 ----------------------------
 				if (((health <= (maxHealth * 0.4)) or (action <= (maxAction * 0.4)) or (mind <= (maxMind * 0.4))) and readData("boba_fett_dungeonScreenplay:spawnState") == 5) then
-      			writeData("boba_fett_dungeonScreenplay:spawnState",6)
+      				writeData("boba_fett_dungeonScreenplay:spawnState",6)
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "bomb", playerObject, "")
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "grenadebomb", bossObject, "")
 				self:spawnSupport(playerObject)
-      			CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
-      			CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
-      			spatialChat(bossObject, "Boss Current Health = 40%")
+      				CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
+      				CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
+      				spatialChat(bossObject, "Boss Current Health = 40%")
 end
 ----------------------------
 --Boba Fett Boss 30% health
 ----------------------------
 				if (((health <= (maxHealth * 03)) or (action <= (maxAction * 0.3)) or (mind <= (maxMind * 0.3))) and readData("boba_fett_dungeonScreenplay:spawnState") == 6) then
-      			writeData("boba_fett_dungeonScreenplay:spawnState",7)
+      				writeData("boba_fett_dungeonScreenplay:spawnState",7)
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "bomb", playerObject, "")
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "grenadebomb", bossObject, "")
 				self:spawnSupport(playerObject)
-      			CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
-      			CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
-      			spatialChat(bossObject, "Boss Current Health = 30%")
+      				CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
+      				CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
+      				spatialChat(bossObject, "Boss Current Health = 30%")
 end
 ----------------------------
 --Boba Fett Boss 20% health
 ----------------------------
 				if (((health <= (maxHealth * 0.2)) or (action <= (maxAction * 0.2)) or (mind <= (maxMind * 0.2))) and readData("boba_fett_dungeonScreenplay:spawnState") == 7) then
-      			writeData("boba_fett_dungeonScreenplay:spawnState",8)
+      				writeData("boba_fett_dungeonScreenplay:spawnState",8)
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "bomb", playerObject, "")
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "grenadebomb", bossObject, "")
 				self:spawnSupport(playerObject)
-      			CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
-      			CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
-      			spatialChat(bossObject, "Boss Current Health = 20%")
+      				CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
+      				CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
+      				spatialChat(bossObject, "Boss Current Health = 20%")
 end
 ----------------------------
 --Boba Fett Boss 10% health
 ----------------------------
 				if (((health <= (maxHealth * 0.1)) or (action <= (maxAction * 0.1)) or (mind <= (maxMind * 0.1))) and readData("boba_fett_dungeonScreenplay:spawnState") == 8) then
-      			writeData("boba_fett_dungeonScreenplay:spawnState",9)
+      				writeData("boba_fett_dungeonScreenplay:spawnState",9)
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "bomb", playerObject, "")
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "grenadebomb", bossObject, "")
 				self:spawnSupport(playerObject)
-      			CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
-      			CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
-      			spatialChat(bossObject, "Boss Current Health = 10%")
+      				CreatureObject(playerObject):sendSystemMessage("Enemy Wave Starting!")
+      				CreatureObject(bossObject):playEffect("clienteffect/incubator_mutation.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/space_command/shp_astromech_effects_04.cef", "")
+      				spatialChat(bossObject, "Boss Current Health = 10%")
 end
 ----------------------------
 --Boba Fett Boss 1% health
 ----------------------------
 				if (((health <= (maxHealth * 0.001)) or (action <= (maxAction * 0.001)) or (mind <= (maxMind * 0.001))) and readData("boba_fett_dungeonScreenplay:spawnState") == 9) then
-      			writeData("boba_fett_dungeonScreenplay:spawnState",10)
+      				writeData("boba_fett_dungeonScreenplay:spawnState",10)
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "bomb", playerObject, "")
 				createEvent(0 * 1000, "boba_fett_dungeonScreenplay", "grenadebomb", bossObject, "")
 				spatialChat(bossObject, "We Shall Meet Again!!!!!!!!")
-      			CreatureObject(bossObject):playEffect("clienteffect/mustafar/som_force_crystal_drain.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/droid_effect_dry_ice.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/death_trooper_infection_03.cef", "")
-      			CreatureObject(bossObject):playEffect("clienteffect/combat_pt_electricalfield.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/mustafar/som_force_crystal_drain.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/droid_effect_dry_ice.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/death_trooper_infection_03.cef", "")
+      				CreatureObject(bossObject):playEffect("clienteffect/combat_pt_electricalfield.cef", "")
 				self:spawnSupport(playerObject)
 			end
 	return 0
