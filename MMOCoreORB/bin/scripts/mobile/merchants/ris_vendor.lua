f3baa88eedf40344726cf8d43354035ea3769a7e
christian <thrax989@yahoo.com>
2022-07-09 22:47:50 -0700

    [updated] ris logic

---
+++ MMOCoreORB/bin/scripts/mobile/merchants/ris_vendor.lua
@@ -0,0 +1,38 @@
+risvendor = Creature:new {
+	objectName = "",
+	customName = "Hutt Cartel Trader (Ris Event Vendor)",
+	socialGroup = "townsperson",
+	pvpFaction = "townsperson",
+	faction = "townsperson",
+	level = 1,
+	chanceHit = 0.25,
+	damageMin = 50,
+	damageMax = 55,
+	baseXp = 113,
+	baseHAM = 180,
+	baseHAMmax = 220,
+	armor = 1,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = NONE,
+	optionsBitmask = 264, --for conversation
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/borvo.iff"},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "RisVendorConvoTemplate",
+	attacks = {
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(risvendor, "risvendor")
--- MMOCoreORB/bin/scripts/mobile/merchants/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/merchants/serverobjects.lua
@@ -1,3 +1,4 @@
 includeFile("merchants/house_merch.lua")
 includeFile("merchants/event_vendor.lua")
 includeFile("merchants/halloween_event_vendor.lua")
+includeFile("merchants/ris_vendor.lua")
