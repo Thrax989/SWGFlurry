1d6fa90555b4cdbad2a77c23cbed446d006734f1
christian <thrax989@yahoo.com>
2022-11-21 10:21:10 -0800

    [Added] Vehicle Checks

--- MMOCoreORB/src/server/zone/objects/creature/LuaCreatureObject.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/LuaCreatureObject.cpp
@@ -19,6 +19,8 @@
 #include "server/zone/managers/skill/SkillModManager.h"
 #include "server/zone/objects/tangible/threat/ThreatMap.h"
 #include "server/chat/ChatManager.h"
+#include "server/zone/objects/creature/VehicleObject.h"
+#include "server/zone/objects/creature/ai/Creature.h"
 
 const char LuaCreatureObject::className[] = "LuaCreatureObject";
 
@@ -153,6 +155,9 @@ Luna<LuaCreatureObject>::RegType LuaCreatureObject::Register[] = {
 		{ "broadcastToDiscord", &LuaCreatureObject::broadcastToDiscord },
 		{ "broadcastToDiscordGcw", &LuaCreatureObject::broadcastToDiscordGcw },
 		{ "broadcastToDiscordUnlock", &LuaCreatureObject::broadcastToDiscordUnlock },
+		{ "isRidingMount", &LuaCreatureObject::isRidingMount },
+		{ "dismount", &LuaCreatureObject::dismount },
+		{ "slotPassenger", &LuaCreatureObject::slotPassenger },
 		{ 0, 0 }
 };
 
@@ -1147,6 +1152,40 @@ int LuaCreatureObject::buffSingleStat(lua_State* L) {
 	return 0;
 }
 
+int LuaCreatureObject::slotPassenger(lua_State* L) {
+	CreatureObject* passenger = (CreatureObject*) lua_touserdata(L, -1);
+
+	SceneObject* vehicle = realObject->getParent().get().get();
+	if (!vehicle->isCreatureObject()) {
+		lua_pushboolean(L, false);
+		return 1;
+	}
+
+
+	if (vehicle->isVehicleObject()) {
+		VehicleObject* speeder = static_cast<VehicleObject*>(vehicle);
+		if (!speeder->hasOpenSeat()) {
+			lua_pushboolean(L, false);
+			return 1;
+		} else {
+			speeder->slotPassenger(passenger);
+			lua_pushboolean(L, true);
+			return 1;
+		}
+	} else {
+		Creature* mount = static_cast<Creature*>(vehicle);
+		if (!mount->hasOpenSeat()) {
+			lua_pushboolean(L, false);
+			return 1;
+		} else {
+			mount->slotPassenger(passenger);
+			lua_pushboolean(L, true);
+			return 1;
+		}
+
+	}
+}
+
 int LuaCreatureObject::removeBuffs(lua_State* L) {
 	Reference<PlayerObject*> player = realObject->getPlayerObject();
 	
@@ -1225,4 +1264,17 @@ int LuaCreatureObject::broadcastToDiscordUnlock(lua_State* L) {
 	ZoneServer* zServ = realObject->getZoneServer();
 	zServ->getChatManager()->handleGeneralDiscordUnlock(nullptr, message);
 	return 1;
+}
+
+int LuaCreatureObject::isRidingMount(lua_State* L) {
+	bool isMounted = realObject->isRidingMount();
+
+	lua_pushboolean(L, isMounted);
+
+	return 1;
+}
+
+int LuaCreatureObject::dismount(lua_State* L) {
+	realObject->dismount();
+	return 0;
 }
